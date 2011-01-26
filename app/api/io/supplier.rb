539279c8d3b628767b082b7a8ec29933815b1db0
Matthew Denner <md12@sanger.ac.uk>
2011-01-26 11:44:44 +0000

    [fixes 9168073] Just the basic supplier and sample manifest API.

---
+++ app/api/io/supplier.rb
@@ -0,0 +1,27 @@
+class ::Io::Supplier < ::Core::Io::Base
+  # This module adds the behaviour we require from the Supplier module.
+  module ApiIoSupport
+    def self.included(base)
+      base.class_eval do
+        # TODO: add any named scopes
+        # TODO: add any associations
+      end
+    end
+
+    # TODO: add any methods
+  end
+
+  set_json_root(:supplier)
+  # set_eager_loading { |model| model }   # TODO: uncomment and add any named_scopes that do includes you need
+  
+  define_attribute_and_json_mapping(%Q{
+            name  => name
+           email  => email
+         address  => address
+    contact_name  => contact_name
+    phone_number  => phone_number
+             fax  => fax
+             url  => url
+    abbreviation  => abbreviation
+  })
+end
