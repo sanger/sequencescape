843db5b1ea20a7707eed5dd31e08c8ea9a9adace
Maxime Bourget <mb14@sanger.ac.uk>
2011-04-13 15:29:03 +0100

    Recursive pull works and generate a ruby file

--- lib/pull_model.rb
+++ lib/pull_model.rb
@@ -3,35 +3,39 @@ $options = { :model => "sample"}
 $objects = []
 
 class ActiveRecord::Base
-  def pull(includes=nil)
+  def pull(includes=nil, &block)
     return [] if @already_pulled
     @already_pulled = true
-    pulled = [self]
+    pulled = [block ? block.call(self) : self ]
 
     case includes
     when Array
       includes.each do |model|
-        pulled += self.send(model).pull([])
+        pulled += self.send(model).pull([], &block)
       end
     when Hash
       includes.each do |model, options|
-        pulled += self.send(model).pull(options)
+        pulled += self.send(model).pull(options, &block)
       end
+    when nil
+    else
+      pulled += self.send(includes).pull([],  &block)
     end
     return pulled
-
   end
+
 end
 
 class Array
-def pull(includes =nil)
-  map {|e| e.pull(includes) }.flatten
+def pull(includes =nil, &block)
+  map {|e| e.pull(includes, &block) }.flatten
 end
 end
 
 optparse = OptionParser.new do |opts|
   opts.on('-s', '--sample id_or_name', 'sample to pull') do |sample|
-    $objects<< [Sample, [:assets, :requests],  sample]
+    $objects<< [Sample, {},  sample]
+    #$objects<< [Sample, {:assets => :requests, :studies => nil },  sample]
   end
   opts.on('-m', '--model', 'model (classname) of the object to pull') do |model|
     $options[:model]=model
@@ -60,7 +64,11 @@ loaded = []
       end
 
       raise RuntimeError, "can't find #{model} '#{name}'" unless object
-      loaded += object.pull(includes)
+      loaded += object.pull(includes) do |object|
+%Q{
+        object = #{object.class.name}.new(#{object.attributes.reject { |k,v| [ :created_at, :updated_at ].include?(k.to_sym) } .inspect}) { |r| r.id = #{id} } #.save_without_validation
+}
+      end
     end
   return loaded
   end
@@ -71,10 +79,5 @@ end
   #puts $options.to_yaml
   #puts $objects.to_yaml
 
-  puts load_objects($objects).to_yaml
-
-
-
-
-
-
+  puts "debugger"
+puts load_objects($objects) 
