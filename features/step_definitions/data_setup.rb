6c7bf8debf3d0e54a4e90966e213bd35729529f2
Maxime Bourget <mb14@sanger.ac.uk>
2011-04-27 11:27:13 +0100

    move import pulled data code to module

--- features/step_definitions/data_setup.rb
+++ features/step_definitions/data_setup.rb
@@ -1,19 +1,11 @@
+require 'import_pulled_data'
 Given /^data are preloaded from "([^\"]+)" renaming:$/ do |file_name, table|
   names_map = {}
   table.rows.each do |old_name, new_name|
     names_map[old_name]= new_name
   end
-  object_parameters = YAML::load(File.read("data/setup/#{file_name}.yml"))
-  object_parameters.each do |parameter|
-    klass = parameter[:class].constantize
-    object_id = parameter[:id]
-    attributes = parameter[:attributes]
-    if name=attributes["name"]
-      attributes["name"] = names_map.fetch(name, name)
-    end
-    object = klass.new(attributes) { |r| r.id = object_id }
-    object.save_without_validation
-  end
+
+  ImportPulledData::import_from_yaml("data/setup/#{file_name}.yml", names_map)
 end
 Given /^data are preloaded from "([^\"]+)"$/ do |file_name|
   Given %Q{data are preloaded from "#{file_name}" renaming:}
