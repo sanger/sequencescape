b7119f1ed038238c68373de556c97f7958b6ae4a
Matthew Denner <matt.denner@gmail.com>
2011-05-02 09:25:08 +0100

    Refactored features into a more appropriate structure.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110426154411) do
+ActiveRecord::Schema.define(:version => 20110427101123) do
 
   create_table "archived_properties", :force => true do |t|
     t.text    "value"
