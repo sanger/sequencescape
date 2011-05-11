222f8695db7ec995dd1ad5e6a06e912b1a920160
Matthew Denner <md12@sanger.ac.uk>
2011-05-11 08:59:02 +0100

    Disable reading all sample manifests due to memory issues.
    The memory required to load a several thousand sample manifest is in the
    ~1.5GB range.  It happens that the first 100 manifests contain several
    of this size which makes it impossible to list them without the process
    crashing.  Whilst research is made into how to reduce the memory
    footprint it's easy enough to turn this ability off as clients only need
    to interact with their own manifests, not everyones.

--- app/api/endpoints/sample_manifests.rb
+++ app/api/endpoints/sample_manifests.rb
@@ -1,6 +1,9 @@
 class ::Endpoints::SampleManifests < ::Core::Endpoint::Base
   model do
-
+    # TODO: For the moment we have to disable the read functionality as it consumes too much memory.
+    # Loading a sample manifest of only a few thousand samples causes the memory to spike at 1.2GB
+    # and when you have 10s of these in a 100 entry page of results that is not good.
+    disable :read
   end
 
   instance do
