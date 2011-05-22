f0903cc814095e893f238ad7b96da44bd7be6dd5
Matthew Denner <matt.denner@gmail.com>
2011-05-22 12:47:38 +0100

    Whole load of code tidy ups

--- app/models/asset_link.rb
+++ app/models/asset_link.rb
@@ -12,4 +12,35 @@ class AssetLink < ActiveRecord::Base
 
   def destroy!
   end
+
+  module Associations
+    def self.included(base)
+      base.class_eval do
+        extend ClassMethods
+
+        has_dag_links :link_class_name => 'AssetLink'
+      end
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def has_one_as_child(name, options = {})
+        has_one(name, options.merge(:through => :links_as_child, :source => :ancestor))
+
+        line = __LINE__ + 1
+        class_eval(%Q{
+          def #{name}=(value)
+            raise RuntimeError, 'Value for #{name} must be saved' if value.new_record?
+            old_value = self.#{name}
+            parents.delete(old_value) if old_value.present?
+            AssetLink.create_edge(value, self)
+          end
+
+          def has_#{name}?
+            #{name}.present?
+          end
+        }, __FILE__, line)
+      end
+    end
+  end
 end
