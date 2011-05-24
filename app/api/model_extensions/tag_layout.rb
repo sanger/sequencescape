bc3223a308d73609d1b356aa042e811356b5c625
Matthew Denner <md12@sanger.ac.uk>
2011-05-24 17:04:15 +0100

    Working on the tag layout API
    Conflicts:
    
    	app/api/core/io/base/json_formatting_behaviour/output.rb

---
+++ app/api/model_extensions/tag_layout.rb
@@ -0,0 +1,10 @@
+# TODO: All of the behaviour in this file should really exist within the TagLayout model.
+module ModelExtensions::TagLayout
+  def self.included(base)
+    base.class_eval do
+      # TODO: Add an associations or named_scopes required
+    end
+  end
+
+  # TODO: Add any instance methods required
+end
