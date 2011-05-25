9d6aecd80311e8ad2363253e7094e6f8ea53dc95
Maxime Bourget <mb14@sanger.ac.uk>
2011-05-25 15:51:24 +0100

    Squashed commit of the following:
    commit e0a9983c903d41fcb1db4b1d5166f7a84440c90c
    Author: Maxime Bourget <mb14@sanger.ac.uk>
    Date:   Wed May 25 15:48:13 2011 +0100
    
        rename swipecard to swipecard
    
    commit ed7b04728821e9db0ae17cd29460549097c621dc
    Author: Maxime Bourget <mb14@sanger.ac.uk>
    Date:   Tue May 24 13:30:17 2011 +0100
    
        Add search user by swipecard code
    
    commit 3c69b099b510f6dde53b221fa72940dd3d14eded
    Author: Maxime Bourget <mb14@sanger.ac.uk>
    Date:   Mon May 23 16:06:24 2011 +0100
    
        Test swipecard set and compare
    
    commit fdede084b4c99c7146539cfe8ae308d9ef8d7f00
    Author: Maxime Bourget <mb14@sanger.ac.uk>
    Date:   Mon May 23 15:02:44 2011 +0100
    
        Test swipecard default value in read
    
    commit 6e0015d22c4e7ce8c269a7d80cc831cb6b88975e
    Author: Maxime Bourget <mb14@sanger.ac.uk>
    Date:   Mon May 23 14:49:35 2011 +0100
    
        Test swipbarcode update in API
    
    commit 3685d9974851f7e87d153828aea808745047aa3b
    Author: Maxime Bourget <mb14@sanger.ac.uk>
    Date:   Mon May 23 13:36:00 2011 +0100
    
        Add API user update features.
    
    commit cd07834e6b4a79e62a26fb5bc73b8ae11ad6c127
    Author: Maxime Bourget <mb14@sanger.ac.uk>
    Date:   Fri May 20 16:27:22 2011 +0100
    
        Test read action for api/user
    
    commit deedf8b386ac093824e25b1786d62ee3374e44f3
    Author: Maxime Bourget <mb14@sanger.ac.uk>
    Date:   Fri May 20 13:06:01 2011 +0100
    
        Add search user by login api features
    
    commit b6419c109e72bca50eb0d6eb68d8b1e34f5b23f5
    Author: Maxime Bourget <mb14@sanger.ac.uk>
    Date:   Thu May 19 16:52:23 2011 +0100
    
        Add api search user by login
    
    commit 927aebab277014affb13819ed4b43987996155db
    Author: Maxime Bourget <mb14@sanger.ac.uk>
    Date:   Thu May 19 16:45:43 2011 +0100
    
        authorize '?' at the end of an attribute in the json mapping
    
    commit 52f92d69af9c23c81501a3eb0f68bd418d08ea25
    Author: Maxime Bourget <mb14@sanger.ac.uk>
    Date:   Thu May 19 11:43:15 2011 +0100
    
        Add swipecard functions and test
    
    commit 8e9e817f0c9b09e265c066ea2f9a1903e8df6794
    Author: Maxime Bourget <mb14@sanger.ac.uk>
    Date:   Wed May 18 14:41:32 2011 +0100
    
        Add generated files for api/user
    
    commit a1e14ff60006bd7202b603c18f0ca9e001c84431
    Author: Maxime Bourget <mb14@sanger.ac.uk>
    Date:   Wed May 18 14:39:54 2011 +0100
    
        Fix api features generator

---
+++ app/api/endpoints/users.rb
@@ -0,0 +1,22 @@
+class ::Endpoints::Users < ::Core::Endpoint::Base
+  model do
+  end
+
+  instance do
+    # We don't need them right now
+    #belongs_to(:workflow, :json => "workflow")
+    #has_many(:batches, :include => [], :json => "batches", :to => "batches")
+    #has_many(:comments, :include => [], :json => "comments", :to => "comments")
+    #has_many(:events, :include => [], :json => "events", :to => "events")
+    #has_many(:items, :include => [], :json => "items", :to => "items")
+    #has_many(:lab_events, :include => [], :json => "lab_events", :to => "lab_events")
+    #has_many(:requests, :include => [], :json => "requests", :to => "requests")
+    
+    # Doesn't exist anymore
+    #has_many(:settings, :include => [], :json => "settings", :to => "settings")
+    #
+    action(:update, :to => :standard_update!)
+    #action_guard(:update) { |guard| guard.authorised? and not guard.finished? }
+
+  end
+end
