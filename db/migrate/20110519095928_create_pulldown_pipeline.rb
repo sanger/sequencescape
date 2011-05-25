c76b99ee75e8869746b94863902de855642df02e
Matthew Denner <matt.denner@gmail.com>
2011-05-25 20:16:19 +0100

    Put in the 3 pipelines for pulldown in a bare form.
    This adds 3 pipelines for the pulldown laboratory, the appropriate
    submission templates, and all of the plate purposes for them.
    
    There may be a fourth pipeline required, multiplexed, but this is not
    required at this stage of development.

---
+++ db/migrate/20110519095928_create_pulldown_pipeline.rb
@@ -0,0 +1,45 @@
+class CreatePulldownPipeline < ActiveRecord::Migration
+  PIPELINES = [
+    'Pulldown WGS',
+    'Pulldown SC',
+    'Pulldown ISC'
+  ]
+
+  def self.up
+    ActiveRecord::Base.transaction do
+      next_gen_sequencing = Submission::Workflow.find_by_key('short_read_sequencing') or raise StandardError, 'Cannot find Next-gen sequencing workflow'
+
+      PIPELINES.each do |pipeline_name|
+        Pipeline.create!(:name => pipeline_name) do |pipeline|
+          pipeline.sorter          = Pipeline.maximum(:sorter) + 1
+          pipeline.automated       = false
+          pipeline.active          = true
+          pipeline.asset_type      = 'LibraryTube'
+          pipeline.group_by_parent = true
+
+          pipeline.location = Location.create!(:name => "#{pipeline_name} freezer")
+
+          pipeline.request_type = RequestType.create!(:workflow => next_gen_sequencing, :name => pipeline_name) do |request_type|
+            request_type.key               = pipeline_name.downcase.gsub(/\s+/, '_')
+            request_type.initial_state     = 'pending'
+            request_type.asset_type        = 'Well'
+            request_type.order             = 1
+            request_type.multiples_allowed = false
+            request_type.request_class     = PulldownLibraryCreationRequest
+          end
+
+          pipeline.workflow = LabInterface::Workflow.create!(:name => pipeline_name)
+        end
+      end
+    end
+  end
+
+  def self.down
+    ActiveRecord::Base.transaction do
+      PIPELINES.each do |pipeline_name|
+        [ Pipeline, LabInterface::Workflow, RequestType ].each { |model| model.find_by_name(pipeline_name).try(:destroy) }
+        Location.find_by_name("#{pipeline_name} freezer").try(:destroy)
+      end
+    end
+  end
+end
