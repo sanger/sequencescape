0db85ef1fe70f4ab85a903bcacfc97d4bbb132c3
Matthew Denner <matt.denner@gmail.com>
2011-05-30 10:57:50 +0100

    TransferRequest should physically do the transfer on start.
    When you start a TransferRequest instance it should behave as though the
    physical transfers are being made: copying the sample but duplicating
    the tags.
    
    The only issue here is that there can be multiple transfer requests into
    one destination and assets cannot hold more than one sample at the
    moment.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,16 @@
 class Tag < ActiveRecord::Base
+  module Associations
+    def self.included(base)
+      base.class_eval do
+        has_one :tag_instance, :through => :links_as_child, :source => :ancestor, :conditions => { :sti_type => 'TagInstance' }
+      end
+    end
+
+    def untag!
+      links_as_child.first(:conditions => { :ancestor_id => tag_instance.id }).destroy
+    end
+  end
+
   acts_as_audited :on => [:destroy, :update]
   cattr_reader :per_page
   @@per_page = 500
@@ -10,11 +22,11 @@ class Tag < ActiveRecord::Base
 
   named_scope :sorted , :order => "map_id ASC"
   named_scope :including_associations_for_json, { :include => [ :uuid_object, { :tag_group => [:uuid_object] } ] }
-  
+
   def self.render_class
     Api::TagIO
   end
-  
+
   def name
     "Tag #{map_id}"
   end
@@ -22,5 +34,10 @@ class Tag < ActiveRecord::Base
   # Creates an instance of this tag that can be attached to a well.
   def create!
     TagInstance.create!(:tag => self)
+  end
+
+  # Connects a tag instance to the specified asset
+  def tag!(asset)
+    AssetLink.create_edge!(create!, asset)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110527122645) do
+ActiveRecord::Schema.define(:version => 20110531103000) do
 
   create_table "archived_properties", :force => true do |t|
     t.text    "value"
