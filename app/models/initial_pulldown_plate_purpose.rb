45e9f01431ca93f582b3b8abfef86e96a672387f
Matthew Denner <md12@sanger.ac.uk>
2011-06-20 14:21:03 +0100

    Performance enhancements.
    Rather than having to set all of the requests leading from the stock
    plate wells, we only need to set those that are not transfer requests,
    for initial pulldown plates.
    
    To calculate the iteration of a plate we only need to look at the parent
    asset links to other plates of the same plate purpose.  The trick is to
    get MySQL to do the row number so that we don't have to.
    
    Tidied up the acts_as_descriptable code because it was a mess.  Still
    needs a lot of work.

--- app/models/initial_pulldown_plate_purpose.rb
+++ app/models/initial_pulldown_plate_purpose.rb
@@ -8,8 +8,13 @@ class InitialPulldownPlatePurpose < PlatePurpose
     start_stock_plate_requests(plate) if state == 'started'
   end
 
+  # Find all of the non-transfer requests from the wells on the stock plate and update them to started.  Really
+  # we should only have one instance of this class associated to a particular stock plate but we have to be 
+  # careful.
   def start_stock_plate_requests(plate)
-    plate.parent.wells.map(&:requests_as_source).flatten.each do |request|
+    plate.parent.wells.map do |well|
+      well.requests_as_source.where_is_not_a?(TransferRequest)
+    end.flatten.each do |request|
       request.update_attributes!(:state => 'started')
     end
   end
--- vendor/plugins/acts_as_descriptable/lib/acts_as_descriptable.rb
+++ vendor/plugins/acts_as_descriptable/lib/acts_as_descriptable.rb
@@ -1,36 +1,18 @@
 module ActiveRecord # :nodoc:
   module Acts #:nodoc:
     module Descriptable
-      
       def self.included(base) # :nodoc:
         base.extend(ClassMethods)
       end
-      
+
       module ClassMethods
         def acts_as_descriptable(*options)
-
-          if options.to_s == 'active'
-            module_eval <<-"end_eval"
-              has_many :descriptors
-            end_eval
-            
-            include ActiveRecord::Acts::Descriptable::ActiveInstanceMethods
-          else
-            module_eval <<-"end_eval"
-              serialize :descriptors
-              serialize :descriptor_fields
-            end_eval
-            
-            include ActiveRecord::Acts::Descriptable::InstanceMethods
-          end
-          
-          
+          include ActiveRecord::Acts::Descriptable::InstanceMethods
           extend ActiveRecord::Acts::Descriptable::SingletonMethods
         end
       end
-      
+
       module SingletonMethods
-        
         def search(descriptors)
           conditions = ""
           for descriptor in descriptors
@@ -41,7 +23,7 @@ module ActiveRecord # :nodoc:
           logger.info "Searching for: " + conditions
           self.find(:all, :conditions => conditions)
         end
-        
+
         def find_descriptors
           logger.info "Finding all descriptors"
           response = []
@@ -52,104 +34,65 @@ module ActiveRecord # :nodoc:
           end
           response
         end
-        
       end
-      
-      module ActiveInstanceMethods
-        
-        def create_descriptors(params)
-          count = 0    
-          params.keys.sort_by{|key| key.to_i}.each do |field_id|
-            count = count + 1
-            descriptor = Descriptor.new(params[field_id])
-            descriptor.sorter = count
-            if params[field_id][:required] == "on"
-              descriptor.required = 1
-            else
-              descriptor.required = 0
-            end
-            descriptors << descriptor            
+
+      module InstanceMethods
+        def self.included(base)
+          base.class_eval do
+            serialize :descriptors
+            serialize :descriptor_fields
           end
         end
 
-        def update_descriptors(params)   
-          delete_descriptors
-          create_descriptors(params)       
-        end
-        
-        def delete_descriptors
-          id = self.descriptors.first
-          self.descriptors.each do |d|
-            d.destroy
-          end
-          self.descriptors = []
-        end
-              
-        def descriptors
-          self.descriptors.find(self.id, :order => 'sorter')
-        end
-          
-      end
-      
-      module InstanceMethods
-        
         def descriptor_xml(options = {})
           xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
           xml.instruct! unless options[:skip_instruct]
 
-          xml.descriptors do |descriptors|
-            if !self.descriptors.nil?
-                self.descriptors.each do |field|
-                  descriptors.descriptor do |descriptor|
-                    descriptor.name field.name.to_s
-                    descriptor.value field.value
-                  end
-                end
+          xml.descriptors {
+            self.descriptors.each do |field|
+              xml.descriptor {
+                descriptor.name  field.name.to_s
+                descriptor.value field.value
+              }
             end
-          end  
+          }
         end
-        
+
         def descriptors
-          initialize_fields
-          descriptor_hash = read_attribute(:descriptors)
-          response = Array.new
-          read_attribute(:descriptor_fields).each do |field|
-            if !field.nil? and field != ""
-              d = Descriptor.new(:name => field, :value => descriptor_hash[field])
-              response.push d
+          [].tap do |response|
+            each_descriptor do |field, value|
+              response.push(Descriptor.new(:name => field, :value => value))
             end
           end
-          response
+        end
+
+        def each_descriptor(&block)
+          descriptor_hash = read_descriptor_hash
+          read_descriptor_fields.each do |field|
+            next if field.blank?
+            yield(field, descriptor_hash[field])
+          end
         end
 
         def descriptor_value(key)
-          if read_attribute(:descriptors).nil?
-            ""
-          else
-            read_attribute(:descriptors)[key]
-          end
+          read_descriptor_hash.fetch(key, '')
         end
-                
+
         def add_descriptor(descriptor)
-          initialize_fields
-          descriptors = read_attribute(:descriptors)
-          write_attribute(:descriptors, descriptors.merge({ descriptor.name => descriptor.value }) )
-          write_attribute(:descriptor_fields, read_attribute(:descriptor_fields).push(descriptor.name))
+          write_attribute(:descriptors,       read_descriptor_hash.merge(descriptor.name => descriptor.value))
+          write_attribute(:descriptor_fields, read_descriptor_fields.push(descriptor.name))
         end
-        
-        private
-        
-        def initialize_fields
-          if read_attribute(:descriptors).nil?
-            write_attribute(:descriptors, {})
-          end
-          if read_attribute(:descriptor_fields).nil?
-            write_attribute(:descriptor_fields, [])
-          end          
+
+        def read_descriptor_hash
+          read_attribute(:descriptors) || {}
         end
-                
+        private :read_descriptor_hash
+
+        def read_descriptor_fields
+          read_attribute(:descriptor_fields) || []
+        end
+        private :read_descriptor_fields
       end
-      
     end
   end
 end
