88301af5c411ef64bd40a0fda6c801a3a2393119
Matthew Denner <md12@sanger.ac.uk>
2011-06-23 15:15:58 +0100

    Expose source transfers for plates

--- app/api/endpoints/plates.rb
+++ app/api/endpoints/plates.rb
@@ -6,6 +6,8 @@ class ::Endpoints::Plates < ::Core::Endpoint::Base
   instance do
     has_many(:requests,                  :json => 'requests', :to => 'requests')
     belongs_to(:plate_purpose,           :json => 'plate_purpose')
+
+    has_many(:transfers_as_source,       :json => 'source_transfers', :to => 'source_transfers')
     belongs_to(:transfer_as_destination, :json => 'creation_transfer')
   end
 end
