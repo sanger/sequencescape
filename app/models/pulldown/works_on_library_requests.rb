cc2bbbb705b956d2d785a61eed2486aba2032b74
Matthew Denner <md12@sanger.ac.uk>
2011-07-08 15:07:12 +0100

    Ensure stock wells are used for the library creation steps.

--- app/models/pulldown/works_on_library_requests.rb
+++ app/models/pulldown/works_on_library_requests.rb
@@ -1,7 +1,14 @@
 module Pulldown::WorksOnLibraryRequests
+  def self.included(base)
+    base.class_eval do
+      include Transfer::WellHelpers
+    end
+  end
+
   def each_well_and_its_library_request(plate, &block)
     plate.wells.each do |well|
-      transfer_request = well.transfer_requests_as_target.first or next
+      stock_well       = locate_stock_well_for(well) or next
+      transfer_request = stock_well.transfer_requests_as_target.first or next
       library_request  = transfer_request.asset.requests_as_source.where_is_a?(PulldownLibraryCreationRequest).first
       yield(well, library_request)
     end
