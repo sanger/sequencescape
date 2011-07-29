1dd847801ae89089b9fc95b90b1af9ba3d0e7742
Matthew Denner <matt.denner@gmail.com>
2011-07-29 20:46:08 +0100

    Ensure samples are in the correct containers.
    The ensures that any samples that are being updated by a manifest are
    tied to the correct container.

--- app/models/sample_manifest/sample_tube_behaviour.rb
+++ app/models/sample_manifest/sample_tube_behaviour.rb
@@ -52,6 +52,12 @@ module SampleManifest::SampleTubeBehaviour
         })
       end
     end
+
+    def validate_sample_container(sample, row, &block)
+      manifest_barcode, primary_barcode = row['SOMETHING'], sample.primary_receptacle.sanger_human_barcode
+      return if primary_barcode == manifest_barcode
+      yield("Tube info for #{sample.sanger_sample_id} mismatch: expected #{primary_barcode} but reported as #{manifest_barcode}")
+    end
   end
 
   # There is no reason for this to need a rapid version as it should be reasonably
