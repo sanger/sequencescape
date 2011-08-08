b6bbb20238a7f1283901b69635fc7ecef39b1343
Matthew Denner <md12@sanger.ac.uk>
2011-08-08 12:17:11 +0100

    Order primary receptacles by created and ID.
    There seems to be a way that SLF create multiple plates all at exactly
    the same time and that leaves MySQL open to pick at random which is the
    primary one.  By ordered by created_at and then by id ASC we get the
    correct one.

--- app/models/aliquot.rb
+++ app/models/aliquot.rb
@@ -76,7 +76,7 @@ class Aliquot < ActiveRecord::Base
 
         has_many :aliquots
         has_many :receptacles, :through => :aliquots, :uniq => true
-        has_one :primary_receptacle, :through => :aliquots, :source => :receptacle, :order => 'assets.created_at'
+        has_one :primary_receptacle, :through => :aliquots, :source => :receptacle, :order => 'assets.created_at, assets.id ASC'
 
         # Unfortunately we cannot use has_many :through because it ends up being a through through a through.  Really we want:
         #   has_many :requests, :through => :assets
