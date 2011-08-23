551536537525904368ffc1202e773a706eccd297
Lars G T Jorgensen <lj3@sanger.ac.uk>
2011-08-23 15:03:57 +0100

    Change QC state to be correct

--- app/models/api/library_tube_io.rb
+++ app/models/api/library_tube_io.rb
@@ -35,7 +35,7 @@ class Api::LibraryTubeIO < Api::Base
   map_attribute_to_json_attribute(:id)
   map_attribute_to_json_attribute(:name)
   map_attribute_to_json_attribute(:barcode)
-  map_attribute_to_json_attribute(:compatible_qc_state, 'qc_state')
+  map_attribute_to_json_attribute(:qc_state)
   map_attribute_to_json_attribute(:closed)
   map_attribute_to_json_attribute(:two_dimensional_barcode)
   map_attribute_to_json_attribute(:concentration)
