43779c601e4303cb863e5369a5f942ce91ae9fe0
Maxime Bourget <mb14@sanger.ac.uk>
2011-08-23 15:09:48 +0100

    Fix and extracte plate_purpose_options to a module

---
+++ app/models/tasks/plate_purpose_behavior.rb
@@ -0,0 +1,11 @@
+module Tasks
+  module PlatePurposeBehavior
+    # Returns a list of valid plate purpose types based on the requests in the current batch.
+    def plate_purpose_options(batch)
+      requests       = batch.requests.map { |r| r.submission ? r.submission.next_requests(r) : [] }.flatten
+      plate_purposes = requests.map(&:request_type).compact.uniq.map(&:acceptable_plate_purposes).flatten.uniq
+      plate_purposes = PlatePurpose.all if plate_purposes.empty?  # Fallback situation for the moment
+      plate_purposes.map { |p| [p.name, p.id] }.sort
+    end
+  end
+end
