346a6f9a01c8e738b6d8beb23b1181633fded8a6
Sean Dunn <sd9@sanger.ac.uk>
2011-08-25 18:24:25 +0100

    Removed lots of unnecesary code from Characterisation handler

--- app/models/tasks/set_characterisation_descriptors_handler.rb
+++ app/models/tasks/set_characterisation_descriptors_handler.rb
@@ -1,17 +1,11 @@
 module Tasks::SetCharacterisationDescriptorsHandler
   def do_set_characterisation_descriptors_task(task, params)
-    @batch = Batch.find(params[:batch_id], :include => [:requests, :pipeline, :lab_events])
-    @rits = @batch.pipeline.request_information_types
-    @requests = @batch.ordered_requests
-
     unless @batch.started? || @batch.failed?
       @batch.start!(current_user)
     end
 
 
-    @workflow = LabInterface::Workflow.find(params[:workflow_id], :include => [:tasks])
-    @task = @workflow.tasks[params[:id].to_i]
-    @stage = params[:id].to_i
+
     @count = 0
     if params[:values].nil?
       @values = {}
@@ -20,76 +14,46 @@ module Tasks::SetCharacterisationDescriptorsHandler
     end
 
     # Perform the necessary updates if we've passed batch creation
-    unless params[:next_stage].nil?
-      updated = 0
+    updated = 0
 
-      @batch.requests.each do |request|
+    @batch.requests.each do |request|
 
-        event = LabEvent.new(:batch_id => @batch.id, :description => @task.name)
+      event = LabEvent.new(:batch_id => @batch.id, :description => @task.name)
 
-        unless params[:descriptors].nil?                
-          event.descriptors = params[:descriptors]
-          event.descriptor_fields = ordered_fields(params[:fields])
+      if params[:requests].present? && params[:requests]["#{request.id}"].present? && params[:requests]["#{request.id}"][:descriptors].present?
+        # Descriptors: create description for event
 
-          # Cache values to populate the next request on the same stage
-          event.descriptors.each do |descriptor|
-            @values[descriptor.name] = descriptor.value
-          end
-        end
+        event.descriptors = params[:requests]["#{request.id}"][:descriptors]
+        event.descriptor_fields = ordered_fields(params[:requests]["#{request.id}"][:fields])
 
-        if !params[:requests].nil? && !params[:requests]["#{request.id}"].nil? && !params[:requests]["#{request.id}"][:descriptors].nil?
-          # Descriptors: create description for event
-
-          event.descriptors = params[:requests]["#{request.id}"][:descriptors]
-          event.descriptor_fields = ordered_fields(params[:requests]["#{request.id}"][:fields])
-
-        end
-
-        unless params[:upload].nil?
-          params[:upload].each_key do |key|
-            event.filename = params[:upload][key].original_filename.gsub(/[^a-zA-Z0-9.]/, '_')
-            event.data = params[:upload][key].read
-            event.add_descriptor Descriptor.new({:name => key, :value => event.filename})
-          end
-        end
-
-        event.save!
-        current_user.lab_events << event
-        request.lab_events << event
-
-        if params[:asset]
-          params[:asset].keys.each do |key|
-            asset = Asset.new()
-            asset.sti_type = Family.find(params[:asset][key][:family_id]).name
-            params[:asset][key].each_key do |field|
-              asset.add_descriptor Descriptor.new({ :name => field, :value => params[:asset][key][field] })
-            end
-            asset.save!
-            asset.parents << request.asset
-          end
-        end
-
-        unless request.asset.try(:resource)
-          EventSender.send_request_update(request.id, "update", "Passed: #{@task.name}")
-        end
-
-        if request.has_passed(@batch, @task) || request.failed?
-          updated += 1
-        end
       end
 
+      event.save!
+      current_user.lab_events << event
+      request.lab_events << event
 
-      if updated == @batch.requests.count
-        eventify_batch @batch, @task
-        return true
-      else
-        # Some requests have yet to pass this task
-        # Construct a URL that contains a nested hash of values to display as defaults for the next request
-        @params = { :batch_id => @batch.id, :workflow_id => @workflow.id, :values => @values }
-        redirect_to url_for(flatten_hash(@params))
+
+      unless request.asset.try(:resource)
+        EventSender.send_request_update(request.id, "update", "Passed: #{@task.name}")
       end
 
+      if request.has_passed(@batch, @task) || request.failed?
+        updated += 1
+      end
     end
+
+
+    # Did all the requests get updated?
+    if updated == @batch.requests.count
+      eventify_batch @batch, @task
+      return true
+    else
+      # Some requests have yet to pass this task
+      # Construct a URL that contains a nested hash of values to display as defaults for the next request
+      @params = { :batch_id => @batch.id, :workflow_id => @workflow.id, :values => @values }
+      redirect_to url_for(flatten_hash(@params))
+    end
+
     false
   end
 
