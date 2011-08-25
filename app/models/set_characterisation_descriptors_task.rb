817807e1ecfd52230841cbf2c64d02b425792a03
Sean Dunn <sd9@sanger.ac.uk>
2011-08-25 17:02:17 +0100

    Pulled out a duplicate of the SetCharacterisationTask and handler Fixed bug in Characterisation.js that was not passing through keystrokes

--- app/models/set_characterisation_descriptors_task.rb
+++ app/models/set_characterisation_descriptors_task.rb
@@ -1,6 +1,24 @@
-class SetCharacterisationDescriptorsTask < SetDescriptorsTask
+class SetCharacterisationDescriptorsTask < Task
   def partial
     "set_characterisation_descriptors"
   end
 
+  def render_task(workflows_controller, params)
+    super
+    workflows_controller.render_set_characterisation_descriptors_task(self, params)
+  end
+
+
+  def do_task(workflows_controller, params)
+    workflows_controller.do_set_characterisation_descriptors_task(self,params)
+  end
+
+  def sub_events_for_event(event)
+    return [] unless event.eventful.respond_to?(:asset)
+    subassets = subassets_for_asset(event.eventful.asset).select do |asset|
+      # we don't want anything except fragment gel so far ...
+      asset.is_a?(Fragment) && self.name == "Gel"
+    end
+    return subassets.map { |a| generate_events_from_descriptors(a) }
+  end
 end
