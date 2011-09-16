9731b0fde59d0bc7b459f9d5088185d111b967f4
Maxime Bourget <mb14@sanger.ac.uk>
2011-09-16 20:25:38 +0100

    Fix unprocessed

--- test/unit/sample_test.rb
+++ test/unit/sample_test.rb
@@ -95,7 +95,7 @@ class SampleTest < ActiveSupport::TestCase
         should "return true" do
           @result = @sample_from_ok.move(@study_from, @study_to, @asset_group_to_new, @new_assets_name, @current_user, "0")
           @new_sub_to = Submission.last
-          asset_submission_to = @new_sub_to.assets
+          asset_submission_to = @new_sub_to.order.assets
           assert_equal true, @result
           #the assets of submission_from is now in submission_to
           assert_equal asset_submission_to, [ @asset_from ]
