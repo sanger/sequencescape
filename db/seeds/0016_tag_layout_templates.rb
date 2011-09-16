4ae3051d1b44b908fd074da9f37cdba06de6adc8
Matthew Denner <matt.denner@gmail.com>
2011-09-16 10:09:56 +0100

    [fixes 18069777] Seeds need to have the correct template layout

--- db/seeds/0016_tag_layout_templates.rb
+++ db/seeds/0016_tag_layout_templates.rb
@@ -1,19 +1,14 @@
 ActiveRecord::Base.transaction do
   TagGroup.find_each do |tag_group|
     TagLayoutTemplate.create!(
-      :name              => tag_group.name,
+      :name              => "#{tag_group.name} in column major order",
       :tag_group         => tag_group,
-      :layout_class_name => 'TagLayout::ByPools'
+      :layout_class_name => 'TagLayout::InColumns'
     )
-
-    # Although this is supported by the code, laying out tags in a row major order makes life
-    # very difficult when the submissions are laid out in column major order.  It's better to
-    # reduce the complexity by not offering this and then limiting the tag templates that can
-    # be applied based on the maximum pool size on the plate.
-#    TagLayoutTemplate.create!(
-#      :name              => "#{tag_group.name} in row major order",
-#      :tag_group         => tag_group,
-#      :layout_class_name => 'TagLayout::InRows'
-#    )
+    TagLayoutTemplate.create!(
+      :name              => "#{tag_group.name} in row major order",
+      :tag_group         => tag_group,
+      :layout_class_name => 'TagLayout::InRows'
+    )
   end
 end
