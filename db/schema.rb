ffe9c4e66a51b82a925bc1eabd43a2dcbc3b2f2e
Matthew Denner <md12@sanger.ac.uk>
2011-10-11 16:09:12 +0100

    [fixes 19531165] Default user workflow to 'Next-gen sequencing'

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110930201202) do
+ActiveRecord::Schema.define(:version => 20111011150028) do
 
   create_table "aliquots", :force => true do |t|
     t.integer  "receptacle_id",    :null => false
