078d47c4cf4f66b6ab17b3b15ab0cb744a1fe327
Jens Raaby <jr16@sanger.ac.uk>
2011-10-17 15:56:18 +0100

    Added document carrierwave uploader, and document model changed to use it

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,14 +2,18 @@ class Document < ActiveRecord::Base
   # This prevents the error message:
   #   "A copy of Technoweenie::AttachmentFu::ActMethods has been removed from the module tree but is still active!"
   # See http://www.pivotaltracker.com/story/show/4293575 for why this kind of works.
-  extend Technoweenie::AttachmentFu::ActMethods
+  # extend Technoweenie::AttachmentFu::ActMethods
 
   belongs_to :documentable, :polymorphic => true
   
-  has_attachment :storage => :multi_db_file,
-                 :max_part_size => 200.kilobytes
-
+  # CarrierWave
+  # mount_uploader :storage, DocumentUploader
+  
+   has_attachment :storage => :multi_db_file,
+                   :max_part_size => 200.kilobytes
+ 
   before_validation do |record|
     record.filename ||= record.uploaded_data.original_filename unless record.uploaded_data.nil?
   end
 end
+ 
