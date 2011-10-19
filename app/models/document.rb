b6bdda23d9978d6bfb25f454a8196612b432fed9
Jens Raaby <jr16@sanger.ac.uk>
2011-10-19 10:29:30 +0100

    Tidied up code, now using cleaner slice routine, comments added, temp dir changed and should be emptied on every store action

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,45 +1,36 @@
 class Document < ActiveRecord::Base
+  
   class DbFile < ActiveRecord::Base
+    # This is the storage itself! Documents are stored here instead of in the filesystem
     set_table_name "db_files"
     belongs_to :document
     # Note: We are constrained by the database to split files into 200kbyte partitions
-
   end
-  # This prevents the error message:
-  #   "A copy of Technoweenie::AttachmentFu::ActMethods has been removed from the module tree but is still active!"
-  # See http://www.pivotaltracker.com/story/show/4293575 for why this kind of works.
-  # extend Technoweenie::AttachmentFu::ActMethods
 
+  # Polymorphic relationship
   belongs_to :documentable, :polymorphic => true
   
-  # Since files are stored in a separate DB table, this allows files to be split into partitions in the database
+  # Creates document.db_files association so when the file is split we can get all the chunks
   has_many  :db_files, :class_name => "Document::DbFile"
-  
-  
-  #CarrierWave
+
+  #CarrierWave uploader - gets the uploaded_data file, but saves the identifier to the "filename" column
   mount_uploader :uploaded_data, DocumentUploader, :mount_on => "filename"
   
+  # Method provided for backwards compatibility
   def current_data
     uploaded_data.read
   end
   
-   # has_attachment :storage => :multi_db_file,
-   #                   :max_part_size => 200.kilobytes
-   # 
-  # before_validation do |record|
-  #    record.filename ||= record.uploaded_data.original_filename unless record.uploaded_data.nil?
-  #  end
-  
- before_save :update_document_attributes
+  # Handle some of the metadata with this callback
+  before_save :update_document_attributes
     
-    private
+  private
     
+    # Save Size/content_type Metadata
     def update_document_attributes
-      #TODO[JR] deal with update as well as create
       if uploaded_data.present? 
         self.content_type = uploaded_data.file.content_type
-        self.size    = uploaded_data.file.size
-        
+        self.size    = uploaded_data.file.size        
       end
     end
 end
