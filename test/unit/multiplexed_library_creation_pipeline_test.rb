1dff5ed6bd9906f1e2b72a567a2e7bd8a9051147
Matthew Denner <md12@sanger.ac.uk>
2011-10-31 14:00:42 +0000

    [fixes 18272943] Do not allow untagged batches out of MX pipeline
    Batches that have not been tagged should not be allowed to be completed
    in the MX library creation pipeline.  So this code adds a final
    validation when the batch is in that state to check.
    
    In the process I discovered that the MX library creation pipeline in
    the seeds file was of the wrong type, which lead to a bug in the batch
    reset feature!

---
+++ test/unit/multiplexed_library_creation_pipeline_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class MultiplexedLibraryCreationPipelineTest < ActiveSupport::TestCase
+  def setup
+    @pipeline = Pipeline.find_by_name('MX Library Preparation [NEW]') or raise StandardError, "Cannot find the MX library creation pipeline"
+    @user     = Factory(:user)
+  end
+
+  context 'batch interaction' do
+    setup do
+      @batch = Factory(:batch, :pipeline => @pipeline)
+      @batch.requests << (1..5).map { |_| Factory(:request_suitable_for_starting, :request_type => @batch.pipeline.request_type) }
+    end
+
+    context 'for completion' do
+      setup do
+        @batch.start!(@user)
+      end
+
+      should 'add errors if there are untagged target asset aliquots' do
+        @batch.requests.map(&:target_asset).map(&:untag!)
+
+        assert_raise(ActiveRecord::RecordInvalid) do
+          @batch.complete!(@user)
+        end
+
+        assert(!@batch.errors.empty?, "There are no errors on the batch")
+      end
+
+      should 'not error if all of the target asset aliquots are tagged' do
+        @batch.requests.each_with_index { |r,i| Factory(:tag, :map_id => i).tag!(r.target_asset) }
+        @batch.complete!(@user)
+
+        assert(@batch.errors.empty?, "There are errors on the batch")
+      end
+    end
+  end
+end
