6d3938dc55c4d565389ea493182b8e4b5119d832
Sean Dunn <sd9@sanger.ac.uk>
2011-11-22 19:02:56 +0000

    Added loading of the submission wide order parameters.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,5 +1,5 @@
 class SubmissionPresenter
-  ATTRIBUTES = [ :submission_template_id, :study_name, :project_name, :agree_requirements ]
+  ATTRIBUTES = [ :submission_template_id, :study_name, :project_name, :comments ]
 
   attr_accessor *ATTRIBUTES
 
@@ -16,6 +16,18 @@ class SubmissionPresenter
     raise "NOT IMPLEMENTED YET"
   end
 
+  # These parameters should be defined by the submission template (to be renamed
+  # order template) the old view code gets them by generating a new instance of
+  # Order and then calling Order#input_field_infos.  This is a wrapper around
+  # until I can refactor it out.
+  def order_parameters
+    order.input_field_infos
+  end
+
+  def order
+    @order ||= template.new_order
+  end
+
   def project
     @project ||= Project.find_by_name(@project_name)
   end
@@ -23,7 +35,9 @@ class SubmissionPresenter
   # Creates a new submission and adds an initial order on the submission using
   # the parameters
   def save!
-    raise "NOT IMPLEMENTED YET"
+    ActiveRecord::Base.transaction do
+      raise "NOT IMPLEMENTED YET"
+    end
   end
 
   def study
@@ -33,6 +47,11 @@ class SubmissionPresenter
   # Returns an array of the names of all studies
   def studies
     @studies ||= Study.all.map(&:name)
+  end
+
+  # Returns the SubmissionTemplate (OrderTemplate) to be used for this Submission.
+  def template
+    @template ||= SubmissionTemplate.find(submission_template_id)
   end
 
   def templates
@@ -62,9 +81,16 @@ class SubmissionsController < ApplicationController
   # Renders the requirements document based on the SubmissionTemplate.
   # TODO[sd9]: Move this to a separate name spaced controller
   def requirements
+    # TODO[sd9]: Change this to an attribute on the presenter
     @submission_template = SubmissionTemplate.find(params[:submission][:submission_template_id])
 
     render :partial => 'submissions/requirements', :layout => false
+  end
+
+  def order_parameters
+    @submission_presenter = SubmissionPresenter.new(current_user, params[:submission])
+
+    render :partial => 'submissions/order_parameters', :layout => false
   end
 end
 
--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,5 +1,41 @@
 module SubmissionsHelper
+
+  # Returns an array (or anything else) as an escaped string for
+  # embedding in javascript
   def stringify_array(projects_array)
     projects_array.inspect
   end
+
+  def order_input_label(field_info)
+    label("submission[order][#{field_info.key}]", field_info.display_name)
+  end
+
+  # Returns a either a text input or a selection tag based on the 'kind'
+  # of the order parameter passed in.
+  # field_info is expected to be FieldInfo [sic]
+  def order_input_tag(field_info)
+    case field_info.kind
+    when "Selection" then order_selection_tag(field_info)
+    when "Text"      then order_text_tag(field_info)
+    end
+  end
+
+  # TODO[sd9]: the form path probably needs to change here.  This tag is
+  # based on the dodgy assumption that we'll creating one order per
+  # time rather than using a collection...
+  def order_selection_tag(field_info)
+    select_tag(
+      "submission[order][#{field_info.key}]",
+      options_for_select(field_info.selection, field_info.value)
+    )
+  end
+  private :order_selection_tag
+
+  def order_text_tag(field_info)
+    text_field_tag(
+      "submission[order][#{field_info.key}]",
+      field_info.value
+    )
+  end
+  private :order_text_tag
 end
