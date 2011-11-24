83d9f866092a00af84560d99e375df2ea1b6ce40
Jens Raaby <jr16@sanger.ac.uk>
2011-11-24 12:31:04 +0000

    Changes are highlighted by animation. Duplicate tags are shaded in red. Checkbox for automatic incrementing needs better wording

--- public/javascripts/assigntags.js
+++ public/javascripts/assigntags.js
@@ -1,65 +1,61 @@
 (function($,undefined){
-	var taggers = $('select.tagchoice');
+    var taggers = $('select.tagchoice');
  
-	// This will return all the tag drop-downs after the current index
-  	function remainingTaggers(currentIndex) {
-	    return taggers.filter(function(index) {
-	      return index > currentIndex;
-	    });
-	  }
+    // This will return all the tag drop-downs after the current index
+    function remainingTaggers(currentIndex) {
+        return taggers.filter(function(index) {
+          return index > currentIndex;
+        });
+      }
   
-	// This callback will indicate other rows using the same tag
-	function highlightDuplicates() {
-		var duplicates = {};
-		taggers.each(function(index){
-			var chosenTagIndex = $(this).prop("selectedIndex");
-			if (duplicates[chosenTagIndex] != null)
-				duplicates[chosenTagIndex].push(index);
-			else
-			{
-				duplicates[chosenTagIndex] = new Array();
-				duplicates[chosenTagIndex].push(index);
-			}	
-			
-		});
-		taggers.each(function(index){
-			var chosenTagIndex = $(this).prop("selectedIndex");
-			if (duplicates[chosenTagIndex].length >1)
-			{
-				console.log('duplicate tag: ', duplicates[chosenTagIndex])
-				for (var i=0, len = duplicates[chosenTagIndex].size(); i<len; i++)
-				{
-					var thisTagger = taggers[duplicates[chosenTagIndex][i]];
-					var tocolour = $(thisTagger).parent().parent();
-					tocolour.css("background", "yellow");
-				}
-			}
-			else
-			{
-				$(taggers[duplicates[chosenTagIndex][0]]).parent().parent().css("background", "white");
-			}	
-		});
-	}
-	
-  	$('select').change(function() {
-		taggerIndex = taggers.index($(this));
-		chosenTagIndex = $(this).prop("selectedIndex");
-	   	// Set remaining indices to increment from the chosen value
-	   	remainingTaggers(taggerIndex).each(function(){$(this).prop("selectedIndex",chosenTagIndex+1);
-		chosenTagIndex++;
-		
-	});
-	highlightDuplicates();
-	});
-	
-	// Convert array to object
-	function oc(a)
-	{
-	  var o = {};
-	  for(var i=0;i<a.length;i++)
-	  {
-	    o[a[i]]='';
-	  }
-	  return o;
-	}
+    // This callback will indicate other rows using the same tag
+    function highlightDuplicates() {
+        var duplicates = {};
+        taggers.each(function(index){
+            var chosenTagIndex = $(this).prop("selectedIndex");
+            if (duplicates[chosenTagIndex] != null)
+                duplicates[chosenTagIndex].push(index);
+            else
+            {
+                duplicates[chosenTagIndex] = new Array();
+                duplicates[chosenTagIndex].push(index);
+            }   
+            
+        });
+        taggers.each(function(index){
+            var thisTagAssignments = duplicates[$(this).prop("selectedIndex")];
+            if (thisTagAssignments.length >1)
+            {
+                for (var i=0, len = thisTagAssignments.size(); i<len; i++)
+                {
+                    var tableRow = $(taggers[thisTagAssignments[i]]).parent().parent();
+                    tableRow.css("background", "#FFB0B0");
+                }
+            }
+            else
+            {
+                $(taggers[thisTagAssignments[0]]).parent().parent().css("background", "white");
+            }   
+        });
+    }
+    
+    $('select.tagchoice').change(function() {   
+        if ($(this).siblings('input:checkbox').is(':checked'))
+        {
+            chosenTagIndex = $(this).prop("selectedIndex");
+            // Set subsequent tags to increment from the chosen value
+            remainingTaggers(taggers.index($(this))).each(function()
+            {
+                    $(this).prop("selectedIndex",chosenTagIndex+1);
+					// A little animation to highlight the changed rows
+                    $(this).parent().parent().animate({
+                        backgroundColor: '#FFF3B0'
+                    }, 2000).animate({
+                        backgroundColor: '#ffffff'
+                    }, 2000);
+                    chosenTagIndex++;
+            });
+        }
+        highlightDuplicates();
+    });
 })(jQuery);
