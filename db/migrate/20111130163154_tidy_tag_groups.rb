e29b68fbd76ea2df0a52a102be329042a1759324
Jens Raaby <jr16@sanger.ac.uk>
2011-12-01 09:57:37 +0000

    Uncommented line

--- db/migrate/20111130163154_tidy_tag_groups.rb
+++ db/migrate/20111130163154_tidy_tag_groups.rb
@@ -1,6 +1,6 @@
 class TidyTagGroups < ActiveRecord::Migration
   def self.up
-    # add_column :tag_groups, :visible, :boolean, :default => true
+    add_column :tag_groups, :visible, :boolean, :default => true
     TagGroup.find_each(:conditions => "name LIKE '%do not use%'") do |hide_this|
       hide_this.visible = false
       hide_this.save!
