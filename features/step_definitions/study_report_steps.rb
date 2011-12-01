ed12def74129ff508c20c1d44f3e55c64cf5b585
Maxime Bourget <bmx007@gmail.com>
2011-12-01 16:36:02 +0000

    fix study report features (Ugly)

--- features/step_definitions/study_report_steps.rb
+++ features/step_definitions/study_report_steps.rb
@@ -29,7 +29,7 @@ Then /^the last report for "([^"]*)" should be:$/ do |study_name, expected_resul
 end
 
 Given /^study "([^"]*)" has a plate "([^"]*)"$/ do |study_name, plate_barcode|
-  plate = Plate.create!(:barcode => plate_barcode)
+  plate = Plate.create!(:barcode => plate_barcode, :plate_purpose => PlatePurpose.find_by_name("Stock Plate"))
   samples = []
   1.upto(3) do |i|
     well = Well.create!(:plate => plate, :map_id => i)
@@ -55,7 +55,7 @@ end
 
 Given /^study "([^"]*)" has a plate "([^"]*)" to be volume checked$/ do |study_name, plate_barcode|
   
-  plate = Plate.create!(:barcode => plate_barcode)
+  plate = Plate.create!(:barcode => plate_barcode, :plate_purpose => PlatePurpose.find_by_name("Stock Plate"))
   1.upto(24) do |i|
     well = Well.create!(:plate => plate, :map_id => i)
     well.aliquots.create!(:sample => Sample.create!(:name => "Sample_#{plate_barcode}_#{i}"))
