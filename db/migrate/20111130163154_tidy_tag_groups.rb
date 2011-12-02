742466f5776b9a87535fc3996236cf10d96d49e6
Jens Raaby <jr16@sanger.ac.uk>
2011-12-02 16:00:53 +0000

    Migration finalised

--- db/migrate/20111130163154_tidy_tag_groups.rb
+++ db/migrate/20111130163154_tidy_tag_groups.rb
@@ -1,7 +1,7 @@
 class TidyTagGroups < ActiveRecord::Migration
   def self.up
     add_column :tag_groups, :visible, :boolean, :default => true
-    TagGroup.update_all('visible = 0', :conditions => ["name LIKE ?","'%do not use%'"])
+    TagGroup.update_all('visible = 0', [ 'name LIKE ?', '%do not use%' ])
   end
 
   def self.down
