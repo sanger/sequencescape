1f5997ffef13ea37935a4b47fd76d98f4f150765
Jens Raaby <jr16@sanger.ac.uk>
2011-12-02 14:09:23 +0000

    Removed duplicate renaming code as production does not need this fix

--- db/migrate/20111124160904_tag_groups_name_uniqueness.rb
+++ db/migrate/20111124160904_tag_groups_name_uniqueness.rb
@@ -1,28 +1,6 @@
 class TagGroupsNameUniqueness < ActiveRecord::Migration
   
-  # This will identify duplicates in an array
-  def dup_hash(ary)
-    ary.inject(Hash.new(0)) { |h,e| h[e] += 1; h }.select { 
-      |k,v| v > 1 }.inject({}) { |r, e| r[e.first] = e.last; r }
-  end
-  
   def self.up
-    
-    names = TagGroup.all.collect(&:name)
-    
-    if (names.size!=names.uniq.size)
-      dup_hash(names).map { |d|
-        suffix = 1
-        TagGroup.all(:conditions => { :name => d[0] }).each { |tg|
-          tg.name = "#{tg.name}#{suffix}"
-          tg.save
-          suffix += 1
-          }
-      }
-      
-      
-    end
-      
     execute <<-SQL
       ALTER TABLE tag_groups
       ADD CONSTRAINT unique_name UNIQUE (name)
