f50cb94aacfec0617f8fb89c0d6ffb5f4014fb14
Sean Dunn <sd9@sanger.ac.uk>
2011-12-04 18:27:47 +0000

    Added new layout and flow ideas

--- public/javascripts/submissions.js
+++ public/javascripts/submissions.js
@@ -5,12 +5,12 @@
   if ( SCAPE.submission_details === undefined) { SCAPE.submission = {}; }
 
 
-  var orderParameterHandler = function(event){
+  var templateChangeHandler = function(event){
     markStageIncomplete();
     // When there's nothing selected reset the parameters element and exit without 
     if ($(this).val() === "") {
       delete SCAPE.submission.template_id;
-      $('#submission-parameters').html('');
+      $('#order-parameters').html('');
       return false;
     }
 
@@ -21,103 +21,20 @@
       '/submissions/order_parameters',
       { submission: SCAPE.submission },
       function(data) {
-        $('#submission-parameters').html(data).fadeIn();
+        $('#order-parameters').html(data).fadeIn();
       }
     );
     return true;
   };
 
-  var checkButtons = function(pane) {
-
-    if ($(pane).hasClass('completed')) {
-      $('#wizard-next').removeAttr('disabled');
-    }
-
-    // Check for the Previous button
-    if ($(pane).prev('li').length === 0) {
-      $('#wizard-previous').attr('disabled', 'disabled');
-    } else {
-      $('#wizard-previous').removeAttr('disabled');
-    }
-  };
-
-  var markStageComplete = function(pane) {
-    var remainingStages = $('.wizard-pane:visible').next('li').length;
-
-    $(pane).addClass('completed');
-
-    $('#submission-breadcrumbs li.active-stage').addClass('completed-stage');
-
-    //TODO should be part of checkButtons
-    if ( remainingStages === 0) {
-      // We're on the last pane of the wizard...
-      $('#wizard-next').attr('disabled', 'disabled');
-      $('#start-submission').removeAttr('disabled');
-
-      return true;
-
-    } else if ( remainingStages >= 1) {
-      // We've just completed one of the other panes...
-      $('#wizard-next').removeAttr('disabled');
-      $('#start-submission').attr('disabled', 'disabled');
-      return true;
-    }
-
-    // Well we're not in Kansas anymore...
-    return false;
-  };
-
-  var markStageIncomplete = function(pane) {
-    $('.wizard-pane:visible').removeClass('completed');
-    $('#submission-breadcrumbs li.active-stage').removeClass('completed-stage');
-    $('#wizard-next, #start-submission').attr('disabled', 'disabled');
-  };
-
-  var nextPaneHandler = function(event){
-    $(this).attr('disabled','disabled');
-
-    $('#submission-breadcrumbs li.active-stage').
-      removeClass('active-stage').
-      addClass('completed-stage').
-      next().
-      addClass('active-stage');
-
-    $('.wizard-pane:visible').fadeOut(300, function(){
-      $(this).next().fadeIn(300, function(){
-        checkButtons(this);
-      });
-    });
-
-    // Return false so that the form won't submit early.
-    return false;
-  };
-
-  var previousPaneHandler = function(event){
-    $(this).attr('disabled','disabled');
-
-    $('#submission-breadcrumbs li.active-stage').
-      removeClass('active-stage').
-      prev().
-      addClass('active-stage');
-
-
-    $('.wizard-pane:visible').fadeOut(300, function(){
-      $(this).prev().fadeIn(300, function(){
-        checkButtons(this);
-      });
-    });
-
-    // Return false so that the form won't submit early.
-    return false;
-  };
 
   // This handler depends on the study template being set earlier in the wizard.
   var studySelectHandler = function(event) {
+    markStageIncomplete();
     SCAPE.submission.study_id = $(this).val();
 
 
     if ($(this).val().length > 0) {
-      markStageIncomplete();
 
       // Load asset groups for the selected study
       $.get(
@@ -151,60 +68,53 @@
     );
   };
 
-  var validateSelection = function(event) {
-    var currentPane = $('.wizard-pane:visible');
 
-    var incompleteFieldCount = currentPane.
-      find('input, select').
-      filter(function(){
-        return $(this).val() === "";
-      }).length;
+  // Returns true if all the input fields in a pane have a value
+  var allFieldsComplete = function(pane) {
+      // This won't work in old IE versions <9.
+      // If we need to support old IE add a conditional enhancement
+      // to the start of the module...
+    return pane.find('input, select').toArray().
+      every(function(element){ return $(element).val(); });
+  };
 
-    if (incompleteFieldCount === 0) {
+  var markStageComplete = function(pane) {
+    $(pane).addClass('completed').removeClass('active');
 
-      // Replace order_params just incase there's any old ones in there...
-      SCAPE.submission.order_params = {};
+    $(pane).next('li').
+      addClass('active').
+      find('input, select').removeAttr('disabled');
+    return true;
+  };
 
-      // If we find any order_params load the values into the submission object.
-      $('#order-params input, #order-params select').
-        each(function(){ 
-        SCAPE.submission.order_params[ $(this).attr('id').replace('submission_order_params_','') ] = $(this).val();
-      });
+  var markStageIncomplete = function(pane) {
+    $(pane).removeClass('completed');
+    return false;
+  };
 
-      if ($('#submission_lanes_of_sequencing').val()) {
-        SCAPE.submission.lanes_of_sequencing = $('#submission_lanes_of_sequencing').val();
-      }
+  var validateSection = function(event) {
+    var currentPane = $(this).closest('#orders li');
 
-      markStageComplete(currentPane);
-    } else if (incompleteFieldCount >= 1) {
-      markStageIncomplete(currentPane);
-    }
+    return allFieldsComplete(currentPane)?
+      markStageComplete(currentPane) : markStageIncomplete(currentPane);
   };
 
 
   // Document Ready stuff...
   $(function(){
-    $('#submission_template_id').change(orderParameterHandler);
+    $('#submission_template_id').change(templateChangeHandler);
 
     $('#submission_project_name').autocomplete({
       source    : SCAPE.user_project_names,
       minLength : 3,
       select    : projectSelectHandler
-
-      // change event needs to check whether the select event fired...
-      // change    : function() { 
-      //   $('#project-details').fadeOut().html(""); 
-      //   markStageIncomplete();
-      // }
     });
 
 
     $('#submission_study_id').change(studySelectHandler);
 
 
-    $('#wizard-next').click(nextPaneHandler);
-    $('#wizard-previous').click(previousPaneHandler);
-    $('.required').live('change',  validateSelection);
+    $('#order-parameters .required').live('change',  validateSection);
   });
 
 })(jQuery);
