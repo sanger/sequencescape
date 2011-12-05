1df8e9f534dc2862cf57402cf16efd7545e8b301
Jens Raaby <jr16@sanger.ac.uk>
2011-12-05 13:43:20 +0000

    Migrations tidied with some cleanup operations moved to initial stage

--- db/migrate/20111031095751_carrier_wave_cleanup.rb
+++ db/migrate/20111031095751_carrier_wave_cleanup.rb
@@ -1,14 +1,5 @@
 class CarrierWaveCleanup < ActiveRecord::Migration
   def self.up
-    # Save space in the database (helps with indexes). 
-    #  Really this should be less but namespacing might make it expand at some point
-    change_column :documents, :documentable_type, :string, :limit => 50, :null => false
-    change_column :db_files, :owner_type, :string, :limit => 50, :null => false
-    
-    # These indexes will help speed up queries for files
-    add_index :db_files, [ :owner_type, :owner_id]
-    add_index :documents, [ :documentable_type, :documentable_id]
-   
     # Delete old file columns
     remove_column :study_reports, :report_file 
     remove_column :sample_manifests, :uploaded_file
