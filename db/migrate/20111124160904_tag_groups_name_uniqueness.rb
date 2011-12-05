d8a1d38a62894f402e126064d78d69693e592906
Jens Raaby <jr16@sanger.ac.uk>
2011-12-05 16:29:46 +0000

    Renamed constraint

--- db/migrate/20111124160904_tag_groups_name_uniqueness.rb
+++ db/migrate/20111124160904_tag_groups_name_uniqueness.rb
@@ -3,14 +3,14 @@ class TagGroupsNameUniqueness < ActiveRecord::Migration
   def self.up
     execute <<-SQL
       ALTER TABLE tag_groups
-      ADD CONSTRAINT unique_name UNIQUE (name)
+      ADD CONSTRAINT tag_groups_unique_name UNIQUE (name)
     SQL
   end
 
   def self.down
     execute <<-SQL
       ALTER TABLE tag_groups
-      DROP INDEX unique_name
+      DROP INDEX tag_groups_unique_name
     SQL
   end
 end
