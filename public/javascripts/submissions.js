e8fd8492ce9e6085167e475fbaeeaf167d3cad0f
Sean Dunn <sd9@sanger.ac.uk>
2011-12-07 18:09:12 +0000

    Started jQuery plugin and moved utility functions to it. Moved build submission button to top of screen. CSS tweaks

--- public/javascripts/submissions.js
+++ public/javascripts/submissions.js
@@ -1,3 +1,69 @@
+// Submission workflow jQuery Plugin...
+(function($,undefined){
+  var methods = {
+    init : function(options) {
+      return this;
+    },
+
+    // Returns true if the input fields in a pane have a value
+    allFieldsComplete : function(pane) {
+      // This won't work in old IE versions <9.
+      // If we need to support old IE add a conditional enhancement
+      // to the start of the module...
+      return this.find('input, select').toArray().every(function(element){ 
+        return $(element).val(); 
+      });
+    },
+
+    currentPane : function() {
+      return this.closest('#orders > li');
+    },
+
+    markPaneIncomplete : function() {
+      this.removeClass('completed');
+
+      // Move this to an initialised callback
+      $('#add-order').attr('disabled',true);
+
+      return this;
+    },
+
+    markPaneInvalid : function() {
+      return this.addClass('invalid');
+    },
+
+    markPaneComplete : function() {
+      this.addClass('completed').
+        removeClass('invalid').
+        find('input, select').attr('disabled', 'true');
+
+      // Move this to an initialised callback
+      this.find('.save-order').fadeOut();
+
+      // Move this to an initialised callback
+      $('#add-order').removeAttr('disabled');
+
+      return this;
+    }
+  };
+
+
+  $.fn.submission = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+
+    } else if (typeof method === 'Object' || !method) {
+
+      return methods.init.apply(this, arguments);
+    } else {
+
+      return $.error('Method ' +  method + ' does not exist on jQuery.submission');
+    }
+  };
+})(jQuery);
+
+
+// Submission page code...
 (function($, undefined){
   // Name spacing stuff...
   if ( window.SCAPE === undefined) { window.SCAPE = {}; }
@@ -6,15 +72,14 @@
 
 
   var templateChangeHandler = function(event){
-    markPaneIncomplete();
     // When there's nothing selected reset the parameters element and exit without 
-    if ($(this).val() === "") {
+    if ($(event.target).val() === "") {
       delete SCAPE.submission.template_id;
       $('#order-parameters').html('');
       return false;
     }
 
-    SCAPE.submission.template_id = $(this).val();
+    SCAPE.submission.template_id = $(event.target).val();
 
     // Load the parameters for the new order
     $.get(
@@ -28,17 +93,15 @@
   };
 
 
-
-  // This handler depends on the study template being set earlier in the wizard.
   var studySelectHandler = function(event) {
-    var currentPane = $(this).closest('#orders > li');
+  // This handler depends on the study template being set earlier in the wizard.
+    var currentPane = $(event.target).submission('currentPane');
 
-    markPaneIncomplete(currentPane);
+    currentPane.submission('markPaneIncomplete');
 
-    SCAPE.submission.study_id = $(this).val();
+    SCAPE.submission.study_id = $(event.target).val();
 
-    if ($(this).val().length > 0) {
-
+    if ($(event.target).val().length > 0) {
       // Load asset groups for the selected study
       $.get(
         '/submissions/study_assets',
@@ -51,21 +114,22 @@
       );
       return true;
     } else {
+      // The study selector has been reset so fade out and reset the field.
       currentPane.find('.study-assets').fadeOut(function(){
         $(this).html("");
       });
+
       return false;
     }
 
   };
 
-  var projectSelectHandler = function(event) {
-    var currentPane                 = $(this).closest('.order');
-    var projectNameElement          = currentPane.find('.submission_project_name');
 
+  var saveOrderHandler = function(event) {
+    var currentPane                 = $(this).submission('currentPane');
+    var projectNameElement          = currentPane.find('.submission_project_name');
     SCAPE.submission.project_name   = projectNameElement.val();
     SCAPE.submission.asset_group_id = $('#submission_asset_group_id').val();
-
 
     $.post(
       '/submissions',
@@ -76,70 +140,39 @@
         if(SCAPE.submission.order_valid) {
           currentPane.fadeOut(function(){
             currentPane.detach().removeClass('active');
-            markPaneComplete(currentPane);
+            currentPane.submission('markPaneComplete');
             $('#order-controls').before(currentPane);
             currentPane.fadeIn();
             $('#blank-order').fadeIn();
+
+            $('#start-submission').removeAttr('disabled');
+            $('.pane').not('#blank-order').addClass('active');
           });
         } else {
-          markPaneInvalid(currentPane);
+          currentPane.submission('markPaneInvalid');
         }
       }
     );
   };
 
 
-  // Change to jQuery method
-  // Returns true if a') the input fields in a pane have a value
-  var allFieldsComplete = function(pane) {
-      // This won't work in old IE versions <9.
-      // If we need to support old IE add a conditional enhancement
-      // to the start of the module...
-    return pane.find('input, select').toArray().
-      every(function(element){ return $(element).val(); });
-  };
-
-  // Change to jQuery method
-  var markPaneComplete = function(pane) {
-    $(pane).
-      addClass('completed').
-      removeClass('invalid').
-      find('input, select').attr('disabled', 'true');
-
-    $(pane).find('.save-order').fadeOut();
-
-    $('#add-order').removeAttr('disabled');
-
-    return true;
-  };
-
-  // Change to jQuery method
-  var markPaneIncomplete = function(pane) {
-    $(pane).removeClass('completed');
-    $('#add-order').attr('disabled',true);
-    return false;
-  };
-
-  // Change to jQuery method
-  var markPaneInvalid = function(pane) {
-    $(pane).addClass('invalid');
-  };
-
   var validateOrderParams = function(event) {
-    var currentPane = $(this).closest('#orders li');
+    var currentPane = $(event.target).submission('currentPane');
 
-    return allFieldsComplete(currentPane)?
-      markPaneComplete(currentPane) : markPaneIncomplete(currentPane);
+    return currentPane.submission('allFieldsComplete')?
+      currentPane.submission('markPaneComplete') : currentPane.submission('markPaneIncomplete');
   };
+
 
   var addOrderHandler = function(event) {
     $('.active').removeClass('active');
+    $('#start-submission').attr('disabled',true);
 
     $('#add-order').attr('disabled', true);
 
     var newOrder = $('<li>').
       html($('#blank-order').html()).
-      addClass('box active order').hide();
+      addClass('pane active order').hide();
 
     newOrder.find('input, select').removeAttr('disabled');
 
@@ -148,26 +181,23 @@
       minLength : 3
     });
 
-    newOrder.find('.save-order').click(projectSelectHandler);
+    newOrder.find('.save-order').click(saveOrderHandler);
 
     $('#blank-order').before(newOrder).fadeOut('fast',function(){
       newOrder.fadeIn();
     });
   };
 
+
   // Document Ready stuff...
-  $(function(){
+  $(function() {
     $('#submission_template_id').change(templateChangeHandler);
-
-
-    // $('.study_id').live('change', studySelectHandler);
-    $('ul#orders').delegate('.study_id','change', studySelectHandler);
-
 
     $('#order-parameters .required').live('change',  validateOrderParams);
 
     $('#add-order').live('click', addOrderHandler);
 
+    $('ul#orders').delegate('.study_id','change', studySelectHandler);
   });
 
 })(jQuery);
