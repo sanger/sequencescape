df7016c62e28ef57c215e8650d3d2c18f9de2a49
Sean Dunn <sd9@sanger.ac.uk>
2012-01-18 14:51:19 +0000

    Moved presenter classes out of the controller file - now in app/model/submission/

---
+++ app/models/submission/order_presenter.rb
@@ -0,0 +1,28 @@
+class OrderPresenter
+  ATTRIBUTES = [
+    :study_id,
+    :project_name,
+    :plate_purpose_id,
+    :sample_names_text,
+    :lanes_of_sequencing_required,
+    :comments,
+  ]
+
+  attr_accessor *ATTRIBUTES
+
+  def initialize(order)
+    @target_order = order
+  end
+
+  # id needs to be defined to stop Object#id being called on the OrderPresenter
+  # instance.
+  def id
+    @target_order.id
+  end
+
+  def method_missing(method, *args, &block)
+    @target_order.send(method, *args, &block)
+  end
+
+end
+
