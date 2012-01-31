b9009d85479dc9304d3122aee3470b61ee8d74d7
Kate Taylor <kt6@mib100236i.internal.sanger.ac.uk>
2012-01-31 16:12:47 +0000

    PT24069051_study_sample_project_constraints tested on production copy from 29 Jan re-write fix migrations in Ruby separate out the alter table not null and adjust timings to make sure they run in correct order catch up with recent changes in production data

--- db/migrate/2012012716103605_orders_fix_foreign_keys.rb
+++ db/migrate/2012012716103605_orders_fix_foreign_keys.rb
@@ -1,17 +1,22 @@
-# 2012012716103605_orders_constraint_dml.rb
+# 2012012716103605_orders_fix_foreign_keys.rb
 
 class OrdersFixForeignKeys < ActiveRecord::Migration
   def self.up
-  	execute "UPDATE orders SET study_id = (SELECT id FROM studies WHERE name = 'Example project') WHERE study_id IN (4, 36)";
-		execute "ALTER TABLE orders MODIFY column study_id int(11) NOT NULL";
-		execute "ALTER TABLE orders MODIFY column project_id int(11) NOT NULL";
+  	#execute "UPDATE orders SET study_id = (SELECT id FROM studies WHERE name = 'Example project') WHERE study_id IN (4, 36)";
 		# nothing to do for project_id
+    ActiveRecord::Base.transaction do
+      Orders.find_by_id(2).update_attributes!(:study_id => 85)
+      Orders.find_by_id(163).update_attributes!(:study_id => 85)
+    end
   end
 
   def self.down
-  	execute "UPDATE orders SET study_id = NULL WHERE study_id IN (4, 36)";
-		execute "ALTER TABLE orders MODIFY column project_id int(11) DEFAULT NULL";
-		execute "ALTER TABLE orders MODIFY column study_id int(11) DEFAULT NULL";
+  	#execute "UPDATE orders SET study_id = 4 WHERE id =2";
+  	#execute "UPDATE orders SET study_id = 36 WHERE id = 163";
 		# nothing to do for project_id
+    ActiveRecord::Base.transaction do
+      Orders.find_by_id(2).update_attributes!(:study_id => 4)
+      Orders.find_by_id(163).update_attributes!(:study_id => 36)
+    end
   end
 end
