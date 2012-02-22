5dddd68664ccf7f76f83fcdf19e1cd6296c990b0
Sean Dunn <sd9@sanger.ac.uk>
2012-02-22 15:59:46 +0000

    Added initial Illumina-C features

--- db/seeds/0001_workflows.rb
+++ db/seeds/0001_workflows.rb
@@ -145,6 +145,61 @@ end.tap do |pipeline|
   PipelineRequestInformationType.create!(:pipeline => pipeline, :request_information_type => RequestInformationType.find_by_label("Concentration"))
 end
 
+MultiplexedLibraryCreationPipeline.create!(:name => 'Illumina-C MX Library Preparation') do |pipeline|
+  pipeline.asset_type  = 'LibraryTube'
+  pipeline.sorter      = 0
+  pipeline.automated   = false
+  pipeline.active      = true
+  pipeline.multiplexed = true
+  pipeline.group_name  = "Library creation"
+
+  pipeline.location = Location.first(
+    :conditions => { :name => 'Library creation freezer' }
+  ) or raise StandardError, "Cannot find 'Library creation freezer' location"
+
+  pipeline.request_type = RequestType.create!(
+    :workflow => Submission::Workflow.find_by_key('short_read_sequencing'),
+    :key      => 'illumina_c_multiplexed_library_creation',
+    :name     => 'Illumina-C Multiplexed Library Creation'
+  ) do |request_type|
+    request_type.billable          = true
+    request_type.initial_state     = 'pending'
+    request_type.asset_type        = 'SampleTube'
+    request_type.order             = 1
+    request_type.multiples_allowed = false
+    request_type.request_class     = MultiplexedLibraryCreationRequest
+    request_type.for_multiplexing  = true
+  end
+
+
+  pipeline.workflow = LabInterface::Workflow.create!(:name => 'Illumina-C MX Library Preparation workflow') do |workflow|
+    workflow.locale   = 'External'
+  end.tap do |workflow|
+    {
+      TagGroupsTask      => { :name => 'Tag Groups',       :sorted => 0 },
+      AssignTagsTask     => { :name => 'Assign Tags',      :sorted => 1 },
+      SetDescriptorsTask => { :name => 'Initial QC',       :sorted => 2, :batched => false },
+      SetDescriptorsTask => { :name => 'Gel',              :sorted => 3, :batched => false },
+      SetDescriptorsTask => { :name => 'Characterisation', :sorted => 4, :batched => true }
+    }.each do |klass, details|
+      klass.create!(details.merge(:workflow => workflow))
+    end
+  end
+end.tap do |pipeline|
+  create_request_information_types(
+    pipeline,
+    "fragment_size_required_from",
+    "fragment_size_required_to",
+    "read_length",
+    "library_type"
+  )
+
+  PipelineRequestInformationType.create!(
+    :pipeline => pipeline,
+    :request_information_type => RequestInformationType.find_by_label("Concentration")
+  )
+end
+
 PulldownLibraryCreationPipeline.create!(:name => 'Pulldown library preparation') do |pipeline|
   pipeline.asset_type = 'LibraryTube'
   pipeline.sorter     = 12
