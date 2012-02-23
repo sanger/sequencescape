3fd598dee5ff09ff0e17a3a1f301cc500128110c
Lars G T Jorgensen <lj3@sanger.ac.uk>
2012-02-23 11:45:40 +0000

    Moved method to public section

--- app/models/accession_service.rb
+++ app/models/accession_service.rb
@@ -126,6 +126,11 @@ class AccessionService
   def dac_visibility(study)
     Protect
   end
+
+  def private?
+    false
+  end
+
 private
 
   def accession_study_set_xml_quarantine(study, studydata)
@@ -259,7 +264,4 @@ private
     end
   end
 
-  def private?
-    false
-  end
 end
