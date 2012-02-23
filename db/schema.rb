e73b47e65475730b740252191c70acd1d87855dd
Sean Dunn <sd9@sanger.ac.uk>
2012-02-23 17:03:10 +0000

    Adds new Illumina-C submission templates

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120221141620) do
+ActiveRecord::Schema.define(:version => 20120222165702) do
 
   create_table "aliquots", :force => true do |t|
     t.integer  "receptacle_id",    :null => false
