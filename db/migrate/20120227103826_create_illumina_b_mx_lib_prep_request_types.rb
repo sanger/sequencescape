a93802e7fecb0c237f737a99b03324c519398fa2
Sean Dunn <sd9@sanger.ac.uk>
2012-02-27 14:12:50 +0000

    Add new request-types and pipelines

---
+++ db/migrate/20120227103826_create_illumina_b_mx_lib_prep_request_types.rb
@@ -0,0 +1,43 @@
+class CreateIlluminaBMxLibPrepRequestTypes < ActiveRecord::Migration
+
+  REQUEST_INFORMATION_TYPES = Hash[RequestInformationType.all.map { |t| [ t.key, t ] }].freeze
+
+  def self.create_request_information_types(pipeline, *keys)
+    PipelineRequestInformationType.create!(
+      keys.map do |k|
+      {
+        :pipeline                 => pipeline,
+        :request_information_type => REQUEST_INFORMATION_TYPES[k]
+      }
+      end
+    )
+  end
+
+  def self.up
+    ActiveRecord::Base.transaction do
+      product_line = ProductLine.find_by_name('Illumina-B')
+      pipeline = MultiplexedLibraryCreationPipeline.find_by_name('Illumina-B MX Library Preparation')
+
+      pipeline.request_types << RequestType.create!(
+        :workflow => Submission::Workflow.find_by_key('short_read_sequencing'),
+        :key      => 'illumina_b_multiplexed_library_creation',
+        :name     => 'Illumina-B Multiplexed Library Creation',
+        :billable          => true,
+        :initial_state     => 'pending',
+        :asset_type        => 'SampleTube',
+        :order             => 1,
+        :multiples_allowed => false,
+        :request_class     => MultiplexedLibraryCreationRequest,
+        :for_multiplexing  => true,
+        :product_line      => product_line
+      )
+    end
+  end
+
+  def self.down
+    ActiveRecord::Base.transaction do
+      RequestType.find_by_key('illumina_b_multiplexed_library_creation').each(&:destroy)
+    end
+
+  end
+end
