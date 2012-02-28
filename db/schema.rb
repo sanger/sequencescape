d3c0a3e129e35cda7c5232f574faffeb34825675
Sean Dunn <sd9@sanger.ac.uk>
2012-02-28 12:01:14 +0000

    Reordered migrations and fix assocications

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120327084706) do
+ActiveRecord::Schema.define(:version => 20120227103826) do
 
   create_table "aliquots", :force => true do |t|
     t.integer  "receptacle_id",    :null => false
@@ -584,7 +584,6 @@ ActiveRecord::Schema.define(:version => 20120327084706) do
     t.integer  "next_pipeline_id"
     t.integer  "previous_pipeline_id"
     t.integer  "location_id"
-    t.integer  "request_type_id"
     t.boolean  "group_by_parent"
     t.string   "asset_type",                    :limit => 50
     t.boolean  "group_by_submission_to_delete"
@@ -601,6 +600,14 @@ ActiveRecord::Schema.define(:version => 20120327084706) do
   end
 
   add_index "pipelines", ["sorter"], :name => "index_pipelines_on_sorter"
+
+  create_table "pipelines_request_types", :id => false, :force => true do |t|
+    t.integer "pipeline_id",     :null => false
+    t.integer "request_type_id", :null => false
+  end
+
+  add_index "pipelines_request_types", ["pipeline_id"], :name => "fk_pipelines_request_types_to_pipelines"
+  add_index "pipelines_request_types", ["request_type_id"], :name => "fk_pipelines_request_types_to_request_types"
 
   create_table "plate_creations", :force => true do |t|
     t.integer  "user_id"
@@ -668,6 +675,10 @@ ActiveRecord::Schema.define(:version => 20120327084706) do
   end
 
   add_index "plate_volumes", ["uploaded_file_name"], :name => "index_plate_volumes_on_uploaded_file_name"
+
+  create_table "product_lines", :force => true do |t|
+    t.string "name", :null => false
+  end
 
   create_table "project_managers", :force => true do |t|
     t.string   "name"
@@ -794,6 +805,7 @@ ActiveRecord::Schema.define(:version => 20120327084706) do
     t.integer  "morphology",                       :default => 0
     t.boolean  "for_multiplexing",                 :default => false
     t.boolean  "billable",                         :default => false
+    t.integer  "product_line_id"
   end
 
   create_table "requests", :force => true do |t|
@@ -1335,13 +1347,6 @@ ActiveRecord::Schema.define(:version => 20120327084706) do
     t.string  "descendant_uuid",        :limit => 36
     t.integer "descendant_internal_id"
     t.string  "descendant_type",        :limit => 50
-  end
-
-  create_table "view_genotyping_statuses", :id => false, :force => true do |t|
-    t.integer "sample_internal_id"
-    t.string  "sample_uuid",             :limit => 36
-    t.string  "genotyping_status"
-    t.string  "genotyping_snp_plate_id"
   end
 
   create_table "view_lanes", :id => false, :force => true do |t|
