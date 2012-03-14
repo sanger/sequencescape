35ceb5c00a789f81a803a08d27af35d012084296
Matthew Denner <md12@sanger.ac.uk>
2012-03-14 13:21:11 +0000

    [fixes #26423693] Ensure multiplier is always 1 if unset

--- app/models/submission/submission_presenter.rb
+++ app/models/submission/submission_presenter.rb
@@ -30,7 +30,7 @@ class PresenterSkeleton
   def lanes_from_request_options
     library_request       = RequestType.find(order.request_types.first)
     sequencing_request    = RequestType.find(order.request_types.last)
-    sequencing_multiplier = order.request_options[:multiplier][sequencing_request.id.to_s].to_i
+    sequencing_multiplier = order.request_options.fetch('multiplier', {}).fetch(sequencing_request.id.to_s, 1)
 
     if library_request.for_multiplexing?
       sequencing_multiplier
