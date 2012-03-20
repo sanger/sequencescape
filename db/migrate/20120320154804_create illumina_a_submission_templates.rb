18a78bba6cb519d75c42f0b6b3bc5e496f5b7f76
James Glover <jg16@sanger.ac.uk>
2012-03-20 16:43:18 +0000

    Submission template migrations for Illumina-A and C

---
+++ db/migrate/20120320154804_create illumina_a_submission_templates.rb
@@ -0,0 +1,23 @@
+class CreateIlluminaASubmissionTemplates < ActiveRecord::Migration
+  def self.up
+    SubmissionTemplate.find(:all, :conditions => ['name LIKE ?','Pulldown%']).each do |submission_template|
+      submission_parameters = submission_template.submission_parameters[:request_type_ids_list]
+      old_request_type = RequestType.find(submission_parameters[:request_type_ids_list][1].first)
+      new_request_type = RequestType.find_by_key("illumina_a_#{old_request_type.key}")
+      submission_parameters[:request_type_ids_list][1] = [new_request_type.id]
+
+      SubmissionTemplate.create!({
+        :name => "Illumina-A - #{submission_template.name}",
+        :submission_parameters => submission_parameters
+      }.reverse_merge(submission_template.attributes).except!('created_at','updated_at'))
+
+      submission_template.update_attributes(:visible=>false)
+    end
+  end
+
+  def self.down
+    SubmissionTemplate.find(:all, :conditions => ['name LIKE ?', 'Illumina-A - Pulldown%']).each do |submission_template|
+      submission_template.destroy
+    end
+  end
+end
---
+++ db/migrate/20120320163750_create illumina_c_submission_templates.rb
@@ -0,0 +1,23 @@
+class CreateIlluminaCSubmissionTemplates < ActiveRecord::Migration
+  def self.up
+    SubmissionTemplate.find(:all, :conditions => ['name LIKE ?','Library creation%']).each do |submission_template|
+      submission_parameters = submission_template.submission_parameters[:request_type_ids_list]
+      old_request_type = RequestType.find(submission_parameters[:request_type_ids_list][1].first)
+      new_request_type = RequestType.find_by_key("illumina_c_#{old_request_type.key}")
+      submission_parameters[:request_type_ids_list][1] = [new_request_type.id]
+
+      SubmissionTemplate.create!({
+        :name => "Illumina-C - #{submission_template.name}",
+        :submission_parameters => submission_parameters
+      }.reverse_merge(submission_template.attributes).except!('created_at','updated_at'))
+
+      submission_template.update_attributes(:visible=>false)
+    end
+  end
+
+  def self.down
+    SubmissionTemplate.find(:all, :conditions => ['name LIKE ?', 'Illumina-C - Library creation%']).each do |submission_template|
+      submission_template.destroy
+    end
+  end
+end
