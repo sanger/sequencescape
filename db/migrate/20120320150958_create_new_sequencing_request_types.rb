5f994acd6100c4dc17c672c4056ada4fb641721e
Sean Dunn <sd9@sanger.ac.uk>
2012-03-20 16:21:30 +0000

    Add Sequencing RequestTypes

---
+++ db/migrate/20120320150958_create_new_sequencing_request_types.rb
@@ -0,0 +1,20 @@
+class CreateNewSequencingRequestTypes < ActiveRecord::Migration
+  def self.up
+    Pipeline.find_all_by_sti_type('SequencingPipeline').each do |sequencing_pipeline|
+      existing_request_type = sequencing_pipeline.request_types.last
+
+      ProductLine.all.each do |product_line|
+        sequencing_pipeline.request_types.create!(
+          {
+            'key'          => "#{product_line.name.underscore}_#{existing_request_type.key}",
+            'name'         => "#{product_line.name} #{existing_request_type.name}",
+            'product_line' => product_line
+          }.reverse_merge(existing_request_type.attributes).except(*%w{id created_at updated_at})
+        )
+      end
+    end
+  end
+
+  def self.down
+  end
+end
