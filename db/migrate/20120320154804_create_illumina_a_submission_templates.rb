3d3fcbe845311d3db58cb6eef3a6811aebf94574
James Glover <jg16@sanger.ac.uk>
2012-03-21 11:55:00 +0000

    Added cherypicking submissions to Illumina-A
    Cherrypicking-sequencing submission templates
    are now duplicated and added to Illumina-A

--- db/migrate/20120320154804_create_illumina_a_submission_templates.rb
+++ db/migrate/20120320154804_create_illumina_a_submission_templates.rb
@@ -1,10 +1,10 @@
 class CreateIlluminaASubmissionTemplates < ActiveRecord::Migration
   def self.up
-    SubmissionTemplate.find(:all, :conditions => ['name LIKE ?','Pulldown%']).each do |submission_template|
-      submission_parameters = submission_template.submission_parameters
-      old_request_type = RequestType.find(submission_parameters[:request_type_ids_list][1].first)
+    SubmissionTemplate.find(:all, :conditions => ['name LIKE ? OR name LIKE ? OR name LIKE ?','Pulldown%', 'Cherrypick for pulldown%','Cherrypicking for pulldown -%']).each do |submission_template|
+      submission_parameters = submission_template.submission_parameters.dup
+      old_request_type = RequestType.find(submission_parameters[:request_type_ids_list].last.first)
       new_request_type = RequestType.find_by_key("illumina_a_#{old_request_type.key}")
-      submission_parameters[:request_type_ids_list][1] = [new_request_type.id]
+      submission_parameters[:request_type_ids_list][-1] = [new_request_type.id]
 
       SubmissionTemplate.create!({
         :name => "Illumina-A - #{submission_template.name}",
@@ -16,8 +16,8 @@ class CreateIlluminaASubmissionTemplates < ActiveRecord::Migration
   end
 
   def self.down
-    SubmissionTemplate.find(:all, :conditions => ['name LIKE ?', 'Illumina-A - Pulldown%']).each do |submission_template|
-      submission_template.destroy
+    SubmissionTemplate.find(:all, :conditions => ['name LIKE ?', 'Illumina-A - %']).each do |submission_template|
+      submission_template.destroy!
     end
   end
 end
