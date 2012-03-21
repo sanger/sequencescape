8a5d113d0fb24b7965a7ce54f00e22a7ff1cdeae
James Glover <jg16@sanger.ac.uk>
2012-03-21 10:18:56 +0000

    Corrected error in migrations

--- db/migrate/20120320163750_create_more_illumina_c_submission_templates.rb
+++ db/migrate/20120320163750_create_more_illumina_c_submission_templates.rb
@@ -1,7 +1,7 @@
 class CreateMoreIlluminaCSubmissionTemplates < ActiveRecord::Migration
   def self.up
     SubmissionTemplate.find(:all, :conditions => ['name LIKE ?','Library creation%']).each do |submission_template|
-      submission_parameters = submission_template.submission_parameters[:request_type_ids_list]
+      submission_parameters = submission_template.submission_parameters
       old_request_type = RequestType.find(submission_parameters[:request_type_ids_list][1].first)
       new_request_type = RequestType.find_by_key("illumina_c_#{old_request_type.key}")
       submission_parameters[:request_type_ids_list][1] = [new_request_type.id]
