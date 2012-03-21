a9edd8c2e0e56b61d4b7636a91dfff493a1a6593
Sean Dunn <sd9@sanger.ac.uk>
2012-03-21 15:20:17 +0000

    Wrapped migrations in transaction

--- db/migrate/20120320154804_create_illumina_a_submission_templates.rb
+++ db/migrate/20120320154804_create_illumina_a_submission_templates.rb
@@ -1,21 +1,30 @@
 class CreateIlluminaASubmissionTemplates < ActiveRecord::Migration
   def self.up
-    SubmissionTemplate.find(:all, :conditions => ['name LIKE ? OR name LIKE ? OR name LIKE ?','Pulldown%', 'Cherrypick for pulldown%','Cherrypicking for pulldown -%']).each do |submission_template|
-      submission_parameters = submission_template.submission_parameters.dup
-      old_request_type = RequestType.find(submission_parameters[:request_type_ids_list].last.first)
-      new_request_type = RequestType.find_by_key("illumina_a_#{old_request_type.key}")
-      submission_parameters[:request_type_ids_list].last = [new_request_type.id]
+    ActiveRecord::Base.transaction do
+      SubmissionTemplate.all(
+        :conditions => ['name LIKE ? OR name LIKE ? OR name LIKE ?','Pulldown%', 'Cherrypick for pulldown%','Cherrypicking for pulldown -%']
+      ).each do |submission_template|
+        submission_parameters       = submission_template.submission_parameters.dup
+        old_sequencing_request_type = RequestType.find(submission_parameters[:request_type_ids_list].last.first)
+        new_sequencing_request_type = RequestType.find_by_key("illumina_a_#{old_sequencing_request_type.key}")
 
-      SubmissionTemplate.create!({
-        :name => "Illumina-A - #{submission_template.name}",
-        :submission_parameters => submission_parameters
-      }.reverse_merge(submission_template.attributes).except!('created_at','updated_at'))
+        submission_parameters[:request_type_ids_list][-1] = [new_sequencing_request_type.id]
 
-      submission_template.update_attributes(:visible=>false)
+        SubmissionTemplate.create!(
+          {
+            :name                  => "Illumina-A - #{submission_template.name}",
+            :submission_parameters => submission_parameters
+          }.reverse_merge(submission_template.attributes).except!('created_at','updated_at')
+        )
+
+        submission_template.update_attributes(:visible => false)
+      end
     end
   end
 
   def self.down
-    SubmissionTemplate.find(:all, :conditions => ['name LIKE ?', 'Illumina-A - %']).each(&:destroy)
+    ActiveRecord::Base.transaction do
+      SubmissionTemplate.all(:conditions => ['name LIKE ?', 'Illumina-A - %']).each(&:destroy)
+    end
   end
 end
