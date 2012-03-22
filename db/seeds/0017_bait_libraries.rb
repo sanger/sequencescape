841c5f9d1028657036d90f2faae2e358fb4e1f34
James Glover <jg16@sanger.ac.uk>
2012-03-22 15:24:14 +0000

    Add bait_Libary_types to baits
    Includes migrations, seeds and model changes

--- db/seeds/0017_bait_libraries.rb
+++ db/seeds/0017_bait_libraries.rb
@@ -1,8 +1,16 @@
 # Generate a few bait libraries.
+['Standard','Custom - Pipeline', 'Custom - Customer'].each do |name|
+  BaitLibraryType.create!(:name=>name)
+end
+
 BaitLibrary::Supplier.create!(:name => 'Agilent').tap do |supplier|
   # Standard bait libraries
-  supplier.bait_libraries.create!(:name => 'Human all exon 50MB', :target_species => 'Human')
-  supplier.bait_libraries.create!(:name => 'Mouse all exon',      :target_species => 'Mouse')
-  supplier.bait_libraries.create!(:name => 'Zebrafish ZV9',       :target_species => 'Zebrafish')
-  supplier.bait_libraries.create!(:name => 'Zebrafish ZV8',       :target_species => 'Zebrafish')
+  supplier.bait_libraries.create!(:name => 'Human all exon 50MB', :target_species => 'Human',
+                                  :bait_library_type => BaitLibraryType.first)
+  supplier.bait_libraries.create!(:name => 'Mouse all exon',      :target_species => 'Mouse',
+                                  :bait_library_type => BaitLibraryType.first)
+  supplier.bait_libraries.create!(:name => 'Zebrafish ZV9',       :target_species => 'Zebrafish',
+                                  :bait_library_type => BaitLibraryType.first)
+  supplier.bait_libraries.create!(:name => 'Zebrafish ZV8',       :target_species => 'Zebrafish',
+                                  :bait_library_type => BaitLibraryType.first)
 end
