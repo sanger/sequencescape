069ef1a4504318c9a7935172831f288dd8b2f92a
James Glover <jg16@sanger.ac.uk>
2012-03-22 15:24:14 +0000

    Add bait_Libary_types to baits
    Includes migrations, seeds and model changes

---
+++ db/migrate/20120321132536_create_bait_library_types.rb
@@ -0,0 +1,25 @@
+class CreateBaitLibraryTypes < ActiveRecord::Migration
+  def self.up
+    ActiveRecord::Base.transaction do
+      create_table :bait_library_types do |t|
+        t.string :name
+        t.timestamps
+      end
+      ['Standard','Custom - Pipeline', 'Custom - Customer'].each do |name|
+        BaitLibraryType.create!(:name => name)
+      end
+
+      add_column :bait_libraries, :bait_library_type_id, :integer
+      BaitLibrary.all.each do |bait_library|
+        bait_library.update_attributes(:bait_library_type => BaitLibraryType.find_by_name('Standard'))
+      end
+    end
+  end
+
+  def self.down
+    ActiveRecord::Base.transaction do
+      drop_table :bait_library_types
+      remove_column :bait_libraries, :bait_library_type_id
+    end
+  end
+end
