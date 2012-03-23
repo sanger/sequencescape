018e434cf0c5b54261d26a1a5af616758cfea7c0
James Glover <jg16@sanger.ac.uk>
2012-03-23 09:02:18 +0000

    Added database constraints
    Database now requires bait_library_type id on
    bait_libraries, and requires a unique name on
    bait_library_type.

--- db/migrate/20120321132536_create_bait_library_types.rb
+++ db/migrate/20120321132536_create_bait_library_types.rb
@@ -2,14 +2,15 @@ class CreateBaitLibraryTypes < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.transaction do
       create_table :bait_library_types do |t|
-        t.string :name
+        t.string :name, :null => false
         t.timestamps
       end
+      add_index :bait_library_types, :name, :unique => true
       ['Standard','Custom - Pipeline', 'Custom - Customer'].each do |name|
         BaitLibraryType.create!(:name => name)
       end
 
-      add_column :bait_libraries, :bait_library_type_id, :integer
+      add_column :bait_libraries, :bait_library_type_id, :integer, :null => false
       BaitLibrary.all.each do |bait_library|
         bait_library.update_attributes(:bait_library_type => BaitLibraryType.find_by_name('Standard'))
       end
