0376b262b3287e360b72319888651f1f792e3f3d
James Glover <jg16@sanger.ac.uk>
2012-03-23 09:02:18 +0000

    Added database constraints
    Database now requires bait_library_type id on
    bait_libraries, and requires a unique name on
    bait_library_type.

--- db/schema.rb
+++ db/schema.rb
@@ -147,7 +147,7 @@ ActiveRecord::Schema.define(:version => 20120321132536) do
     t.string   "target_species",           :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "bait_library_type_id"
+    t.integer  "bait_library_type_id",     :null => false
   end
 
   add_index "bait_libraries", ["bait_library_supplier_id", "name"], :name => "bait_library_names_are_unique_within_a_supplier", :unique => true
@@ -171,10 +171,12 @@ ActiveRecord::Schema.define(:version => 20120321132536) do
   add_index "bait_library_suppliers", ["name"], :name => "index_bait_library_suppliers_on_name", :unique => true
 
   create_table "bait_library_types", :force => true do |t|
-    t.string   "name"
+    t.string   "name",       :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "bait_library_types", ["name"], :name => "index_bait_library_types_on_name", :unique => true
 
   create_table "barcode_prefixes", :force => true do |t|
     t.string "prefix", :limit => 3
