2582022b3cff0044328f098996787314624ad388
Sean Dunn <sd9@sanger.ac.uk>
2012-04-04 13:12:04 +0100

    Reordered and fixed illumina-a & b submission template migrations Removed fail pulldown migration. Removed old debugger statement left study_summary partial Fixed missing ControlRequestTypeCreation include

---
+++ db/migrate/20120320150960_create_illumina_c_submission_templates.rb
@@ -0,0 +1,53 @@
+class CreateIlluminaCSubmissionTemplates < ActiveRecord::Migration
+  class << self
+    def up
+      ActiveRecord::Base.transaction do
+        mx_submission_templates.each do |submission_template|
+          submission_parameters = submission_template.submission_parameters.dup
+
+          submission_parameters[:request_type_ids_list] = new_request_types(submission_parameters[:request_type_ids_list])
+
+          SubmissionTemplate.create!(
+            {
+              :name                  => "Illumina-C - #{submission_template.name}",
+              :submission_parameters => submission_parameters
+            }.reverse_merge(submission_template.attributes).except!('created_at','updated_at')
+          )
+
+          submission_template.update_attributes(:visible => false)
+        end
+      end
+    end
+
+    def new_request_type(old_request_type_id_arr)
+      # Remember to pull the id out of the wrapping array...
+      old_request_type = RequestType.find(old_request_type_id_arr.first)
+
+      RequestType.find_by_key("illumina_c_#{old_request_type.key}")
+    end
+
+    def new_request_types(old_request_types_list)
+      new_lib_request_type        = new_request_type(old_request_types_list.first)
+      new_sequencing_request_type = new_request_type(old_request_types_list.last)
+
+      [ [new_lib_request_type.id], [new_sequencing_request_type.id] ]
+    end
+
+    # Return the id original multiplexed library creation request type
+    def orig_req_id
+      @orig_req_id ||= RequestType.find_by_key('multiplexed_library_creation').id
+    end
+
+    def mx_submission_templates
+      @mx_templates ||= SubmissionTemplate.all.select do |template|
+        template.submission_parameters[:request_type_ids_list].include?([orig_req_id])
+      end
+    end
+
+    def down
+      ActiveRecord::Base.transaction do
+        SubmissionTemplate.find(:all, :conditions => ["name like ?", 'Illumina-C - %']).each(&:destroy)
+      end
+    end
+  end
+end
