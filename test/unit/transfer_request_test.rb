4f98fda38659b7c4016356a7627c5daacba504ef
James Glover <jg16@sanger.ac.uk>
2012-04-05 11:01:18 +0100

    [#25387227] Extracted duplicate test code
    Tests look at creation of transfer requests directly,
    and via the factories. Extracted the methods to
    remove unecessary duplication of tests.

--- test/unit/transfer_request_test.rb
+++ test/unit/transfer_request_test.rb
@@ -1,26 +1,48 @@
 require 'test_helper'
 
 class TransferRequestTest < ActiveSupport::TestCase
+
+  def shared_setup
+    @source = LibraryTube.create!.tap { |tube| tube.aliquots.create!(:sample => Factory(:sample)) }
+    Factory(:tag).tag!(@source)
+    @destination = LibraryTube.create!
+  end
+
+  def self.shared_tests
+    should 'duplicate the aliquots' do
+      expected_aliquots = @source.aliquots.map { |a| [ a.sample_id, a.tag_id ] }
+      target_aliquots   = @destination.aliquots.map { |a| [ a.sample_id, a.tag_id ] }
+      assert_equal(expected_aliquots, target_aliquots)
+    end
+
+    should 'have the correct attributes' do
+      assert @transfer_request.request_type == RequestType.find_by_key('transfer')
+      assert @transfer_request.sti_type == 'TransferRequest'
+      assert @transfer_request.state == 'pending'
+      assert @transfer_request.asset_id == @source.id
+      assert @transfer_request.target_asset_id == @destination.id
+    end
+  end
+
   context 'TransferRequest' do
-    context 'when starting the request' do
+    context 'when starting the request directly' do
+
       setup do
-        @source = LibraryTube.create!.tap { |tube| tube.aliquots.create!(:sample => Factory(:sample)) }
-        Factory(:tag).tag!(@source)
-
-        @destination = LibraryTube.create!
-
+        shared_setup
         @transfer_request = TransferRequest.create!(:asset => @source, :target_asset => @destination)
       end
 
-      should 'duplicate the aliquots' do
-        expected_aliquots = @source.aliquots.map { |a| [ a.sample_id, a.tag_id ] }
-        target_aliquots   = @destination.aliquots.map { |a| [ a.sample_id, a.tag_id ] }
-        assert_equal(expected_aliquots, target_aliquots)
+      shared_tests
+
+    end
+
+    context 'when using the constuctor' do
+      setup do
+        shared_setup
+        @transfer_request = RequestType.transfer.create!(:asset => @source, :target_asset => @destination)
       end
 
-      should 'have a request type' do
-        assert @transfer_request.request_type == RequestType.find_by_key('transfer')
-      end
+      shared_tests
 
     end
 
@@ -30,30 +52,5 @@ class TransferRequestTest < ActiveSupport::TestCase
       assert_raises(ActiveRecord::RecordInvalid) { RequestType.transfer.create!(:asset => asset, :target_asset => asset) }
     end
 
-    context 'when using the constuctor' do
-      setup do
-        @source = LibraryTube.create!.tap { |tube| tube.aliquots.create!(:sample => Factory(:sample)) }
-        Factory(:tag).tag!(@source)
-
-        @destination = LibraryTube.create!
-
-        @transfer_request = RequestType.transfer.create!(:asset => @source, :target_asset => @destination)
-      end
-
-      should 'duplicate the aliquots' do
-        expected_aliquots = @source.aliquots.map { |a| [ a.sample_id, a.tag_id ] }
-        target_aliquots   = @destination.aliquots.map { |a| [ a.sample_id, a.tag_id ] }
-        assert_equal(expected_aliquots, target_aliquots)
-      end
-
-      should 'have the correct attributes' do
-        assert @transfer_request.request_type == RequestType.find_by_key('transfer')
-        assert @transfer_request.sti_type == 'TransferRequest'
-        assert @transfer_request.state == 'pending'
-        assert @transfer_request.asset_id == @source.id
-        assert @transfer_request.target_asset_id == @destination.id
-      end
-
-    end
   end
 end
