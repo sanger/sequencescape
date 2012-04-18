2d1b70ebbde383f2438c1b5170b8462be8a587eb
Sean Dunn <sd9@sanger.ac.uk>
2012-04-18 11:23:18 +0100

    Create product lined Lib prep RequestTypes Create all Illumin-C SubmissionTemplates in 1 migration Added for_request_type named_scope to Pipeline

--- db/migrate/20120319145254_hide_old_submission_templates.rb
+++ db/migrate/20120319145254_hide_old_submission_templates.rb
@@ -1,8 +1,15 @@
 class HideOldSubmissionTemplates < ActiveRecord::Migration
 
+  KEYS_TO_HIDE = [
+    'multiplexed_library_creation', 
+    'library_creation'
+  ]
+
   # Return the id original multiplexed library creation request type
-  def self.orig_req_id
-    @orig_req_id ||= RequestType.find_by_key('multiplexed_library_creation').id
+  def self.orig_req_ids
+    @orig_req_ids ||= RequestType.all(
+      :conditions => ['`key` IN (?)', KEYS_TO_HIDE ]
+    ).map(&:id).map(&:to_a)
   end
 
   # This migration adds a flag to the submission_templates table and then hides
@@ -11,8 +18,8 @@ class HideOldSubmissionTemplates < ActiveRecord::Migration
     add_column :submission_templates, :visible, :boolean, :null => false, :default => true
 
     ActiveRecord::Base.transaction do
-      orig_templates = SubmissionTemplate.all.select do |template|
-        template.submission_parameters[:request_type_ids_list].include?([orig_req_id])
+      orig_templates = SubmissionTemplate.all.reject do |template|
+        (template.submission_parameters[:request_type_ids_list] & (orig_req_ids)).empty?
       end
 
       orig_templates.each do |template|
