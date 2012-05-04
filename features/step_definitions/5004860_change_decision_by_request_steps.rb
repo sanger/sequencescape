dcbb888e3ebd7b3ea8fa81529218254bf1365045
James Glover <jg16@sanger.ac.uk>
2012-05-04 16:17:48 +0100

    [#29003307] Improve change decision interface + features

--- features/step_definitions/5004860_change_decision_by_request_steps.rb
+++ features/step_definitions/5004860_change_decision_by_request_steps.rb
@@ -3,9 +3,9 @@ Given /^sequencescape is setup for 5004860$/ do
   library1 = Factory(:empty_library_tube, :qc_state => 'pending')
   library1.parents << sample
   lane = Factory :lane, :qc_state => 'pending'
-  request_one = Factory :request, :asset => library1, :target_asset => lane
-  
+  request_one = Factory :request, :asset => library1, :target_asset => lane, :state => 'passed'
+
   reference = BillingEvent.build_reference(request_one)
-  Factory :billing_event, :reference => reference,  :quantity => 7, :kind => "charge"  
-     
+  Factory :billing_event, :reference => reference,  :quantity => 7, :kind => "charge"
+
 end
