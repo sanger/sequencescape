99978aaa32ef8117842fb16b69966af06249629d
James Glover <jg16@sanger.ac.uk>
2012-05-25 10:47:34 +0100

    [#30074555] Add models form Illumina-B plate purposes

---
+++ app/models/illumina_b/plate_purposes.rb
@@ -0,0 +1,12 @@
+module IlluminaB::PlatePurposes
+  PULLDOWN_PLATE_PURPOSE_FLOWS = [
+    [
+      'ILB_STD_INPUT',
+      'ILB_STD_PCR'
+    ]
+  ]
+
+  PULLDOWN_PLATE_PURPOSE_LEADING_TO_QC_PLATES = [
+    'ILB_STD_PCR'
+  ]
+end
