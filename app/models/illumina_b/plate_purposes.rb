38555192e19757399401812b3c766041c24eb5d0
James Glover <jg16@sanger.ac.uk>
2012-05-25 11:16:24 +0100

    [#30074555] Remove reference to non-existent Illumina B QC Plates

--- app/models/illumina_b/plate_purposes.rb
+++ app/models/illumina_b/plate_purposes.rb
@@ -5,7 +5,9 @@ module IlluminaB::PlatePurposes
       'ILB_STD_PCRXP'
     ]
   ]
+
   STOCK_PLATE_PURPOSE = 'ILB_STD_INPUT'
+
   # Don't have ILllumina B QC plates at the momnet...
   PULLDOWN_PLATE_PURPOSE_LEADING_TO_QC_PLATES = [
   ]
