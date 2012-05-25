e7c803d9936d78c18de927c3b354ed597b6b8675
James Glover <jg16@sanger.ac.uk>
2012-05-25 10:51:32 +0100

    [#30074555] Should track PCRXP, rather than PCR

--- app/models/illumina_b/plate_purposes.rb
+++ app/models/illumina_b/plate_purposes.rb
@@ -2,11 +2,11 @@ module IlluminaB::PlatePurposes
   PULLDOWN_PLATE_PURPOSE_FLOWS = [
     [
       'ILB_STD_INPUT',
-      'ILB_STD_PCR'
+      'ILB_STD_PCRXP'
     ]
   ]
 
   PULLDOWN_PLATE_PURPOSE_LEADING_TO_QC_PLATES = [
-    'ILB_STD_PCR'
+    'ILB_STD_PCRXP'
   ]
 end
