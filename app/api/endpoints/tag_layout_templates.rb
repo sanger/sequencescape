91571317ba4cafc8f57b927ebeb001f37504adca
James Glover <jg16@sanger.ac.uk>
2012-05-30 11:43:58 +0100

    [#30133499] Plate owner is updated with events

--- app/api/endpoints/tag_layout_templates.rb
+++ app/api/endpoints/tag_layout_templates.rb
@@ -6,8 +6,22 @@ class ::Endpoints::TagLayoutTemplates < ::Core::Endpoint::Base
   instance do
     action(:create) do |request, _|
       ActiveRecord::Base.transaction do
+        plate_from_request(request).owner = user_from_request(request)
         request.create!(::Io::TagLayout.map_parameters_to_attributes(request.json).reverse_merge(:user => request.user))
       end
     end
+
+    def user_from_request(request)
+      User.find(Uuid.find_id(request.json["transfer"]["user"]))
+    end
+
+    def plate_from_request(request)
+      plate_from_uuid(request.json["transfer"]["plate"])
+    end
+
+    def plate_from_uuid(uuid)
+      Plate.find(Uuid.find_id(uuid))
+    end
+
   end
 end
