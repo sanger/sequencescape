99b983843b32e2285b3e4b75d1892f3340fb2ead
James Glover <jg16@sanger.ac.uk>
2012-06-06 14:19:30 +0100

    [#30074991] Repaired issues with migrations
    Removed class instance variables
    Removed modifications of PlatePurpose as we don't
    need control request types for pulldown type pipelines

--- db/migrate/20120525111543_add_new_request_types_for_plate_based_illumina_b_mx_library_creation.rb
+++ db/migrate/20120525111543_add_new_request_types_for_plate_based_illumina_b_mx_library_creation.rb
@@ -1,49 +1,54 @@
-require 'control_request_type_creation'
-
-Pipeline.send(:include, ControlRequestTypeCreation)
 
 class AddNewRequestTypesForPlateBasedIlluminaBMxLibraryCreation < ActiveRecord::Migration
-  @product_line_id = ProductLine.find_by_name('Illumina-B').id
-  @workflow_id = Submission::Workflow.find_by_key('short_read_sequencing').id
-  @plate_purpose = PlatePurpose.find_by_name('ILB_STD_INPUT')
-  @request_types = [
-    {
-      :key => 'illumina_b_std',
-      :name => 'Illumina-B STD',
-      :workflow_id => @workflow_id,
-      :asset_type => 'Well',
-      :order => 1,
-      :initial_state => 'pending',
-      :target_asset_type => 'MultiplexedLibraryTube',
-      :multiples_allowed => 0,
-      :request_class_name => 'IlluminaB::Requests::StdLibraryRequest',
-      :morphology => 0,
-      :for_multiplexing => 1,
-      :billable => 1,
-      :product_line_id => @product_line_id
-    }
-  ]
-  @old_request_type = 'illumina_b_multiplexed_library_creation'
+
+  # require 'control_request_type_creation'
+  #
+  # class Pipeline < ActiveRecord::Base
+  #   include ControlRequestTypeCreation
+  # end
+
+  def plate_purpose
+    PlatePurpose.find_by_name('ILB_STD_INPUT')
+  end
+
+  def request_types
+    [
+      {
+        :key => 'illumina_b_std',
+        :name => 'Illumina-B STD',
+        :workflow_id => Submission::Workflow.find_by_key('short_read_sequencing').id,
+        :asset_type => 'Well',
+        :order => 1,
+        :initial_state => 'pending',
+        :target_asset_type => 'MultiplexedLibraryTube',
+        :multiples_allowed => 0,
+        :request_class_name => 'IlluminaB::Requests::StdLibraryRequest',
+        :morphology => 0,
+        :for_multiplexing => 1,
+        :billable => 1,
+        :product_line_id => ProductLine.find_by_name('Illumina-B').id
+      }
+    ]
+  end
 
   def self.up
     ActiveRecord::Base.transaction do
 
-      @request_types.each do |config|
+      request_types.each do |config|
         RequestType.create!(config).tap do |request_type|
-          request_type.acceptable_plate_purposes  << @plate_purpose
+          request_type.acceptable_plate_purposes  << plate_purpose
         end
       end
 
-      RequestType.update_all({:deprecated => true}, ['`key` = ?', @old_request_type])
     end
   end
 
   def self.down
     ActiveRecord::Base.transaction do
-      @request_types.each do |config|
+      request_types.each do |config|
         RequestType.find_by_key(config[:key]).destroy
       end
-      RequestType.update_all({:deprecated => false}, ['`key` = ?', @old_request_type])
     end
   end
+
 end
