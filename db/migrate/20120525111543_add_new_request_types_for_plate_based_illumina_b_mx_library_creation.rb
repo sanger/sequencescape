5d00bc368443cdc223738c82430e0c3752de2c5c
James Glover <jg16@sanger.ac.uk>
2012-06-06 15:21:39 +0100

    [#30074991] Fix migration issues

--- db/migrate/20120525111543_add_new_request_types_for_plate_based_illumina_b_mx_library_creation.rb
+++ db/migrate/20120525111543_add_new_request_types_for_plate_based_illumina_b_mx_library_creation.rb
@@ -1,19 +1,9 @@
 
 class AddNewRequestTypesForPlateBasedIlluminaBMxLibraryCreation < ActiveRecord::Migration
 
-  # require 'control_request_type_creation'
-  #
-  # class Pipeline < ActiveRecord::Base
-  #   include ControlRequestTypeCreation
-  # end
-
-  def plate_purpose
-    PlatePurpose.find_by_name('ILB_STD_INPUT')
-  end
-
-  def request_types
-    [
-      {
+  def self.up
+    ActiveRecord::Base.transaction do
+      RequestType.create!(
         :key => 'illumina_b_std',
         :name => 'Illumina-B STD',
         :workflow_id => Submission::Workflow.find_by_key('short_read_sequencing').id,
@@ -27,26 +17,16 @@ class AddNewRequestTypesForPlateBasedIlluminaBMxLibraryCreation < ActiveRecord::
         :for_multiplexing => 1,
         :billable => 1,
         :product_line_id => ProductLine.find_by_name('Illumina-B').id
-      }
-    ]
-  end
-
-  def self.up
-    ActiveRecord::Base.transaction do
-
-      request_types.each do |config|
-        RequestType.create!(config).tap do |request_type|
-          request_type.acceptable_plate_purposes  << plate_purpose
-        end
+      ).tap do |request_type|
+        request_type.acceptable_plate_purposes  << PlatePurpose.find_by_name('ILB_STD_INPUT')
       end
-
     end
   end
 
   def self.down
     ActiveRecord::Base.transaction do
       request_types.each do |config|
-        RequestType.find_by_key(config[:key]).destroy
+        RequestType.find_by_key('Illumina-B STD').destroy
       end
     end
   end
