0770729de6f3d2f974c440e286b9483dbded614b
Sean Dunn <sd9@sanger.ac.uk>
2012-06-12 11:29:17 +0100

    Fixes LibraryCreation inheritance.

--- app/models/plate_purpose/stock.rb
+++ app/models/plate_purpose/stock.rb
@@ -1,6 +1,6 @@
 module PlatePurpose::Stock
   def _pool_wells(wells)
-    wells.pooled_as_source_by(Pulldown::Requests::LibraryCreation)
+    wells.pooled_as_source_by(Request::LibraryCreation)
   end
   private :_pool_wells
 
@@ -14,7 +14,7 @@ module PlatePurpose::Stock
     return 'pending' if wells_with_aliquots.empty?
 
     # All of the wells with aliquots must have requests for us to be considered passed
-    full_wells_with_requests = plate.wells.requests_as_source_is_a?(Pulldown::Requests::LibraryCreation).count(:conditions => { :id => wells_with_aliquots.map(&:id) })
+    full_wells_with_requests = plate.wells.requests_as_source_is_a?(Request::LibraryCreation).count(:conditions => { :id => wells_with_aliquots.map(&:id) })
     full_wells_with_requests == wells_with_aliquots.size ? 'passed' : 'pending'
   end
 end
