133e18393d6b5266df38b2ded78ebfc93feaef82
James Glover <jg16@sanger.ac.uk>
2012-06-12 11:50:46 +0100

    Refactor Request changes

--- app/models/request/library_creation.rb
+++ app/models/request/library_creation.rb
@@ -1,2 +1,10 @@
 class Request::LibraryCreation < Request
+  # Override the behaviour of Request so that we do not copy the aliquots from our source asset
+  # to the target when we are passed.  This is actually done by the TransferRequest from plate
+  # to plate as it goes through being processed.
+  include Request::StandardBillingStrategy
+
+  def on_started
+    # Override the default behaviour to not do the transfer
+  end
 end
