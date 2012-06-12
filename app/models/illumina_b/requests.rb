4cd91a1b0c7927ed28b4e97524bb6984fa0915f3
Sean Dunn <sd9@sanger.ac.uk>
2012-06-12 11:29:17 +0100

    Fixes LibraryCreation inheritance.

--- app/models/illumina_b/requests.rb
+++ app/models/illumina_b/requests.rb
@@ -3,7 +3,7 @@ module IlluminaB::Requests
   # Override the behaviour of Request so that we do not copy the aliquots from our source asset
   # to the target when we are passed.  This is actually done by the TransferRequest from plate
   # to plate as it goes through being processed.
-  class LibraryCreation < Request
+  class LibraryCreation < Request::LibraryCreation
     include Request::StandardBillingStrategy
 
     def on_started
