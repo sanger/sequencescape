a3c70d64ee246f0b463bb1ccb8794e75af65b30e
James Glover <jg16@sanger.ac.uk>
2012-06-26 14:43:08 +0100

    [#29980455] Refine migration for create_asset requests

--- db/migrate/20120622110117_remove_invalid_create_asset_requests.rb
+++ db/migrate/20120622110117_remove_invalid_create_asset_requests.rb
@@ -3,13 +3,13 @@ class RemoveInvalidCreateAssetRequests < ActiveRecord::Migration
     ActiveRecord::Base.transaction do
       say "Removing create asset requests for wells on non-stock plates"
       execute  <<-SQL
-        DELETE
+        DELETE `requests`
         FROM `requests`
-        LEFT OUTER JOIN `assets` ON `assets`.id = `requests`.`asset_id` AND `assets`.sti_type = 'Well'
+        LEFT OUTER JOIN `assets` ON `assets`.id = `requests`.`asset_id`
         LEFT OUTER JOIN `container_associations` ON `content_id` = `assets`.`id`
         LEFT OUTER JOIN `assets` AS plate ON `container_id` = plate.id
         LEFT OUTER JOIN `plate_purposes` ON `plate_purposes`.id = plate.plate_purpose_id
-        WHERE `requests`.sti_type = 'CreateAssetRequest' AND `plate_purposes`.name != 'Stock Plate';
+        WHERE `requests`.sti_type = 'CreateAssetRequest' AND `plate_purposes`.name != 'Stock Plate' AND plate.id IS NOT NULL AND `assets`.sti_type = 'Well';
       SQL
     end
   end
