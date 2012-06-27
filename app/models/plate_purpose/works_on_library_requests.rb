7347f26808f7b956446c3710a4c6afbeae89bccc
Matthew Denner <matt.denner@gmail.com>
2012-06-27 21:40:07 +0100

    Faster plate state changing by using better eager loading

--- app/models/plate_purpose/works_on_library_requests.rb
+++ app/models/plate_purpose/works_on_library_requests.rb
@@ -6,9 +6,12 @@ module PlatePurpose::WorksOnLibraryRequests
   end
 
   def each_well_and_its_library_request(plate, &block)
-    locate_stock_wells_for(plate).each do |well, stock_wells|
-      library_request = stock_wells.first.requests_as_source.where_is_a?(Request::LibraryCreation).first or next
-      yield(well, library_request)
+    well_to_stock_id = Hash[locate_stock_wells_for(plate).map { |well,stock_wells| [well.id, stock_wells.first.id] }]
+    requests         = Request::LibraryCreation.for_asset_id(well_to_stock_id.values).include_request_metadata.group_by(&:asset_id)
+    plate.wells.all(:include => { :aliquots => :library }).each do |well|
+      stock_id       = well_to_stock_id[well.id] or raise "No stock well for #{well.id.inspect} (#{well_to_stock_id.inspect})"
+      stock_requests = requests[stock_id]        or raise "No requests for stock well #{stock_id.inspect} (#{requests.inspect})"
+      yield(well, stock_requests.first)
     end
   end
   private :each_well_and_its_library_request
