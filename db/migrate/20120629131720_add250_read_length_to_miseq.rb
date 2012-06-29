289d052a40c28de2013b60acbfdc5bbe1cabbbd6
Matthew Denner <md12@sanger.ac.uk>
2012-06-29 15:46:59 +0100

    Refactored migration to deal with production system

--- db/migrate/20120629131720_add250_read_length_to_miseq.rb
+++ db/migrate/20120629131720_add250_read_length_to_miseq.rb
@@ -1,23 +1,27 @@
 class Add250ReadLengthToMiseq < ActiveRecord::Migration
   def self.up
-    SubmissionTemplate.find(:all, :conditions => 'name LIKE "%MiSeq%"').each do |submission_template|
-      submission_template.submission_parameters[:input_field_infos].select do |variable|
-        variable.ivars["display_name"] == "Read length"
-      end.each do |variable|
-        variable.ivars["parameters"][:selection]<<"250"
-      end
-      submission_template.save!
+    change_templates do |parameters|
+      parameters[:selection] << '250'
     end
   end
 
   def self.down
-    SubmissionTemplate.find(:all, :conditions => 'name LIKE "%MiSeq%"').each do |submission_template|
-      submission_template.submission_parameters[:input_field_infos].select do |variable|
-        variable.ivars["display_name"] == "Read length"
-      end.each do |variable|
-        variable.ivars["parameters"][:selection].delete("250")
+    change_templates do |parameters|
+      parameters.delete('250')
+    end
+  end
+
+  def self.change_templates(&block)
+    ActiveRecord::Base.transaction do
+      SubmissionTemplate.find(:all, :conditions => 'name LIKE "%MiSeq%"').each do |submission_template|
+        field_infos = submission_template.submission_parameters[:input_field_infos] or next
+        field_infos.select do |variable|
+          variable.ivars["display_name"] == "Read length"
+        end.each do |variable|
+          yield(variable.ivars["parameters"])
+        end
+        submission_template.save!
       end
-      submission_template.save!
     end
   end
 end
