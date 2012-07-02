360af6f80abc319ee8bf5790901fbbdf28bedfdb
James Glover <jg16@sanger.ac.uk>
2012-07-02 15:06:43 +0100

    [#29980455] Use rails to remove invalid requests

--- db/migrate/20120608124657_remove_create_asset_requests_on_plates.rb
+++ db/migrate/20120608124657_remove_create_asset_requests_on_plates.rb
@@ -2,13 +2,25 @@ class RemoveCreateAssetRequestsOnPlates < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.transaction do
 
-      execute <<-SQL
-      DELETE `requests`
-        FROM `requests`
-        LEFT OUTER JOIN `assets` ON `assets`.id = `requests`.`asset_id`
-        WHERE `requests`.sti_type = 'CreateAssetRequest'
-          AND `assets`.`sti_type` IN ('Plate','ControlPlate','GelDilutionPlate','PicoAssayAPlate','PicoAssayBPlate','PicoDilutionPlate','SequenomQcPlate','WorkingDilutionPlate');
-      SQL
+      CreateAssetRequest.find(
+      :all,
+      :joins => 'LEFT OUTER JOIN `assets` ON `assets`.id = `requests`.`asset_id`',
+      :conditions => {
+        :sti_type => 'CreateAssetRequest',
+        :assets => {:sti_type => [
+          'Plate','ControlPlate','GelDilutionPlate','PicoAssayAPlate',
+          'PicoAssayBPlate','PicoDilutionPlate','SequenomQcPlate','WorkingDilutionPlate'
+          ]
+        }
+      }).map(&:destroy)
+
+      # execute <<-SQL
+      # DELETE `requests`
+      #   FROM `requests`
+      #   LEFT OUTER JOIN `assets` ON `assets`.id = `requests`.`asset_id`
+      #   WHERE `requests`.sti_type = 'CreateAssetRequest'
+      #     AND `assets`.`sti_type` IN ('Plate','ControlPlate','GelDilutionPlate','PicoAssayAPlate','PicoAssayBPlate','PicoDilutionPlate','SequenomQcPlate','WorkingDilutionPlate');
+      # SQL
 
     end
   end
--- db/migrate/20120622110117_remove_invalid_create_asset_requests.rb
+++ db/migrate/20120622110117_remove_invalid_create_asset_requests.rb
@@ -2,15 +2,27 @@ class RemoveInvalidCreateAssetRequests < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.transaction do
       say "Removing create asset requests for wells on non-stock plates"
-      execute  <<-SQL
-        DELETE `requests`
-        FROM `requests`
-        LEFT OUTER JOIN `assets` ON `assets`.id = `requests`.`asset_id`
-        LEFT OUTER JOIN `container_associations` ON `content_id` = `assets`.`id`
-        LEFT OUTER JOIN `assets` AS plate ON `container_id` = plate.id
-        LEFT OUTER JOIN `plate_purposes` ON `plate_purposes`.id = plate.plate_purpose_id
-        WHERE `requests`.sti_type = 'CreateAssetRequest' AND `plate_purposes`.name != 'Stock Plate' AND plate.id IS NOT NULL AND `assets`.sti_type = 'Well';
-      SQL
+
+      CreateAssetRequest.find(
+      :all,
+      :joins => [
+        'LEFT OUTER JOIN `assets` ON `assets`.id = `requests`.asset_id',
+        'LEFT OUTER JOIN `container_associations` ON `content_id` = `assets`.id',
+        'LEFT OUTER JOIN `assets` AS plate ON `container_id` = plate.id',
+        'LEFT OUTER JOIN `plate_purposes` ON `plate_purposes`.id = plate.plate_purpose_id'
+        ],
+      :conditions => "`requests`.sti_type = 'CreateAssetRequest' AND `plate_purposes`.name != 'Stock Plate' AND plate.id IS NOT NULL AND `assets`.sti_type = 'Well'"
+      ).map(&:destroy)
+
+      # execute  <<-SQL
+      #   DELETE `requests`
+      #   FROM `requests`
+      #   LEFT OUTER JOIN `assets` ON `assets`.id = `requests`.`asset_id`
+      #   LEFT OUTER JOIN `container_associations` ON `content_id` = `assets`.`id`
+      #   LEFT OUTER JOIN `assets` AS plate ON `container_id` = plate.id
+      #   LEFT OUTER JOIN `plate_purposes` ON `plate_purposes`.id = plate.plate_purpose_id
+      #   WHERE `requests`.sti_type = 'CreateAssetRequest' AND `plate_purposes`.name != 'Stock Plate' AND plate.id IS NOT NULL AND `assets`.sti_type = 'Well';
+      # SQL
     end
   end
 
