7b237e5adb1896b42551c5b7932aaead5466c144
Matthew Denner <md12@sanger.ac.uk>
2012-07-11 09:48:46 +0100

    Added some tests of the various request type statemachines

--- app/models/illumina_b/pcr_plate_purpose.rb
+++ app/models/illumina_b/pcr_plate_purpose.rb
@@ -1,7 +1,9 @@
 class IlluminaB::PcrPlatePurpose < PlatePurpose
+  alias_method(:default_transition_to, :transition_to)
+
   def transition_to(plate, state, contents = nil)
-    nudge_parent_plate(plate, start, contents)
-    super
+    nudge_parent_plate(plate, state, contents)
+    default_transition_to(plate, state, contents)
   end
 
   def nudge_parent_plate(plate, state, contents)
