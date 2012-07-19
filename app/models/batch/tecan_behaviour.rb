3738ab951092392882edc76a0fbc9969850cdff7
James Glover <jg16@sanger.ac.uk>
2012-07-19 11:35:14 +0100

    Provide full namespace for TECAN Generator

--- app/models/batch/tecan_behaviour.rb
+++ app/models/batch/tecan_behaviour.rb
@@ -58,19 +58,19 @@ module Batch::TecanBehaviour
 
   def tecan_layout_plate_barcodes(target_barcode)
     data_object = generate_tecan_data(target_barcode)
-    dest_barcode_index = Generator.barcode_to_plate_index(data_object["destination"])
-    source_barcode_index = Generator.source_barcode_to_plate_index(data_object["destination"])
+    dest_barcode_index = Sanger::Robots::Tecan::Generator.barcode_to_plate_index(data_object["destination"])
+    source_barcode_index = Sanger::Robots::Tecan::Generator.source_barcode_to_plate_index(data_object["destination"])
     [dest_barcode_index,source_barcode_index]
   end
 
   def tecan_gwl_file_as_text(target_barcode, volume_required = 13, plate_type = nil)
     data_object = generate_tecan_data(target_barcode, plate_type)
-    Generator.mapping(data_object,  volume_required.to_i)
+    Sanger::Robots::Tecan::Generator.mapping(data_object,  volume_required.to_i)
   end
 
   def tecan_gwl_file(target_barcode,volume_required=13)
     data_object = generate_tecan_data(target_barcode)
-    gwl_data  = Generator.mapping(data_object,  volume_required.to_i)
+    gwl_data  = Sanger::Robots::Tecan::Generator.mapping(data_object,  volume_required.to_i)
     begin
       year= Time.now.year
       base_directory ="#{configatron.tecan_files_location}/#{year}"
