8916fd2691c143bc83280f312775f1f5cecc66a3
Matthew Denner <md12@sanger.ac.uk>
2012-07-19 11:45:18 +0100

    [#32601045] Migrations needed completely reworking

--- db/migrate/20120525083146_add_new_request_types_for_plate_based_illumina_b_mx_library_creation.rb
+++ db/migrate/20120525083146_add_new_request_types_for_plate_based_illumina_b_mx_library_creation.rb
@@ -17,17 +17,13 @@ class AddNewRequestTypesForPlateBasedIlluminaBMxLibraryCreation < ActiveRecord::
         :for_multiplexing => 1,
         :billable => 1,
         :product_line_id => ProductLine.find_by_name('Illumina-B').id
-      ).tap do |request_type|
-        request_type.acceptable_plate_purposes  << PlatePurpose.find_by_name('ILB_STD_INPUT')
-      end
+      )
     end
   end
 
   def self.down
     ActiveRecord::Base.transaction do
-      request_types.each do |config|
-        RequestType.find_by_key('Illumina-B STD').destroy
-      end
+      RequestType.find_by_key('Illumina-B STD').destroy
     end
   end
 
