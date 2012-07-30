4aa89003a002fdfa352f9bb1fdae38b7c548f74c
Sean Dunn <sd9@sanger.ac.uk>
2012-07-30 17:34:09 +0100

    Removed old FinalPlatePurpose#transition_to method

--- app/models/illumina_b/final_plate_purpose.rb
+++ app/models/illumina_b/final_plate_purpose.rb
@@ -1,8 +1,3 @@
 class IlluminaB::FinalPlatePurpose < PlatePurpose
   include PlatePurpose::Library
-
-  def transition_to(plate, state, contents = nil)
-    plate.parent.parent.transition_to(state, contents) if state == 'passed'
-    super
-  end
 end
