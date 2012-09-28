7e0d8dd42656a333d9cbb18f869a136593311a98
James Glover <jg16@sanger.ac.uk>
2012-09-28 09:35:49 +0100

    [#36375279] Migration ensures record_timestamps it returned to true

--- db/migrate/20120926081459_repair_billing_events_without_request_id.rb
+++ db/migrate/20120926081459_repair_billing_events_without_request_id.rb
@@ -2,23 +2,31 @@
 class RepairBillingEventsWithoutRequestId < ActiveRecord::Migration
 
   class BillingEvent < ActiveRecord::Base
+
+    def self.do_not_record_timestamps(&block)
+      @record_timestamps = false
+      yield
+    ensure
+      @record_timestamps = true
+    end
+
   end
 
   def self.up
     ActiveRecord::Base.transaction do
-      BillingEvent.record_timestamps = false
+      BillingEvent.do_not_record_timestamps do
 
-      BillingEvent.find_all_by_request_id(nil).each do |billing_event|
-        request_match = /RT*([0-9]*)/.match(billing_event.reference)
+        BillingEvent.find_all_by_request_id(nil).each do |billing_event|
+          request_match = /RT*([0-9]*)/.match(billing_event.reference)
 
-        raise "#{billing_event.reference} is in unexpected reference format and has not been repaired. Aborting." if request_match.nil?
+          raise "#{billing_event.reference} is in unexpected reference format and has not been repaired. Aborting." if request_match.nil?
 
-        request_id = request_match[1]
-        say "Repairing #{billing_event.id}:#{billing_event.reference} => #{request_id}"
-        billing_event.update_attributes!(:request_id => request_id)
+          request_id = request_match[1]
+          say "Repairing #{billing_event.id}:#{billing_event.reference} => #{request_id}"
+          billing_event.update_attributes!(:request_id => request_id)
+        end
+
       end
-
-      BillingEvent.record_timestamps = true
     end
   end
 
