2edd4489939b8114f3e842e498cfe2c198984f5f
Matthew Denner <md12@sanger.ac.uk>
2012-11-12 09:56:02 +0000

    [#38415325] Refactored requests for pooling to named_scope
    This allows for additional eager loading to be included in the query.

--- app/api/model_extensions/plate.rb
+++ app/api/model_extensions/plate.rb
@@ -34,18 +34,7 @@ module ModelExtensions::Plate
   # ignored within the returned result.
   def pools
     ActiveSupport::OrderedHash.new.tap do |pools|
-      Request.find_by_sql(%Q{
-        SELECT uuids.external_id AS pool_id, GROUP_CONCAT(DISTINCT pw_location.description SEPARATOR ',') AS pool_into, requests.*
-        FROM container_associations
-        INNER JOIN assets AS pw        ON container_associations.content_id=pw.id
-        INNER JOIN maps AS pw_location ON pw.map_id=pw_location.id
-        INNER JOIN well_links          ON well_links.target_well_id=pw.id AND well_links.type='stock'
-        INNER JOIN requests            ON well_links.source_well_id=requests.asset_id
-        INNER JOIN submissions         ON requests.submission_id=submissions.id
-        INNER JOIN uuids               ON uuids.resource_id=submissions.id AND uuids.resource_type='Submission'
-        WHERE container_associations.container_id=#{id}
-        GROUP BY submissions.id
-      }).each do |request|
+      Request.include_request_metadata.for_pooling_of(self).each do |request|
         pools[request.pool_id] = { :wells => request.pool_into.split(',') }.tap do |pool_information|
           request.update_pool_information(pool_information)
         end unless request.pool_id.nil?
