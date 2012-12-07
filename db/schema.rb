be1c7fe25f708a6b554b56246c37bd40edfc08d2
James Glover <jg16@sanger.ac.uk>
2012-12-07 12:24:16 +0000

    [#40835189] Include migration in schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121201111647) do
+ActiveRecord::Schema.define(:version => 20121206100000) do
 
   create_table "aliquots", :force => true do |t|
     t.integer  "receptacle_id",    :null => false
