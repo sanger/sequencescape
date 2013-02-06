288de1914315bbdccda8e29ab41244327d086551
James Glover <jg16@sanger.ac.uk>
2013-02-06 15:22:07 +0000

    [#40187689] Add flowcell descripto earlier

---
+++ db/migrate/20130206143136_add_flowcell_barcode_capture.rb
@@ -0,0 +1,26 @@
+class AddFlowcellBarcodeCapture < ActiveRecord::Migration
+  def self.up
+    ActiveRecord::Base.transaction do
+      pipelines.each do |name|
+        SetDescriptorsTask.create!(
+          :name => 'Add flowcell chip barcode',
+          :pipeline_workflow_id => Pipeline.find_by_name(name).workflow.id,
+          :sorted => 4,
+          :batched => 1
+          ).tap do |task|
+            Descriptor.create!(:name=>'Chip barcode', :sorter=>1, :task=>task, :kind=>'Text')
+        end
+      end
+    end
+  end
+
+  def self.down
+    pipelines.each do |name|
+      Pipeline.find_by_name(name).workflow.tasks.each {|t| t.destroy if t.name=='Add flowcell chip barcode'}
+    end
+  end
+
+  def self.pipelines
+    ['HiSeq 2500 PE (spiked in controls)','HiSeq 2500 SE (spiked in controls)']
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130114112731) do
+ActiveRecord::Schema.define(:version => 20130206143136) do
 
   create_table "aliquots", :force => true do |t|
     t.integer  "receptacle_id",    :null => false
