be254013b4c9c630fa42e4b8c9c0c9e98972e9f7
James Glover <jg16@sanger.ac.uk>
2013-02-13 10:42:27 +0000

    [#44078709] Use inverse location association
    An inverse location needs to be defined to stop
    the touch causing issues with unsaved records.

--- app/models/location_association.rb
+++ app/models/location_association.rb
@@ -14,7 +14,7 @@ class LocationAssociation < ActiveRecord::Base
   module Locatable
     def self.included(base)
       base.class_eval do
-        has_one :location_association, :foreign_key => :locatable_id
+        has_one :location_association, :foreign_key => :locatable_id, :inverse_of => :locatable
 
         has_one :location, :through => :location_association
         delegate :location_id, :to => :location_association, :allow_nil => true
