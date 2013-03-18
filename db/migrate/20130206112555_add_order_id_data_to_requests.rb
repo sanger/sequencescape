1717bfc4dad16b7a21821caffe2d1b06e3d6a361
James Glover <jg16@sanger.ac.uk>
2013-03-18 10:53:41 +0000

    [#43834837] Add order_id to existing requests

--- db/migrate/20130206112555_add_order_id_data_to_requests.rb
+++ db/migrate/20130206112555_add_order_id_data_to_requests.rb
@@ -1,8 +1,32 @@
 class AddOrderIdDataToRequests < ActiveRecord::Migration
+
+  class Request < ActiveRecord::Base
+    belongs_to :submission
+    belongs_to :order
+
+  end
+
   def self.up
-    raise "TODO"
+    Request.find_in_batches(:include=>{:submission=>:orders}, :conditions => ['submission_id IS NOT NULL']) do |batch|
+      ActiveRecord::Base.transaction do
+        say "Processing requests #{batch.first.id} to #{batch.last.id}"
+        batch.each do |request|
+          if request.submission.orders.count == 1
+            request.update_attributes!(:order=>request.submission.orders.first)
+          end
+        end
+      end
+    end
   end
 
   def self.down
+    Request.all.find_in_batches(:conditions => ['order_id IS NOT NULL']) do |batch|
+      ActiveRecord::Base.transaction do
+        say "Processing requests #{batch.first.id} to #{batch.last.id}"
+        batch.each do |request|
+          request.update_attributes!(:order=>nil)
+        end
+      end
+    end
   end
 end
