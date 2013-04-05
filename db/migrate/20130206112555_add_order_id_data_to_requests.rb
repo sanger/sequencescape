66e9abe476031420babfa1b305576a1ec079e33f
James Glover <jg16@sanger.ac.uk>
2013-04-05 16:01:00 +0100

    [#46378661] Handle submissionless requests

--- db/migrate/20130206112555_add_order_id_data_to_requests.rb
+++ db/migrate/20130206112555_add_order_id_data_to_requests.rb
@@ -7,10 +7,11 @@ class AddOrderIdDataToRequests < ActiveRecord::Migration
   end
 
   def self.up
-    Request.find_in_batches(:include=>{:submission=>:orders}, :conditions => ['submission_id IS NOT NULL']) do |batch|
+    Request.find_in_batches(:include=>{:submission=>:orders}, :conditions => ['submission_id IS NOT NULL AND order_id IS NULL']) do |batch|
       ActiveRecord::Base.transaction do
         say "Processing requests #{batch.first.id} to #{batch.last.id}"
         batch.each do |request|
+          next if request.submission.nil?
           if request.submission.orders.count == 1
             request.update_attributes!(:order=>request.submission.orders.first)
           end
@@ -20,7 +21,7 @@ class AddOrderIdDataToRequests < ActiveRecord::Migration
   end
 
   def self.down
-    Request.all.find_in_batches(:conditions => ['order_id IS NOT NULL']) do |batch|
+    Request.find_in_batches(:conditions => ['order_id IS NOT NULL']) do |batch|
       ActiveRecord::Base.transaction do
         say "Processing requests #{batch.first.id} to #{batch.last.id}"
         batch.each do |request|
