ea10c8725e5e160399c560b37b595c1fe7d3ccda
James Glover <jg16@sanger.ac.uk>
2013-05-24 16:24:38 +0100

    [#50503379] Add Illumina C Generic Request Types

--- app/models/illumina_c/requests.rb
+++ app/models/illumina_c/requests.rb
@@ -28,4 +28,50 @@ module IlluminaC::Requests
     include TransferRequest::InitialTransfer
   end
 
+  module Helpers
+
+    def create_request_types
+      each_request_type do |params|
+        RequestType.create!(params)
+      end
+      IlluminaC::PlatePurposes::STOCK_PLATE_PURPOSE_TO_OUTER_REQUEST.each do |purpose,request|
+        RequestType.find_by_key(request).acceptable_plate_purposes << Purpose.find_by_name(purpose)
+      end
+    end
+
+    def destroy_request_types
+      each_request_type do |params|
+        RequestType.find_by_name(params[:name]).destroy
+      end
+    end
+
+    def each_request_type
+      [
+        {
+          :name => 'Illumina-C Library Creation PCR',
+          :key => 'illumina_c_pcr',
+          :request_class_name =>'IlluminaC::Requests::PcrLibraryRequest'
+        },
+        {
+          :name => 'Illumina-C Library Creation No PCR',
+          :key => 'illumina_c_nopcr',
+          :request_class_name =>'IlluminaC::Requests::NoPcrLibraryRequest'
+        }
+      ].each do |params|
+         params.merge!({
+          :workflow => Submission::Workflow.find_by_name("Next-gen sequencing"),
+          :asset_type => 'Well',
+          :order =>1,
+          :initial_state =>'pending',
+          :for_multiplexing =>true,
+          :billable =>true,
+          :target_purpose =>Purpose.find_by_name('ILC Lib Pool Norm')
+        })
+        yield(params)
+      end
+    end
+
+  end
+  extend Helpers
+
 end
--- db/seeds/2000_illumina_c_workflows.rb
+++ db/seeds/2000_illumina_c_workflows.rb
@@ -2,4 +2,5 @@ ActiveRecord::Base.transaction do
   IlluminaC::PlatePurposes.create_plate_purposes
   IlluminaC::PlatePurposes.create_tube_purposes
   IlluminaC::PlatePurposes.create_branches
+  IlluminaC::Requests.create_request_types
 end
