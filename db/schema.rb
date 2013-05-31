0e7836c72bd9f5f614199e3b4bd9728343f1d80d
James Glover <jg16@sanger.ac.uk>
2013-05-31 14:21:32 +0100

    Correct submission templates

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130502095908) do
+ActiveRecord::Schema.define(:version => 20130531111745) do
 
   create_table "aliquots", :force => true do |t|
     t.integer  "receptacle_id",    :null => false
