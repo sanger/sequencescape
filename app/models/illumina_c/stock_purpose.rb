987a4c1b6f2b5049d4a59916b5630ab7ff576f89
James Glover <jg16@sanger.ac.uk>
2013-06-04 09:17:42 +0100

    [#50503541] Cancel outer requests on well failure
    Still need to handle the failure of the wells themselves

--- app/models/illumina_c/stock_purpose.rb
+++ app/models/illumina_c/stock_purpose.rb
@@ -1,3 +1,10 @@
 class IlluminaC::StockPurpose < PlatePurpose
   include PlatePurpose::Stock
+
+  def transition_to(plate, state, contents = nil)
+    return unless ['failed','cancelled'].include?(state)
+    plate.wells.located_at(contents).each do |well|
+      well.requests.each {|r| r.cancel_before_started! if r.is_a?(IlluminaC::Requests::LibraryRequest) }
+    end
+  end
 end
