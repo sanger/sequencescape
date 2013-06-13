4b209b20383fd34b5fac99659462fe1c8fd4113c
James Glover <jg16@sanger.ac.uk>
2013-06-13 12:35:39 +0100

    fix search

--- app/models/search/find_illumina_a_stock_plates.rb
+++ app/models/search/find_illumina_a_stock_plates.rb
@@ -2,5 +2,5 @@ class Search::FindIlluminaAStockPlates < Search::FindIlluminaAPlates
   def illumina_a_plate_purposes
     PlatePurpose.find_all_by_name(IlluminaHtp::PlatePurposes::STOCK_PLATE_PURPOSE)
   end
-  private :illumina_b_plate_purposes
+  private :illumina_a_plate_purposes
 end
