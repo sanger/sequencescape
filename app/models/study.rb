06f6c2599a1afd20730ee33280116b942e4b7522
James Glover <jg16@sanger.ac.uk>
2013-07-22 17:02:08 +0100

    [#53788909] Add seperate y chromosome flag

--- app/models/study.rb
+++ app/models/study.rb
@@ -197,6 +197,7 @@ class Study < ActiveRecord::Base
     attribute(:study_description, :required => true)
     attribute(:contaminated_human_dna, :required => true, :in => YES_OR_NO)
     attribute(:remove_x_and_autosomes, :required => true, :default => 'No', :in => YES_OR_NO)
+    attribute(:seperate_y_chromosome_data, :required => true, :default=> false, :boolean => true)
     attribute(:study_project_id)
     attribute(:study_abstract)
     attribute(:study_study_title)
@@ -299,6 +300,14 @@ class Study < ActiveRecord::Base
     validates_associated  :data_release_non_standard_agreement, :if => :non_standard_agreement?
 
     validate :valid_policy_url?
+
+    validate :sanity_check_y_separation, :if => :seperate_y_chromosome_data?
+
+    def sanity_check_y_separation
+      self.errors.add(:seperate_y_chromosome_data,'cannot be selected with remove x and autosomes.') if remove_x_and_autosomes?
+      !remove_x_and_autosomes?
+    end
+
 
     before_validation do |record|
       if not record.non_standard_agreement? and not record.data_release_non_standard_agreement.nil?
