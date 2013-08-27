4f8d57b41baf2cf49ab87b061224d891d8e34fb6
James Glover <jg16@sanger.ac.uk>
2013-08-27 13:45:23 +0100

    Final tube should pass outer requests

--- app/models/illumina_c/mx_tube_purpose.rb
+++ app/models/illumina_c/mx_tube_purpose.rb
@@ -6,4 +6,9 @@ class IlluminaC::MxTubePurpose < IlluminaHtp::MxTubePurpose
   def stock_plate(tube)
     tube.requests_as_target.where_is_a?(IlluminaC::Requests::LibraryRequest).first.asset.plate
   end
+
+  def request_state(request,state)
+    state
+  end
+  private :request_state
 end
