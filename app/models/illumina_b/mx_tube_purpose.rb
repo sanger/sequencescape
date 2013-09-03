f1455fb93b06a3a300c9a4d00223925f040ff854
James Glover <jg16@sanger.ac.uk>
2013-09-03 09:26:52 +0100

    [fixes #56249176] Outer requests passed on tube pass

--- app/models/illumina_b/mx_tube_purpose.rb
+++ app/models/illumina_b/mx_tube_purpose.rb
@@ -2,4 +2,9 @@ class IlluminaB::MxTubePurpose < IlluminaHtp::MxTubePurpose
   def stock_plate(tube)
     tube.requests_as_target.where_is_a?(IlluminaB::Requests::StdLibraryRequest).first.asset.plate
   end
+
+  def request_state(request,state)
+    state
+  end
+  private :request_state
 end
