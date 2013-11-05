0a1041d69e6164c94904107994bc5784621612b6
James Glover <jg16@sanger.ac.uk>
2013-11-05 16:22:23 +0000

    Complete authentication checks

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -112,21 +112,35 @@ module AuthenticatedSystem
         end
       end
     end
-    
+
+    def lab_manager_login_required
+      setup_current_user
+      respond_to do |accepts|
+        accepts.html   { logged_in? && authorized? && current_user.lab_manager? ? true : access_denied }
+        if configatron.disable_api_authentication == true
+          accepts.xml  { true }
+          accepts.json { true }
+        else
+          accepts.xml  { logged_in? && authorized? && current_user.lab_manager? ? true : access_denied }
+          accepts.json { logged_in? && authorized? && current_user.lab_manager? ? true : access_denied }
+        end
+      end
+    end
+
     def slf_manager_login_required
       setup_current_user
       respond_to do |accepts|
         accepts.html   { logged_in? && authorized? && (current_user.slf_manager? || current_user.administrator? ) ? true : access_denied }
       end
     end
-    
+
     def slf_gel_login_required
       setup_current_user
       respond_to do |accepts|
         accepts.html   { logged_in? && authorized? && (current_user.slf_manager? || current_user.slf_gel? || current_user.administrator? ) ? true : access_denied }
       end
     end
-    
+
     def setup_current_user
       username, passwd = get_auth_data
       self.current_user ||= User.authenticate(username, passwd) || :false if username && passwd
