2897d070ab2e4708a33f453320e2ecbfc3f9b065
James <james.glover@sanger.ac.uk>
2013-11-20 10:49:57 +0000

    Update 20131107111801_adjust2500_request_class.rb
    Adjust paired not single ended request types

--- db/migrate/20131107111801_adjust2500_request_class.rb
+++ db/migrate/20131107111801_adjust2500_request_class.rb
@@ -3,13 +3,13 @@ class Adjust2500RequestClass < ActiveRecord::Migration
   # Use deployment project to deploy
 
   def self.up
-    RequestType.find(:all, :conditions=>'name LIKE "%_hiseq_2500_single_end_sequencing"').each do |request_type|
+    RequestType.find(:all, :conditions=>'name LIKE "%_hiseq_2500_paired_end_sequencing"').each do |request_type|
       request_type.update_attributes!(:request_class_name=>'HiSeq2500SequencingRequest')
     end
   end
 
   def self.down
-    RequestType.find(:all, :conditions=>'name LIKE "%_hiseq_2500_single_end_sequencing"').each do |request_type|
+    RequestType.find(:all, :conditions=>'name LIKE "%_hiseq_2500_paired_end_sequencing"').each do |request_type|
       request_type.update_attributes!(:request_class_name=>'HiSeqSequencingRequest')
     end
   end
