e090a5c67d580db9eee17d7bb85fba3223f167a4
James <james.glover@sanger.ac.uk>
2013-11-20 11:47:23 +0000

    Update hi_seq_2500_sequencing_request.rb

--- app/models/hi_seq_2500_sequencing_request.rb
+++ app/models/hi_seq_2500_sequencing_request.rb
@@ -1,5 +1,13 @@
 class HiSeq2500SequencingRequest < HiSeqSequencingRequest
   READ_LENGTHS = [75, 100, 150]
+  
+  has_metadata :as => Request  do
+    #redundant with library creation , but THEY are using it .
+    attribute(:fragment_size_required_from, :required =>true, :integer => true)
+    attribute(:fragment_size_required_to, :required =>true, :integer =>true)
+
+    attribute(:read_length, :integer => true, :required => true, :in => READ_LENGTHS)
+  end
 
   class RequestOptionsValidator < DelegateValidation::Validator
     delegate_attribute :read_length, :to => :target, :type_cast => :to_i
