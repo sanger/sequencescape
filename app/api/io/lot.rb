64ae493903719a939ad6ca2e5236df977dd346ab
James Glover <jg16@mib106452i.internal.sanger.ac.uk>
2014-02-14 10:37:01 +0000

    Add Eager Loading

--- app/api/io/lot.rb
+++ app/api/io/lot.rb
@@ -2,13 +2,15 @@ class ::Io::Lot < ::Core::Io::Base
   set_model_for_input(::Lot)
   set_json_root(:lot)
 
+  set_eager_loading { |model| model.include_lot_type.include_template }
+
   define_attribute_and_json_mapping(%Q{
                                            lot_number <=> lot_number
                                           recieved_at <=> recieved_at
                                         template.name  => template_name
                                          lot_type.name => lot_type_name
                                              lot_type <= lot_type
-                                             user <= user
+                                                 user <= user
                                              template <= template
   })
 end
--- app/api/io/qcable.rb
+++ app/api/io/qcable.rb
@@ -1,11 +1,11 @@
-class Io::Qcable
- < Core::Io::Base
+class Io::Qcable < Core::Io::Base
   set_model_for_input(::Qcable)
   set_json_root(:qcable)
+
+  set_eager_loading { |model| model.include_asset }
 
   define_attribute_and_json_mapping(%Q{
        asset.barcode  => asset_barcode
                state  => state
-                user <=> user
   })
 end
