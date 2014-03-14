b896d6e5ece3310ac4fea9b96cfe4e85fbe443ec
James Glover <james.glover@sanger.ac.uk>
2014-03-14 16:51:58 +0000

    [#65190270] Can find QCables by barcode

--- app/models/qcable.rb
+++ app/models/qcable.rb
@@ -31,6 +31,35 @@ class Qcable < ActiveRecord::Base
       :order => 'stamps.created_at ASC, stamp_qcables.order ASC'
   }
 
+  # We accept not only an individual barcode but also an array of them.  This builds an appropriate
+  # set of conditions that can find any one of these barcodes.  We map each of the individual barcodes
+  # to their appropriate query conditions (as though they operated on their own) and then we join
+  # them together with 'OR' to get the overall conditions.
+  named_scope :with_machine_barcode, lambda { |*barcodes|
+    query_details = barcodes.flatten.map do |source_barcode|
+      barcode_number = Barcode.number_to_human(source_barcode)
+      prefix_string  = Barcode.prefix_from_barcode(source_barcode)
+      barcode_prefix = BarcodePrefix.find_by_prefix(prefix_string)
+
+      if barcode_number.nil? or prefix_string.nil? or barcode_prefix.nil?
+        { :query => 'FALSE' }
+      else
+        { :query => '(wam_asset.barcode=? AND wam_asset.barcode_prefix_id=?)', :conditions => [ barcode_number, barcode_prefix.id ] }
+      end
+    end.inject({ :query => ['FALSE'], :conditions => [nil], :joins=>['LEFT JOIN assets AS wam_asset ON qcables.asset_id = wam_asset.id'] }) do |building, current|
+      building.tap do
+        building[:joins]      << current[:joins]
+        building[:query]      << current[:query]
+        building[:conditions] << current[:conditions]
+      end
+    end
+
+    {
+      :conditions => [ query_details[:query].join(' OR '), *query_details[:conditions].flatten.compact ],
+      :joins => query_details[:joins].compact.uniq
+    }
+  }
+
   def stamp_index
     return nil if stamp_qcable.nil?
     lot.qcables.stamped.index(self)
