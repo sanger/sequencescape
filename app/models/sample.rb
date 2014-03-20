76ce855c9e4a5678c4c8fcf251abf5c3d4618b8d
James Glover <james.glover@sanger.ac.uk>
2014-03-20 14:34:03 +0000

    Update age regular expression to allow ranges

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -213,7 +213,7 @@ class Sample < ActiveRecord::Base
   GENDERS         = [ 'Male', 'Female', 'Mixed', 'Hermaphrodite', 'Unknown', 'Not Applicable' ]
   DNA_SOURCES     = [ 'Genomic', 'Whole Genome Amplified', 'Blood', 'Cell Line','Saliva','Brain','FFPE' ]
   SRA_HOLD_VALUES = [ 'Hold', 'Public', 'Protect' ]
-  AGE_REGEXP      = '\d+(?:\.\d+)?\s+(?:second|minute|day|week|month|year)s?|Not Applicable|N/A|To be provided'
+  AGE_REGEXP      = '\d+(?:\.\d+|\-\d+|\.\d+\-\d+\.\d+|\.\d+\-\d+\.\d+)?\s+(?:second|minute|day|week|month|year)s?|Not Applicable|N/A|To be provided'
   DOSE_REGEXP     = '\d+(?:\.\d+)?\s+\w+(?:\/\w+)?|Not Applicable|N/A|To be provided'
 
   extend Metadata
