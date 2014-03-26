7478714e19476abbefa013f22d1a2d4915400866
James Glover <james.glover@sanger.ac.uk>
2014-03-26 09:48:31 +0000

    Return empty scope when batch not found

--- app/models/search/find_lot_by_batch_id.rb
+++ app/models/search/find_lot_by_batch_id.rb
@@ -1,6 +1,8 @@
 class Search::FindLotByBatchId < Search
   def scope(criteria)
-    root_asset = Batch.find(criteria['batch_id']).parent_of_purpose('Tag PCR')
+    root_asset =  Batch.find_by_id(criteria['batch_id']).tap do |batch|
+      return Lot.find(:all, :conditions => 'FALSE') if batch.nil?
+    end.parent_of_purpose('Tag PCR')
     Lot.with_qc_asset(root_asset)
   end
 end
