6abc25d80004577286a8b914a70bed994f9d8779
James Glover <james.glover@sanger.ac.uk>
2014-05-08 10:02:24 +0100

    [#70861470] Tag PCR plate applies library information to aliquots
    Occurs as part of transition to. Note that no transition actually
    occurs, and the plate still uses the Qcable to represent its state.

---
+++ app/models/qcable_library_plate_purpose.rb
@@ -0,0 +1,37 @@
+class QcableLibraryPlatePurpose < PlatePurpose
+
+  module ClassBehaviour
+
+    def state_of(plate)
+      qcable_for(plate).state
+    end
+
+    def transition_to(plate, state, _=nil)
+      assign_library_information_to_wells(plate)
+    end
+
+    private
+
+    def qcable_for(plate)
+      Qcable.find_by_asset_id(plate.id)
+    end
+
+    # Ensure that the library information within the aliquots of the well is correct.
+    def assign_library_information_to_wells(plate)
+      plate.wells.each do |well|
+        library_type, insert_size = 'QA1', 100
+
+        well.aliquots.each do |aliquot|
+          aliquot.library      ||= well
+          aliquot.library_type ||= library_type
+          aliquot.insert_size  ||= insert_size
+          aliquot.save!
+        end
+      end
+    end
+
+  end
+
+  include ClassBehaviour
+
+end
--- db/seeds/9999_tag_qc_setup.rb
+++ db/seeds/9999_tag_qc_setup.rb
@@ -20,7 +20,7 @@ tube = BarcodePrinterType.find_by_name('1D Tube')
 plate = BarcodePrinterType.find_by_name('96 Well PLate')
 
 purpose_order = [
-      {:class=>PlatePurpose,    :name=>'Tag PCR', :barcode_printer_type => plate, :size => 96, :asset_shape => Map::AssetShape.find_by_name('Standard')},
+      {:class=>QcableLibraryPlatePurpose,    :name=>'Tag PCR', :barcode_printer_type => plate, :size => 96, :asset_shape => Map::AssetShape.find_by_name('Standard')},
       {:class=>PlatePurpose,    :name=>'Tag PCR-XP', :barcode_printer_type => plate, :size => 96, :asset_shape => Map::AssetShape.find_by_name('Standard')},
       {:class=>Tube::StockMx,   :name=>'Tag Stock-MX', :target_type=>'StockMultiplexedLibraryTube', :barcode_printer_type => tube},
       {:class=>Tube::StandardMx,:name=>'Tag MX', :target_type=>'MultiplexedLibraryTube', :barcode_printer_type => tube},
