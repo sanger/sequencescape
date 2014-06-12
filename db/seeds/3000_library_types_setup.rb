5f44ae04ef9794ad4cce18ac393a64c0219a8b11
James Glover <james.glover@sanger.ac.uk>
2014-06-12 16:49:35 +0100

    Add basic models and seeds

---
+++ db/seeds/3000_library_types_setup.rb
@@ -0,0 +1,51 @@
+module SetupLibraryTypes
+  def self.existing_associations_for(request_type)
+    {
+      "LibraryCreationRequest"=>["No PCR", "High complexity and double size selected", "Illumina cDNA protocol", "Agilent Pulldown", "Custom", "High complexity", "ChiP-seq", "NlaIII gene expression", "Standard", "Long range", "Small RNA", "Double size selected", "DpnII gene expression", "TraDIS", "qPCR only", "Pre-quality controlled", "DSN_RNAseq", "RNA-seq dUTP"],
+      "MultiplexedLibraryCreationRequest"=>["No PCR", "High complexity and double size selected", "Illumina cDNA protocol", "Agilent Pulldown", "Custom", "High complexity", "ChiP-seq", "NlaIII gene expression", "Standard", "Long range", "Small RNA", "Double size selected", "DpnII gene expression", "TraDIS", "qPCR only", "Pre-quality controlled", "DSN_RNAseq", "RNA-seq dUTP"],
+      "Pulldown::Requests::WgsLibraryRequest"=>["Standard"],
+      "Pulldown::Requests::ScLibraryRequest"=>["Agilent Pulldown"],
+      "Pulldown::Requests::IscLibraryRequest"=>["Agilent Pulldown"],
+      "IlluminaB::Requests::StdLibraryRequest"=>["No PCR", "High complexity and double size selected", "Illumina cDNA protocol", "Agilent Pulldown", "Custom", "High complexity", "ChiP-seq", "NlaIII gene expression", "Standard", "Long range", "Small RNA", "Double size selected", "DpnII gene expression", "TraDIS", "qPCR only", "Pre-quality controlled", "DSN_RNAseq"],
+      "IlluminaHtp::Requests::SharedLibraryPrep"=>["No PCR", "High complexity and double size selected", "Illumina cDNA protocol", "Agilent Pulldown", "Custom", "High complexity", "ChiP-seq", "NlaIII gene expression", "Standard", "Long range", "Small RNA", "Double size selected", "DpnII gene expression", "TraDIS", "qPCR only", "Pre-quality controlled", "DSN_RNAseq"],
+      "IlluminaHtp::Requests::LibraryCompletion"=>["No PCR", "High complexity and double size selected", "Illumina cDNA protocol", "Agilent Pulldown", "Custom", "High complexity", "ChiP-seq", "NlaIII gene expression", "Standard", "Long range", "Small RNA", "Double size selected", "DpnII gene expression", "TraDIS", "qPCR only", "Pre-quality controlled", "DSN_RNAseq"],
+      "Pulldown::Requests::IscLibraryRequestPart"=>["Agilent Pulldown"],
+      "IlluminaC::Requests::PcrLibraryRequest"=>["Manual Standard WGS (Plate)", "ChIP-Seq Auto", "TruSeq mRNA (RNA Seq)", "Small RNA (miRNA)", "RNA-seq dUTP eukaryotic", "RNA-seq dUTP prokaryotic"],
+      "IlluminaC::Requests::NoPcrLibraryRequest"=>["No PCR (Plate)"]
+    }.tap {|h| h.default = [] }[request_type.request_class_name]
+  end
+
+  def self.existing_defaults_for(request_type)
+    {
+      "LibraryCreationRequest"=>"Standard",
+     "MultiplexedLibraryCreationRequest"=>"Standard",
+     "Pulldown::Requests::WgsLibraryRequest"=>"Standard",
+     "Pulldown::Requests::ScLibraryRequest"=>"Agilent Pulldown",
+     "Pulldown::Requests::IscLibraryRequest"=>"Agilent Pulldown",
+     "IlluminaB::Requests::StdLibraryRequest"=>"Standard",
+     "IlluminaHtp::Requests::SharedLibraryPrep"=>"Standard",
+     "IlluminaHtp::Requests::LibraryCompletion"=>"Standard",
+     "Pulldown::Requests::IscLibraryRequestPart"=>"Agilent Pulldown",
+     "IlluminaC::Requests::PcrLibraryRequest"=>"Manual Standard WGS (Plate)",
+     "IlluminaC::Requests::NoPcrLibraryRequest"=>"No PCR (Plate)"
+   }[request_type.request_class_name]
+  end
+end
+LibraryType.create!([
+  "No PCR", "High complexity and double size selected", "Illumina cDNA protocol",
+  "Agilent Pulldown", "Custom", "High complexity", "ChiP-seq", "NlaIII gene expression",
+  "Standard", "Long range", "Small RNA", "Double size selected", "DpnII gene expression",
+  "TraDIS", "qPCR only", "Pre-quality controlled", "DSN_RNAseq", "RNA-seq dUTP",
+  "Manual Standard WGS (Plate)", "ChIP-Seq Auto", "TruSeq mRNA (RNA Seq)", "Small RNA (miRNA)",
+  "RNA-seq dUTP eukaryotic", "RNA-seq dUTP prokaryotic", "No PCR (Plate)"
+].map {|name| {:name=>name} })
+
+RequestType.find_each do |request_type|
+
+  library_types = LibraryType.find_all_by_name(SetupLibraryTypes.existing_associations_for(request_type))
+
+  next if library_types.empty?
+  library_types.each do |library_type|
+    LibraryTypesRequestType.create!(:request_type=>request_type,:library_type=>library_type,:is_default=>library_type.name == SetupLibraryTypes.existing_defaults_for(request_type))
+  end
+end
