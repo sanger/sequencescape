cff259e92f90919cbe429a6b078042b6ab316dd6
James Glover <james.glover@sanger.ac.uk>
2014-06-26 09:21:18 +0100

    Add basic tests for view validity

--- lib/views_schema.rb
+++ lib/views_schema.rb
@@ -1,15 +1,18 @@
 module ViewsSchema
 
   def self.each_view
+    all_views.each do |name|
+      definition = ActiveRecord::Base.connection.execute("SHOW CREATE TABLE #{name}").first["Create View"].gsub(/DEFINER=`[^`]*`@`[^`]*` /,'')
+      yield(name,definition)
+    end
+  end
+
+  def self.all_views
     ActiveRecord::Base.connection.execute(%Q{
       SELECT TABLE_NAME AS name
       FROM INFORMATION_SCHEMA.VIEWS
       WHERE TABLE_SCHEMA = '#{ActiveRecord::Base.connection.current_database}';}
-    ).each do |view|
-      name = view['name']
-      definition = ActiveRecord::Base.connection.execute("SHOW CREATE TABLE #{name}").first["Create View"].gsub(/DEFINER=`[^`]*`@`[^`]*` /,'')
-      yield(name,definition)
-    end
+    ).map {|v| v['name']}
   end
 
   def self.create_view(name,definition)
---
+++ test/unit/report_contracts_test.rb
@@ -0,0 +1,32 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ReportContractTest < ActiveSupport::TestCase
+
+  # Contracted views are those we've agreed to maintain
+  # We'll track all views in these tests, but this'll
+  # pick up any that get 'dropped' by accident
+  contracted_views = [
+    'view_aliquots',
+    'view_asset_links',
+    'view_lanes',
+    'view_library_tubes',
+    'view_plates',
+    'view_requests',
+    'view_requests_new',
+    'view_sample_study_reference_genome',
+    'view_sample_tubes',
+    'view_samples',
+    'view_started_requests',
+    'view_studies',
+    'view_tags',
+    'view_wells'
+  ]
+
+  ViewsSchema.all_views.concat(contracted_views).uniq.each do |view|
+    context "View #{view}" do
+      should "respond to Select * from" do
+        assert ActiveRecord::Base.connection.execute("SELECT * FROM #{view};")
+      end
+    end
+  end
+end
