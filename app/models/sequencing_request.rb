b15d6b7e8fc9c97331b56e28a031e32a8f3b5ccc
James Glover <james.glover@sanger.ac.uk>
2014-07-25 15:22:40 +0100

    Update X10 workflow

--- app/models/sequencing_request.rb
+++ app/models/sequencing_request.rb
@@ -46,4 +46,13 @@ class SequencingRequest < Request
   def self.delegate_validator
     SequencingRequest::RequestOptionsValidator
   end
+
+  def concentration
+    return "&nbsp" if request.lab_events_for_batch(batch).empty?
+    conc = request.lab_events_for_batch(batch).first.descriptor_value("Concentration")
+    return "#{conc}&#x3BC;l" if conc.present?
+    dna = request.lab_events_for_batch(batch).first.descriptor_value("DNA Volume")
+    rsb = request.lab_events_for_batch(batch).first.descriptor_value("RSB Volume")
+    "#{dna}&#x3BC;l DNA in #{rsb}}&#x3BC;l RSB"
+  end
 end
---
+++ db/migrate/20140725092946_update_x10_fields.rb
@@ -0,0 +1,143 @@
+class UpdateX10Fields < ActiveRecord::Migration
+  def self.up
+    switch_to(:new)
+  end
+
+  def self.down
+    switch_to(:old)
+  end
+
+  def self.switch_to(option)
+    ActiveRecord::Base.transaction do
+      each_workflow do |workflow|
+
+        workflow.tasks.each do |task|
+          next unless changes(task.name).present?
+
+          new_descriptors = changes(task.name)[option].map do |name,kind,sort|
+            {
+              :name => name,
+              :selection => {"1"=>""},
+              :task_id => task.id,
+              :kind => kind,
+              :required => true,
+              :sorter => sort
+            }
+          end
+
+          Descriptor.create!(new_descriptors)
+        end
+      end
+
+    end
+  end
+
+  def self.changes(step)
+    {
+      "Specify Dilution Volume"=>{
+        :new=>[
+          ["DNA Volume", "Text", 1],
+          ["RSB Volume", "Text", 2]
+        ],
+        :old=>[
+          ["Concentration", "Text", 1]
+        ]
+      },
+      # "Add Spiked in control"=>{
+      #   :new=>[],
+      #   :old=>[]
+      # },
+      "Cluster generation"=>{
+        :new=>[
+          ["Chip Barcode", "Text", 1],
+          ["Operator", "Text", 2],
+          ["CBOT", "Text", 3],
+          ["-20 Temp. Read 1 Cluster Kit Lot #", "Text", 4],
+          ["-20 Temp. Cluster Kit RGT #", "Text", 5],
+          ["Pipette Carousel", "Text", 6],
+          ["Comment", "Text", 7]
+        ],
+        :old=>[
+          ["Chip Barcode", "Text", 1],
+          ["Operator", "Text", 2],
+          ["Cluster Station", "Text", 3],
+          ["Room Temp. Read 1 Cluster Kit Lot #", "Text", 4],
+          ["-20 Temp. Read 1 Cluster Kit Lot #", "Text", 5],
+          ["Pipette Carousel", "Text", 6],
+          ["Comment", "Text", 7],
+          ["Waste Weight", "Text", 8]
+        ]
+      },
+      "Read 1 Lin/block/hyb/load"=>{
+        :new=>[
+          ["Chip Barcode", "Text", 1],
+          ["Operator", "Text", 2],
+          ["Pipette Carousel", "Text", 3],
+          ["-20 Seq kit lot #", "Text", 4],
+          ["-20 Seq kit RGT #", "Text", 5],
+          ["+4 Seq kit lot #", "Text", 6],
+          ["+4 Seq kit RGT #", "Text", 7],
+          ['Patterned incorporation mix (PIM)', 'Text',8],
+          ['Patterned SBS buffer 1 (PB1)','Text',9],
+          ['Patterned scan mix (PSM)','Text',10],
+          ['Patterned SBS buffer 2 (PB2)','Text',11],
+          ['Patterned cleavage mix (PCM)','Text',12],
+          ['iPCR batch #','Text',13],
+          ["Comments", "Text", 14]
+        ],
+        :old=>[
+          ["Chip Barcode", "Text", 1],
+          ["Operator", "Text", 2],
+          ["Cluster Station", "Text", 3],
+          ["Pipette Carousel", "Text", 4],
+          ["Scan Mix", "Text", 5],
+          ["Long Read FFN Mix", "Text", 6],
+          ["RDP 36", "Text", 7],
+          ["Incorporation Mix", "Text", 8],
+          ["Cleavage Mix", "Text", 9],
+          ["High Salt Buffer", "Text", 10],
+          ["Incorporation Buffer", "Text", 11],
+          ["Cleavage Buffer", "Text", 12],
+          ["Comments", "Text", 13]
+        ]
+      },
+      "Read 2 Cluster/Lin/block/hyb/load"=>{
+        :new=>[
+          ["Operator", "Text", 1],
+          ["Sequencing Machine", "Text", 2],
+          ["-20 Temp. Read 2 Cluster Kit Lot  #", "Text", 3],
+          ["-20 Temp. Read 2 kit RGT #", "Text", 4],
+          ['Patterned resynthesis mix (PRM)','Text',5],
+          ['Patterned linearization mix (PLM2)','Text',6],
+          ['Patterned amplification mix (PAM)','Text',7],
+          ['Patterned amp premix (PPM)','Text',8],
+          ['Patterned denaturation mix (PDR)','Text',9],
+          ['Primer mix (HP11)','Text',10],
+          ['Primer mix (HP12)','Text',11],
+          ["Comments", "Text", 12]
+        ],
+        :old=>[
+          ["Operator", "Text", 1],
+          ["Sequencing Machine", "Text", 2],
+          ["Pipette Carousel", "Text", 3],
+          ["-20 Temp. Read 2 Cluster Kit Lot  #", "Text", 4],
+          ["Scan Mix", "Text", 5],
+          ["Long Read FFN Mix", "Text", 6],
+          ["RDP 36", "Text", 7],
+          ["Incorporation Mix", "Text", 8],
+          ["Cleavage Mix", "Text", 9],
+          ["High Salt Buffer", "Text", 10],
+          ["Incorporation Buffer", "Text", 11],
+          ["Cleavage Buffer", "Text", 12],
+          ["Comments", "Text", 13]
+        ]
+      }
+    }[step]
+  end
+
+  def self.each_workflow
+    ["HiSeq X Ten PE (spiked in controls)","HiSeq X Ten PE (no controls)"].each do |name|
+      yield LabInterface::Workflow.find_by_name!(name)
+    end
+  end
+end
