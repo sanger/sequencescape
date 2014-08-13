0c0cb37793ed365d4dab6bf3a1ccfe22bfd4fa08
James <james.glover@sanger.ac.uk>
2014-08-13 17:07:50 +0100

    Update 3000_library_types_setup.rb
    Remove xten library type refernce

--- db/seeds/3000_library_types_setup.rb
+++ db/seeds/3000_library_types_setup.rb
@@ -81,11 +81,3 @@ end
   RequestType::Validator.create!(:request_type => rt, :request_option=> "read_length", :valid_options=>[125,75])
 end
 
-
-['a', 'b'].each do |pipeline|
-  rt = RequestType.find_by_key!("illumina_#{pipeline}_hiseq_xten_paired_end_sequencing")
-  RequestType::Validator.create!(:request_type => rt, :request_option=> "read_length", :valid_options=>[150])
-  rt.library_types << LibraryType.find_by_name('Standard')
-  RequestType::Validator.create!(:request_type=>rt, :request_option=> "library_type", :valid_options=>RequestType::Validator::LibraryTypeValidator.new(rt.id))
-end
-
