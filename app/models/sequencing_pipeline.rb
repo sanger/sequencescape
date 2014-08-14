4d92a8a754ddf8592f37ed749bc753de1d61cf62
James Glover <james.glover@sanger.ac.uk>
2014-08-14 13:57:01 +0100

    Associate metadata with new request immediately

--- app/models/sequencing_pipeline.rb
+++ app/models/sequencing_pipeline.rb
@@ -8,25 +8,25 @@ class SequencingPipeline < Pipeline
   end
 
   def is_read_length_consistent_for_batch?(batch)
-    
+
     if (batch.requests.size == 0) || (batch.requests.first.request_metadata.nil?)
-      # No requests selected or the pipeline doesn't contain metadata to check       
+      # No requests selected or the pipeline doesn't contain metadata to check
       return true
     end
-    
+
     read_length_list = batch.requests.map { |request|
       request.request_metadata.read_length
     }.compact
-    
+
     # The pipeline doen't contain the read_length attribute
     return true if read_length_list.size == 0
-    
+
     # There are some requests that don't have the read_length_attribute
     return false if read_length_list.size != batch.requests.size
-    
+
     return (read_length_list.uniq.size == 1)
-  end  
-  
+  end
+
   # The guys in sequencing want to be able to re-run a request in another batch.  What we've agreed is that
   # the request will be failed and then an identical request will be resubmitted to their inbox.  The
   # "failed" request should not be charged for.
@@ -34,10 +34,13 @@ class SequencingPipeline < Pipeline
     request.fail!
 
     # Note that the request metadata also needs to be cloned for this to work.
-    request.clone.tap do |request_clone|
-      request_clone.update_attributes!(:state => 'pending', :target_asset_id => nil, :request_metadata => request.request_metadata.clone)
-      request_clone.comments.create!(:description => "Automatically created clone of request #{request.id} which was removed from Batch #{batch.id} at #{DateTime.now()}")
-      request.comments.create!(:description => "The request #{request_clone.id} is an automatically created clone of this one")
+    ActiveRecord::Base.transaction do
+      request.clone.tap do |request_clone|
+        rma = request.request_metadata.attributes.merge(:request=>request_clone)
+        request_clone.update_attributes!(:state => 'pending', :target_asset_id => nil,:request_metadata_attributes => rma)
+        request_clone.comments.create!(:description => "Automatically created clone of request #{request.id} which was removed from Batch #{batch.id} at #{DateTime.now()}")
+        request.comments.create!(:description => "The request #{request_clone.id} is an automatically created clone of this one")
+      end
     end
   end
 end
