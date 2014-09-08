3cf9bbc391ee0014f3268312d15d32b1ceb670ef
James Glover <james.glover@sanger.ac.uk>
2014-09-08 15:17:56 +0100

    Avoid read only plates in migration

--- db/migrate/20140826140733_move_illumina_a_and_b_labware_to_new_freezer_location.rb
+++ db/migrate/20140826140733_move_illumina_a_and_b_labware_to_new_freezer_location.rb
@@ -1,5 +1,5 @@
 class MoveIlluminaAAndBLabwareToNewFreezerLocation < ActiveRecord::Migration
-  
+
   def self.plate_purposes
     names = IlluminaB::PlatePurposes::PLATE_PURPOSE_FLOWS.flatten.concat(IlluminaHtp::PlatePurposes::PLATE_PURPOSE_FLOWS.flatten)
     PlatePurpose.find_all_by_name(names)
@@ -12,27 +12,26 @@ class MoveIlluminaAAndBLabwareToNewFreezerLocation < ActiveRecord::Migration
   def self.freezer
     freezer_name = 'Illumina high throughput freezer'
     Location.find_by_name(freezer_name) or raise ActiveRecord::RecordNotFound, freezer_name
-  end    
+  end
 
   def self.up
     ActiveRecord::Base.transaction do
       illumina_freezer = self.freezer
-      ['Library creation freezer', 'Pulldown freezer'].map do |freezer_name| 
+      ['Library creation freezer', 'Pulldown freezer'].map do |freezer_name|
         Location.find_by_name(freezer_name)
       end.each do |location|
         changed = 0
-        stock_changed = 0
-        plates = Plate.include_plate_metadata.include_plate_purpose.with_plate_purpose(self.plate_purposes).with_no_outgoing_transfers.located_in(location)
-        plates.each do |plate|
+
+        plate_ids = Plate.with_plate_purpose(self.plate_purposes).
+                          with_no_outgoing_transfers.
+                          located_in(location).
+                          find(:all,:select=>'assets.id').map(&:id)
+        say "Found #{plate_ids.length} plate ids."
+        Plate.find_each(:conditions=>{:id=>plate_ids}) do |plate|
           plate.update_attributes(:location => illumina_freezer)
           changed += 1
         end
-        stock_plates = Plate.include_plate_metadata.include_plate_purpose.with_plate_purpose(self.stock_plate_purposes).with_no_outgoing_transfers.located_in(location)
-        stock_plates.each do |plate|
-          plate.update_attributes(:location => illumina_freezer)
-          stock_changed += 1
-        end
-        puts "Moved #{changed} plates and #{stock_changed} stock plates from #{location.name}"
+        say "Moved #{changed} plates from #{location.name}"
       end
     end
   end
