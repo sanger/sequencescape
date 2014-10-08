9143aa4121bba4b7c9219943422e0e2093d03b76
James Glover <james.glover@sanger.ac.uk>
2014-10-08 16:22:13 +0100

    Expose quantity in study report

--- app/models/study_report/well_details.rb
+++ app/models/study_report/well_details.rb
@@ -46,7 +46,8 @@ module StudyReport::WellDetails
       :pico_date => self.pico_date,
       :qc_started_date => self.plate.qc_started_date,
       :sequenom_stamp_date => self.plate.sequenom_stamp_date,
-      :quantity => self.well_attribute.quantity_in_micro_grams.try(:round,3)
+      :quantity => self.well_attribute.quantity_in_micro_grams.try(:round,3),
+      :initial_volume => self.well_attribute.initial_volume
     })
     qc_data[:genotyping_status] = self.genotyping_status
     qc_data[:genotyping_barcode] = self.primary_aliquot.sample.genotyping_snp_plate_id if primary_aliquot.present?
