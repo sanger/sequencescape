fc7fe85de0a36c592b5e47b44bee5b7d59983a14
James Glover <james.glover@sanger.ac.uk>
2014-11-05 16:27:04 +0000

    Rename Xten in seeds to X instead

--- db/seeds/2110_x10_submission_templates.rb
+++ db/seeds/2110_x10_submission_templates.rb
@@ -1,27 +1,14 @@
   outlines=
     [
-      {:pipeline=>'Illumina-A', :name => 'Pulldown WGS',    :infos=>'wgs',  :request_types=>['illumina_a_pulldown_wgs']},
-      {:pipeline=>'Illumina-B', :name => 'Pooled HWGS',     :infos=>'full', :request_types=>['illumina_b_shared', 'illumina_b_pool'],   :role=>'ILB HWGS' },
-      {:pipeline=>'Illumina-B', :name => 'Pippin HWGS',     :infos=>'full', :request_types=>['illumina_b_shared', 'illumina_b_pippin'], :role=>'ILB HWGS' }
+      {:pipeline=>'Illumina-C', :name => 'General PCR',     :infos=>'wgs', :request_types=>['illumina_c_pcr'], :role=>'HSqX' }
     ].map do |outline|
-      
-      def infos(type)
-        Hiseq2500Helper.input_fields(['150'],{
-          'wgs'  => ["Standard"],
-          'isc'  => ["Agilent Pulldown"],
-          'full' => ["NlaIII gene expression","Standard","Long range","Small RNA","DpnII gene expression","qPCR only",
-                    "High complexity and double size selected","Illumina cDNA protocol","Custom","High complexity",
-                    "Double size selected","No PCR","Agilent Pulldown","ChiP-seq","Pre-quality controlled"]
-          }[type])
-      end
-    
+
       def seq_x10_for(pipeline)
-        hash ||= Hash.new {|h,i| h[i]= [RequestType.find_by_key("#{i.underscore}_hiseq_xten_paired_end_sequencing").id]}
+        hash ||= Hash.new {|h,i| h[i]= [RequestType.find_by_key("illumina_b_hiseq_x_paired_end_sequencing").id]}
         hash[pipeline]
       end
-      
+
       paras = {
-          :input_field_infos => infos(outline[:infos]),
           :request_type_ids_list => outline[:request_types].map {|rt| [RequestType.find_by_key!(rt).id] } << seq_x10_for(outline[:pipeline]),
           :workflow_id => 1
         }
@@ -34,8 +21,8 @@
       }
       template
     end
-      
-  
+
+
   outlines.each do |template|
     SubmissionTemplate.create!(template)
   end
