e6cfb27ce83606942df84fc34149ec301510826b
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2014-11-14 16:57:21 +0000

    Creates only one tag as 888

--- db/migrate/20141113163130_update_hyb_buffer_tag_index888.rb
+++ db/migrate/20141113163130_update_hyb_buffer_tag_index888.rb
@@ -1,26 +1,24 @@
 class UpdateHybBufferTagIndex888 < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.transaction do
-      tag_group = TagGroup.create!(:name => "Control Tag Group - Sanger_168tags - 10 mer tags")
+      tag_group = TagGroup.create!(:name => "Control Tag Group 888")
       template = TagLayoutTemplate.create!(
         :direction_algorithm => "TagLayout::InColumns",
         :tag_group => tag_group,
-        :name => "Control Tag - Sanger_168tags - 10 mer tags",
+        :name => "Control Tag 888",
         :walking_algorithm => "TagLayout::WalkWellsByPools")
-      copied_tag_group = TagGroup.find_by_name("Sanger_168tags - 10 mer tags")
-      copied_tag_group.tags.each do |tag|
-        tag_group.tags.create!(:map_id => tag.map_id + 887, :oligo => tag.oligo)
-      end
+      copied_tag = TagGroup.find_by_name("Sanger_168tags - 10 mer tags").tags.select {|t| t.map_id==1}.first
+      tag_group.tags.create!(:map_id => 888, :oligo => copied_tag.oligo)
     end
   end
 
   def self.down
     ActiveRecord::Base.transaction do
-      t = TagGroup.find_by_name("Control Tag Group - Sanger_168tags - 10 mer tags")
+      t = TagGroup.find_by_name("Control Tag Group 888")
       t.tags.each do |tag|
         tag.destroy
       end
-      TagLayoutTemplate.find_by_name("Control Tag - Sanger_168tags - 10 mer tags").destroy
+      TagLayoutTemplate.find_by_name("Control Tag 888").destroy
       t.destroy
     end
   end
