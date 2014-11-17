a90efc2e8cd61b8722516795ef27ab6303007ae2
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2014-11-17 11:54:57 +0000

    Parametrized configuration for hyb_buffer_tag info

--- db/migrate/20141113163130_update_hyb_buffer_tag_index888.rb
+++ db/migrate/20141113163130_update_hyb_buffer_tag_index888.rb
@@ -1,24 +1,21 @@
 class UpdateHybBufferTagIndex888 < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.transaction do
-      tag_group = TagGroup.create!(:name => "Control Tag Group 888")
-      template = TagLayoutTemplate.create!(
-        :direction_algorithm => "TagLayout::InColumns",
-        :tag_group => tag_group,
-        :name => "Control Tag 888",
-        :walking_algorithm => "TagLayout::WalkWellsByPools")
-      copied_tag = TagGroup.find_by_name("Sanger_168tags - 10 mer tags").tags.select {|t| t.map_id==1}.first
-      tag_group.tags.create!(:map_id => 888, :oligo => copied_tag.oligo)
+      configatron.phyx_tag.tag_group_name
+      tag_group = TagGroup.create!(:name => configatron.phyx_tag.tag_group_name)
+      copied_tag = TagGroup.find_by_name("Sanger_168tags - 10 mer tags").tags.select do |t|
+        t.map_id==168
+      end.first
+      tag_group.tags.create!(:map_id => configatron.phyx_tag.tag_map_id, :oligo => copied_tag.oligo)
     end
   end
 
   def self.down
     ActiveRecord::Base.transaction do
-      t = TagGroup.find_by_name("Control Tag Group 888")
+      t = TagGroup.find_by_name(configatron.phyx_tag.tag_group_name)
       t.tags.each do |tag|
         tag.destroy
       end
-      TagLayoutTemplate.find_by_name("Control Tag 888").destroy
       t.destroy
     end
   end
