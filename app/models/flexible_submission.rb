80747b62951fd213a8cedc24aad97d3d59e1af69
James Glover <james.glover@sanger.ac.uk>
2015-01-09 13:19:07 +0000

    [#84300234] Extensively refactor request graph building
    I always found the previous builder a confusing mess of recursive method
    calls, messy variable assignment, long methods and mutating arrays.
    This attempts to refactor things to use builder objects. It results
    in more code, but I'm hoping it is easier to follow.

--- app/models/flexible_submission.rb
+++ app/models/flexible_submission.rb
@@ -1,8 +1,9 @@
+require 'submission/flexible_request_graph'
 ##
 # FlexibleSubmissions allow multiplexing based on
 # pooling properties defined on the multiplexed request type
 class FlexibleSubmission < Order
-  include Submission::FlexibleRequestGraph
+  include Submission::FlexibleRequestGraph::OrderMethods
 
   def request_type_ids=(id_list)
     self.request_type_ids_list = id_list.map {|i| [i] }
--- app/models/submission/flexible_request_graph.rb
+++ app/models/submission/flexible_request_graph.rb
@@ -1,115 +1,214 @@
-
 module Submission::FlexibleRequestGraph
 
-  def build_request_graph!(multiplexing_assets = nil, &block)
-    ActiveRecord::Base.transaction do
-      create_request_chain!(
-        build_request_type_multiplier_pairs,
-        assets.map { |asset| [ asset, :old_item ] },
-        multiplexing_assets,
-        &block
-      )
+  class RequestChainError < RuntimeError; end
+
+  class RequestChain
+
+    attr_reader :order, :source_assets, :preplexed, :built, :multiplexed
+    alias_method :built?, :built
+    alias_method :multiplexed?, :multiplexed
+    alias_method :preplexed?, :preplexed
+
+    def initialize(order,source_assets, multiplexing_assets)
+      @order = order
+      @source_assets = source_assets
+      @multiplexing_assets = multiplexing_assets
+      @preplexed = multiplexing_assets.present?
+      @built = false
+      @multiplexed = false
     end
+
+    def build!
+      raise RequestChainError, "Request chains can only be built once" if built?
+      raise StandardError, 'No request types specified!' if request_types.empty?
+      request_types.inject(source_assets) do |source_assets,request_type|
+        link = ChainLink.build!(request_type,multiplier_for(request_type),source_assets,self)
+        break if preplexed && link.multiplexed?
+        link.target_assets
+      end
+      @built = true
+    end
+
+    ##
+    # multiplexing_assets returns @multiplexing_assets if present
+    # otherwise it yields to any presented block and assumes it returns
+    # the multiplexing_assets
+    def multiplexing_assets
+      @multiplexed = true
+      @multiplexing_assets ||= yield if block_given?
+    end
+
+    private
+
+    def request_types
+      order.request_types.map {|request_type_id| RequestType.find(request_type_id) }
+    end
+
+    def multiplier_for(request_type)
+      multipliers[request_type.id.to_s]
+    end
+
+    def multipliers
+      @multipliers ||= Hash.new { |h,k| h[k] = 1 }.tap do |multipliers|
+        requested_multipliers = order.request_options.try(:[], :multiplier) || {}
+        requested_multipliers.each { |k,v| multipliers[k.to_s] = v.to_i }
+      end
+    end
+
   end
 
-  # Generates a list of RequestType and multiplier pairs for the instance.
-  def build_request_type_multiplier_pairs
-    # Ensure that the keys of the multipliers hash are strings, otherwise we get weirdness!
-    multipliers = Hash.new { |h,k| h[k] = 1 }.tap do |multipliers|
-      requested_multipliers = request_options.try(:[], :multiplier) || {}
-      requested_multipliers.each { |k,v| multipliers[k.to_s] = v.to_i }
+  ##
+  # Builds all requests of a given request type and any target_assets
+  # The build! method automatically creates a link of the appropriate class
+  module ChainLink
+
+    def self.included(base)
+      base.class_eval do
+        attr_reader :request_type, :multiplier, :source_assets, :target_assets, :chain
+      end
     end
 
-    request_types.dup.map do |request_type_id|
-      [ RequestType.find(request_type_id), multipliers[request_type_id.to_s] ]
+    def self.build!(request_type,multiplier,assets,chain)
+      link_class = request_type.for_multiplexing? ? MultiplexedLink : UnplexedLink
+      link_class.new(request_type,multiplier,assets,chain).tap do |link|
+        link.build!
+        link.target_assets
+      end
     end
-  end
-  private :build_request_type_multiplier_pairs
 
-
-  def create_target_asset_for!(request_type, source_asset = nil)
-    request_type.create_target_asset! do |asset|
-      asset.generate_barcode
-      asset.generate_name(source_asset.try(:name) || asset.barcode.to_s)
+    def initialize(request_type,multiplier,source_assets,chain)
+      @request_type   = request_type
+      @multiplier     = multiplier
+      @source_assets  = source_assets
+      @chain          = chain
     end
-  end
-  private :create_target_asset_for!
 
-  # Creates the next step in the request graph, taking the first request type specified and building
-  # enough requests for the source requests.  It will recursively call itself if there are more requests
-  # that need creating.
-  def create_request_chain!(request_type_and_multiplier_pairs, source_asset_item_pairs, multiplexing_assets, &block)
-    raise StandardError, 'No request types specified!' if request_type_and_multiplier_pairs.empty?
-    request_type, multiplier = request_type_and_multiplier_pairs.shift
-    t = Time.now
-    multiplier.times do |_|
-      # If the request type is for multiplexing it means that all of the assets end up in one target asset.
-      # Otherwise there are the same number of target assets as source.
-      target_assets =
-        if request_type.for_multiplexing?
-          multiplexing_assets || request_type.pool_count.times.map { create_target_asset_for!(request_type) }
-        else
-          source_asset_item_pairs.map { |source_asset, _| create_target_asset_for!(request_type, source_asset) }
-        end
+    def multiplexed?; false; end
 
-      yield(target_assets) if block_given? and request_type.for_multiplexing?
+    def build!
+      multiplier.times do |_|
+        # Now we can iterate over the source assets and target assets building the requests between them.
+        # Ensure that the request has the correct comments on it, and that the aliquots of the source asset
+        # are transferred into the destination if the request does not do this in some manner itself.
+        source_asset_target_assets do |source_asset, target_asset|
 
+          chain.order.create_request_of_type!(
+            request_type,
+            :asset => source_asset, :target_asset => target_asset
+          ).tap do |request|
 
-      # Now we can iterate over the source assets and target assets building the requests between them.
-      # Ensure that the request has the correct comments on it, and that the aliquots of the source asset
-      # are transferred into the destination if the request does not do this in some manner itself.
-      source_asset_item_pairs.each_with_index do |(source_asset, item), index|
-        eip = Time.now
-        index = request_type.pool_index_for(source_asset) if request_type.for_multiplexing?
-        target_asset = target_assets[index]
+            AssetLink.create_edge!(source_asset, target_asset) if source_asset.present? and target_asset.present?
 
-        create_request_of_type!(
-          request_type,
-          :asset => source_asset, :target_asset => target_asset
-        ).tap do |request|
-          # TODO: AssetLink is supposed to disappear at some point in the future because it makes no real sense
-          # given that the request graph describes this relationship.
-          AssetLink.create_edge!(source_asset, target_asset) if source_asset.present? and target_asset.present?
-
-          comments.each do |comment|
-            request.comments.create!(:user => user, :description => comment)
-          end if comments.present?
+            comments.each do |comment|
+              request.comments.create!(:user => user, :description => comment)
+            end if comments.present?
+          end
         end
       end
+      associate_built_requests!
+    end
 
-      # Now we can continue to the next request type in the chain, using the target assets we've created.
-      # We need to de-duplicate the multiplexed assets.  Note that we duplicate the pairs here so that
-      # they don't get disrupted by the shift operation at the start of this method.
-      next if request_type_and_multiplier_pairs.empty?
+    private
 
-      target_assets_items =  if request_type.for_multiplexing?   # May have many nil assets for non-multiplexing
-        if multiplexing_assets.nil?
-          target_assets.map { |asset| [ asset, nil ] }
-        else
-          associate_built_requests(target_assets.compact); []
-        end
-      else
-        target_assets.each_with_index.map do |asset,index|
-          source_asset = request_type.no_target_asset? ? source_asset_item_pairs[index].first : asset
-          [ source_asset, source_asset_item_pairs[index].last ]
+    def comments
+      chain.order.comments
+    end
+
+    def source_asset_target_assets
+      new_target_assets = generate_target_assets
+      source_assets_with_index do |source_asset,index|
+        yield(source_asset,new_target_assets[index])
+      end
+    end
+
+    def associate_built_requests!
+      #Do Nothing
+    end
+
+    def create_target_asset(source_asset = nil)
+      request_type.create_target_asset! do |asset|
+        asset.generate_barcode
+        asset.generate_name(source_asset.try(:name) || asset.barcode.to_s)
+      end
+    end
+  end
+
+  class MultiplexedLink
+    include ChainLink
+
+    def initialize(request_type,multiplier,assets,chain)
+      raise RequestChainError unless request_type.for_multiplexing?
+      raise RequestChainError, 'Cannot multiply multiplexed requests' if multiplier > 1
+      super
+    end
+
+    def multiplexed?; true; end
+
+    private
+
+    def source_assets_with_index
+      source_assets.each do |asset|
+        yield(asset,request_type.pool_index_for(asset))
+      end
+    end
+
+    # We can only do this once for multiplexed request types
+    def generate_target_assets
+      @target_assets ||= chain.multiplexing_assets do
+        # We yield only if we don't have any multiplexing assets
+        request_type.pool_count.times.map { create_target_asset }
+      end
+    end
+
+    def associate_built_requests!
+      downstream_requests.each do |request|
+        request.update_attributes!(:initial_study => nil) if request.initial_study != study
+        request.update_attributes!(:initial_project => nil) if request.initial_project != project
+        comments.each do |comment|
+          request.comments.create!(:user => user, :description => comment)
+        end if comments.present?
+      end
+    end
+
+    def downstream_requests
+      target_assets.uniq.compact.map(&:requests).flatten
+    end
+
+  end
+
+  class UnplexedLink
+    include ChainLink
+
+    def initialize(request_type,multiplier,assets,chain)
+      raise RequestChainError if request_type.for_multiplexing?
+      super
+    end
+
+    def generate_target_assets
+      source_assets.map do |source_asset|
+        create_target_asset(source_asset)
+      end.tap do |new_target_assets|
+        @target_assets ||= []
+        @target_assets.concat(new_target_assets)
+      end
+    end
+
+    def source_assets_with_index(&block)
+      source_assets.each_with_index(&block)
+    end
+  end
+
+  module OrderMethods
+
+    def build_request_graph!(multiplexing_assets = nil, &block)
+      ActiveRecord::Base.transaction do
+        RequestChain.new(self,assets, multiplexing_assets).tap do |chain|
+          chain.build!
+          yield chain.multiplexing_assets if chain.multiplexed?
         end
       end
-
-      create_request_chain!(request_type_and_multiplier_pairs.dup, target_assets_items, multiplexing_assets, &block)
     end
-  end
-  private :create_request_chain!
 
-  def associate_built_requests(assets)
-    assets.map(&:requests).flatten.each do |request|
-      request.update_attributes!(:initial_study => nil) if request.initial_study != study
-      request.update_attributes!(:initial_project => nil) if request.initial_project != project
-      comments.each do |comment|
-        request.comments.create!(:user => user, :description => comment)
-      end if comments.present?
-    end
   end
-  private :associate_built_requests
-
 
 end
