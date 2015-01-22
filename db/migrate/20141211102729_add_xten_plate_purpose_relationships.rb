ebdeebf7e74fc84033f7d105dbffd958fee683d9
James Glover <james.glover@sanger.ac.uk>
2015-01-22 12:34:57 +0000

    Lib Norm plates can be QC completed

--- db/migrate/20141211102729_add_xten_plate_purpose_relationships.rb
+++ db/migrate/20141211102729_add_xten_plate_purpose_relationships.rb
@@ -1,26 +1,15 @@
 class AddXtenPlatePurposeRelationships < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.transaction do
-      PlatePurpose::Relationship.create!(
-        :parent => Purpose.find_by_name!("Lib Norm"),
-        :child => Purpose.find_by_name!("Lib Norm 2"),
-        :transfer_request_type => RequestType.transfer
-        )
-      PlatePurpose::Relationship.create!(
-        :parent => Purpose.find_by_name!("Lib Norm 2"),
-        :child => Purpose.find_by_name!("Lib Norm 2 Pool"),
-        :transfer_request_type => RequestType.transfer
-        )
-      PlatePurpose::Relationship.create!(
-        :parent => Purpose.find_by_name!("Lib PCR-XP"),
-        :child => Purpose.find_by_name!("Lib Norm"),
-        :transfer_request_type => RequestType.transfer
-        )
-
+      IlluminaHtp::PlatePurposes.create_branch(["Lib PCR-XP","Lib Norm","Lib Norm 2","Lib Norm 2 Pool"])
     end
-
   end
 
   def self.down
+    ActiveRecord::Base.transaction do
+      ["Lib Norm","Lib Norm 2","Lib Norm 2 Pool"].inject("Lib PCR-XP") do |parent,child|
+        Purpose.find_by_name(parent).plate_purpose_relationships.find_by_child_id(Purpose.find_by_name(child).id).destroy
+      end
+    end
   end
 end
