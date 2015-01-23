749259c810d2d8a8295883cdee0fbb6b3a1870e8
James Glover <james.glover@sanger.ac.uk>
2015-01-23 11:58:04 +0000

    Pipelines should be SequencingPipelines
    Building across the relationship on workflow
    created a Pipeline class pipeline only

--- db/migrate/20141218134634_xten_strip_tube_pipeline.rb
+++ db/migrate/20141218134634_xten_strip_tube_pipeline.rb
@@ -1,12 +1,23 @@
 class XtenStripTubePipeline < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.transaction do
-      each_pipeline do |pipeline|
-        pipeline.workflow.deep_copy(' Striptube').tap do |workflow|
-          workflow.pipeline.request_types = [RequestType.find_by_key('hiseq_x_paired_end_sequencing')]
-          workflow.pipeline.group_by_parent = true
-          workflow.pipeline.save!
-        end
+      SequencingPipeline.create!(
+        :name => "HiSeq X PE (spiked in controls) from strip-tubes",
+        :automated => false,
+        :active => true,
+        :location => Location.find_by_name("Cluster formation freezer"),
+        :group_by_parent => true,
+        :asset_type => "Lane",
+        :sorter => 9,
+        :paginate => false,
+        :max_size => 8,
+        :min_size => 1,
+        :summary => true,
+        :group_name => "Sequencing",
+        :control_request_type_id => 0
+      ) do |pipeline|
+        pipeline.workflow = LabInterface::Workflow.find_by_name!('HiSeq X PE (spiked in controls)').deep_copy(' from strip-tubes', true)
+        pipeline.request_types = [RequestType.find_by_key('hiseq_x_paired_end_sequencing')]
       end
     end
   end
@@ -14,16 +25,12 @@ class XtenStripTubePipeline < ActiveRecord::Migration
   def self.down
     ActiveRecord::Base.transaction do
       each_pipeline do |pipeline|
-        Pipeline.find_by_name!("#{pipeline} Striptube").tap do |pl|
+        Pipeline.find_by_name!("#{pipeline} from strip-tubes").tap do |pl|
           pl.workflow.destroy
         end.destroy
       end
     end
   end
 
-  def self.each_pipeline
-    ['HiSeq X PE (spiked in controls)'].each do |name|
-      yield Pipeline.find_by_name!(name)
-    end
-  end
 end
+
