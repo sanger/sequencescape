a60dd998333dd1ca7dfd28b9e9ac490007bcf5e9
James Glover <james.glover@sanger.ac.uk>
2015-02-05 15:23:23 +0000

    Add copyright application script

---
+++ script/license_application.rb
@@ -0,0 +1,219 @@
+#!/usr/bin/env ruby
+# This file is part of SEQUENCESCAPE; License information can be found in
+# the file LICENSE in the root of this repository
+# Copyright (C) 2015 Genome Research Ltd.
+module WTSI
+  ##
+  # Controls the addition of license files to an application
+  # WTSI::LicenseApplication.new yields itself for configuration
+  # It provides the following attribute writers
+  # application: The name of the application
+  # initial_range: A range covering years outside the git repo
+  # date_line: The line that will include date information. %s indicates where dates will be inserted
+  # license_text: Sting that will be inserted at the top of each file. %s will be replaced with the application name
+  class LicenseApplication
+
+    attr_accessor :application, :initial_range, :date_line
+    attr_writer :license_text
+
+    ##
+    # comment_prefix will be applied to the start of each line
+    # comment_open and comment_close will wrap the start and end lines
+    Filetype = Struct.new(:extension,:comment_prefix,:comment_open,:comment_close)
+
+    def initialize
+      yield self
+      @license_text_compiled = @license_text%application
+    end
+
+    ##
+    # Add a filetype to be tagged with copyright information
+    # extension: The file extension
+    # prefix: The string that will be prepended to a comment (eg. # for ruby)
+    # open: The string that opens a multi-line comment (OPTIONAL)
+    # close: The string that closes a multi-line comment (OPTIONAL)
+    def add_filetype(extension,prefix,open=nil,close=nil)
+      filetypes[extension] = Filetype.new(extension,prefix,open,close)
+    end
+
+    ##
+    # Provide a root folder to exclude from the process.
+    # Useful for excluding eg. the vendor folder in older rails apps.
+    def exclude_folder(folder)
+      excluded_folders << folder
+    end
+
+    ##
+    # Indicates that files in the root directory should be excluded
+    def exclude_root!
+      @exclude_root = true
+    end
+
+    ##
+    # Begin the license application process
+    def apply_licenses
+      files_to_license.each do |filename|
+        TargetFile.new(filename,self).apply_license
+      end
+      return 0
+    end
+
+    ##
+    # Retrieve the filetype configuration for extension
+    def filetype_for(ext)
+      filetypes.fetch(ext)
+    end
+
+    ##
+    # Retrieve the license_text with the application included
+    def license_text
+      @license_text_compiled
+    end
+
+    ##
+    # The initial date range in string format
+    def range_string
+      @rs||="#{initial_range.begin}-#{initial_range.end}"
+    end
+
+    private
+
+    def filetypes
+      @filetype ||= Hash.new
+    end
+
+    def excluded_folders
+      @excluded_folders ||= Array.new
+    end
+
+    def excluded_root
+      @exclude_root || false
+    end
+
+    def extension_string
+      filetypes.keys.map {|ext| "*.#{ext}"}.join(' ')
+    end
+
+    def files_to_license
+      `git ls-files #{extension_string}`.split.reject {|file| excluded?(file) }
+    end
+
+    def excluded?(file_path)
+      path = file_path.split('/')
+      return excluded_root if path.one?
+      excluded_folders.include?(path.first)
+    end
+
+  end
+
+  class TargetFile
+
+    attr_reader :filename, :licenser, :filetype, :old_file
+
+    def initialize(filename,licenser)
+      @filename = filename
+      @licenser = licenser
+      @filetype = licenser.filetype_for(extension)
+    end
+
+    def apply_license
+
+      begin
+        return if existing_license?
+        STDOUT.print "."
+        new_file.write(license_text)
+        old_file.each_line {|line| new_file.puts(line) }
+      rescue => exception
+        STDERR.puts "Something went wrong applying license to #{filename}:"
+        raise exception
+      ensure
+        old_file.close unless old_file.closed?
+        new_file.close unless @new_file.nil? || new_file.closed?
+      end
+
+      File.delete(filename)
+      File.rename("#{filename}.updated",filename)
+    end
+
+    private
+
+    def old_file
+      @old_file ||= File.open(filename,'r')
+    end
+
+    def new_file
+      @new_file ||= File.new(new_filename,'w')
+    end
+
+    def new_filename
+      "#{filename}.updated"
+    end
+
+    def existing_license?
+      old_file.rewind
+      [
+        filetype.comment_open,
+        license_body
+      ].to_s.lines.all? do |line|
+        line == old_file.gets
+      end
+    ensure
+      old_file.rewind
+    end
+
+    def license_text
+      [
+        filetype.comment_open,
+        license_body,
+        license_dates,
+        filetype.comment_close
+      ].compact.to_s
+    end
+
+    def license_body
+      licenser.license_text.gsub(/^/,filetype.comment_prefix)
+    end
+
+    def date_stamps
+      [licenser.range_string].concat(updated_dates).join(',')
+    end
+
+    def license_dates
+      [
+        filetype.comment_prefix,
+        licenser.date_line%date_stamps
+      ]
+    end
+
+    def updated_dates
+      `git log --date=short --format=format:"%ad" -- follow -- #{filename} |
+      awk -F"-" '{print $1}' |
+      uniq |
+      sort`.split
+    end
+
+    def extension
+      filename.match(/\.([^\.]*)$/)[1]
+    end
+
+
+  end
+end
+WTSI::LicenseApplication.new do |config|
+
+  config.application = 'SEQUENCESCAPE'
+  config.license_text = <<HEREDOC
+This file is part of %s; License information can be found in
+the file LICENSE in the root of this repository
+HEREDOC
+  config.date_line = 'Copyright (C) %s Genome Research Ltd.'
+  config.initial_range = (2007..2010)
+
+  config.add_filetype('rb','#')
+  config.add_filetype('js','#')
+  config.add_filetype('js','#','<%','%>')
+
+  config.exclude_folder 'lib'
+  config.exclude_folder 'vendor'
+
+end.apply_licenses
