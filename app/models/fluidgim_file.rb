8247e46ea195eea157e024cdb5795a06f0beb53e
James Glover <james.glover@sanger.ac.uk>
2015-02-09 12:56:38 +0000

    Add support for pulling fluidigm content from directory
    Includes code refactoring to move behaviour into
    the main codebase. This makes it easier to
    diagnose and fix any fluidigm issues from the console.

--- app/models/fluidgim_file.rb
+++ app/models/fluidgim_file.rb
@@ -1,5 +1,51 @@
 class FluidigmFile
 
+  module Finder
+
+    class Directory
+      def initialize(barcode)
+        @barcode = barcode
+      end
+      def empty?
+        !File.exists?("#{configatron.fluidigm_data.directory}/#{@barcode}/#{@barcode}.csv")
+      end
+      def content
+        file_content = nil
+        File.open("#{configatron.fluidigm_data.directory}/#{@barcode}/#{@barcode}.csv") do |file|
+          file_content = file.read
+        end
+        file_content
+      end
+    end
+
+    class Irods
+      def initialize(barcode)
+        @data = IrodsReader::DataObj.find('seq','dcterms:audience'=>configatron.irods_audience, :fluidigm_plate=>barcode)
+      end
+
+      def empty?
+        @data.empty?
+      end
+
+      def content(index=nil)
+        raise StandardError, "Multiple files found" if data.size > 1 && index.nil?
+        @data[index||0].retrive
+      end
+    end
+
+    def self.default
+      {
+        'irods' => Irods,
+        'directory' => Directory
+      }.fetch(configatron.fluidigm_data.source)
+    end
+
+    def self.find(barcode)
+      default.new(barcode)
+    end
+
+  end
+
   class InvalidFile < StandardError; end
 
   class Assay
