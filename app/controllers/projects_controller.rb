25578433689cd2c442f0605cdb932f4ef607ffb4
James Glover <james.glover@sanger.ac.uk>
2015-02-10 09:37:31 +0000

    Apply copyright message to controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,6 @@
+#This file is part of SEQUENCESCAPE is distributed under the terms of GNU General Public License version 1 or later;
+#Please refer to the LICENSE and README files for information on licensing and authorship of this file.
+#Copyright (C) 2007-2011,2013 Genome Research Ltd.
 class ProjectsController < ApplicationController
   before_filter :login_required
   before_filter :set_variables_for_project, :only => [:show, :edit, :update, :destroy, :studies]
