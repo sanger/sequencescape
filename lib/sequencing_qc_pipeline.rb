f7c3e1b70ca30a68681dd02cd91a7eab4023d48b
James Glover <james.glover@sanger.ac.uk>
2015-02-10 11:37:56 +0000

    Apply copyright information to lib files

--- lib/sequencing_qc_pipeline.rb
+++ lib/sequencing_qc_pipeline.rb
@@ -1,3 +1,6 @@
+#This file is part of SEQUENCESCAPE is distributed under the terms of GNU General Public License version 1 or later;
+#Please refer to the LICENSE and README files for information on licensing and authorship of this file.
+#Copyright (C) 2007-2011 Genome Research Ltd.
 module SequencingQcPipeline
   # Returns Quality Control pipeline found using Regexp
   def qc_auto_pipeline_id
