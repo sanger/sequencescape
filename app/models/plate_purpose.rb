c6a0096b94729203a9d597be2cab0ff1b02fd95d
James Glover <james.glover@sanger.ac.uk>
2015-02-12 17:18:22 +0000

    Purposeless plates can be viewed/edited

--- app/models/plate_purpose.rb
+++ app/models/plate_purpose.rb
@@ -1,6 +1,6 @@
 #This file is part of SEQUENCESCAPE is distributed under the terms of GNU General Public License version 1 or later;
 #Please refer to the LICENSE and README files for information on licensing and authorship of this file.
-#Copyright (C) 2007-2011,2011,2012,2013,2014 Genome Research Ltd.
+#Copyright (C) 2007-2011,2011,2012,2013,2014,2015 Genome Research Ltd.
 class PlatePurpose < Purpose
   module Associations
     def self.included(base)
@@ -26,8 +26,8 @@ class PlatePurpose < Purpose
   end
 
   include Relationship::Associations
-  
-  named_scope :compatible_with_purpose, lambda {|purpose| { :conditions => ["(target_type is null and 'Plate'=?)  or target_type=?",purpose.target_plate_type, purpose.target_plate_type], :order=>"name ASC" } }  
+
+  named_scope :compatible_with_purpose, lambda {|purpose| purpose.nil? ? {:conditions=>'FALSE'} : { :conditions => ["(target_type is null and 'Plate'=?)  or target_type=?",purpose.target_plate_type, purpose.target_plate_type], :order=>"name ASC" } }
 
   named_scope :cherrypickable_as_target, :conditions => { :cherrypickable_target => true }
   named_scope :cherrypickable_as_source, :conditions => { :cherrypickable_source => true }
