aa48213023e67733212670fae0b297979ba8f83a
James Glover <james.glover@sanger.ac.uk>
2015-02-20 09:35:34 +0000

    Control lanes use custom lane_id

--- app/models/api/messages/flowcell_io.rb
+++ app/models/api/messages/flowcell_io.rb
@@ -41,6 +41,10 @@ class Api::Messages::FlowcellIO < Api::Base
           spiked_in_buffer.present? ? spiked_in_buffer.aliquots : []
         end
 
+        def lane_identifier
+          target_asset_id
+        end
+
       end
     end
   end
@@ -82,6 +86,10 @@ class Api::Messages::FlowcellIO < Api::Base
 
         def controls
           asset.aliquots
+        end
+
+        def lane_identifier
+          'control_lane'
         end
 
       end
@@ -155,7 +163,7 @@ class Api::Messages::FlowcellIO < Api::Base
     map_attribute_to_json_attribute(:priority)
     map_attribute_to_json_attribute(:mx_library,'id_pool_lims')
     map_attribute_to_json_attribute(:external_release,'external_release')
-    map_attribute_to_json_attribute(:target_asset_id, 'entity_id_lims')
+    map_attribute_to_json_attribute(:lane_identifier, 'entity_id_lims')
 
     with_nested_has_many_association(:samples) do # actually aliquots
 
@@ -204,7 +212,6 @@ class Api::Messages::FlowcellIO < Api::Base
       with_association(:study) do
         map_attribute_to_json_attribute(:uuid, 'study_uuid')
       end
-      map_attribute_to_json_attribute(:id, 'entity_id_lims')
       map_attribute_to_json_attribute(:library_id, 'legacy_library_id')
       map_attribute_to_json_attribute(:external_library_id, 'id_library_lims')
       map_attribute_to_json_attribute(:control_aliquot_type,'entity_type')
