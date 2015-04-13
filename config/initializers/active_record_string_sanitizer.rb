58b92c412b1d8cc639e1dc3e223a9d6853713ebc
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2015-04-13 16:54:32 +0100

    Uses the rails squish method instead of the custom implementation

--- config/initializers/active_record_string_sanitizer.rb
+++ config/initializers/active_record_string_sanitizer.rb
@@ -1,17 +1,13 @@
 module ActiveRecord::StringSanitizer
   def self.extended(base)
     base.instance_eval do
-      def squish(str)
-        str.gsub(/\s+/, " ")
-      end
-
       def squishify(*names)
         line = __LINE__ + 1
         class_eval(%Q{
           before_create do |record|
             names.each do |name|
               value = record.send(name)
-              record.send(name.to_s + '=', squish(value)) if value.is_a? String
+              record.send(name.to_s + '=', value.squish) if value.is_a? String
             end
           end
           }, __FILE__, line)
