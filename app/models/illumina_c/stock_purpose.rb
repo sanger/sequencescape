3286672c271411a7e41be6f45b95a72fb10a092e
James Glover <james.glover@sanger.ac.uk>
2015-05-05 15:36:19 +0100

    [housekeeping] Eager load requests as target for performance

--- app/models/illumina_c/stock_purpose.rb
+++ app/models/illumina_c/stock_purpose.rb
@@ -1,12 +1,12 @@
 #This file is part of SEQUENCESCAPE is distributed under the terms of GNU General Public License version 1 or later;
 #Please refer to the LICENSE and README files for information on licensing and authorship of this file.
-#Copyright (C) 2013 Genome Research Ltd.
+#Copyright (C) 2013,2015 Genome Research Ltd.
 class IlluminaC::StockPurpose < PlatePurpose
   include PlatePurpose::Stock
 
   def transition_to(plate, state, contents = nil,customer_accepts_responsibility=false)
     return unless ['failed','cancelled'].include?(state)
-    plate.wells.located_at(contents).each do |well|
+    plate.wells.include_requests_as_target.located_at(contents).each do |well|
       well.requests.each {|r| r.send(transition_from(r.state)) if r.is_a?(IlluminaC::Requests::LibraryRequest) }
       well.requests_as_target.each {|r| r.transition_to('failed') if r.is_a?(TransferRequest)}
     end
