dbeae3c36d6d560e4873c89148e652da080362bb
James Glover <james.glover@sanger.ac.uk>
2015-05-05 15:40:45 +0100

    [housekeeping] Eager load requests as source for performance

--- app/models/illumina_c/stock_purpose.rb
+++ app/models/illumina_c/stock_purpose.rb
@@ -6,7 +6,7 @@ class IlluminaC::StockPurpose < PlatePurpose
 
   def transition_to(plate, state, contents = nil,customer_accepts_responsibility=false)
     return unless ['failed','cancelled'].include?(state)
-    plate.wells.include_requests_as_target.located_at(contents).each do |well|
+    plate.wells.include_requests_as_target.include_requests_as_source.located_at(contents).each do |well|
       well.requests.each {|r| r.send(transition_from(r.state)) if r.is_a?(IlluminaC::Requests::LibraryRequest) }
       well.requests_as_target.each {|r| r.transition_to('failed') if r.is_a?(TransferRequest)}
     end
