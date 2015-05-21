a0a5345e527807e424861cca2119f0f00e1c0d1d
James Glover <james.glover@sanger.ac.uk>
2015-05-21 17:00:26 +0100

    [#92432798] Minimum volume isn't per robot
    Unfortunately the robot used might change between
    the cherrypick pipeline and robot verification.
    This means that we'd have to calculate ALL our
    volumes at Tecan file generation, requiring
    significant re-writing of the pipeline. Something
    for the future perhaps, but not for here.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150415082735) do
+ActiveRecord::Schema.define(:version => 20150414151730) do
 
   create_table "aliquots", :force => true do |t|
     t.integer  "receptacle_id",    :null => false
@@ -1133,7 +1133,6 @@ ActiveRecord::Schema.define(:version => 20150415082735) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "barcode"
-    t.float    "minimum_volume"
   end
 
   create_table "roles", :force => true do |t|
