4a54cf904c8e653534a53b4298b47e010cad672e
James Glover <james.glover@sanger.ac.uk>
2015-05-27 13:00:46 +0100

    [#94395714] Allow pooling of index tags

---
+++ db/migrate/20150527111421_tags_uniqueness_depends_on_index_tag.rb
@@ -0,0 +1,14 @@
+#This file is part of SEQUENCESCAPE is distributed under the terms of GNU General Public License version 1 or later;
+#Please refer to the LICENSE and README files for information on licensing and authorship of this file.
+#Copyright (C) 2015 Genome Research Ltd.
+class TagsUniquenessDependsOnIndexTag < ActiveRecord::Migration
+  def self.up
+    remove_index "aliquots", :name => "aliquot_tags_are_unique_within_receptacle"
+    add_index "aliquots", ["receptacle_id", "tag_id","index_tag_id"], :name => "aliquot_tags_and_index_tags_are_unique_within_receptacle", :unique => true
+  end
+
+  def self.down
+    remove_index "aliquots", :name =>  "aliquot_tags_and_index_tags_are_unique_within_receptacle"
+    add_index "aliquots", ["receptacle_id", "tag_id"], :name => "aliquot_tags_are_unique_within_receptacle", :unique => true
+  end
+end
--- test/unit/aliquot_test.rb
+++ test/unit/aliquot_test.rb
@@ -1,6 +1,6 @@
 #This file is part of SEQUENCESCAPE is distributed under the terms of GNU General Public License version 1 or later;
 #Please refer to the LICENSE and README files for information on licensing and authorship of this file.
-#Copyright (C) 2011 Genome Research Ltd.
+#Copyright (C) 2011,2015 Genome Research Ltd.
 require "test_helper"
 
 class AliquotTest < ActiveSupport::TestCase
@@ -8,23 +8,81 @@ class AliquotTest < ActiveSupport::TestCase
     setup do
       @tag1 = Factory :tag
       @tag2 = Factory :tag
+
       @sample1 = Factory :sample
       @sample2 = Factory :sample
+
+      @asset = Factory :empty_sample_tube
+
     end
 
-    should "match aliquots with same tags " do
-      assert Aliquot.new(:tag => @tag1) =~ Aliquot.new(:tag => @tag1)
+    should "match aliquots with same tags and index_tags" do
+      assert Aliquot.new(:tag => @tag1, :index_tag => @tag1) =~ Aliquot.new(:tag => @tag1, :index_tag => @tag1)
+      assert Aliquot.new(:tag => @tag1, :index_tag => @tag1).matches?(Aliquot.new(:tag => @tag1, :index_tag => @tag1))
     end
-    should "not match aliquots with different tags " do
-      assert Aliquot.new(:tag => @tag1) !~ Aliquot.new(:tag => @tag2)
+
+    should "not match aliquots with different tags" do
+      assert Aliquot.new(:tag => @tag1, :index_tag => @tag1) !~ Aliquot.new(:tag => @tag2, :index_tag => @tag1)
+      assert ! Aliquot.new(:tag => @tag1, :index_tag => @tag1).matches?(Aliquot.new(:tag => @tag2, :index_tag => @tag1))
+    end
+
+    should "not match aliquots with different index tags" do
+      assert Aliquot.new(:tag => @tag1, :index_tag => @tag1) !~ Aliquot.new(:tag => @tag1, :index_tag => @tag2)
+      assert ! Aliquot.new(:tag => @tag1, :index_tag => @tag1).matches?(Aliquot.new(:tag => @tag1, :index_tag => @tag2))
     end
 
     should " match aliquots with missing tags " do
       assert Aliquot.new(:tag => @tag1) =~ Aliquot.new()
+      assert Aliquot.new(:tag => @tag1).matches?(Aliquot.new())
+    end
+
+    should " match aliquots with missing index tags " do
+      assert Aliquot.new(:index_tag => @tag1) =~ Aliquot.new()
+      assert Aliquot.new(:index_tag => @tag1).matches?(Aliquot.new())
+    end
+
+    should " match aliquots with missing tags but present index tags " do
+      assert Aliquot.new(:tag => @tag1, :index_tag => @tag1) =~ Aliquot.new(:index_tag => @tag1)
+      assert Aliquot.new(:tag => @tag1, :index_tag => @tag1).matches?(Aliquot.new(:index_tag => @tag1))
+    end
+
+    should " match aliquots with missing index tags but present tags " do
+      assert Aliquot.new(:tag => @tag1, :index_tag => @tag1) =~ Aliquot.new(:tag => @tag1)
+      assert Aliquot.new(:tag => @tag1, :index_tag => @tag1).matches?(Aliquot.new(:tag => @tag1))
     end
 
     should "not match aliquots with different samples" do
       assert Aliquot.new(:tag => @tag1, :sample => @sample1) !~ Aliquot.new(:tag => @tag1, :sample => @sample2)
+      assert ! Aliquot.new(:tag => @tag1, :sample => @sample1).matches?(Aliquot.new(:tag => @tag1, :sample => @sample2))
+    end
+
+    should "allow mixing different tags with no tag_index" do
+      @asset.aliquots << Aliquot.new(:tag => @tag1, :sample=>@sample1) << Aliquot.new(:tag => @tag2, :sample=>@sample2)
+      @asset.save!
+    end
+
+    should "allow mixing different tags with a tag index" do
+      @asset.aliquots << Aliquot.new(:tag => @tag1, :index_tag => @tag1, :sample=>@sample1) << Aliquot.new(:tag => @tag2, :index_tag => @tag1, :sample=>@sample2)
+      @asset.save!
+    end
+
+    should "allow mixing same tags with a different tag index" do
+      @asset.aliquots << Aliquot.new(:tag => @tag1, :index_tag => @tag1, :sample=>@sample1) << Aliquot.new(:tag => @tag1, :index_tag => @tag2, :sample=>@sample2)
+      @asset.save!
+    end
+
+    should "disallow mixing same tags with no index tag" do
+      assert_raise ActiveRecord::RecordInvalid do
+        @asset.aliquots << Aliquot.new(:tag => @tag1, :sample=>@sample1) << Aliquot.new(:tag => @tag1, :sample=>@sample2)
+        @asset.save!
+      end
+    end
+
+    should "disallow mixing same tags with same index tag" do
+      assert_raise ActiveRecord::RecordInvalid do
+        @asset.aliquots << Aliquot.new(:tag => @tag1, :index_tag => @tag2, :sample=>@sample1) << Aliquot.new(:tag => @tag1, :index_tag => @tag2, :sample=>@sample2)
+        @asset.save!
+      end
     end
 
 
