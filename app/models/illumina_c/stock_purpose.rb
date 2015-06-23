043d224b4c0c83faf50aa2ea6e2d6e79668731d2
James Glover <james.glover@sanger.ac.uk>
2015-06-23 14:36:02 +0100

    Fix failures on stock plate

--- app/models/illumina_c/stock_purpose.rb
+++ app/models/illumina_c/stock_purpose.rb
@@ -6,8 +6,8 @@ class IlluminaC::StockPurpose < PlatePurpose
 
   def transition_to(plate, state, contents = nil,customer_accepts_responsibility=false)
     return unless ['failed','cancelled'].include?(state)
-    plate.wells.include_requests_as_target.include_requests_as_source.located_at(contents).each do |well|
-      well.requests.each {|r| r.send(transition_from(r.state)) if r.is_a?(IlluminaC::Requests::LibraryRequest) }
+    plate.wells.located_at(contents).include_requests_as_target.include_requests_as_source.each do |well|
+      well.requests.each {|r| r.send(transition_from(r.state)) if r.is_a?(IlluminaC::Requests::LibraryRequest) && transition_from(r.state) }
       well.requests_as_target.each {|r| r.transition_to('failed') if r.is_a?(TransferRequest)}
     end
   end
