f573a0e936fd18f76e806e78bc8f6c41268c2383
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2015-06-23 09:48:14 +0100

    Corrects syntax error

--- app/models/well.rb
+++ app/models/well.rb
@@ -33,7 +33,7 @@ class Well < Aliquot::Receptacle
 
   def is_in_fluidigm?
     sta_plate_purpose_name = "STA"
-    !self.target_wells.detect{|w| w.events.detect {|e| e.family == event_family_for_fluidigm(sta_plate_purpose_name)}.nil?
+    !self.target_wells.detect{|w| w.events.detect {|e| e.family == event_family_for_fluidigm(sta_plate_purpose_name)}.nil?}
   end
 
   named_scope :include_stock_wells, { :include => { :stock_wells => :requests_as_source } }
