27bfe9fdd5864e28a6fcda64ae4b478b4cfc4525
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2015-09-21 13:01:22 +0100

    Updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150902112414) do
+ActiveRecord::Schema.define(:version => 20150914104607) do
 
   create_table "aliquot_indices", :force => true do |t|
     t.integer  "aliquot_id",    :null => false
