7730d03fcad63efe9a60644a7d72f286ba231bfe
James Glover <james.glover@sanger.ac.uk>
2015-09-25 11:02:23 +0100

    ss204 Add user to transition tests

--- test/unit/illumina_b/final_plate_purpose_test.rb
+++ test/unit/illumina_b/final_plate_purpose_test.rb
@@ -12,7 +12,7 @@ class IlluminaB::FinalPlatePurposeTest < ActiveSupport::TestCase
 
     context '#transition_to' do
       setup do
-        @child, @parent, @grandparent = mock('PCR XP'), mock('PCR'), mock('PRE PCR')
+        @child, @parent, @grandparent, @user = mock('PCR XP'), mock('PCR'), mock('PRE PCR'), mock('user')
         @child.stubs(:parent).returns(@parent)
         @parent.stubs(:parent).returns(@grandparent)
 
@@ -23,21 +23,21 @@ class IlluminaB::FinalPlatePurposeTest < ActiveSupport::TestCase
       ['passed', 'cancelled'].each do |state|
         should "not alter pre-pcr plate when transitioning entire plate to #{state}" do
           @purpose.expects(:transition_state_requests).with(@child_wells, state)
-          @purpose.transition_to(@child, state, nil)
+          @purpose.transition_to(@child, state, @user, nil)
         end
       end
 
       should "fail the pre-pcr plate when failing the entire plate" do
-        @grandparent.expects(:transition_to).with('failed', nil,false)
+        @grandparent.expects(:transition_to).with('failed',@user,nil,false)
         @purpose.expects(:transition_state_requests).with(@child_wells, 'failed')
-        @purpose.transition_to(@child, 'failed', nil, nil)
+        @purpose.transition_to(@child, 'failed', @user, nil)
       end
 
       should "fail the pre-pcr well when failing a well" do
         @child_wells.expects(:located_at).with(['A1']).returns(@child_wells)
-        @grandparent.expects(:transition_to).with('failed', ['A1'],false)
+        @grandparent.expects(:transition_to).with('failed', @user, ['A1'],false)
         @purpose.expects(:transition_state_requests).with(@child_wells, 'failed')
-        @purpose.transition_to(@child, 'failed', nil, ['A1'])
+        @purpose.transition_to(@child, 'failed', @user, ['A1'])
       end
     end
   end
