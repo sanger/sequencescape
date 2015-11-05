4044b5286633e7ec421e6eb9d173aa1047db837e
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2015-11-05 16:55:33 +0000

    Added LabWhereClient module. LabView shows the storage information coming from ETS or LabWhere. The spreadsheet of Cherrypick batches shows now the storage info provided by LabWhere if it's available.

--- lib/lab_where_client.rb
+++ lib/lab_where_client.rb
@@ -4,6 +4,7 @@
 require 'rest-client'
 
 module LabWhereClient
+  @@CACHED_INSTANCES= {}
 
   def self.load_params(obj, params)
     params.each do |k,v|
@@ -29,11 +30,13 @@ module LabWhereClient
   end
 
   def self.json_retriever(url)
-    JSON.parse(RestClient.get(url))
+    json = RestClient.get(url)
+    return nil if json == 'null'
+    JSON.parse(json)
   end
 
   def self.build_from_url(klass, url)
-    LabWhereClient.build_from_object(klass, LabWhereClient.json_retriever(url))
+    @@CACHED_INSTANCES[url] ||= LabWhereClient.build_from_object(klass, LabWhereClient.json_retriever(url))
   end
 
   def self.included(base)
@@ -42,6 +45,9 @@ module LabWhereClient
         klass_name = params[:as] || name.to_s.classify
         class_eval %Q{
           def #{name.to_s}
+            if instance_variable_get("@#{name.to_s}")=="Empty"
+              return nil
+            end
             LabWhereClient::build_from_url(#{klass_name}, configatron.labwhere_api+instance_variable_get("@#{name.to_s}"))
           end
         }
@@ -60,6 +66,8 @@ module LabWhereClient
 
 
   class Location
+    @@MAX_DEPTH_ANCESTORS_LOCATIONS = 10
+
     include LabWhereClient
 
     define_rest_accessor :labwares
@@ -67,14 +75,40 @@ module LabWhereClient
     define_rest_accessor :parent, :as => Location
     define_rest_accessor :children, :as => Location
 
+    def location_type
+      LabWhereClient::build_from_url(LabWhereClient::LocationType, "#{configatron.labwhere_api}/api/location_types/#{location_type_id}")
+    end
+
     def self.find_by_barcode(barcode)
       LabWhereClient::build_from_url(LabWhereClient::Location, "#{configatron.labwhere_api}/api/locations/#{barcode}")
     end
 
+    def ancestors(depth=0)
+      return [self, parent.ancestors(depth+1)].flatten.compact unless (parent.nil? || (depth > @@MAX_DEPTH_ANCESTORS_LOCATIONS))
+      [self]
+    end
 
+    def ancestors_classified_by_location_type(location_type_list)
+      location_type_list.map do |location_type|
+        ancestors.select{|l| l.location_type.name == location_type}
+      end
+    end
+
+    def location_info
+      info = HashWithIndifferentAccess.new
+      { 'storage_device' => [ "Box",  "Shelf", "Freezer -80C", "Incubator 37C"],
+        'storage_area' => ["Room"],
+        'building' => ["Building"],
+        'building_area' => ["Site"]
+      }.each do |location_tag,location_type_list|
+        info[location_tag] = ancestors_classified_by_location_type(location_type_list).flatten.compact.map(&:name).join('|')
+      end
+      return info
+    end
   end
 
   class Labware
+
     include LabWhereClient
     define_json_accessor :location
     define_rest_accessor :audits
@@ -84,6 +118,7 @@ module LabWhereClient
     def self.find_by_barcode(barcode)
       LabWhereClient::build_from_url(LabWhereClient::Labware, "#{configatron.labwhere_api}/api/labwares/#{barcode}")
     end
+
   end
 
   class Parent
@@ -91,6 +126,10 @@ module LabWhereClient
   end
 
   class RecordData
+    include LabWhereClient
+  end
+
+  class LocationType
     include LabWhereClient
   end
 
