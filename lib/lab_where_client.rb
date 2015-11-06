4b1eb7e2af4fd19803d78676ad5e21847c80c3ee
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2015-11-06 17:00:19 +0000

    Added exception LabwhereException to detect Labwhere falls. Recoding of LabWhereClient in a more concise and clear way.

--- lib/lab_where_client.rb
+++ lib/lab_where_client.rb
@@ -4,104 +4,70 @@
 require 'rest-client'
 
 module LabWhereClient
-  def self.load_params(obj, params)
-    params.each do |k,v|
-      obj.instance_variable_set("@#{k.to_s}", v)
-      unless obj.methods.include?(k.to_s)
-        obj.class.instance_eval do
-          attr_reader "#{k}"
-        end
-      end
+  class LabWhere
+    include Singleton
+    def base_url
+        configatron.labwhere_api
     end
-    obj
-  end
 
-  def self.build_from_object(klass, params)
-    return params if params.nil?
-    if params.kind_of? Array
-      params.map do |obj|
-        LabWhereClient.load_params(klass.new, obj)
-      end
-    else
-      LabWhereClient.load_params(klass.new, params)
+    def path_to(instance, target)
+      [base_url, instance.endpoint, target].join('/')
     end
+
+    def get(instance, target)
+      JSON.parse(RestClient.get(path_to(instance,target)))
+    rescue Errno::ECONNREFUSED => e
+      raise LabwhereException.new(e), "LabWhere service is down", e.backtrace
+    end
+
+    def post(instance, target, payload)
+    end
+
   end
 
-  def self.json_retriever(url)
-    json = RestClient.get(url)
-    return nil if json == 'null'
-    JSON.parse(json)
-  end
+  class Endpoint
 
-  def self.build_from_url(klass, url)
-    LabWhereClient.build_from_object(klass, LabWhereClient.json_retriever(url))
-  end
+    def self.endpoint_name(name)
+      @endpoint = name
+    end
 
-  def self.included(base)
-    base.instance_eval do
-      def define_rest_accessor(name, params={})
-        klass_name = params[:as] || name.to_s.classify
-        class_eval %Q{
-          def #{name.to_s}
-            if instance_variable_get("@#{name.to_s}")=="Empty"
-              return nil
-            end
-            LabWhereClient::build_from_url(#{klass_name}, configatron.labwhere_api+instance_variable_get("@#{name.to_s}"))
-          end
-        }
-      end
-      def define_json_accessor(name, params={})
-        klass_name = params[:as] || name.to_s.classify
-        class_eval %Q{
-          def #{name.to_s}
-            LabWhereClient::build_from_object(#{klass_name}, instance_variable_get("@#{name.to_s}"))
-          end
-        }
-      end
+    class << self
+      attr_reader :endpoint
+    end
 
+    def initialize(params)
     end
   end
 
+  class LabwhereException < Exception
+  end
 
-  class Location
-    @@MAX_DEPTH_ANCESTORS_LOCATIONS = 10
 
-    include LabWhereClient
+  class Labware < Endpoint
+    endpoint_name 'labwares'
 
-    define_rest_accessor :labwares
-    define_rest_accessor :audits
-    define_rest_accessor :parent, :as => Location
-    define_rest_accessor :children, :as => Location
-
-    def location_type
-      LabWhereClient::build_from_url(LabWhereClient::LocationType, "#{configatron.labwhere_api}/api/location_types/#{location_type_id}")
-    end
+    attr_reader :barcode
+    attr_reader :location
 
     def self.find_by_barcode(barcode)
-      LabWhereClient::build_from_url(LabWhereClient::Location, "#{configatron.labwhere_api}/api/locations/#{barcode}")
+      new(LabWhere.instance.get(self, barcode))
     end
 
-    def ancestors(depth=0)
-      return [self, parent.ancestors(depth+1)].flatten.compact unless (parent.nil? || (depth > @@MAX_DEPTH_ANCESTORS_LOCATIONS))
-      [self]
+    def initialize(params)
+      @barcode = params['barcode']
+      @location = Location.new(params['location'])
     end
+  end
 
-    def ancestors_classified_by_location_type(location_type_list)
-      location_type_list.map do |location_type|
-        ancestors.select{|l| l.location_type.name == location_type}
-      end
-    end
+  class Location < Endpoint
+    endpoint_name 'locations'
 
-    def location_info_ets_format
-      info = HashWithIndifferentAccess.new
-      { 'storage_device' => [ "Box",  "Shelf", "Freezer -80C", "Incubator 37C"],
-        'storage_area' => ["Room"],
-        'building' => ["Building"],
-        'building_area' => ["Site"]
-      }.each do |location_tag,location_type_list|
-        info[location_tag] = ancestors_classified_by_location_type(location_type_list).flatten.compact.map(&:name).join('|')
-      end
-      return info
+    attr_reader :name
+    attr_reader :parentage
+
+    def initialize(params)
+      @name = params['name']
+      @parentage = params['parentage']
     end
 
     def location_info
@@ -109,35 +75,7 @@ module LabWhereClient
     end
   end
 
-  class Labware
-
-    include LabWhereClient
-    define_json_accessor :location
-    define_rest_accessor :audits
-    define_rest_accessor :parent, :as => Location
-    define_rest_accessor :children, :as => Location
-
-    def self.find_by_barcode(barcode)
-      LabWhereClient::build_from_url(LabWhereClient::Labware, "#{configatron.labwhere_api}/api/labwares/#{barcode}")
-    end
-
+  class LocationType < Endpoint
+    endpoint_name 'location_types'
   end
-
-  class Parent
-    include LabWhereClient
-  end
-
-  class RecordData
-    include LabWhereClient
-  end
-
-  class LocationType
-    include LabWhereClient
-  end
-
-  class Audit
-    include LabWhereClient
-    define_json_accessor :record_data, :as => RecordData
-  end
-
 end
