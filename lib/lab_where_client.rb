dd7a2e88d27d332066a5d8072e9406aa9470df6f
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2015-11-09 12:02:54 +0000

    Warns when the LabWhere url hasn't been set

--- lib/lab_where_client.rb
+++ lib/lab_where_client.rb
@@ -9,11 +9,13 @@ module LabWhereClient
 
   class LabWhere
     include Singleton
+
     def base_url
       configatron.labwhere_api
     end
 
     def path_to(instance, target)
+      raise LabwhereException.new, "LabWhere service URL not set" if base_url.nil?
       [base_url, instance.endpoint, target].join('/')
     end
 
