16b824b8c001a57a682f3d675bcccc515dbeae77
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2015-11-19 11:34:31 +0000

    Modifies the filename of the manifest to filter invalid characters to avoid problems with Excel

--- app/models/tasks/generate_manifest_handler.rb
+++ app/models/tasks/generate_manifest_handler.rb
@@ -2,13 +2,20 @@
 #Please refer to the LICENSE and README files for information on licensing and authorship of this file.
 #Copyright (C) 2007-2011,2012 Genome Research Ltd.
 module Tasks::GenerateManifestHandler
+  def sanitize_manifest_filename(str)
+  end
+
+  def manifest_filename
+    ["#{study.name}", "#{batch.id}", "manifest.csv"].join("_").gsub(/[^A-Za-z0-9_\-]/,"")
+  end
+
   def generate_manifest
     batch = Batch.find(params[:id])
     study = Study.find(params[:study_id])
     csv_string = GenerateManifestsTask.generate_manifests(batch,study)
     send_data csv_string,
       :type => "text/csv",
-      :filename=>"#{study.name}_#{batch.id}_manifest.csv",
+      :filename=> manifest_filename,
       :disposition => 'attachment'
   end
 
