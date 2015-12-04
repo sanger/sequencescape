092ce2b7e17de4539f5815c69606b83f4259d29b
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2015-12-04 14:57:22 +0000

    Studies summary tabs

---
+++ app/assets/javascripts/sequencescape/ajax_link_handling.js
@@ -0,0 +1,32 @@
+//This file is part of SEQUENCESCAPE is distributed under the terms of GNU General Public License version 1 or later;
+//Please refer to the LICENSE and README files for information on licensing and authorship of this file.
+//Copyright (C) 2015 Genome Research Ltd.
+
+( function($, undefined){
+  "use strict";
+
+  var attachEvents;
+
+  attachEvents = function(){
+    $('[data-remote=true]').each(function(pos, domObj) {
+      var node = $(domObj);
+      node.bind("ajax:beforeSend",  function(){
+        $(this.dataset.throbber || '#update_loader').show();
+        $(this.dataset.update).html('');
+      })
+      .bind("ajax:complete", function(){
+        $(this.dataset.throbber || '#update_loader').hide();
+      })
+      .bind("ajax:success", function(xhr, data, status) {
+        var target = this.dataset.success ||  this.dataset.update;
+        $(target).html(data);
+      }).bind('ajax:error', function(xhr, data, status) {
+        var target = this.dataset.failure ||  this.dataset.update;
+        $(target).html(data);
+      });
+    });
+  };
+
+  $(document).ready( attachEvents );
+
+})(jQuery);
