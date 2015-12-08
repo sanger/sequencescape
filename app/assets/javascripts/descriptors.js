e08f63bdc7ba780fac852dd976c8c4eb012deedc
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2015-12-08 16:34:01 +0000

    Converted descriptors.js file to jQuery

--- app/assets/javascripts/descriptors.js
+++ app/assets/javascripts/descriptors.js
@@ -5,7 +5,7 @@ var controller_name;
 var model_name;
 
 function field_change(ident) {
-  $('field_selection_' + ident).style.display = ($('field_type_' + ident).value == 'Selection') ? 'block' : 'none';
+  $('#field_selection_' + ident).style.display = ($('#field_type_' + ident).value == 'Selection') ? 'block' : 'none';
 }
 
 function set_controller(name) {
@@ -29,11 +29,11 @@ function handleErrorResponse(response) {
 }
 
 // When dealing with Ajax that updates forms you need to ensure that you maintain the values that
-// were present before the innerHTML gets updated.  This is because the innerHTML is what is 
+// were present before the innerHTML gets updated.  This is because the innerHTML is what is
 // present, not what is present when you substitute all of the values.
 //
 // NOTE: We have to maintain two separate maps (one from general INPUT field to value, and one
-// from INPUT[type=checkbox] to its checked status, because of the way that Rails generates a 
+// from INPUT[type=checkbox] to its checked status, because of the way that Rails generates a
 // input[type=checkbox] and a input[type=hidden] for checkbox fields.
 function maintainFormValues(form_id, callback) {
   var inputValues    = new Array();
@@ -59,7 +59,7 @@ function maintainFormValues(form_id, callback) {
 }
 
 function walkAllFormElements(form_id, callback) {
-  var formElement = $(form_id);
+  var formElement = $('#'+form_id);
   if (formElement == null) { return; }
   for (i = 0; i < formElement.elements.length; ++i) {
     callback(formElement.elements[ i ]);
@@ -68,13 +68,13 @@ function walkAllFormElements(form_id, callback) {
 
 function removeRow(type, identity) {
   decrement_counter('count', function() {
-    row = $(type + '_' + identity);
+    row = $('#'+type + '_' + identity);
     row.parentNode.removeChild(row);
   });
 }
 
 function increment_counter(ident, callback) {
-  var counterElement    = $(ident);
+  var counterElement    = $('#'+ident);
   var currentCount      = parseInt(counterElement.value);
   currentCount         += 1;
   counterElement.value  = currentCount;
@@ -82,7 +82,7 @@ function increment_counter(ident, callback) {
 }
 
 function decrement_counter(ident, callback) {
-  var counterElement = $(ident);
+  var counterElement = $('#'+ident);
   var currentCount   = parseInt(counterElement.value);
   if (currentCount > 1) {
     currentCount         -= 1;
@@ -95,8 +95,8 @@ function addDescriptor() {
   increment_counter('count', function(count) {
     url = controller() + "/new_field/" + count;
     new Ajax.Request(
-      url, { 
-        method:'GET', asynchronous:true, evalScripts:true, 
+      url, {
+        method:'GET', asynchronous:true, evalScripts:true,
         onSuccess: function(response) { handleDescriptor(response, model() + '_form'); },
         onFailure: handleErrorResponse
       }
@@ -106,11 +106,11 @@ function addDescriptor() {
 
 function addAsset(family) {
   increment_counter('count', function(count) {
-    $('descriptors').style.display = 'table';
+    $('#descriptors').style.display = 'table';
     url = controller() + "/new/" + count + "?family=" + family;
     new Ajax.Request(
       url, {
-        method:'GET', asynchronous:true, evalScripts:true, 
+        method:'GET', asynchronous:true, evalScripts:true,
         onSuccess: function(response) { handleDescriptor(response, model() + '_form'); },
         onFailure: handleErrorResponse
       }
@@ -122,11 +122,11 @@ function addOption(field, controller, model) {
   increment_counter('option_count_' + field, function(new_ident) {
     url = "/" + controller + "/new_option/" + field + "?option=" + new_ident;
     new Ajax.Request(
-      url, { 
-        method:'GET', asynchronous:true, evalScripts:true, 
-        onSuccess: function(response) { 
+      url, {
+        method:'GET', asynchronous:true, evalScripts:true,
+        onSuccess: function(response) {
           maintainFormValues(model + '_form', function() {
-            optionsField = $('field_options_' + field);
+            optionsField = $('#field_options_' + field);
             optionsField.innerHTML = optionsField.innerHTML + response.responseText;
           });
         },
@@ -141,7 +141,7 @@ function removeAsset(ident)      { removeRow('asset', ident);      }
 
 function removeOption(field, ident) {
   decrement_counter('option_count_' + field, function() {
-    row = $('field_' + field + "_option_" + ident);
+    row = $('#field_' + field + "_option_" + ident);
     row.parentNode.removeChild(row);
   });
 }
@@ -152,7 +152,7 @@ function removeOption(field, ident) {
 // their form contents.
 function handleDescriptor(response, form_id) {
   maintainFormValues(form_id, function() {
-    descriptorsElement = $('descriptors');
+    descriptorsElement = $('#descriptors');
     descriptorsElement.innerHTML = descriptorsElement.innerHTML + response.responseText;
   });
 }
