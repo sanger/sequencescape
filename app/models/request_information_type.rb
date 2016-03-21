6a6f166c065a955bfeb5622421bad5331981848e
James Glover <james.glover@sanger.ac.uk>
2016-03-21 09:07:17 +0000

    Re-run copyright date tagging script
    While updating to rails 3/ruby 1.9+ it was necessary
    to make changes that affected many files at once.
    Manually updating the timestampls would be a pain, not to
    mention the cause of multiple merge conflicts. So I left updating them
    until everything was good to go. Unfortunately this also means that
    we seem to have applied a timestamp for the initial application of
    the copyright message. Ideally we'd filter out all such insubstantial
    commits.

--- app/models/request_information_type.rb
+++ app/models/request_information_type.rb
@@ -1,6 +1,7 @@
-#This file is part of SEQUENCESCAPE is distributed under the terms of GNU General Public License version 1 or later;
+#This file is part of SEQUENCESCAPE; it is distributed under the terms of GNU General Public License version 1 or later;
 #Please refer to the LICENSE and README files for information on licensing and authorship of this file.
-#Copyright (C) 2007-2011 Genome Research Ltd.
+#Copyright (C) 2007-2011,2015,2016 Genome Research Ltd.
+
 class RequestInformationType < ActiveRecord::Base
   has_many :pipeline_request_information_types
   has_many :pipelines, :through => :pipeline_request_information_types
