dbc29447241ec292f87e1dfb09484604b50a7e58
James Glover <james.glover@sanger.ac.uk>
2016-03-22 13:53:38 +0000

    Do not use hash for pico sets controller rendering

--- app/controllers/pico_set_results_controller.rb
+++ app/controllers/pico_set_results_controller.rb
@@ -15,19 +15,21 @@ class PicoSetResultsController < ApplicationController
       pico_assay_plate = PicoAssayPlate.find_from_machine_barcode(pico_set_result[:assay_barcode])
     end
 
+    message = {}
+
     respond_to do |format|
       if pico_assay_plate.nil?
-        flash[:error] = "Barcode #{pico_set_result[:assay_barcode]} not found"
+        message[:error] = "Barcode #{pico_set_result[:assay_barcode]} not found"
         format.xml  { render :xml  => flash.to_xml, :status => :not_found }
         format.json { render :json => flash.to_json, :status => :not_found }
       else
         # create method call using pico_assay_plate[:state] and use send for 2 method calls
         if pico_assay_plate.upload_pico_results(pico_set_result[:state], pico_set_result[:failure_reason], pico_set_result[:wells])
-          flash[:notice] = 'Updated concentrations'
+          message[:notice] = 'Updated concentrations'
           format.xml  { render :xml  => flash.to_xml, :status => :ok }
           format.json { render :json => flash.to_json, :status => :ok }
         else
-          flash[:error] = "Couldn't upload results"
+          message[:error] = "Couldn't upload results"
           format.xml  { render :xml  => flash.to_xml, :status => :unprocessable_entity }
           format.json { render :json => flash.to_json, :status => :unprocessable_entity }
         end
