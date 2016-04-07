ec1e40c8f4f9681ff121a5c379a33e8f9f766897
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2016-04-07 10:29:57 +0100

    Adds support to create Pico Assay plates from Agena working dilution

--- db/migrate/20160404085303_add_sequenom_working_dilution.rb
+++ db/migrate/20160404085303_add_sequenom_working_dilution.rb
@@ -7,13 +7,13 @@ class AddSequenomWorkingDilution < ActiveRecord::Migration
         :valid_options => {:valid_dilution_factors => [12.5,20.0,15.0,50.0]} )
 
       Plate::Creator.find_by_name!("Pico dilution").parent_plate_purposes << plate_purpose
+      Plate::Creator.find_by_name!("Pico Assay Plates").parent_plate_purposes << plate_purpose
     end
   end
 
   def down
     ActiveRecord::Base.transaction do
       plate_purpose = DilutionPlatePurpose.find_by_name!('Agena Working Dilution')
-      #Purpose::Relationship.select{|r| r.parent == plate_purpose}.each(&:destroy!)
       Plate::Creator.find_by_name!(plate_purpose.name).destroy
       plate_purpose.destroy
     end
