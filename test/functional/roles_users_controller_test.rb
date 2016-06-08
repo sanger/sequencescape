0025d093fe6590538949352664b9e3ad308ec46e
James Glover <james.glover@sanger.ac.uk>
2016-06-08 11:59:00 +0100

    Update class name

--- test/functional/roles_users_controller_test.rb
+++ test/functional/roles_users_controller_test.rb
@@ -6,12 +6,12 @@ require "test_helper"
 require 'samples_controller'
 
 # Re-raise errors caught by the controller.
-class Roles::UsersController; def rescue_action(e) raise e end; end
+class Admin::Roles::UsersController; def rescue_action(e) raise e end; end
 
-class Roles::UsersControllerTest < ActionController::TestCase
-  context "Roles::UsersControllercontroller" do
+class Admin::Roles::UsersControllerTest < ActionController::TestCase
+  context "Admin::Roles::UsersControllercontroller" do
     setup do
-      @controller = Roles::UsersController.new
+      @controller = Admin::Roles::UsersController.new
       @request    = ActionController::TestRequest.new
       @response   = ActionController::TestResponse.new
     end
