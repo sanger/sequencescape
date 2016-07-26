6506ce8cc69ce6f5b9053f407b317e5121b5916a
James Glover <james.glover@sanger.ac.uk>
2016-07-26 11:07:11 +0100

    Adjust validation to ensure data_access_group stored for open studies

--- test/unit/data_release_test.rb
+++ test/unit/data_release_test.rb
@@ -2,7 +2,7 @@
 #Please refer to the LICENSE and README files for information on licensing and authorship of this file.
 #Copyright (C) 2007-2011,2015 Genome Research Ltd.
 
-require File.join(File.dirname(__FILE__), *%w[.. test_helper])
+require_relative '../test_helper'
 
 class DataReleaseTest < ActiveSupport::TestCase
   context "A study" do
