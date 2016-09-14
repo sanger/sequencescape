5685c84ec983a79ed9e85e30164247b4d97ef36c
James Glover <james.glover@sanger.ac.uk>
2016-09-14 09:52:52 +0100

    Switch flash for flash.now when rendering

--- app/controllers/get_your_qc_completed_tubes_here_controller.rb
+++ app/controllers/get_your_qc_completed_tubes_here_controller.rb
@@ -12,11 +12,11 @@ class GetYourQcCompletedTubesHereController < ApplicationController
         flash[:notice] = "QC Completed tubes successfully created for #{@generator.plate.sanger_human_barcode}. Go celebrate!"
         redirect_to study_asset_groups_path(@generator.study.id)
       else
-        flash[:error] = "Oh dear, your tubes weren't created. It's not you its me so please contact PSD."
+        flash.now[:error] = "Oh dear, your tubes weren't created. It's not you its me so please contact PSD."
         render :new
       end
     else
-      flash[:error] = @generator.errors.full_messages.join(", ")
+      flash.now[:error] = @generator.errors.full_messages.join(", ")
       render :new
     end
   end
