e9eac23370f2a01b4ab731a6abff9542fb430d87
James Glover <james.glover@sanger.ac.uk>
2016-09-14 11:40:33 +0100

    Add user requested changes to pipleines
    During UAT a number of modifications were requested. These
    have been performed in a separate migration:
    1) To allow for the migration to be run in UAT. Rolling back the
    prevous version would result in id changes breaking batches that have passed through
    2) To ensure that the code we deploy is the same code that was subject to UAT.

--- db/seeds/0001_workflows.rb
+++ db/seeds/0001_workflows.rb
@@ -1502,9 +1502,8 @@ end
         {kind: 'Text', sorter: 2, name:'Operator'},
         {kind: 'Text', sorter: 3, name:'Pipette Carousel #'},
         {kind: 'Text', sorter: 4, name:'CBOT'},
-        {kind: 'Text', sorter: 5, name:'-20 Temp. Read 1 Cluster Kit Lot #'},
-        {kind: 'Text', sorter: 6, name:'-20 Temp. Read 1 Cluster Kit RGT #'},
-        {kind: 'Text', sorter: 7, name:'Pipette Carousel #'},
+        {kind: 'Text', sorter: 5, name:'-20 Temp. Read 1 Cluster Kit (Box 1 of 2) Lot #'},
+        {kind: 'Text', sorter: 6, name:'-20 Temp. Read 1 Cluster Kit (Box 1 of 2) RGT #'},
         {kind: 'Text', sorter: 8, name:'Comment'}
       ])
     end
@@ -1534,12 +1533,12 @@ end
         task.descriptors.build([
           {kind: 'Text', sorter: 1, name: 'Operator'},
           {kind: 'Text', sorter: 2, name: 'Pipette Carousel #'},
-          {kind: 'Text', sorter: 3, name: '-20 Temp. Read 1 Cluster Kit Lot #'},
-          {kind: 'Text', sorter: 4, name: '-20 Temp. Read 1 Cluster Kit RGT #'},
+          {kind: 'Text', sorter: 3, name: '-20 Temp. Read 2 Cluster Kit (Box 2 of 2) Lot #'},
+          {kind: 'Text', sorter: 4, name: '-20 Temp. Read 2 Cluster Kit (Box 2 of 2) RGT #'},
           {kind: 'Text', sorter: 5, name: 'Resynthesis Mix (HRM)'},
           {kind: 'Text', sorter: 6, name: 'Linearization Mix 2 (HLM2)'},
           {kind: 'Text', sorter: 7, name: 'Amplification Mix (HAM)'},
-          {kind: 'Text', sorter: 8, name: 'AMP premix (HDR)'},
+          {kind: 'Text', sorter: 8, name: 'AMP premix (HPM)'},
           {kind: 'Text', sorter: 9, name: 'Denaturation Mix (HDR)'},
           {kind: 'Text', sorter: 10, name: 'Primer Mix Read 2 (HP11)'},
           {kind: 'Text', sorter: 11, name: 'Indexing Primer Mix (HP14)'},
@@ -1567,7 +1566,7 @@ end
   end
 
  SequencingPipeline.create!(
-        name: 'HiSeq 4000 PE',
+        name: 'HiSeq 4000 PE (spiked in controls)',
         asset_type: 'Lane',
         automated: false,
         active: true,
@@ -1586,7 +1585,7 @@ end
       end
 
       SequencingPipeline.create!(
-        name: 'HiSeq 4000 SE',
+        name: 'HiSeq 4000 SE (spiked in controls)',
         asset_type: 'Lane',
         automated: false,
         active: true,
