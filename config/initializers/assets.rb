e59e4abfbcf56c8788c1fe3d1eec3c523c935db4
James Glover <james.glover@sanger.ac.uk>
2016-10-03 12:51:56 +0100

    Add js to precompile index

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,16 @@
-Rails.application.config.assets.precompile += %w( sessions.css tabview.css print.css pipeline.js )
+Rails.application.config.assets.precompile += [
+  'sessions.css',
+  'tabview.css',
+  'print.css',
+  'pipeline.js',
+  'assign-tubes-to-wells-task.js',
+  'assigntags.js',
+  'characterisation.js',
+  'descriptors.js',
+  'fail_batch.js',
+  'labwhere_reception.js',
+  'organism_validation.js',
+  'pipeline.js',
+  'sample_move.js',
+  'submissions.js'
+])
