a37b949f16c786a8fc3172f397e968b7ed34921d
James Glover <james.glover@sanger.ac.uk>
2016-10-06 15:07:19 +0100

    ss530 - Add migrations

---
+++ db/migrate/20161005140505_add_chromium_request_type.rb
@@ -0,0 +1,33 @@
+class AddChromiumRequestType < ActiveRecord::Migration
+  def up
+    ActiveRecord::Base.transaction do
+      rt = RequestType.create!(
+        :name               => 'Illumina-C Chromium library creation',
+        :key                => 'illumina_c_chromium_library',
+        :request_class_name => 'IlluminaC::Requests::LibraryRequest', # See class deprecation notice above
+        :for_multiplexing   => false,
+        :workflow => Submission::Workflow.find_by_name("Next-gen sequencing"),
+        :asset_type => 'Well',
+        :order =>1,
+        :initial_state =>'pending',
+        :billable =>true,
+        :product_line => ProductLine.find_by_name('Illumina-C'),
+        :request_purpose => RequestPurpose.standard
+      )
+      rt.acceptable_plate_purposes << Purpose.find_by_name('ILC Stock')
+      rt.library_types = LibraryType.where(name:['Chromium genome','Chromium exome','Chromium single cell'])
+      RequestType.find_by_key('illumina_c_multiplexing').acceptable_plate_purposes << Purpose.find_by_name!('ILC Lib Chromium')
+      RequestType::Validator.create!(
+        :request_type   => rt,
+        :request_option => 'library_type',
+        :valid_options  => RequestType::Validator::LibraryTypeValidator.new(rt.id)
+      )
+    end
+  end
+
+  def down
+    ActiveRecord::Base.transaction do
+      RequestType.find_by_key('illumina_c_chromium_library').destroy
+    end
+  end
+end
