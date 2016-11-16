b856c3b3d7ae1274aafce423557abc6d7153408f
James Glover <james.glover@sanger.ac.uk>
2016-11-16 10:46:29 +0000

    Disable plugin reloading in test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,7 +34,6 @@ Sequencescape::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-  config.reload_plugins = true
 
   config.time_zone = 'London'
 
