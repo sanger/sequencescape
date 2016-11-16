ebe7558d27db6dc746e80fd21798e1bcace829c3
James Glover <james.glover@sanger.ac.uk>
2016-11-16 12:23:04 +0000

    Test gems avaialble to cucumber

--- config/environment.rb
+++ config/environment.rb
@@ -1,27 +1,5 @@
 # Load the Rails application.
-begin_path = $:.dup
-puts "==BEGIN=="
-puts begin_path
 require File.expand_path('../application', __FILE__)
-mid_path = $:.dup
-puts "==MID=="
-puts mid_path
-puts "==MID=DELTA="
-(mid_path-begin_path).each do |path|
-  puts "+#{path}"
-end
-(begin_path-mid_path).each do |path|
-  puts "-#{path}"
-end
+
 # Initialize the rails application
 Sequencescape::Application.initialize!
-end_path = $:.dup
-puts "==END=="
-puts end_path
-puts "==END=DELTA="
-(end_path-begin_path).each do |path|
-  puts "+#{path}"
-end
-(begin_path-end_path).each do |path|
-  puts "-#{path}"
-end
