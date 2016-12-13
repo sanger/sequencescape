ed2ef5e0a40eff04e427b8fadd24ad927fbe9182
Yana Proskurina <yana.proskurina@sanger.ac.uk>
2016-12-13 15:27:08 +0000

    Fixed cucumber on local, 3 nodes

--- features/support/env.rb
+++ features/support/env.rb
@@ -55,3 +55,6 @@ end
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :transaction
+
+require 'minitest/spec'
+World(MultiTest::MinitestWorld)
