8b674acee50e8ac2fc31c260ceb6dd4702b80b56
Yana Proskurina <yana.proskurina@sanger.ac.uk>
2017-02-06 08:54:26 +0000

    ss618 Field for hmdmc number appears in form for managed studies

---
+++ spec/features/studies/hmdmc_number_on_create_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+require 'rails_helper'
+require 'pry'
+
+feature 'Studies controller' do
+  let(:user) { create :admin }
+
+  scenario 'managed study can not be created without hmdmc number', js: true do
+    login_user user
+    visit new_study_path
+    select("Open (ENA)", from: "study_study_metadata_attributes_data_release_strategy")
+    expect(page).not_to have_content('HMDMC approval number')
+    select("Managed (EGA)", from: "study_study_metadata_attributes_data_release_strategy")
+    expect(page).to have_content('HMDMC approval number')
+    click_button 'Create'
+    expect(page).not_to have_content "Study metadata hmdmc approval number can't be blank"
+  end
+
+  def login_user(user)
+    visit login_path
+    fill_in 'Username', with: user.login
+    fill_in 'Password', with: 'password'
+    click_button 'Login'
+    true
+  end
+end
