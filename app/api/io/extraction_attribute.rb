581eac2a620c9e940ad282d86553fd122014fc6c
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-02-14 20:45:11 +0000

    Quotes without escaping

--- app/api/io/extraction_attribute.rb
+++ app/api/io/extraction_attribute.rb
@@ -3,8 +3,8 @@ class ::Io::ExtractionAttribute < ::Core::Io::Base
   set_json_root(:extraction_attribute)
   # set_eager_loading { |model| model }   # TODO: uncomment and add any named_scopes that do includes you need
 
-  define_attribute_and_json_mapping(" \
-                       created_by <=> created_by \
-                    attributes_update <=> attributes_update \
+  define_attribute_and_json_mapping("
+                       created_by <=> created_by
+                    attributes_update <=> attributes_update
   ")
 end
