6fe689e7ef8890095a4f8f10d55156d796510987
Yana Proskurina <yana.proskurina@sanger.ac.uk>
2017-02-24 15:27:15 +0000

    Put the test for a tag group back

--- spec/lib/sample_manifest_excel/configuration_spec.rb
+++ spec/lib/sample_manifest_excel/configuration_spec.rb
@@ -5,14 +5,21 @@ RSpec.describe SampleManifestExcel::Configuration, type: :model, sample_manifest
   let(:configuration) { SampleManifestExcel::Configuration.new }
 
   it 'should be comparable' do
-    expect(configuration).to eq(SampleManifestExcel::Configuration.new) 
+    expect(configuration).to eq(SampleManifestExcel::Configuration.new)
   end
 
   it 'should be able to add a new file' do
     configuration.add_file 'a_new_file'
-    expect(configuration.files.length).to eq(SampleManifestExcel::Configuration::FILES.length + 1) 
+    expect(configuration.files.length).to eq(SampleManifestExcel::Configuration::FILES.length + 1)
     expect(configuration.files).to include(:a_new_file)
     expect(configuration).to respond_to('a_new_file=')
+  end
+
+  it 'should be able to set and get a tag group' do
+    expect(configuration.tag_group).to be nil
+    configuration.tag_group = 'Main test group'
+    expect(configuration.tag_group).to be_an_instance_of(TagGroup)
+    expect(configuration.tag_group.name).to eq 'Main test group'
   end
 
   context 'without a folder' do
@@ -39,24 +46,24 @@ RSpec.describe SampleManifestExcel::Configuration, type: :model, sample_manifest
 
     it 'will load the columns' do
       columns = SampleManifestExcel::ColumnList.new(configuration.load_file(folder, 'columns'), configuration.conditional_formattings)
-      expect(configuration.columns.all).to eq(columns) 
+      expect(configuration.columns.all).to eq(columns)
       configuration.manifest_types.each do |k, v|
-        expect(configuration.columns.send(k)).to eq(columns.extract(v.columns)) 
+        expect(configuration.columns.send(k)).to eq(columns.extract(v.columns))
         expect(configuration.columns.find(k)).to eq(columns.extract(v.columns))
-        expect(configuration.columns.find(k.to_sym)).to eq(columns.extract(v.columns)) 
+        expect(configuration.columns.find(k.to_sym)).to eq(columns.extract(v.columns))
       end
     end
 
     it 'load the conditional formattings' do
-      expect(configuration.conditional_formattings).to eq(SampleManifestExcel::ConditionalFormattingDefaultList.new(configuration.load_file(folder, 'conditional_formattings'))) 
+      expect(configuration.conditional_formattings).to eq(SampleManifestExcel::ConditionalFormattingDefaultList.new(configuration.load_file(folder, 'conditional_formattings')))
     end
 
     it 'load the manifest types' do
-      expect(configuration.manifest_types).to eq(SampleManifestExcel::ManifestTypeList.new(configuration.load_file(folder, 'manifest_types'))) 
+      expect(configuration.manifest_types).to eq(SampleManifestExcel::ManifestTypeList.new(configuration.load_file(folder, 'manifest_types')))
     end
 
     it 'load the ranges' do
-      expect(configuration.ranges).to eq(SampleManifestExcel::RangeList.new(configuration.load_file(folder, 'ranges'))) 
+      expect(configuration.ranges).to eq(SampleManifestExcel::RangeList.new(configuration.load_file(folder, 'ranges')))
     end
 
     it 'freeze all of the configuration options' do
