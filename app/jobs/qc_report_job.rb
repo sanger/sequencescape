799ee489798aef354786d32c6c4f3670de0df969
James Glover <JamesGlover@users.noreply.github.com>
2017-03-17 16:31:18 +0000

    Remove innacurate comment

--- app/jobs/qc_report_job.rb
+++ app/jobs/qc_report_job.rb
@@ -1,6 +1,3 @@
-# Generate CreateAssetRequests for the provided assets, linking them to the study.
-# Currently used in Tube sample manifests.
-# JG: Not entirely sure this is all that useful any more.
 QcReportJob = Struct.new(:qc_report_id) do
   def perform
     QcReport.find(qc_report_id).generate!
