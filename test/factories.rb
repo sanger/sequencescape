1de13df9dea54b99371c92d1937e8e44990ac697
Yana Proskurina <yana.proskurina@sanger.ac.uk>
2017-03-30 13:25:09 +0100

    qpcr pooling, rubocoped

--- test/factories.rb
+++ test/factories.rb
@@ -417,7 +417,6 @@ FactoryGirl.define do
     key { generate :purpose_name }
   end
 
-
   factory :transfer_request do
     association(:asset, factory: :well)
     association(:target_asset, factory: :well)
