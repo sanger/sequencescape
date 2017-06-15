b43bef9f46828d2fb7f33d129617671f7249af7e
James Glover <james.glover@sanger.ac.uk>
2017-06-15 16:15:29 +0100

    Only apply new behaviour to FromPlateToSpecificTubesByPool

--- app/models/transfer/from_plate_to_specific_tubes.rb
+++ app/models/transfer/from_plate_to_specific_tubes.rb
@@ -10,11 +10,12 @@ class Transfer::FromPlateToSpecificTubes < Transfer::BetweenPlateAndTubes
     @targets = Uuid.lookup_many_uuids(uuids_for_tubes).map(&:resource)
   end
 
+  private
+
   def locate_mx_library_tube_for(well, stock_wells)
     return nil if stock_wells.empty?
     @tubes_to_pick  ||= targets.dup
     @pools_to_tubes ||= Hash.new { |h, k| h[k] = @tubes_to_pick.shift or raise 'Not enough tubes to pick for pool' }
     @pools_to_tubes[well.pool_id]
   end
-  private :locate_mx_library_tube_for
 end
