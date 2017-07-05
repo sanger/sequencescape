e1871d44eab53248cf317eaaed1986f7105e2604
James Glover <james.glover@sanger.ac.uk>
2017-07-05 16:34:08 +0100

    General code cleanup

--- app/models/tasks/start_batch_handler.rb
+++ app/models/tasks/start_batch_handler.rb
@@ -7,6 +7,6 @@
 module Tasks::StartBatchHandler
   def do_start_batch_task(task, params)
     return unless task.lab_activity?
-    Batch.find(params[:batch_id]).start!(current_user) unless @batch.started? or @batch.failed?
+    Batch.find(params[:batch_id]).start!(current_user) if @batch.may_start?
   end
 end
