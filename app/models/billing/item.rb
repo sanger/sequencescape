01f722ecc10997635a3525a8b0b564d19caf24cf
Yana Proskurina <yana.proskurina@sanger.ac.uk>
2017-09-15 15:47:27 +0100

    Updated comments

--- app/models/billing/item.rb
+++ app/models/billing/item.rb
@@ -1,6 +1,6 @@
 module Billing
   # Simple record that stores (dynamic) data required to generate billing BIF file
-  # it store request_id, project_cost_code, units, fin_product_code, fin_product_description, request_passed_date
+  # it store request_id, project_cost_code, units, billing_product_code, billing_product_description, request_passed_date
   class Item < ActiveRecord::Base
     belongs_to :request
 
