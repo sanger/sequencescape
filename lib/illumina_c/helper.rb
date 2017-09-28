4135f56c285a33562ec59189f02017e743ac4fde
James Glover <james.glover@sanger.ac.uk>
2017-09-28 10:36:20 +0100

    Reorganise Order class
    - Reorder code to meet style guide
    - Move Order::OrderRole to OrderRole

--- lib/illumina_c/helper.rb
+++ lib/illumina_c/helper.rb
@@ -111,7 +111,7 @@ module IlluminaC::Helper
       {
         request_type_ids_list: request_type_ids(cherrypick, sequencing),
         workflow_id: Submission::Workflow.find_by(key: 'short_read_sequencing').id,
-        order_role_id: Order::OrderRole.find_or_create_by(role: role).id,
+        order_role_id: OrderRole.find_or_create_by(role: role).id,
         info_differential: Submission::Workflow.find_by(key: 'short_read_sequencing').id
       }
     end
