5e5b40af00fe634c4b8e4071942a4a46e4956bbd
Yana Proskurina <yana.proskurina@sanger.ac.uk>
2017-10-11 16:17:51 +0100

    Partial multiplexed library manifest upload works

--- lib/sample_manifest_excel/sample_manifest_excel/tags/validator.rb
+++ lib/sample_manifest_excel/sample_manifest_excel/tags/validator.rb
@@ -13,7 +13,7 @@ module SampleManifestExcel
           tag_oligo_column = upload.columns.find_by(:name, :tag_oligo)
           tag2_oligo_column = upload.columns.find_by(:name, :tag2_oligo)
           if tag_oligo_column.present? & tag2_oligo_column.present?
-            combinations = upload.data.column(tag_oligo_column.number).zip(upload.data.column(tag2_oligo_column.number))
+            combinations = upload.rows.data_at(tag_oligo_column.number).zip(upload.rows.data_at(tag2_oligo_column.number))
             errors.add(:tags_combinations, 'are not unique') unless combinations.length == combinations.uniq.length
           end
         end
