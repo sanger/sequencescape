3d713d0330ab6005caba464151ff1692c3075bbf
Yana Proskurina <yana.proskurina@sanger.ac.uk>
2017-10-18 18:07:43 +0100

    Updated AgressoProduct

--- app/models/billing/agresso_product.rb
+++ app/models/billing/agresso_product.rb
@@ -5,7 +5,7 @@ module Billing
     self.table_name = 'AGR55.UVIOPSPRODMAPPING'
 
     def self.billing_product_code(billing_product_name)
-      product = find_by(ops_prod_name: billing_product_name)
+      product = find_by(fin_prod_name: billing_product_name)
       product.present? ? product.fin_prod : 'LM9999'
     end
   end
