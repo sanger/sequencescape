1a0db494c8585b89b260331f11cf928f15988917
James Glover <james.glover@sanger.ac.uk>
2017-10-27 11:47:15 +0100

    We're returning an array, not a request

--- app/models/work_completion.rb
+++ app/models/work_completion.rb
@@ -33,30 +33,31 @@ class WorkCompletion < ApplicationRecord
     target_wells.each do |target_well|
       next if target_well.stock_wells.empty?
       # Upstream requests our on our stock wells.
-      upstream = detect_upstream_requests(target_well)
+      detect_upstream_requests(target_well).each do |upstream|
 
-      # We need to find the downstream requests BEFORE connecting the upstream
-      # This is because submission.next_requests tries to take a shortcut through
-      # the target_asset if it is defined.
-      downstream = upstream.submission.next_requests(upstream)
-      downstream.each { |ds| ds.update_attributes!(asset: target_well) }
+        # We need to find the downstream requests BEFORE connecting the upstream
+        # This is because submission.next_requests tries to take a shortcut through
+        # the target_asset if it is defined.
+        downstream = upstream.submission.next_requests(upstream)
+        downstream.each { |ds| ds.update_attributes!(asset: target_well) }
 
-      # In some cases, such as the Illumina-C pipelines, requests might be
-      # connected upfront. We don't want to touch these.
-      upstream.target_asset ||= target_well
-      # We don't try and pass failed requests.
-      # I'm not 100% convinced this decision belongs here, and instead
-      # we may want to let the client specify wells to pass, and perform
-      # validation to ensure this is correct. However this increases
-      # the complexity of both the code and the interface, with only
-      # marginal system simplification.
-      upstream.pass if upstream.may_pass?
-      upstream.save!
+        # In some cases, such as the Illumina-C pipelines, requests might be
+        # connected upfront. We don't want to touch these.
+        upstream.target_asset ||= target_well
+        # We don't try and pass failed requests.
+        # I'm not 100% convinced this decision belongs here, and instead
+        # we may want to let the client specify wells to pass, and perform
+        # validation to ensure this is correct. However this increases
+        # the complexity of both the code and the interface, with only
+        # marginal system simplification.
+        upstream.pass if upstream.may_pass?
+        upstream.save!
+      end
     end
   end
 
   def detect_upstream_requests(target_well)
-    upstream_requests = target_well.stock_wells.each_with_object(upstream_requests) do |source_well, upstream_requests|
+    upstream_requests = target_well.stock_wells.each_with_object([]) do |source_well, upstream_requests|
       # We may have multiple requests out of each well, however we're only concerned
       # about those associated with the active submission.
       # We've already eager loaded requests out of the stock wells, so filter in Ruby.
