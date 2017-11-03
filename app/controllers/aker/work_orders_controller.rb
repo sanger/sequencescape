2c6effe1955c7b0e76732703465f319a0b9ae973
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-11-03 11:05:14 +0000

    Added functionality to be able to complete and cancel work orders using the UI

--- app/controllers/aker/work_orders_controller.rb
+++ app/controllers/aker/work_orders_controller.rb
@@ -10,6 +10,7 @@ module Aker
     def show
       @work_order = current_resource
       @aker_work_order = JSON.parse(RestClient::Request.execute(
+        verify_ssl: false,
         method: :get,
         url: "#{Rails.configuration.aker['urls']['work_orders']}/work_orders/#{@work_order.aker_id}",
         headers: { content_type: :json, Accept: :json },
@@ -20,25 +21,31 @@ module Aker
     def complete
       work_order = current_resource
       response = RestClient::Request.execute(
+        verify_ssl: false,
         method: :post,
         url: "#{Rails.configuration.aker['urls']['work_orders']}/work_orders/#{work_order.aker_id}/complete",
         payload: { work_order: {work_order_id: work_order.aker_id, comment: params[:comment]} }.to_json,
         headers: { content_type: :json },
         proxy: nil
       )
-      render json: response
+      flash[:notice] = JSON.parse(response.body)['message']
+
+      redirect_to aker_work_order_path(work_order)
     end
 
     def cancel
       work_order = current_resource
       response = RestClient::Request.execute(
+        verify_ssl: false,
         method: :post,
         url: "#{Rails.configuration.aker['urls']['work_orders']}/work_orders/#{work_order.aker_id}/cancel",
         payload: { work_order: {work_order_id: work_order.aker_id, comment: params[:comment]} }.to_json, 
         headers: {content_type: :json},
         proxy: nil
       )
-      render json: response
+      flash[:notice] = JSON.parse(response.body)['message']
+      
+      redirect_to aker_work_order_path(work_order)
     end
 
     private
