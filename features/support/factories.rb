226e705f99444d90f19436ef6d4af5d4b2afeb62
Stephen Inglis <si5@sanger.ac.uk>
2018-01-09 11:39:32 +0000

    Rubocopped. Removed references to test factories.

--- features/support/factories.rb
+++ features/support/factories.rb
@@ -4,8 +4,6 @@
 # authorship of this file.
 # Copyright (C) 2011,2015 Genome Research Ltd.
 
-require File.expand_path(File.join(Rails.root, %w{test factories.rb}))
-
-Dir.glob(File.expand_path(File.join(Rails.root, %w{test factories ** *.rb}))) do |factory_filename|
+Dir.glob(File.expand_path(File.join(Rails.root, %w{spec factories ** *.rb}))) do |factory_filename|
   require factory_filename
 end
