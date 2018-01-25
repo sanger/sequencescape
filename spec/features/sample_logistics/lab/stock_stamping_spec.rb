a968c7c3f8bae73156b2dde0d8413a46cf8e0516
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2018-01-25 11:45:20 +0000

    Fixed rake tests. Minor change to spec

--- spec/features/sample_logistics/lab/stock_stamping_spec.rb
+++ spec/features/sample_logistics/lab/stock_stamping_spec.rb
@@ -7,8 +7,11 @@ feature 'stamping of stock', js: true do
   let(:user) { create :admin, barcode: 'ID41440E' }
   let(:plate) { create :plate_with_3_wells, barcode: '1' }
   let!(:barcode_printer) { create :barcode_printer }
-  let!(:plate_type1) { create :plate_type, name: 'ABgene_0800', maximum_volume: 180 }
-  let!(:plate_type2) { create :plate_type, name: 'ABgene_0765', maximum_volume: 800 }
+
+  before do
+    create :plate_type, name: 'ABgene_0800', maximum_volume: 180
+    create :plate_type, name: 'ABgene_0765', maximum_volume: 800
+  end
 
   scenario 'stamping of stock' do
     plate.wells.first.set_current_volume(1000)
