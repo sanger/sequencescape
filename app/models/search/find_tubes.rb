68322be895454e64ff1eda67afea8a8f02093809
James Glover <james.glover@sanger.ac.uk>
2018-01-26 11:02:47 +0000

    Fix api for tube search and tags

--- app/models/search/find_tubes.rb
+++ app/models/search/find_tubes.rb
@@ -4,12 +4,14 @@ class Search::FindTubes < Search
   def scope(user_criteria)
     criteria = default_parameters.stringify_keys.merge(user_criteria)
 
-    if criteria['plate_purpose_uuids']
-      criteria['plate_purpose_ids'] = Uuid.where(resource_type: 'Purpose', external_id: criteria['plate_purpose_uuids'])
-                                          .pluck(:resource_id)
-    end
+    purpose_ids = if criteria['tube_purpose_uuids']
+                    Uuid.where(resource_type: 'Purpose', external_id: criteria['tube_purpose_uuids'])
+                        .pluck(:resource_id)
+                  else
+                    criteria['tube_purpose_ids']
+                  end
 
-    Tube.with_purpose(criteria['plate_purpose_ids'])
+    Tube.with_purpose(purpose_ids)
         .including_used_plates?(criteria['include_used'])
         .page(criteria['page']).limit(criteria['limit']).order(id: :desc)
   end
