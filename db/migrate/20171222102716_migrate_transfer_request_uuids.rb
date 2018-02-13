c2ab793ee1831a989285e154424a777620dac6ca
James Glover <james.glover@sanger.ac.uk>
2018-02-13 14:53:04 +0000

    Fix broken migration

--- db/migrate/20171222102716_migrate_transfer_request_uuids.rb
+++ db/migrate/20171222102716_migrate_transfer_request_uuids.rb
@@ -6,7 +6,7 @@ class MigrateTransferRequestUuids < ActiveRecord::Migration[5.1]
     ActiveRecord::Base.transaction do
       Uuid.where(resource_type: 'Request')
           .joins('INNER JOIN transfer_requests ON transfer_requests.id = resource_id')
-          .joins('LEFT OUTER JOIN equests ON requests.id = resource_id')
+          .joins('LEFT OUTER JOIN requests ON requests.id = resource_id')
           .where('transfer_requests.id IS NOT NULL')
           .where('requests.id IS NULL')
           .update_all(resource_type: 'TransferRequest') # rubocop:disable Rails/SkipsModelValidations
