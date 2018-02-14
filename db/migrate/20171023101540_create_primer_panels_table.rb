7774310acac05d626322434013416d7eaf242efb
James Glover <james.glover@sanger.ac.uk>
2018-02-14 11:37:24 +0000

    Fix table name in migration

--- db/migrate/20171023101540_create_primer_panels_table.rb
+++ db/migrate/20171023101540_create_primer_panels_table.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class CreatePrimerPanelTable < ActiveRecord::Migration[5.1]
+class CreatePrimerPanelsTable < ActiveRecord::Migration[5.1]
   def change
-    create_table :primer_panel do |t|
+    create_table :primer_panels do |t|
       t.string :name, null: false
       t.integer :snp_count, null: false
       t.timestamps null: false
