69177d73ce9f54d88c00b586f882b4d17758d426
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2018-03-23 16:45:16 +0000

    Added extra conf for Capybara in feature tests

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -4,4 +4,6 @@ Capybara.configure do |config|
   config.server = :puma
 end
 
+Capybara.save_path = 'tmp/capybara'
+Capybara.default_max_wait_time = 10
 Capybara.javascript_driver = :selenium_chrome_headless
