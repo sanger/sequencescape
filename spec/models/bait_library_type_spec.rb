34de94cd034275a8aea0afb2256c12a5a539ae81
James Glover <james.glover@sanger.ac.uk>
2018-03-27 11:58:20 +0100

    Simplify bait library test
    It isn't entirely clear why this test was so verbose. Drastically simplified it.

--- spec/models/bait_library_type_spec.rb
+++ spec/models/bait_library_type_spec.rb
@@ -22,32 +22,12 @@ describe BaitLibraryType do
   end
 
   context 'A request with a bait library' do
-    before do
-      @sample = create :sample
 
-      @pulldown_request_type = create :request_type, name: 'Bait Pulldown', target_asset_type: nil
-      @sequencing_request_type = create :request_type, name: 'Single ended sequencing2'
-      @submission = FactoryHelp.submission(request_types: [@pulldown_request_type, @sequencing_request_type].map(&:id), asset_group_name: 'to avoid asset errors')
-      @item = create :item, submission: @submission
-
-      @genotype_pipeline = create :pipeline, name: 'Cluster formation SE2', request_types: [@sequencing_request_type]
-      @pulldown_pipeline = create :pipeline, name: 'Bait Pulldown', request_types: [@pulldown_request_type], next_pipeline_id: @genotype_pipeline.id, asset_type: 'LibraryTube'
-
-      @request1 = create(
-        :request_without_assets,
-        item: @item,
-        asset: create(:empty_sample_tube).tap { |sample_tube| sample_tube.aliquots.create!(sample: @sample) },
-        target_asset: nil,
-        submission: @submission,
-        request_type: @pulldown_request_type,
-        pipeline: @pulldown_pipeline
-      )
-
-      # @request1.request_metadata.bait_library = create(:bait_library)
-    end
+    let(:request_type) { create :request_type, name: 'Bait Pulldown', target_asset_type: nil }
+    let(:request) { create :isc_request }
 
     it 'have a bait library type' do
-      expect(BaitLibrary.find(@request1.request_metadata.bait_library_id).bait_library_type).to be_truthy
+      expect(request.request_metadata.bait_library.bait_library_type).to be_truthy
     end
   end
 end
