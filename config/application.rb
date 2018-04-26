dfe06e00a67e5af6e61295f84f94bcc6e1c37fc0
Stephen Inglis <si5@sanger.ac.uk>
2018-04-26 11:56:00 +0100

    Rubocopped.

--- config/application.rb
+++ config/application.rb
@@ -109,7 +109,7 @@ module Sequencescape
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
   end
