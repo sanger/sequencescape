67ed63021e2a5f53645e0d31ecb3f83344786a9f
Harriet Craven <hc6@sanger.ac.uk>
2018-05-03 14:25:57 +0100

    Rename factory job, to jobs

---
+++ spec/factories/aker/jobs.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+FactoryGirl.define do
+  factory :aker_job, class: Aker::Job do
+    sequence(:aker_job_id) { |n| n }
+
+    factory :aker_job_with_samples do
+      samples { create_list(:sample_for_job, 5) }
+    end
+  end
+end
