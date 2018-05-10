6144fcca8e2089e0f1cfef75ee7743b6fd33c2b3
James Glover <james.glover@sanger.ac.uk>
2018-05-10 13:09:29 +0100

    Add plate endpoint

--- app/resources/api/v2/plate_resource.rb
+++ app/resources/api/v2/plate_resource.rb
@@ -4,40 +4,36 @@ module Api
   module V2
     # Provides a JSON API representation of receptacle
     # See: http://jsonapi-resources.com/ for JSONAPI::Resource documentation
-    class WellResource < BaseResource
+    class PlateResource < BaseResource
       # Constants...
 
       immutable # uncomment to make the resource immutable
 
-      default_includes :uuid_object, :map, plate: :barcode_prefix
+      default_includes :uuid_object, :barcode_prefix
 
       # Associations:
       has_many :samples, readonly: true
       has_many :studies, readonly: true
       has_many :projects, readonly: true
-      has_many :qc_results, readonly: true
+      has_many :wells, readonly: true
 
       # Attributes
       attribute :uuid, readonly: true
       attribute :name, delegate: :display_name, readonly: true
-      attribute :position, readonly: true
       attribute :labware_barcode, readonly: true
 
       # Filters
+      filter :barcode, apply: (lambda do |records, value, _options|
+        records.with_machine_barcode(value)
+      end)
 
       # Custom methods
       # These shouldn't be used for business logic, and are more about
       # I/O and isolating implementation details.
       def labware_barcode
         {
-          'ean13_barcode' => _model.plate&.ean13_barcode,
-          'sanger_human_barcode' => _model.plate&.sanger_human_barcode
-        }
-      end
-
-      def position
-        {
-          'name' => _model.map_description
+          'ean13_barcode' => _model.ean13_barcode,
+          'sanger_human_barcode' => _model.sanger_human_barcode
         }
       end
 
--- spec/resources/api/v2/plate_resource_spec.rb
+++ spec/resources/api/v2/plate_resource_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
-require './app/resources/api/v2/well_resource'
+require './app/resources/api/v2/plate_resource'
 
-RSpec.describe Api::V2::WellResource, type: :resource do
-  let(:resource_model) { create :well, plate: plate, map: position }
+RSpec.describe Api::V2::PlateResource, type: :resource do
+  let(:resource_model) { create :plate, barcode: 11, well_count: 3 }
   subject(:resource) { described_class.new(resource_model, {}) }
 
-  shared_examples 'a well resource' do
+  shared_examples 'a plate resource' do
     # Test attributes
     it { is_expected.to have_attribute :uuid }
     it { is_expected.to have_attribute :name }
@@ -16,7 +16,6 @@ RSpec.describe Api::V2::WellResource, type: :resource do
     it { is_expected.to_not have_updatable_field(:id) }
     it { is_expected.to_not have_updatable_field(:uuid) }
     it { is_expected.to_not have_updatable_field(:name) }
-    it { is_expected.to_not have_updatable_field(:position) }
     it { is_expected.to_not have_updatable_field(:labware_barcode) }
 
     # Updatable fields
@@ -26,10 +25,9 @@ RSpec.describe Api::V2::WellResource, type: :resource do
     # eg. it { is_expected.to filter(:order_type) }
 
     # Associations
-    it { is_expected.to have_many(:samples).with_class_name('Sample') }
+    it { is_expected.to have_many(:wells).with_class_name('Well') }
     it { is_expected.to have_many(:projects).with_class_name('Project') }
     it { is_expected.to have_many(:studies).with_class_name('Study') }
-    it { is_expected.to have_many(:qc_results).with_class_name('QcResult') }
 
     # Custom method tests
     # Add tests for any custom methods you've added.
@@ -37,26 +35,10 @@ RSpec.describe Api::V2::WellResource, type: :resource do
       subject { resource.labware_barcode }
       it { is_expected.to eq(expected_barcode_hash) }
     end
-
-    describe '#position' do
-      subject { resource.position }
-      it { is_expected.to eq(expected_position) }
-    end
   end
 
   context 'on a plate' do
-    let(:plate) { create :plate, barcode: '11' }
-    let(:position) { create :map, description: 'A1' }
     let(:expected_barcode_hash) { { 'ean13_barcode' => '1220000011748', 'sanger_human_barcode' => 'DN11J' } }
-    let(:expected_position) { { 'name' => 'A1' } }
-    it_behaves_like 'a well resource'
-  end
-
-  context 'off a plate' do
-    let(:plate) { nil }
-    let(:position) { nil }
-    let(:expected_barcode_hash) { { 'ean13_barcode' => nil, 'sanger_human_barcode' => nil } }
-    let(:expected_position) { { 'name' => nil } }
-    it_behaves_like 'a well resource'
+    it_behaves_like 'a plate resource'
   end
 end
