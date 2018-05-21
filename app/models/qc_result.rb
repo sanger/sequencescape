ce82f1cb9488bf56c120cf5a3d75e1ee0e06f87c
Rubocop <sequencescape@sanger.ac.uk>
2018-05-21 11:40:54 +0100

    Rubocop - autofixes
    This is an autogenerated commit message following a rubocop autocorrect

--- app/models/qc_result.rb
+++ app/models/qc_result.rb
@@ -6,14 +6,14 @@ class QcResult < ApplicationRecord
 
   belongs_to :asset, required: true
   belongs_to :qc_assay, required: false
-  
+
   after_save :update_asset
   after_commit :broadcast_qc_result, on: [:create, :update]
 
   validates :key, :value, :units, presence: true
-  
+
   private
-  
+
   def update_asset
     asset.update_from_qc(self)
   end
