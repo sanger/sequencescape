9b582934d4aecaa70952799ab62920c4e2b6b43e
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2018-05-24 12:04:18 +0100

    Rubocopped

--- lib/psd_formatter.rb
+++ lib/psd_formatter.rb
@@ -2,7 +2,7 @@ require 'syslog/logger'
 require 'ostruct'
 
 class PsdFormatter < Syslog::Logger::Formatter
-  Format = "(thread-%s) [%s] %5s -- : %s\n".freeze
+  LINE_FORMAT = "(thread-%s) [%s] %5s -- : %s\n".freeze
 
   def initialize(deployment_info)
     info = OpenStruct.new(deployment_info)
@@ -12,13 +12,13 @@ class PsdFormatter < Syslog::Logger::Formatter
 
   def call(severity, timestamp, progname, msg)
     thread_id = Thread.current.object_id
-    Format % [thread_id, @app_tag, format_severity(severity), msg]
+    format(LINE_FORMAT, thread_id, @app_tag, format_severity(severity), msg)
   end
 
   private
 
   # Severity label for logging (max 5 chars).
-  SEV_LABEL = %w(DEBUG INFO WARN ERROR FATAL ANY).each(&:freeze).freeze
+  SEV_LABEL = %w[DEBUG INFO WARN ERROR FATAL ANY].each(&:freeze).freeze
 
   def format_severity(severity)
     if severity.is_a?(Integer)
