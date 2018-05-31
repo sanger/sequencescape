606a3d0ba86907942b063bff6cf3cda1e0b4341f
Andrew Sparkes <as28@sanger.ac.uk>
2018-05-31 13:49:37 +0100

    rubocopped

--- spec/sample_manifest_excel/column_spec.rb
+++ spec/sample_manifest_excel/column_spec.rb
@@ -5,11 +5,11 @@ require 'rails_helper'
 RSpec.describe SampleManifestExcel::Column, type: :model, sample_manifest_excel: true do
   let(:range_list)  { build(:range_list, ranges_data: { FactoryBot.attributes_for(:validation)[:range_name] => FactoryBot.attributes_for(:range) }) }
   let(:worksheet)   { Axlsx::Workbook.new.add_worksheet }
-  let(:options)     {
+  let(:options)     do
     { heading: 'PUBLIC NAME', name: :public_name, type: :string, value: 10, number: 125, attribute: :barcode,
       validation: FactoryBot.attributes_for(:validation),
       conditional_formattings: { simple: FactoryBot.attributes_for(:conditional_formatting), complex: FactoryBot.attributes_for(:conditional_formatting_with_formula) } }
-  }
+  end
 
   it 'must have a heading' do
     expect(SampleManifestExcel::Column.new(options).heading).to eq(options[:heading])
