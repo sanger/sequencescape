dc787f73605eb231cc207a9d2b620c7adabaa92a
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2018-07-10 10:27:13 +0100

    Removed unused default_attributes concern.

--- app/models/transfer/from_plate_to_tube.rb
+++ app/models/transfer/from_plate_to_tube.rb
@@ -20,17 +20,8 @@ class Transfer::FromPlateToTube < Transfer
 
   private
 
-  def each_transfer
-    # Partition the source plate wells into ones that are good and others that are bad.  The
-    # bad wells will be eliminated after we've done the transfers for the good ones.
-    bad_wells, good_wells = source.wells.includes(:aliquots, :transfer_requests_as_target).located_at_position(transfers).with_pool_id.partition do |well|
-      well.nil? or well.aliquots.empty? or well.failed? or well.cancelled?
-    end
-
-    good_wells.each { |well| yield(well, destination) }
-
-    # Eliminate any of the transfers that were not made because of the bad source wells
-    self.transfers = transfers - bad_wells.map { |well| well.map.description }
+  def default_to_all_wells
+    self.transfers ||= source.wells.includes(:map).map(&:map_description)
   end
 
   def update_tube_name
@@ -39,7 +30,17 @@ class Transfer::FromPlateToTube < Transfer
     destination.update_attributes!(name: "#{source_barcode} #{range}")
   end
 
-  def default_to_all_wells
-    self.transfers ||= source.wells.includes(:map).map(&:map_description)
+  def each_transfer
+    # Partition the source plate wells into ones that are good and others that are bad.  The
+    # bad wells will be eliminated after we've done the transfers for the good ones.
+    source_wells = source.wells.includes(:aliquots, :transfer_requests_as_target)
+    bad_wells, good_wells = source_wells.located_at_position(transfers).with_pool_id.partition do |well|
+      well.nil? or well.aliquots.empty? or well.failed? or well.cancelled?
+    end
+
+    good_wells.each { |well| yield(well, destination) }
+
+    # Eliminate any of the transfers that were not made because of the bad source wells
+    self.transfers = transfers - bad_wells.map { |well| well.map.description }
   end
 end
