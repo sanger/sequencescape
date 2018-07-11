58d8defc979ed7fe7da4f4106665737bcb74c692
James Glover <james.glover@sanger.ac.uk>
2018-07-11 16:58:48 +0100

    Rouch batches_controler tweaks

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,18 +1,20 @@
 
+# frozen_string_literal: true
+
 class BatchesController < ApplicationController
   # WARNING! This filter bypasses security mechanisms in rails 4 and mimics rails 2 behviour.
   # It should be removed wherever possible and the correct Strong  Parameter options applied in its place.
 
   before_action :evil_parameter_hack!
 
-  before_action :login_required, except: %i(released qc_criteria)
-  before_action :find_batch_by_id, only: %i(
+  before_action :login_required, except: %i[released qc_criteria]
+  before_action :find_batch_by_id, only: %i[
     show edit update qc_information qc_batch save fail fail_items
     fail_batch control add_control print_labels print_plate_labels print_multiplex_labels
     print verify verify_tube_layout reset_batch previous_qc_state filtered swap
     download_spreadsheet gwl_file pacbio_sample_sheet sample_prep_worksheet
-  )
-  before_action :find_batch_by_batch_id, only: %i(sort print_multiplex_barcodes print_pulldown_multiplex_tube_labels print_plate_barcodes print_barcodes)
+  ]
+  before_action :find_batch_by_batch_id, only: %i[sort print_multiplex_barcodes print_pulldown_multiplex_tube_labels print_plate_barcodes print_barcodes]
 
   def index
     if logged_in?
@@ -37,7 +39,8 @@ class BatchesController < ApplicationController
         @pipeline = @batch.pipeline
         @tasks    = @batch.tasks.sort_by(&:sorted)
         @rits = @pipeline.request_information_types
-        @input_assets, @output_assets = [], []
+        @input_assets = []
+        @output_assets = []
 
         if @pipeline.group_by_parent
           @input_assets = @batch.input_group
@@ -96,10 +99,10 @@ class BatchesController < ApplicationController
     end
   rescue ActiveRecord::RecordInvalid => exception
     respond_to do |format|
-      format.html {
+      format.html do
         flash[:error] = exception.record.errors.full_messages
         redirect_to(pipeline_path(@pipeline))
-      }
+      end
       format.xml { render xml: @batch.errors.to_xml }
     end
   end
@@ -114,18 +117,17 @@ class BatchesController < ApplicationController
     @batch.qc_complete
 
     @batch.batch_requests.each do |br|
-      if br && params[(br.request_id).to_s]
-        qc_state = params[(br.request_id).to_s]['qc_state']
-        target = br.request.target_asset
-        if qc_state == 'fail'
-          target.set_qc_state('failed')
-          EventSender.send_fail_event(br.request_id, '', 'Failed manual QC', @batch.id)
-        elsif qc_state == 'pass'
-          target.set_qc_state('passed')
-          EventSender.send_pass_event(br.request_id, '', 'Passed manual QC', @batch.id)
-        end
-        target.save
+      next unless br && params[br.request_id.to_s]
+      qc_state = params[br.request_id.to_s]['qc_state']
+      target = br.request.target_asset
+      if qc_state == 'fail'
+        target.set_qc_state('failed')
+        EventSender.send_fail_event(br.request_id, '', 'Failed manual QC', @batch.id)
+      elsif qc_state == 'pass'
+        target.set_qc_state('passed')
+        EventSender.send_pass_event(br.request_id, '', 'Passed manual QC', @batch.id)
       end
+      target.save
     end
 
     @batch.release_without_user!
@@ -137,14 +139,14 @@ class BatchesController < ApplicationController
     # The params fallback here reflects an older route where pipeline got passed in as :id. It should be removed
     # in the near future.
     @pipeline = Pipeline.find(params[:pipeline_id] || params[:id])
-    @batches = @pipeline.batches.pending.order(id: :desc).includes(%i(user pipeline)).page(params[:page])
+    @batches = @pipeline.batches.pending.order(id: :desc).includes(%i[user pipeline]).page(params[:page])
   end
 
   def started
     # The params fallback here reflects an older route where pipeline got passed in as :id. It should be removed
     # in the near future.
     @pipeline = Pipeline.find(params[:pipeline_id] || params[:id])
-    @batches = @pipeline.batches.started.order(id: :desc).includes(%i(user pipeline)).page(params[:page])
+    @batches = @pipeline.batches.started.order(id: :desc).includes(%i[user pipeline]).page(params[:page])
   end
 
   def released
@@ -152,7 +154,7 @@ class BatchesController < ApplicationController
     # in the near future.
     @pipeline = Pipeline.find(params[:pipeline_id] || params[:id])
 
-    @batches = @pipeline.batches.released.order(id: :desc).includes(%i(user pipeline)).page(params[:page])
+    @batches = @pipeline.batches.released.order(id: :desc).includes(%i[user pipeline]).page(params[:page])
     respond_to do |format|
       format.html
       format.xml { render layout: false }
@@ -163,22 +165,22 @@ class BatchesController < ApplicationController
     # The params fallback here reflects an older route where pipeline got passed in as :id. It should be removed
     # in the near future.
     @pipeline = Pipeline.find(params[:pipeline_id] || params[:id])
-    @batches = @pipeline.batches.completed.order(id: :desc).includes(%i(user pipeline)).page(params[:page])
+    @batches = @pipeline.batches.completed.order(id: :desc).includes(%i[user pipeline]).page(params[:page])
   end
 
   def failed
     # The params fallback here reflects an older route where pipeline got passed in as :id. It should be removed
     # in the near future.
     @pipeline = Pipeline.find(params[:pipeline_id] || params[:id])
-    @batches = @pipeline.batches.failed.order(id: :desc).includes(%i(user pipeline)).page(params[:page])
+    @batches = @pipeline.batches.failed.order(id: :desc).includes(%i[user pipeline]).page(params[:page])
   end
 
   def fail
-    if @batch.workflow.source_is_internal?
-      @fail_reasons = FAILURE_REASONS['internal']
-    else
-      @fail_reasons = FAILURE_REASONS['external']
-    end
+    @fail_reasons = if @batch.workflow.source_is_internal?
+                      FAILURE_REASONS['internal']
+                    else
+                      FAILURE_REASONS['external']
+                    end
   end
 
   def fail_items
@@ -250,8 +252,7 @@ class BatchesController < ApplicationController
     redirect_to batch_path(@batch)
   end
 
-  def print_labels
-  end
+  def print_labels; end
 
   def print_stock_labels
     @batch = Batch.find(params[:id])
@@ -264,18 +265,14 @@ class BatchesController < ApplicationController
     @output_assets = @batch.plate_group_barcodes || []
 
     @output_assets.each do |parent, _children|
-      unless parent.nil?
-        plate_barcode = parent.human_barcode
-        if plate_barcode.present?
-          @output_barcodes << plate_barcode
-        end
-      end
+      next if parent.nil?
+      plate_barcode = parent.human_barcode
+      @output_barcodes << plate_barcode if plate_barcode.present?
     end
 
     if @output_barcodes.blank?
       flash[:error] = 'Output plates do not have barcodes to print'
       redirect_to controller: 'batches', action: 'show', id: @batch.id
-    else
     end
   end
 
@@ -287,11 +284,11 @@ class BatchesController < ApplicationController
       # We are trying to find the MX library tube or the stock MX library
       # tube. I've added a filter so it doesn't pick up Lanes.
       children = request.target_asset.children.last.children.select { |a| a.is_a?(Tube) }
-      if children.empty?
-        @asset = request.target_asset.children.last
-      else
-        @asset = request.target_asset.children.last.children.last
-      end
+      @asset = if children.empty?
+                 request.target_asset.children.last
+               else
+                 request.target_asset.children.last.children.last
+               end
     else
       flash[:notice] = 'There is no multiplexed library available.'
     end
@@ -318,46 +315,20 @@ class BatchesController < ApplicationController
   end
 
   def print_multiplex_barcodes
-    print_job = LabelPrinter::PrintJob.new(params[:printer],
-                                           LabelPrinter::Label::BatchMultiplex,
-                                           count: params[:count], printable: params[:printable], batch: @batch)
-    if print_job.execute
-      flash[:notice] = print_job.success
-    else
-      flash[:error] = print_job.errors.full_messages.join('; ')
-    end
-
-    redirect_to controller: 'batches', action: 'show', id: @batch.id
+    print_handler(LabelPrinter::Label::BatchMultiplex)
   end
 
   def print_plate_barcodes
-    print_job = LabelPrinter::PrintJob.new(params[:printer],
-                                           LabelPrinter::Label::BatchPlate,
-                                           count: params[:count], printable: params[:printable], batch: @batch)
-    if print_job.execute
-      flash[:notice] = print_job.success
-    else
-      flash[:error] = print_job.errors.full_messages.join('; ')
-    end
-
-    redirect_to controller: 'batches', action: 'show', id: @batch.id
+    print_handler(LabelPrinter::Label::BatchPlate)
   end
 
   def print_barcodes
     if @batch.requests.empty?
       flash[:notice] = 'Your batch contains no requests.'
+      redirect_to controller: 'batches', action: 'show', id: @batch.id
     else
-      print_job = LabelPrinter::PrintJob.new(params[:printer],
-                                             LabelPrinter::Label::BatchTube,
-                                             stock: params[:stock], count: params[:count], printable: params[:printable], batch: @batch)
-      if print_job.execute
-        flash[:notice] = print_job.success
-      else
-        flash[:error] = print_job.errors.full_messages.join('; ')
-      end
-
+      print_handler(LabelPrinter::Label::BatchTube)
     end
-    redirect_to controller: 'batches', action: 'show', id: @batch.id
   end
 
   def print
@@ -415,8 +386,7 @@ class BatchesController < ApplicationController
     redirect_to batch_url(@batch)
   end
 
-  def filtered
-  end
+  def filtered; end
 
   def swap
     if @batch.swap(current_user, 'batch_1' => { 'id' => params['batch']['1'], 'lane' => params['batch']['position']['1'] },
@@ -499,6 +469,19 @@ class BatchesController < ApplicationController
 
   private
 
+  def print_handler(template)
+    print_job = LabelPrinter::PrintJob.new(params[:printer],
+                                           template,
+                                           count: params[:count], printable: params[:printable], batch: @batch)
+    if print_job.execute
+      flash[:notice] = print_job.success
+    else
+      flash[:error] = print_job.errors.full_messages.join('; ')
+    end
+
+    redirect_to controller: 'batches', action: 'show', id: @batch.id
+  end
+
   def pipeline_error_on_batch_creation(message)
     respond_to do |format|
       flash[:error] = message
@@ -558,14 +541,14 @@ class BatchesController < ApplicationController
     end
 
     respond_to do |format|
-      format.html {
+      format.html do
         if @pipeline.has_controls?
           flash[:notice] = 'Batch created - now add a control'
           redirect_to action: :control, id: @batch.id
         else
           redirect_to action: :show, id: @batch.id
         end
-      }
+      end
       format.xml { head :created, location: batch_url(@batch) }
     end
   end
