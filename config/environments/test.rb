d5462ec7e313cab4194dd4ab2845e115d4fe831c
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2018-07-23 15:54:48 +0100

    Removed ETS oracle database lookup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,8 +37,6 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :raise
 
-  config.ets_enabled = true
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
