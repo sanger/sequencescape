5d6282a49e881ed22257a7e0d304f05c917a67c4
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2018-09-13 16:04:07 +0100

    Fixed printing behaviour for 384 plates in plate page

---
+++ lib/label_printer/label_printer/label/asset_plate_double.rb
@@ -0,0 +1,24 @@
+require_relative 'base_plate_double'
+
+module LabelPrinter
+  module Label
+    class AssetPlateDouble < BasePlateDouble
+      attr_reader :plates
+
+      def initialize(plates)
+        @plates = plates
+      end
+
+      def create_label(plate)
+        { left_text: plate.human_barcode,
+          right_text: "#{plate.prefix} #{plate.barcode_number}",
+          barcode: barcode(plate) }
+      end
+
+      def create_extra_label(plate)
+        { left_text: date_today,
+          right_text: plate.purpose.name }
+      end
+    end
+  end
+end
