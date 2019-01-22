9d122d351d1b79bd1c6cdcc71d746e0fed1f7215
Stephen Inglis <si5@sanger.ac.uk>
2019-01-22 15:21:02 +0000

    Made request resource immutable.

--- app/resources/api/v2/request_resource.rb
+++ app/resources/api/v2/request_resource.rb
@@ -7,7 +7,7 @@ module Api
     class RequestResource < BaseResource
       # Constants...
 
-      # immutable # uncomment to make the resource immutable
+      immutable # uncomment to make the resource immutable
 
       # model_name / model_hint if required
 
