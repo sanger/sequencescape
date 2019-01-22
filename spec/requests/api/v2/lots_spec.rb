c33c9a9e5a5805f9a7c1c441864c09d14e2a2cca
James Glover <james.glover@sanger.ac.uk>
2019-01-22 13:21:40 +0000

    ss773 - Extend API with tag groups and qcables
    Added endpoints for custom tag interface

---
+++ spec/requests/api/v2/lots_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Lots API', with: :api_v2 do
+  context 'with multiple Lots' do
+    before do
+      create_list(:lot, 5)
+    end
+
+    it 'sends a list of lots' do
+      api_get '/api/v2/lots'
+      # test for the 200 status-code
+      expect(response).to have_http_status(:success)
+      # check to make sure the right amount of messages are returned
+      expect(json['data'].length).to eq(5)
+    end
+
+    # Check filters, ESPECIALLY if they aren't simple attribute filters
+  end
+
+  context 'with a Lot' do
+    let(:resource_model) { create :lot }
+
+    it 'sends an individual Lot' do
+      api_get "/api/v2/lots/#{resource_model.id}"
+      expect(response).to have_http_status(:success)
+      expect(json.dig('data', 'type')).to eq('lots')
+    end
+
+    let(:payload) do
+      {
+        'data' => {
+          'id' => resource_model.id,
+          'type' => 'lots',
+          'attributes' => {
+            # Set new attributes
+          }
+        }
+      }
+    end
+
+    # Remove if immutable
+    it 'allows update of a Lot' do
+      api_patch "/api/v2/lots/#{resource_model.id}", payload
+      expect(response).to have_http_status(:success)
+      expect(json.dig('data', 'type')).to eq('lots')
+      # Double check at least one of the attributes
+      # eg. expect(json.dig('data', 'attributes', 'state')).to eq('started')
+    end
+  end
+end
---
+++ spec/requests/api/v2/tag_groups_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'TagGroups API', with: :api_v2 do
+  context 'with multiple TagGroups' do
+    before do
+      create_list(:tag_group, 5)
+    end
+
+    it 'sends a list of tag_groups' do
+      api_get '/api/v2/tag_groups'
+      # test for the 200 status-code
+      expect(response).to have_http_status(:success)
+      # check to make sure the right amount of messages are returned
+      expect(json['data'].length).to eq(5)
+    end
+
+    # Check filters, ESPECIALLY if they aren't simple attribute filters
+  end
+
+  context 'with a TagGroup' do
+    let(:resource_model) { create :tag_group }
+
+    it 'sends an individual TagGroup' do
+      api_get "/api/v2/tag_groups/#{resource_model.id}"
+      expect(response).to have_http_status(:success)
+      expect(json.dig('data', 'type')).to eq('tag_groups')
+    end
+
+    let(:payload) do
+      {
+        'data' => {
+          'id' => resource_model.id,
+          'type' => 'tag_groups',
+          'attributes' => {
+            # Set new attributes
+          }
+        }
+      }
+    end
+  end
+end
