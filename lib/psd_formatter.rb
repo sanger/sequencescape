b038a16bf7e0e5163a8b2da69444a5073c445aea
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2019-02-04 20:58:36 +0000

    Updated deployed_version and psd_formatter

--- lib/psd_formatter.rb
+++ lib/psd_formatter.rb
@@ -4,11 +4,11 @@ require 'syslog/logger'
 require 'ostruct'
 
 class PsdFormatter < Syslog::Logger::Formatter
-  LINE_FORMAT = "(thread-%s) [%s] %5s -- : %s\n".freeze
+  LINE_FORMAT = "(thread-%s) [%s] %5s -- : %s\n"
 
   def initialize(deployment_info)
     info = OpenStruct.new(deployment_info)
-    @app_tag = [info.name, info.version, info.environment].compact.join(':').freeze
+    @app_tag = "#{info.version}:#{info.environment}"
     super()
   end
 
