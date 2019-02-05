f8de2a10fec455a0e06decdb0cc52b5a47476eaa
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2019-02-05 17:06:19 +0000

    Fixed batch_plate_spec

--- spec/lib/label_printer/batch_plate_spec.rb
+++ spec/lib/label_printer/batch_plate_spec.rb
@@ -17,9 +17,10 @@ shared_examples 'a correct single label printer' do
           }
         }]
       }
-      expect(subject.to_h).to eq(expected_label)
-    end
+    }
+    expect(subject.to_h).to eq(expected_label)
   end
+end
 
 shared_examples 'a correct double label printer' do
   it 'should produce the correct label' do
@@ -39,94 +40,94 @@ shared_examples 'a correct double label printer' do
           }
         }]
       }
-      expect(subject.to_h).to eq(expected_label)
+    }
+    expect(subject.to_h).to eq(expected_label)
+  end
+end
+
+shared_examples 'a correct multi-copy printer' do
+  let(:count) { '3' }
+
+  it 'prints multiple copies' do
+    expect(subject.create_labels.count).to eq(expected_count)
+  end
+end
+
+shared_examples 'a correct filter' do
+  let(:printables) do
+    { plate1.human_barcode => 'on',
+      plate2.human_barcode => 'on' }
+  end
+
+  it 'should have the correct plates' do
+    expect(subject.assets).to eq([plate1, plate2])
+  end
+end
+
+context 'printing labels' do
+  let(:count) { '1' }
+  let(:date_today) { Time.zone.today.strftime('%e-%^b-%Y') }
+  let(:batch) { create :batch }
+  let(:study) { create :study }
+  let(:request1) do
+    order = create(:order,
+                   order_role: OrderRole.new(role: 'test_role'),
+                   study: study,
+                   assets: [create(:empty_sample_tube)])
+    create(:well_request,
+           asset: create(:well_with_sample_and_plate),
+           target_asset: create(:well_with_sample_and_plate),
+           order: order)
+  end
+  let(:request2) do
+    order = create(:order,
+                   order_role: OrderRole.new(role: 'test_role'),
+                   study: study,
+                   assets: [create(:empty_sample_tube)])
+    create(:well_request,
+           asset: create(:well_with_sample_and_plate),
+           target_asset: create(:well_with_sample_and_plate),
+           order: order)
+  end
+  let(:plate1) { request1.target_asset.plate }
+  let(:plate2) { request2.target_asset.plate }
+  let(:printables) { { plate1.human_barcode => 'on' } }
+  let(:options) { { count: count, printable: printables, batch: batch } }
+
+  setup do
+    batch.requests << request1
+    batch.requests << request2
+  end
+
+  subject { described_class.new(label_options) }
+
+  describe LabelPrinter::Label::BatchPlateDouble do
+    context 'printing double labels' do
+      let(:label_options) { { count: count, printable: printables, batch: batch } }
+      let(:expected_count) { count.to_i * 2 }
+
+      it_behaves_like 'a correct double label printer'
+      it_behaves_like 'a correct multi-copy printer'
+      it_behaves_like 'a correct filter'
     end
   end
 
-  shared_examples 'a correct multi-copy printer' do
-    let(:count) { '3' }
+  describe LabelPrinter::Label::BatchPlate do
+    context 'returning single label class when selecting a 96 plate printer' do
+      let(:label_options) { options.merge(printer_type_class: BarcodePrinterType) }
+      let(:expected_count) { count.to_i }
 
-    it 'prints multiple copies' do
-      expect(subject.create_labels.count).to eq(expected_count)
+      it_behaves_like 'a correct single label printer'
+      it_behaves_like 'a correct multi-copy printer'
+      it_behaves_like 'a correct filter'
     end
   end
 
-  shared_examples 'a correct filter' do
-    let(:printables) do
-      { plate1.human_barcode => 'on',
-        plate2.human_barcode => 'on' }
-    end
+  describe LabelPrinter::Label::BatchRedirect do
+    context 'returning double label class when selecting a 384 well printer' do
+      let(:label_options) { options.merge(printer_type_class: BarcodePrinterType384DoublePlate) }
 
-    it 'should have the correct plates' do
-      expect(subject.assets).to eq([plate1, plate2])
-    end
-  end
-
-  context 'printing labels' do
-    let(:count) { '1' }
-    let(:date_today) { Time.zone.today.strftime('%e-%^b-%Y') }
-    let(:batch) { create :batch }
-    let(:study) { create :study }
-    let(:request1) do
-      order = create(:order,
-                     order_role: OrderRole.new(role: 'test_role'),
-                     study: study,
-                     assets: [create(:empty_sample_tube)])
-      create(:well_request,
-             asset: create(:well_with_sample_and_plate),
-             target_asset: create(:well_with_sample_and_plate),
-             order: order)
-    end
-    let(:request2) do
-      order = create(:order,
-                     order_role: OrderRole.new(role: 'test_role'),
-                     study: study,
-                     assets: [create(:empty_sample_tube)])
-      create(:well_request,
-             asset: create(:well_with_sample_and_plate),
-             target_asset: create(:well_with_sample_and_plate),
-             order: order)
-    end
-    let(:plate1) { request1.target_asset.plate }
-    let(:plate2) { request2.target_asset.plate }
-    let(:printables) { { plate1.human_barcode => 'on' } }
-    let(:options) { { count: count, printable: printables, batch: batch } }
-
-    setup do
-      batch.requests << request1
-      batch.requests << request2
-    end
-
-    subject { described_class.new(label_options) }
-
-    describe LabelPrinter::Label::BatchPlateDouble do
-      context 'printing double labels' do
-        let(:label_options) { { count: count, printable: printables, batch: batch } }
-        let(:expected_count) { count.to_i * 2 }
-
-        it_behaves_like 'a correct double label printer'
-        it_behaves_like 'a correct multi-copy printer'
-        it_behaves_like 'a correct filter'
-      end
-    end
-
-    describe LabelPrinter::Label::BatchPlate do
-      context 'returning single label class when selecting a 96 plate printer' do
-        let(:label_options) { options.merge(printer_type_class: BarcodePrinterType) }
-        let(:expected_count) { count.to_i }
-
-        it_behaves_like 'a correct single label printer'
-        it_behaves_like 'a correct multi-copy printer'
-        it_behaves_like 'a correct filter'
-      end
-    end
-
-    describe LabelPrinter::Label::BatchRedirect do
-      context 'returning double label class when selecting a 384 well printer' do
-        let(:label_options) { options.merge(printer_type_class: BarcodePrinterType384DoublePlate) }
-
-        it_behaves_like 'a correct double label printer'
-      end
+      it_behaves_like 'a correct double label printer'
     end
   end
 end
