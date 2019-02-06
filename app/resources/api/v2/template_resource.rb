bb402c22577719cbccea0536358286c77851a3d1
Andrew Sparkes <as28@sanger.ac.uk>
2019-02-06 11:55:21 +0000

    Added plate and tag layout template relationships for API v2

--- app/resources/api/v2/template_resource.rb
+++ app/resources/api/v2/template_resource.rb
@@ -4,6 +4,8 @@ module Api
   module V2
     # This stub class is here to appease a problem with JSONAPI v0.9
     # and polymorphic relationships. Also stubbing the TemplatesController.
-    class TemplateResource < BaseResource; end
+    class TemplateResource < BaseResource
+      abstract
+    end
   end
 end
