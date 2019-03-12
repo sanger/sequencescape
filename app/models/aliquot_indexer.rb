41426919b344e6bd65814dfc3b1313aff8ca8378
James Glover <james.glover@sanger.ac.uk>
2019-03-12 14:43:14 +0000

    Bug: Aliquot indexer tags all aliquots
    Sample manifests were creating aliquots with only an i5 tag. While this
    behaviour isn't actually required in the long term, it resulted in the
    generation of lanes with no indicies. Subsequent repair of the data
    would not trigger re-indexing.
    
    There is little reason to make the aliquot indexer conditional so this
    has been removed to improve the robustness of the process.

--- app/models/aliquot_indexer.rb
+++ app/models/aliquot_indexer.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+
+#
+# The aliquot indexer is passed a Receptacle (usually a Lane) and
+# generates an index for all aliquots. This is a unique index that NPG
+# use to identify dplexed sequencing data. It replaces the previous role of
+# the map id of tag 1 (the i7) and allows a single identifier to be used to
+# represent both indices.
+# - Aliquots are sorted by the tag_2 map id, then the tag_1 map id.
+# - Tags associated with the PhiX tube are excluded. This includes the default
+#   phix tag index (configatron.phix_tag.tag_map_id) [888 at time of writing]
+#   as well as the actual index if they happen to differ.
 class AliquotIndexer
   attr_reader :lane, :aliquots
 
@@ -9,6 +21,7 @@ class AliquotIndexer
     end
   end
 
+  # Include in Receptacles to allow idempotent indexing by calling index_aliquots
   module Indexable
     def index_aliquots
       # Skip indexing if already present. Makes the call idempotent and also
@@ -18,6 +31,11 @@ class AliquotIndexer
     end
   end
 
+  #
+  # Generate an aliquot index for lane
+  # @param lane [Receptacle] The receptacle to index. Probably a lane.
+  #
+  # @return [Bool] Returns true if the index was successful
   def self.index(lane)
     new(lane).index
   end
@@ -27,24 +45,33 @@ class AliquotIndexer
     @index = 0
   end
 
+  # The actual index of the PhiX used in the lane.
+  # Note: PhiX isn't actually present as an aliquot within the lane, instead the PhiX
+  # tube is associated with the Lane as a parent asset.
+  # @return [Integer] map_id of the PhiX in the lane.
   def phix_map_id
     return nil unless lane.spiked_in_buffer.present?
 
     @phix_map_id ||= lane.spiked_in_buffer.primary_aliquot.tag.try(:map_id)
   end
 
+  # Returns all aliquots due to be indexed.
+  # @return [Array<Aliquot>] An sorted array of aliquots to index
   def aliquots
-    @aliquots ||= lane.aliquots.sorted_for_indexing.reject { |a| a.untagged? }
+    @aliquots ||= lane.aliquots.sorted_for_indexing
   end
 
+  def index
+    @lane.aliquot_indicies.build(aliquots.map { |a| { aliquot: a, aliquot_index: next_index } })
+    @lane.save
+  end
+
+  private
+
+  # The next index suitable for the aliquot (skips those associated with PhiX)
   def next_index
     @index += 1
     next_index if [phix_map_id, configatron.phix_tag.tag_map_id].include?(@index)
     @index
-  end
-
-  def index
-    @lane.aliquot_indicies.build(aliquots.each_with_index.map { |a, _i| { aliquot: a, aliquot_index: next_index } })
-    @lane.save
   end
 end
