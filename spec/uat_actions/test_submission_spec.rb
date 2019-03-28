80f9d3a4ef1e8b5e04eef7a5b6ef244a470257f1
James Glover <james.glover@sanger.ac.uk>
2019-03-28 13:43:57 +0000

    Fix specs

--- spec/uat_actions/test_submission_spec.rb
+++ spec/uat_actions/test_submission_spec.rb
@@ -4,17 +4,22 @@ require 'rails_helper'
 
 describe UatActions::TestSubmission do
   context 'valid options' do
-    let(:parameters) { {} }
+    before do
+      expect(PlateBarcode).to receive(:create).and_return(build(:plate_barcode, barcode: 2))
+    end
+    let(:submission_template) { create :limber_wgs_submission_template }
+    let(:parameters) { { submission_template_name: submission_template.name } }
     let(:uat_action) { described_class.new(parameters) }
     let(:report) do
       # A report is a hash of key value pairs which get returned to the user.
       # It should include information such as barcodes and identifiers
-      {}
+      { 'plate_barcode_0' => 'DN2T' }
     end
 
     it 'can be performed' do
       expect(uat_action.perform).to eq true
-      expect(uat_action.report).to eq report
+      expect(uat_action.report['plate_barcode_0']).to eq report['plate_barcode_0']
+      expect(uat_action.report['submission_id']).to be_a Integer
     end
   end
 
