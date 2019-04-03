64f7f4f49b32f26978ed8783f16cbad594555cd4
Andrew Sparkes <as28@sanger.ac.uk>
2019-04-03 11:43:41 +0100

    Added UAT action to create a tag group
    Co-authored-by: JamesGlover <james.glover@sanger.ac.uk>

---
+++ spec/uat_actions/generate_tag_group_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe UatActions::GenerateTagGroup do
+  context 'valid options' do
+    let(:parameters) { { name: 'Test group', size: '3' } }
+    let(:uat_action) { described_class.new(parameters) }
+    let(:report) do
+      # A report is a hash of key value pairs which get returned to the user.
+      # It should include information such as barcodes and identifiers
+      { name: 'Test group' }
+    end
+
+    it 'can be performed' do
+      expect(uat_action.perform).to eq true
+      expect(uat_action.report).to eq report
+      expect(TagGroup.find_by(name: 'Test group').tags.count).to eq 3
+    end
+  end
+
+  it 'returns a default' do
+    expect(described_class.default).to be_a described_class
+  end
+end
