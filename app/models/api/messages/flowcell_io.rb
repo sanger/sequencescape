484fbb8c73add0c9eb5c9b2868ebb076e1a0ce20
James Glover <james.glover@sanger.ac.uk>
2019-05-22 10:05:33 +0100

    Add i5 to FlowcellIO

--- app/models/api/messages/flowcell_io.rb
+++ app/models/api/messages/flowcell_io.rb
@@ -239,6 +239,14 @@ class Api::Messages::FlowcellIO < Api::Base
           map_attribute_to_json_attribute(:name, 'tag_set_name')
         end
       end
+      with_association(:tag2) do
+        map_attribute_to_json_attribute(:oligo, 'tag2_sequence')
+        map_attribute_to_json_attribute(:tag_group_id, 'tag2_set_id_lims')
+        with_association(:tag_group) do
+          map_attribute_to_json_attribute(:name, 'tag2_set_name')
+        end
+        map_attribute_to_json_attribute(:map_id, 'tag2_identifier')
+      end
       map_attribute_to_json_attribute(:library_type, 'pipeline_id_lims')
       with_association(:sample) do
         map_attribute_to_json_attribute(:uuid, 'sample_uuid')
