896871ac119958184bca22434e6e4b6928d93ca3
Rubocop <sequencescape@sanger.ac.uk>
2019-05-23 14:16:45 +0100

    Rubocop - autofixes
    This is an autogenerated commit message following a rubocop autocorrect

--- db/migrate/20190415133129_add_volume_to_transfer_requests.rb
+++ db/migrate/20190415133129_add_volume_to_transfer_requests.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class AddVolumeToTransferRequests < ActiveRecord::Migration[5.1]
+  # Adding volume to transfer_requests
   def change
     add_column :transfer_requests, :volume, :float, null: true
   end
