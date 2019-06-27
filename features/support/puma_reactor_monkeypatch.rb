9d79ea187075ac7486c1bd55b1ca3925d20092c9
Luca Da Rin Fioretto <ldrf@sanger.ac.uk>
2019-06-27 16:59:13 +0100

    Disabling puma monkey patch

--- features/support/puma_reactor_monkeypatch.rb
+++ features/support/puma_reactor_monkeypatch.rb
@@ -4,31 +4,31 @@
 # https://gist.github.com/2rba/74d57775ac83ffcb0ff1da5eb5371212
 # See issue:
 # https://github.com/puma/puma/issues/1582
-require 'puma/reactor'
+# require 'puma/reactor'
 
-module CoreExtensions
-  module Puma
-    # The overriding of time functionality with TimeCop or the
-    # ActiveSupport::TimeHelpers causes puma to get confused and generate
-    # invalid time-outs, which get it caught in a loop.
-    module Reactor
-      def calculate_sleep
-        if @timeouts.empty?
-          @sleep_for = ::Puma::Reactor::DefaultSleepFor
-        else
-          diff = @timeouts.first.timeout_at.to_f - ::Time.now.to_f
+# module CoreExtensions
+#   module Puma
+#     # The overriding of time functionality with TimeCop or the
+#     # ActiveSupport::TimeHelpers causes puma to get confused and generate
+#     # invalid time-outs, which get it caught in a loop.
+#     module Reactor
+#       def calculate_sleep
+#         if @timeouts.empty?
+#           @sleep_for = ::Puma::Reactor::DefaultSleepFor
+#         else
+#           diff = @timeouts.first.timeout_at.to_f - ::Time.now.to_f
 
-          @sleep_for = if diff < 0.0
-                         0
-                       elsif diff > 60
-                         ::Puma::Reactor::DefaultSleepFor
-                       else
-                         diff
-                       end
-        end
-      end
-    end
-  end
-end
+#           @sleep_for = if diff < 0.0
+#                          0
+#                        elsif diff > 60
+#                          ::Puma::Reactor::DefaultSleepFor
+#                        else
+#                          diff
+#                        end
+#         end
+#       end
+#     end
+#   end
+# end
 
-Puma::Reactor.prepend CoreExtensions::Puma::Reactor
+# Puma::Reactor.prepend CoreExtensions::Puma::Reactor
