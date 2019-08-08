db411bd8e8b181caaf61a0206f0c0719bb0bea10
James Glover <james.glover@sanger.ac.uk>
2019-08-08 16:49:53 +0100

    Sort wells in columns

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -506,6 +506,14 @@ class Plate < Labware
     end
   end
 
+  def wells_in_column_order
+    if wells.loaded?
+      wells.sort_by(&:column_order)
+    else
+      wells.in_column_major_order
+    end
+  end
+
   private
 
   def lookup_stock_plate
