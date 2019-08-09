95c7aa313b7464b75bce080f53b634b5c540ebb1
James Glover <james.glover@sanger.ac.uk>
2019-08-09 12:01:47 +0100

    Add purpose to labware

--- app/models/labware.rb
+++ app/models/labware.rb
@@ -72,6 +72,7 @@ class Labware < Asset
     scope :for_lab_searches_display, -> { includes(:barcodes, requests: %i[pipeline batch]).order('requests.pipeline_id ASC') }
   end
 
+  belongs_to :purpose, foreign_key: :plate_purpose_id, optional: true, inverse_of: :labware
   has_one :spiked_in_buffer, through: :spiked_in_buffer_links, source: :ancestor
 
   scope :named, ->(name) { where(name: name) }
