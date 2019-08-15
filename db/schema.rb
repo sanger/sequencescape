d9b6a2b382986ba338f812a0296a0a6af54e7b9f
James Glover <james.glover@sanger.ac.uk>
2019-08-15 12:17:18 +0100

    Update the database defaults to utf8mb4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190815092606) do
+ActiveRecord::Schema.define(version: 20190815110602) do
 
   create_table "aker_containers", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "barcode"
