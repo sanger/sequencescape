610a13ff92e7848845ba1be99e84bba66df6ec60
James Glover <JamesGlover@users.noreply.github.com>
2019-08-15 14:38:53 +0100

    Add library_plate to barcode page

--- lib/label_printer/label_printer/label/sample_manifest_redirect.rb
+++ lib/label_printer/label_printer/label/sample_manifest_redirect.rb
@@ -11,15 +11,13 @@ module LabelPrinter
 
       def to_h
         case sample_manifest.asset_type
-        when 'plate'
+        when 'plate', 'library_plate'
           if @printer_type_class.double_label?
             SampleManifestPlateDouble.new(options).to_h
           else
             SampleManifestPlate.new(options).to_h
           end
-        when '1dtube'
-          SampleManifestTube.new(options).to_h
-        when 'library'
+        when '1dtube', 'library'
           SampleManifestTube.new(options).to_h
         when 'multiplexed_library'
           SampleManifestMultiplex.new(options).to_h
