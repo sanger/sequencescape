c36958e48e6c5f5cf68e02fc14f01d18c8c09265
James Glover <james.glover@sanger.ac.uk>
2019-08-30 15:14:22 +0100

    Add character validations to warehoused fields
    THe warehouse is utf8 format not utf8mb4, which means it doesn't support
    four byte supplementary characters, which include most emoji 😭 as well
    as other elements, like rare kanji. We add validation to columns which
    get sent to the warehouse (and aren't otherwise restricted) to ensure
    that the messages dont' get rejected there.
    
    We also apply some additional constraints on selected columns to relieve
    concerns from NPG that our database changes will result in the
    propagation of more special characters downstream, that some of their
    pipleine may be unable to handle. These restrictions may be adjusted
    over time to better reflect the actual limitations, and to ensure that
    all relevant fields are covered.

---
+++ app/validators/latin1_validator.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# NPG expressed concerns that switching the Sequencescape to Utf8 would result in an
+# increase in characters which could not be processed by their pipelines. This validator
+# checks that the characters can be represented in latin1, ensuring that we don't make the
+# problem worse in the short term.
+# This will likely be replaced by more specific validations as we become aware of exactly what
+# is supported.
+# It has been applied to the key fields that get added to headers and the like.
+class Latin1Validator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    return true if value.blank?
+
+    value.encode('cp1252')
+    return true if value.encode('cp1252').valid_encoding?
+  rescue Encoding::UndefinedConversionError => e
+    record.errors[attribute] << (
+      options[:message] ||
+      "contains unsupported characters (non-latin characters), remove or replace them: #{e.error_char}"
+    )
+  end
+end
