11bd2af3a9dc373a95a712cd89e95272511995fa
James Glover <james.glover@sanger.ac.uk>
2019-09-27 16:56:36 +0100

    Add doc to some js elements

--- app/assets/javascripts/sequencescape/comment_dropdown_handler.js
+++ app/assets/javascripts/sequencescape/comment_dropdown_handler.js
@@ -1,5 +1,13 @@
-
-
+// Sets up a drop down animation for show and hide comment links
+//
+// Binds to: DOM elements with the class .show-comment (Show)
+//           DOM elements with the class .hide-comment (Hide)
+//
+// Will use the following data attributes
+// data-comment-field: A JQuery identifier (eg. #id or .class) for the DOM element
+//                     to show/hide
+//
+// Dependent on: jquery
 ( function($, undefined){
   "use strict";
 
