f0ff1efbfe2ff1439ed1379443bf45a36942d69d
Katy Taylor <kt17@mib113793i.internal.sanger.ac.uk>
2019-10-28 13:15:34 +0000

    add different column as foreign key, because we want to call it labware_id rather than tube_rack_id

--- app/models/tube_rack.rb
+++ app/models/tube_rack.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-class TubeRack < ApplicationRecord
-  has_many :rackable_tubes, dependent: :destroy
+class TubeRack < Labware
+  include Barcode::Barcodeable
+
+  has_many :rackable_tubes, foreign_key: :labware_id, dependent: :destroy
   has_many :tubes, through: :rackable_tubes
 end
