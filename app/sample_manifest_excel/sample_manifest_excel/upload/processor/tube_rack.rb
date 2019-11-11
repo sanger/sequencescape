11d3f9051efe01d9bcd159e60f1b1ee139b1ba18
Katy Taylor <kt17@sanger.ac.uk>
2019-11-11 17:00:16 +0000

    small fixes and refactoring

--- app/sample_manifest_excel/sample_manifest_excel/upload/processor/tube_rack.rb
+++ app/sample_manifest_excel/sample_manifest_excel/upload/processor/tube_rack.rb
@@ -9,18 +9,23 @@ module SampleManifestExcel
     module Processor
       # TODO: had to explicitly specify the namespace for Base here otherwise it picks up Upload::Base
       class TubeRack < SampleManifestExcel::Upload::Processor::Base
+
+        def initialize(upload)
+          super(upload)
+          @tube_rack_information_processed = false
+
+          @tube_rack_barcodes_from_manifest = @upload.data.description_info.select { |key, value| key.start_with?('Rack barcode (') }.values
+          @tube_barcodes_from_manifest = @upload.data.column(1).compact   # TODO: do this based on the column name, not number
+          @should_process_tube_rack_information = should_process_tube_rack_information?
+        end
+
         def run(tag_group)
           return unless valid?
-
-          @tube_rack_information_processed = false
-          @tube_rack_barcodes = @upload.data.description_info.select { |key, value| key.start_with?('Rack barcode (') }.values
-          @tube_barcodes = @upload.data.column(1).compact   # TODO: do this based on the column name, not number
-          @should_process_tube_rack_information = should_process_tube_rack_information?
 
           if @should_process_tube_rack_information
             @rack_size = @upload.sample_manifest.tube_rack_purpose.size
 
-            if retrieve_scan_results && validate_against_scan_results && validate_coordinates(@rack_size, @barcode_to_scan_results)
+            if retrieve_scan_results && validate_against_scan_results && validate_coordinates(@rack_size, @rack_barcode_to_scan_results)
               create_tube_racks_and_link_tubes
               @tube_rack_information_processed = true
             else
@@ -34,7 +39,7 @@ module SampleManifestExcel
         # if a tube rack record already exists for any of the rack barcodes in the manifest,
         # it has been processed before and should not be re-processed
         def should_process_tube_rack_information?
-          @tube_rack_barcodes.each do |barcode|
+          @tube_rack_barcodes_from_manifest.each do |barcode|
             existing_barcode_record = Barcode.includes(:asset).find_by(barcode: barcode)
             return false if(existing_barcode_record != nil && existing_barcode_record.asset != nil)
           end
@@ -42,14 +47,14 @@ module SampleManifestExcel
         end
 
         def retrieve_scan_results
-          @barcode_to_scan_results = {}
+          @rack_barcode_to_scan_results = {}
           @tube_barcode_to_rack_barcode = {}
 
-          @tube_rack_barcodes.each do |tube_rack_barcode|
+          @tube_rack_barcodes_from_manifest.each do |tube_rack_barcode|
             results = retrieve_tube_rack_scan_from_microservice(tube_rack_barcode)
             return false if results == nil
 
-            @barcode_to_scan_results[tube_rack_barcode] = results
+            @rack_barcode_to_scan_results[tube_rack_barcode] = results
             results.keys.each { |tube_barcode| @tube_barcode_to_rack_barcode[tube_barcode] = tube_rack_barcode }
           end
 
@@ -83,7 +88,7 @@ module SampleManifestExcel
 
 
         def validate_against_scan_results
-          if @tube_barcodes.sort == @tube_barcode_to_rack_barcode.keys.sort
+          if @tube_barcodes_from_manifest.sort == @tube_barcode_to_rack_barcode.keys.sort
             return true
           else
             error_message = "The scan and the manifest do not contain identical tube barcodes."
@@ -92,7 +97,7 @@ module SampleManifestExcel
           end
         end
 
-        def validate_coordinates(rack_size, barcode_to_scan_results)
+        def validate_coordinates(rack_size, rack_barcode_to_scan_results)
           rack_layout_map = {
             48 => {
               "rows" => 6,
@@ -109,7 +114,7 @@ module SampleManifestExcel
           valid_row_values = TubeRack.generate_valid_row_values(num_rows)
           valid_column_values = (1..num_columns).to_a
 
-          barcode_to_scan_results.each_value do |scan_results|
+          rack_barcode_to_scan_results.each_value do |scan_results|
             scan_results.each_value do |coordinate|
               row = coordinate.split(//)[0].capitalize
               column = coordinate.split(//)[1]
@@ -137,7 +142,7 @@ module SampleManifestExcel
 
         def create_tube_racks_and_link_tubes
           tube_rack_barcode_to_tube_rack = {}
-          @tube_rack_barcodes.each do |tube_rack_barcode|
+          @tube_rack_barcodes_from_manifest.each do |tube_rack_barcode|
             tube_rack_barcode_to_tube_rack[tube_rack_barcode] = create_tube_rack_if_not_existing(tube_rack_barcode)
           end
 
@@ -145,12 +150,14 @@ module SampleManifestExcel
             # create a barcode for the tube
             tube_barcode = row.value('tube_barcode')
             tube = @upload.cache.find_by(sanger_sample_id: row.sanger_sample_id).asset.labware
+            puts "*** tube: #{tube}"
+            puts "*** tube 2: #{row.labware}"
             Barcode.create!(asset_id: tube.id, barcode: tube_barcode, format: 7)
 
             # link the tube to the tube rack
             tube_rack_barcode = @tube_barcode_to_rack_barcode[tube_barcode]
             tube_rack = tube_rack_barcode_to_tube_rack[tube_rack_barcode]
-            tube_barcode_to_coordinate = @barcode_to_scan_results[tube_rack_barcode]
+            tube_barcode_to_coordinate = @rack_barcode_to_scan_results[tube_rack_barcode]
             RackedTube.create!(tube_rack: tube_rack, tube: tube, coordinate: tube_barcode_to_coordinate[tube_barcode])
           end
         end
