79633b19060b27ce88bb66b6a0d67ff677c419d6
Katy Taylor <kt17@sanger.ac.uk>
2019-11-18 15:11:02 +0000

    move method that calls the csv parser microservice to its own file

--- app/sample_manifest_excel/sample_manifest_excel/upload/processor/tube_rack.rb
+++ app/sample_manifest_excel/sample_manifest_excel/upload/processor/tube_rack.rb
@@ -63,7 +63,7 @@ module SampleManifestExcel
           @rack_barcode_to_scan_results = {}
           @tube_barcode_to_rack_barcode = {}
           @tube_rack_barcodes_from_manifest.each do |tube_rack_barcode|
-            results = retrieve_tube_rack_scan_from_microservice(tube_rack_barcode)
+            results = CsvParserClient::get_tube_rack_scan_results(tube_rack_barcode, upload)
             return false if results.nil?
 
             @rack_barcode_to_scan_results[tube_rack_barcode] = results
@@ -71,30 +71,6 @@ module SampleManifestExcel
           end
 
           true
-        end
-
-        def retrieve_tube_rack_scan_from_microservice(tube_rack_barcode)
-          host_name = Rails.configuration.tube_rack_scans_microservice_endpoint
-          path = '/tube_rack/' + tube_rack_barcode
-          port = Rails.configuration.tube_rack_scans_microservice_port
-          response = Net::HTTP.get_response(host_name, path, port)
-
-          begin
-            scan_results = JSON.parse(response.body)
-          rescue JSON::JSONError => e
-            error_message = "Response when trying to retrieve scan (tube rack with barcode #{tube_rack_barcode}) was not valid JSON so could not be understood. Error message: #{e.message}"
-            upload.errors.add(:base, error_message)
-            return nil
-          end
-
-          unless response.code == '200'
-            error_message = "Scan could not be retrieved for tube rack with barcode #{tube_rack_barcode}. Service responded with status code #{response.code}"
-            error_message += " and the following message: #{scan_results['error']}"
-            upload.errors.add(:base, error_message)
-            return nil
-          end
-
-          scan_results['layout'] || nil
         end
 
         def validate_against_scan_results
