d7a25cb7a65f08599dcb9c375ce9c819c53f5edf
Katy Taylor <kt17@sanger.ac.uk>
2019-12-06 16:28:20 +0000

    failing test to fix. commented out the others for now for convenience

--- spec/features/studies/edit_study_spec.rb
+++ spec/features/studies/edit_study_spec.rb
@@ -4,34 +4,48 @@ require 'rails_helper'
 
 describe 'Edit a study' do
   let(:user) { create :admin }
-  let!(:study) { create :study }
+  let!(:study) { create :not_app_study }
 
-  it 'edit open study', js: true do
-    study.study_metadata.bam = false
+  # it 'edit open study', js: true do
+  #   study.study_metadata.bam = false
+  #   study.save
+  #   login_user(user)
+  #   visit study_path(study)
+  #   click_link 'Edit'
+  #   expect(page).to have_content('Alignments in BAM')
+  #   expect(find('#study_study_metadata_attributes_bam')).not_to be_checked
+  #   check 'study_study_metadata_attributes_bam'
+  #   expect(find('#study_study_metadata_attributes_bam')).to be_checked
+  #   click_button 'Save Study'
+  #   expect(page).to have_content('Your study has been updated')
+  #   click_link 'Study details'
+  #   expect(page).to have_content('Alignments in BAM: true')
+  # end
+
+  # it 'add external customer information', js: true do
+  #   login_user(user)
+  #   visit study_path(study)
+  #   click_link 'Edit'
+  #   fill_in 'S3 email list', with: 'aa1@sanger.ac.uk;aa2@sanger.ac.uk;aa3@sanger.ac.uk'
+  #   select('3 months', from: 'Data deletion period')
+  #   click_button 'Save Study'
+  #   expect(page).to have_content('Your study has been updated')
+  #   study.reload
+  #   expect(study.study_metadata.s3_email_list).to eq('aa1@sanger.ac.uk;aa2@sanger.ac.uk;aa3@sanger.ac.uk')
+  #   expect(study.study_metadata.data_deletion_period).to eq('3 months')
+  # end
+
+  it 'edit open study error test', js: true do
+    study.study_metadata.data_release_strategy = 'not applicable'
     study.save
     login_user(user)
     visit study_path(study)
     click_link 'Edit'
-    expect(page).to have_content('Alignments in BAM')
-    expect(find('#study_study_metadata_attributes_bam')).not_to be_checked
-    check 'study_study_metadata_attributes_bam'
-    expect(find('#study_study_metadata_attributes_bam')).to be_checked
+    expect(page).to have_content('What is the data release strategy for this study?')
+    expect(page).to have_content('Open (ENA)')
+    choose('Open (ENA)', allow_label_click: true)
+    # choose('study_study_metadata_attributes_data_release_prevention_approval_yes', allow_label_click: true)
     click_button 'Save Study'
     expect(page).to have_content('Your study has been updated')
-    click_link 'Study details'
-    expect(page).to have_content('Alignments in BAM: true')
-  end
-
-  it 'add external customer information', js: true do
-    login_user(user)
-    visit study_path(study)
-    click_link 'Edit'
-    fill_in 'S3 email list', with: 'aa1@sanger.ac.uk;aa2@sanger.ac.uk;aa3@sanger.ac.uk'
-    select('3 months', from: 'Data deletion period')
-    click_button 'Save Study'
-    expect(page).to have_content('Your study has been updated')
-    study.reload
-    expect(study.study_metadata.s3_email_list).to eq('aa1@sanger.ac.uk;aa2@sanger.ac.uk;aa3@sanger.ac.uk')
-    expect(study.study_metadata.data_deletion_period).to eq('3 months')
   end
 end
