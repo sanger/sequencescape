76f6b6ce0d3f719866942e5a0e3f52a58a922373
Philip Jansen van Vuuren <pj5@sanger.ac.uk>
2020-04-07 16:59:56 +0100

    Fix mistype

--- db/migrate/20200403133731_add_tube_rack_status_table.rb
+++ db/migrate/20200403133731_add_tube_rack_status_table.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # Add table to track tube rack statuses
+class AddTubeRackStatusTable < ActiveRecord::Migration[5.2]
   def change
     create_table :tube_rack_statuses do |t|
       t.string 'barcode', null: false
