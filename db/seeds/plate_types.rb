ecbc29b768606ca6b89d7fe600a78ddfd304df5e
James Glover <james.glover@sanger.ac.uk>
2020-04-22 08:22:23 +0100

    Add Kingfisher Plate type to PlateType list
    - Add Kingfisher Plate Type to seeds
    - Add a RecordLoader for maintaining PlateTypes
    - rake plate_type:update will automatically update plate
    types
    
    Alternatively:
    > bundle exec rails c
    > PlateType.create_with(maximum_volume: 2000).find_or_create_by!(name: 'KingFisher 96 2ml')

--- db/seeds/plate_types.rb
+++ db/seeds/plate_types.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
-plate_types_with_maximum_volume = { 'ABgene_0765' => 800, 'ABgene_0800' => 180, 'FluidX075' => 500, 'FluidX03' => 280 }
+plate_types_with_maximum_volume = {
+  'ABgene_0765' => 800,
+  'ABgene_0800' => 180,
+  'FluidX075' => 500,
+  'FluidX03' => 280,
+  'KingFisher 96 2ml' => 2000
+}
 
 plate_types_with_maximum_volume.each do |name, maximum_volume|
   PlateType.create!(name: name, maximum_volume: maximum_volume)
