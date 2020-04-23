21d01ada1bad8eff7c661b623acfe7bf5a633fc3
James Glover <james.glover@sanger.ac.uk>
2020-04-23 08:37:43 +0100

    Reduce number of wells generated
    Generating 4 full 96 well plates is expensive
    and we only need two wells for this. This has
    the added advantage that it means we will ensure
    we support plates with 'missing' wells.

--- spec/models/plate/quad_creator_spec.rb
+++ spec/models/plate/quad_creator_spec.rb
@@ -6,7 +6,7 @@ require 'rails_helper'
 # and transfers them onto a new 384 well plate
 RSpec.describe Plate::QuadCreator, type: :model do
   context 'with 4 parent plates' do
-    let(:parents) { create_list :full_plate_with_samples, 4 }
+    let(:parents) { create_list :plate_with_untagged_wells, 4, occupied_well_index: [0,95] }
     let(:target_purpose) { create :plate_purpose, size: 384 }
     let(:parents_hash) do
       {
@@ -31,25 +31,27 @@ RSpec.describe Plate::QuadCreator, type: :model do
       expect(PlateBarcode).to receive(:create).and_return(build(:plate_barcode, barcode: 1000))
     end
 
-    it 'will create a new plate of the selected purpose' do
-      quad_creator.save
-      expect(quad_creator.target_plate.purpose).to eq target_purpose
-    end
+    describe '#save' do
+      before { quad_creator.save }
 
-    it 'will transfer the material from the source plates' do
-      quad_creator.save
-      well_hash = quad_creator.target_plate.wells.index_by(&:map_description)
-      expect(well_hash['A1'].samples).to eq(quad_1_wells['A1'].samples)
-      expect(well_hash['B1'].samples).to eq(quad_2_wells['A1'].samples)
-      expect(well_hash['A2'].samples).to eq(quad_3_wells['A1'].samples)
-      expect(well_hash['B2'].samples).to eq(quad_4_wells['A1'].samples)
-      expect(well_hash['O23'].samples).to eq(quad_1_wells['H12'].samples)
-      expect(well_hash['P23'].samples).to eq(quad_2_wells['H12'].samples)
-      expect(well_hash['O24'].samples).to eq(quad_3_wells['H12'].samples)
-      expect(well_hash['P24'].samples).to eq(quad_4_wells['H12'].samples)
-    end
+      it 'will create a new plate of the selected purpose' do
+        expect(quad_creator.target_plate.purpose).to eq target_purpose
+      end
 
-    it 'will set each parent as a parent plate of the target'
-    it 'records an asset creation'
+      it 'will transfer the material from the source plates' do
+        well_hash = quad_creator.target_plate.wells.index_by(&:map_description)
+        expect(well_hash['A1'].samples).to eq(quad_1_wells['A1'].samples)
+        expect(well_hash['B1'].samples).to eq(quad_2_wells['A1'].samples)
+        expect(well_hash['A2'].samples).to eq(quad_3_wells['A1'].samples)
+        expect(well_hash['B2'].samples).to eq(quad_4_wells['A1'].samples)
+        expect(well_hash['O23'].samples).to eq(quad_1_wells['H12'].samples)
+        expect(well_hash['P23'].samples).to eq(quad_2_wells['H12'].samples)
+        expect(well_hash['O24'].samples).to eq(quad_3_wells['H12'].samples)
+        expect(well_hash['P24'].samples).to eq(quad_4_wells['H12'].samples)
+      end
+
+      it 'will set each parent as a parent plate of the target'
+      it 'records an asset creation'
+    end
   end
 end
