0af0caf7da5744a73fd91ef3b891128dc1b0614b
Andrew Sparkes <as28@sanger.ac.uk>
2020-06-04 16:06:50 +0100

    Add plate barcodes method for hamilton behaviour

--- app/models/batch/hamilton_behaviour.rb
+++ app/models/batch/hamilton_behaviour.rb
@@ -1,5 +1,12 @@
 # Holds robot behaviour specific to the Hamilton
 module Batch::HamiltonBehaviour
+  def hamilton_layout_plate_barcodes(target_barcode)
+    data_object = generate_picking_data(target_barcode)
+    dest_barcode_index = Sanger::Robots::Hamilton::Generator.barcode_to_plate_index(data_object['destination'])
+    source_barcode_index = Sanger::Robots::Hamilton::Generator.source_barcode_to_plate_index(data_object['destination'])
+    [dest_barcode_index, source_barcode_index]
+  end
+
   def hamilton_csv_file_as_text(target_barcode)
     data_object = generate_picking_data(target_barcode)
     Sanger::Robots::Hamilton::Generator.mapping(data_object)
