e40cc60fd3c8f19fa487b087f5f9b0289cf65663
James Glover <james.glover@sanger.ac.uk>
2020-06-18 14:02:20 +0100

    Fix intermittent spec failure
    Asset_path disambiguation will handle any cases where there is
    not a clash in labware and receptacle barcodes. However, if there
    is a clash, this page will not redirect and will ask the user
    for confirmation. This happens more often if the test falls
    earlier in the test suite.
    
    This fix uses the correct path, labware_path, rather than the
    asset_path

--- spec/features/assets/show_plate_spec.rb
+++ spec/features/assets/show_plate_spec.rb
@@ -12,7 +12,7 @@ describe 'Show plate', js: true do
 
   it 'the samples table shows empty wells' do
     login_user user
-    visit asset_path(plate)
+    visit labware_path(plate)
     expect(fetch_table('#plate-samples-table')).to eq([
       ['Well', 'Sample Name', 'Sanger Sample Id', 'Tag', 'Tag2', 'Control?'],
       ['A1',   '[Empty]',     '',                 '',    '',     ''],
