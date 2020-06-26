7e2aa89572e99e0e7cf40a0eb1a14e4a11fddaa9
James Glover <james.glover@sanger.ac.uk>
2020-06-26 09:19:30 +0100

    Update cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -17,6 +17,7 @@ end
 require 'cucumber/rails'
 require 'factory_bot_rails'
 
+require_relative 'capybara'
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
