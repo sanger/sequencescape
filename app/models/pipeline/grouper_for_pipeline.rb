ecb1b340a72cc35352d2a04d77ff285ca9d38f7b
James Glover <james.glover@sanger.ac.uk>
2020-07-10 09:17:32 +0100

    GPL-564 UAT cherrypicking - Batch creation memory problem Fixes #2821
    This fixes the immediate problem, but needs urgent refactoring.
    Can have seperate groupers, rather than conditional logic.

--- app/models/pipeline/grouper_for_pipeline.rb
+++ app/models/pipeline/grouper_for_pipeline.rb
@@ -19,20 +19,38 @@ class Pipeline::GrouperForPipeline
   end
 
   def all(selected_groups)
-    selected_groups.map { |group| extract_conditions(group) }
-                   .reduce { |scope, query| scope.or(query) }
+    queries = selected_groups.map { |group| extract_conditions(group) }
+    base_scope.where(queries.join(' OR '))
   end
 
   private
+
+  def query_string
+    if group_by_parent? && group_by_submission?
+      '(`requests`.`submission_id` = :submission_id AND `receptacles`.`labware_id` = :labware_id)'
+    elsif group_by_parent?
+      '(`receptacles`.`labware_id` = :labware_id)'
+    elsif group_by_submission?
+      '(`requests`.`submission_id` = :submission_id)'
+    else
+      raise 'Invalid Grouper'
+    end
+  end
 
   # This extracts the container/submission values from the group
   # and uses them to generate a query.
   def extract_conditions(group)
     condition = {}.tap do |building_condition|
       keys = group.split(', ')
-      building_condition[LABWARE_ID_COLUMN] = keys.first.to_i if group_by_parent?
+      building_condition[:labware_id] = keys.first.to_i if group_by_parent?
       building_condition[:submission_id] = keys.last.to_i if group_by_submission?
     end
-    base_scope.where(condition)
+    # base_scope.where(condition)
+    requests.sanitize_sql(
+      [
+        '(`requests`.`submission_id` = :submission_id AND `receptacles`.`labware_id` = :labware_id)',
+        condition
+      ]
+    )
   end
 end
