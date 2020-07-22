e2cae24c181ec127839fd6112ac8c829c7edbadb
Eduardo Martin Rojo <emr@sanger.ac.uk>
2020-07-22 14:02:35 +0100

    Trying by reinstantiating the well

--- spec/features/shared_examples/cherrypicking.rb
+++ spec/features/shared_examples/cherrypicking.rb
@@ -276,7 +276,9 @@ shared_examples 'a cherrypicking procedure' do
                 # Assuming there is only one asset for the TransferRequest,
                 # As pooling is not supported in cherrypicking,
                 # ie. a well can only have one input sample
-                plates_and_controls_barcodes.include?(r.first.asset.plate.human_barcode)
+                puts "DEBUG: request.asset.plate: #{r.asset.plate}"
+                puts "DEBUG: well plate: #{Well.find(r.asset.id).plate}"
+                plates_and_controls_barcodes.include?(Well.find(r.asset.id).plate.human_barcode)
               end
               expected_num_lines = input_wells_requests_for_current_pick.count + NUM_HAMILTON_HEADER_LINES
               expect(generated_lines.length).to eq(expected_num_lines)
