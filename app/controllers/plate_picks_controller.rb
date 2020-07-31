abd960bba695e9fd2af58c93122a8f0bc0984677
James Glover <james.glover@sanger.ac.uk>
2020-07-31 08:49:56 +0100

    Scaffold out vue app and controller
    - bundle exec rails g controller PlatePicks
    - bundle exec rails webpacker:install:vue
    
    Along with:
    - Adding suggested unsafe-eval mode to dev mode
    - Making Rubocop happy
    - Removing a few un-needed scaffoled files

---
+++ app/controllers/plate_picks_controller.rb
@@ -0,0 +1,2 @@
+class PlatePicksController < ApplicationController
+end
---
+++ spec/requests/plate_picks_request_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'PlatePicks', type: :request do
+  it 'makes rubocop happy' do
+    value = 1 + 1 == 2
+    expect(value).to be true
+  end
+end
