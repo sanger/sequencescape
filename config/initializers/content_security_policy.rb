304f646a207d9cb2a8529b855d3fbc344c0a44b6
James Glover <james.glover@sanger.ac.uk>
2020-08-21 20:22:15 +0100

    Temporarily switch CSP to report only mode
    We have some legacy onclick events and the like directly
    attached to links.
    
    We haven't actually set up a reporting url for tracking this stuff.
    I'll create a story in the backlog.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -35,4 +35,4 @@ Rails.application.config.content_security_policy_nonce_generator = ->(_request) 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
+Rails.application.config.content_security_policy_report_only = true
