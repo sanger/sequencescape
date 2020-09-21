f72206932eb94adffaaa4814dba0a0aac792fe47
James Glover <james.glover@sanger.ac.uk>
2020-09-21 10:21:32 +0100

    Add verbose query logging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
