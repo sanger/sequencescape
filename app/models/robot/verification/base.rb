9aa9258989e79a2155dbb7666abf1ae48bfa0983
Katy Taylor <kt17@sanger.ac.uk>
2020-10-01 15:39:47 +0100

    remove comments

--- app/models/robot/verification/base.rb
+++ app/models/robot/verification/base.rb
@@ -53,8 +53,6 @@ class Robot::Verification::Base
   # @param [Integer] max_beds The max_beds for plates on the robot
   def all_picks(batch, max_beds)
     # @note No optimization yet.
-    # replace output_labware with output_plates, which is sorted
-    # output_plate uses order
     batch.output_plates.each_with_object({}) do |plate, store|
       store[plate.machine_barcode] = pick_number_to_expected_layout(batch, plate.machine_barcode, max_beds)
     end
