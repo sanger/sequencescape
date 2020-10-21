90c9653c460ae885f77c9a5f8b8d12d7562dd4b2
Stephen Inglis <si5@sanger.ac.uk>
2020-10-21 09:24:39 +0100

    Updated printer.

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -9,7 +9,6 @@ class Sprint < ApplicationRecord
   # Currently implementing a proof of concept
   # Using HTTP instead of GraphQL client, 'stub' printer and variable placeholders
   def self.print_request
-    
     # GraphQL print mutation
     query = "mutation Print($printRequest: PrintRequest!, $printer: String!) {
       print(printRequest: $printRequest, printer: $printer) {
@@ -17,9 +16,8 @@ class Sprint < ApplicationRecord
       }
     }"
 
-    # Using printer called stub which is a fake printer. 
-    # This will treat the request like a request to a printer, but not actually try and print anything.
-    printer = "stub"
+    # Using heron-bc1 for proof of concept.
+    printer = 'heron-bc1'
 
     print_request = {
       "layouts": [
@@ -34,42 +32,42 @@ class Sprint < ApplicationRecord
               "x": 15,
               "y": 5,
               "cellWidth": 0.4,
-              "barcodeType": "datamatrix",
-              "value": "#barcode#"
+              "barcodeType": 'datamatrix',
+              "value": '#barcode#'
             }
           ],
           "textFields": [
             {
               "x": 1,
               "y": 4,
-              "value": "#barcode_text#",
-              "font": "proportional",
+              "value": '#barcode_text#',
+              "font": 'proportional',
               "fontSize": 2.9
             },
             {
               "x": 1,
               "y": 8,
-              "value": "#date#",
-              "font": "proportional",
+              "value": '#date#',
+              "font": 'proportional',
               "fontSize": 1.8
             }
           ]
-        }    
+        }
       ]
     }
 
     body = {
       "query": query,
-      "variables": { 
+      "variables": {
         "printer": printer,
         "printRequest": print_request
       }
     }
-    
+
     reponse = Net::HTTP.post URI(configatron.sprint_url),
-                body.to_json,
-                "Content-Type" => "application/json"
-    
+                             body.to_json,
+                             'Content-Type' => 'application/json'
+
     puts reponse.body
     # rubocop:enable Rails/Output
   end
