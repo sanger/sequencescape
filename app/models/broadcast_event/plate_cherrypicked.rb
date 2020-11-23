dbdede06947fdaae3e3f37d8f0593b824ab14131
Eduardo Martin Rojo <emr@sanger.ac.uk>
2020-11-23 17:40:48 +0000

    Added tests for new event

--- app/models/broadcast_event/plate_cherrypicked.rb
+++ app/models/broadcast_event/plate_cherrypicked.rb
@@ -1,4 +1,9 @@
 class BroadcastEvent::PlateCherrypicked < BroadcastEvent
+  ROBOT_ROLE_TYPE = 'robot'
+  SOURCE_PLATES_ROLE_TYPE = 'cherrypicking_source'
+  SAMPLE_ROLE_TYPE = 'sample'
+  DESTINATION_PLATE_ROLE_TYPE = 'cherrypicking_destination_labware'
+
   include BroadcastEvent::Helpers::ExternalSubjects
 
   set_event_type 'lh_beckman_cp_destination_created'
@@ -6,5 +11,25 @@ class BroadcastEvent::PlateCherrypicked < BroadcastEvent
   seed_class Plate
   seed_subject :plate 
 
-  #validate :robot_present, :source_plates_present
+  validates :properties, presence: true
+  validate :robot_present, :source_plates_present, :samples_present, if: :properties
+
+
+  def robot_present
+    unless properties[:subjects].any?{|sub| sub[:role_type] == ROBOT_ROLE_TYPE}
+      errors.add(:robot, 'not provided')
+    end
+  end
+
+  def source_plates_present
+    unless properties[:subjects].any?{|sub| sub[:role_type] == SOURCE_PLATES_ROLE_TYPE}
+      errors.add(:source_plates, 'not provided')
+    end
+  end
+
+  def samples_present
+    unless properties[:subjects].any?{|sub| sub[:role_type] == SAMPLE_ROLE_TYPE}
+      errors.add(:subjects, 'not provided')
+    end
+  end
 end
--- spec/models/broadcast_events/plate_cherrypicked_spec.rb
+++ spec/models/broadcast_events/plate_cherrypicked_spec.rb
@@ -1,39 +1,86 @@
 require 'rails_helper'
 
-def subject_record(role_type, subject_type, friendly_name, uuid)
-  {
-    "role_type": role_type,
-    "subject_type": subject_type,
-    "friendly_name": friendly_name,
-    "uuid": uuid
-  }
-end
-
 RSpec.describe BroadcastEvent::PlateCherrypicked, type: :model, broadcast_event: true do
-  let(:plate1) { subject_record('plate', 'cherrypicking_source', '000001', uuids[0]) }
-  let(:plate2) { subject_record('plate', 'cherrypicking_source', '000002', uuids[1]) }
-  let(:sample1) { subject_record('sample', 'sample', 'ASDF001-000001_A01-AP-Positive', uuids[2]) }
-  let(:sample2) { subject_record('plate', 'cherrypicking_source', 'ASDF001-000001_B01-AP-Negative', uuids[3]) }
-  let(:robot) { subject_record('robot', 'robot', 'RB00001', uuids[4]) }
+  def subject_record(subject_type, role_type, friendly_name, uuid)
+    {
+      "role_type": role_type,
+      "subject_type": subject_type,
+      "friendly_name": friendly_name,
+      "uuid": uuid
+    }
+  end
+  
+  let(:uuids) { 6.length.times.map{ SecureRandom.uuid } }
+  let(:destination_plate) { create :plate }
+  let(:plate1) { 
+    subject_record('plate', BroadcastEvent::PlateCherrypicked::SOURCE_PLATES_ROLE_TYPE, '000001', uuids[0]) 
+  }
+  let(:plate2) { 
+    subject_record('plate', BroadcastEvent::PlateCherrypicked::SOURCE_PLATES_ROLE_TYPE, '000002', uuids[1]) 
+  }
+  let(:sample1) { 
+    subject_record('sample', BroadcastEvent::PlateCherrypicked::SAMPLE_ROLE_TYPE, 'ASDF001-000001_A01-AP-Positive', uuids[2]) 
+  }
+  let(:sample2) { 
+    subject_record('sample', BroadcastEvent::PlateCherrypicked::SAMPLE_ROLE_TYPE, 'ASDF001-000001_B01-AP-Negative', uuids[3]) 
+  }
+  let(:robot) { 
+    subject_record('robot', BroadcastEvent::PlateCherrypicked::ROBOT_ROLE_TYPE, 'RB00001', uuids[4]) 
+  }
+  let(:plate3) {
+    subject_record('plate', BroadcastEvent::PlateCherrypicked::SOURCE_PLATES_ROLE_TYPE, '000003', uuids[5]) 
+  }
+  
   it 'is not directly instantiated' do
     expect(described_class.new).not_to be_valid
   end
 
   it 'cannot be created without robot' do
     props = {subjects: [plate1, plate2, sample1, sample2]}
-    expect(described_class.new(properties: props)).not_to be_valid
+    expect(described_class.new(seed: destination_plate, properties: props)).not_to be_valid
   end
   
   it 'cannot be created without source plates' do
-
-    expect(described_class.new).not_to be_valid
+    props = {subjects: [sample1, sample2, robot]}
+    expect(described_class.new(seed: destination_plate, properties: props)).not_to be_valid
   end
 
   it 'cannot be created without samples' do
-    expect(described_class.new).not_to be_valid
+    props = {subjects: [plate1, plate2, robot]}
+    expect(described_class.new(seed: destination_plate, properties: props)).not_to be_valid
   end
 
-  it 'can be created when all required subjects are defined' do
-    expect(described_class.new).to be_valid
+  context 'with the right contents in properties and seed' do
+    let(:props) {  { subjects: [plate1, plate2, sample1, sample2, robot]  } }
+    let(:instance) { described_class.new(seed: destination_plate, properties: props) }
+
+    it 'can be created when all required subjects are defined' do
+      expect(instance).to be_valid
+    end
+
+    it 'can be persisted' do
+      expect(instance.save).to be_truthy
+    end
+
+    context 'with a created instance' do
+      before do
+        instance.save
+      end
+      
+      context '#to_json' do
+        it 'can generate a json object' do          
+          expect(instance.to_json).to include_json({
+            event: {
+              event_type: 'lh_beckman_cp_destination_created'
+            }
+          })
+        end
+        it 'includes all required subjects in the message' do
+          event_info = JSON.parse(instance.to_json)
+          expect(event_info['event'].has_key?('subjects')).to be_truthy
+          expect(event_info['event']['subjects'].size).to eq(6)
+        end
+      end
+    end
   end
 end
