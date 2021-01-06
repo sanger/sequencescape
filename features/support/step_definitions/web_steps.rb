61a7eb5096468b7a48ddf00d8ced256ff054fd68
James Glover <james.glover@sanger.ac.uk>
2021-01-06 10:28:42 +0000

    Move webmock configuration

--- features/support/step_definitions/web_steps.rb
+++ features/support/step_definitions/web_steps.rb
@@ -9,9 +9,6 @@
 require 'uri'
 require 'cgi'
 require_relative '../paths'
-require 'webmock/cucumber'
-WebMock.disable_net_connect!(allow_localhost: true)
-
 module WithinHelpers
   def with_scope(locator)
     locator ? within(locator) { yield } : yield
