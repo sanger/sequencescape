0942da457c384aba34688e499db8a9653ec8cc7e
James Glover <james.glover@sanger.ac.uk>
2021-01-29 13:13:59 +0000

    Ensure multiple cucumber coverage files
    Cucumber seems to be re-initialized after each queue
    chunk, causing simplecov to clobber the queue files.
    This adds a timestamp to the files to prevent that.

--- features/support/knapsack.rb
+++ features/support/knapsack.rb
@@ -2,7 +2,8 @@
 
 require 'knapsack_pro'
 # https://knapsackpro.com/faq/question/how-to-use-simplecov-in-queue-mode
-KnapsackPro::Hooks::Queue.before_queue do |_queue_id|
-  SimpleCov.command_name("cucumber_ci_node_#{KnapsackPro::Config::Env.ci_node_index}")
+KnapsackPro::Hooks::Queue.before_queue do |queue_id|
+  run_id = "#{queue_id}_#{Time.now.to_i}"
+  SimpleCov.command_name("cucumber_ci_node_#{KnapsackPro::Config::Env.ci_node_index}_#{run_id}")
 end
 KnapsackPro::Adapters::CucumberAdapter.bind
