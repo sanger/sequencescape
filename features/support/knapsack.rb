090728d06a38e84153b97f577a8a65ae16de0ccb
James Glover <JamesGlover@users.noreply.github.com>
2021-03-04 12:37:33 +0000

    Update .simplecov (#66)
    Fix coverage reports for cucumber

--- features/support/knapsack.rb
+++ features/support/knapsack.rb
@@ -3,7 +3,13 @@
 require 'knapsack_pro'
 # https://knapsackpro.com/faq/question/how-to-use-simplecov-in-queue-mode
 KnapsackPro::Hooks::Queue.before_queue do |queue_id|
-  run_id = "#{queue_id}_#{Time.now.to_i}"
-  SimpleCov.command_name("cucumber_ci_node_#{KnapsackPro::Config::Env.ci_node_index}_#{run_id}")
+  command = "cucumber_ci_node_#{KnapsackPro::Config::Env.ci_node_index}_#{queue_id}"
+  SimpleCov.command_name(command)
 end
+
 KnapsackPro::Adapters::CucumberAdapter.bind
+# So the instructions above are actually insufficient, as Knpasack Pro spins up
+# a seperate cucumber process for each subset of the queue. This results in Simplecov
+# clobbering the coverage. Before queue runs only the once, whereas this will run
+# on each invokation
+SimpleCov.command_name("#{SimpleCov.command_name}_#{ENV['KNAPSACK_PRO_SUBSET_QUEUE_ID']}")
