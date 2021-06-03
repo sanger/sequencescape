4285ec50fc91ebe9c0f1a556d2bedfe9eb3a409e
James Glover <james.glover@sanger.ac.uk>
2021-06-03 16:15:36 +0100

    Fix Qcable Resource

--- app/resources/api/v2/qcable_resource.rb
+++ app/resources/api/v2/qcable_resource.rb
@@ -15,7 +15,7 @@ module Api
 
       # Associations:
       has_one :lot
-      has_one :asset, polymorphic: true
+      has_one :asset, class_name: 'Labware'
 
       # Attributes
       attribute :uuid, readonly: true
