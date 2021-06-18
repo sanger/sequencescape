36392a518366414d9b18f418e0a5aada03e25493
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-06-18 11:55:49 +0100

    Add relations purpose and parent to labware

--- app/resources/api/v2/labware_resource.rb
+++ app/resources/api/v2/labware_resource.rb
@@ -12,6 +12,13 @@ module Api
       has_many :comments, readonly: true
       has_many :direct_submissions, readonly: true
 
+      has_one :purpose, readonly: true, foreign_key: :plate_purpose_id
+
+      has_many :ancestors, readonly: true, polymorphic: true
+      has_many :descendants, readonly: true, polymorphic: true
+      has_many :parents, readonly: true, polymorphic: true
+      has_many :children, readonly: true, polymorphic: true
+
       filter :purpose_name,
              apply:
                (lambda { |records, value, _options| records.joins(:purpose).where(plate_purposes: { name: value }) })
