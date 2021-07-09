9869d30177c32b030a1ad025e1b7f0799d542d4b
James Glover <james.glover@sanger.ac.uk>
2021-07-09 15:39:46 +0100

    Remove deep copy

--- test/unit/lab_interface/workflow_test.rb
+++ test/unit/lab_interface/workflow_test.rb
@@ -18,39 +18,5 @@ class WorkflowTest < ActiveSupport::TestCase
     end
 
     subject { @workflow }
-
-    context '#deep_copy' do
-      setup do
-        @labinterface_workflow_count = Workflow.count
-        @task_count = Task.count
-        @pipeline_count = Pipeline.count
-        @descriptor_count = Descriptor.count
-        @workflow.deep_copy
-      end
-
-      should 'change Workflow.count by 1' do
-        assert_equal 1, Workflow.count - @labinterface_workflow_count, 'Expected Workflow.count to change by 1'
-      end
-
-      should 'change Task.count by 1' do
-        assert_equal 1, Task.count - @task_count, 'Expected Task.count to change by 1'
-      end
-
-      should 'change Pipeline.count by 1' do
-        assert_equal 1, Pipeline.count - @pipeline_count, 'Expected Pipeline.count to change by 1'
-      end
-
-      should 'change Descriptor.count by 2' do
-        assert_equal 2, Descriptor.count - @descriptor_count, 'Expected Descriptor.count to change by 2'
-      end
-
-      should 'duplicate workflow' do
-        assert_equal 'Workflow for WorkflowTest_dup', Workflow.last.name
-      end
-
-      should 'duplicate pipeline' do
-        assert_equal 'Pipeline for WorkflowTest_dup', Pipeline.last.name
-      end
-    end
   end
 end
