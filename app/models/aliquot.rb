e297738f95477d0795079cabc2b808f64cca7ae0
James Glover <james.glover@sanger.ac.uk>
2021-07-23 15:27:54 +0100

    Avoid fetching tags in no_tag1?/no_tag2?

--- app/models/aliquot.rb
+++ app/models/aliquot.rb
@@ -126,7 +126,7 @@ class Aliquot < ApplicationRecord # rubocop:todo Metrics/ClassLength
   # This essentially meant that tag clashes would result in sample dropouts.
   # (presumably because << triggers save not save!)
   def no_tag1?
-    tag_id == UNASSIGNED_TAG || tag.nil?
+    tag_id == UNASSIGNED_TAG || tag_id.nil? && tag.nil?
   end
 
   def tag1?
@@ -134,7 +134,7 @@ class Aliquot < ApplicationRecord # rubocop:todo Metrics/ClassLength
   end
 
   def no_tag2?
-    tag2_id == UNASSIGNED_TAG || tag2.nil?
+    tag2_id == UNASSIGNED_TAG || tag2_id.nil? && tag2.nil?
   end
 
   def tag2?
