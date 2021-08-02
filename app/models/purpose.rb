aa3658bfd280ed68880d0630bc665c3850166c8d
Stuart McHattie <sm49@sanger.ac.uk>
2021-08-02 14:32:55 +0100

    Fix typo in method name for the singular source_plate lookup

--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -59,7 +59,7 @@ class Purpose < ApplicationRecord
     ActiveSupport::Deprecation.silence do
       # Rails 6 lets us do this:
       # ActiveSupport::Deprecation.allow(:stock_plate) do
-      source_purpose_id.present? ? labware.ancestors_of_purpose(source_purpose_id) : labware.try(:stock_plate)
+      source_purpose_id.present? ? labware.ancestor_of_purpose(source_purpose_id) : labware.try(:stock_plate)
     end
   end
 
