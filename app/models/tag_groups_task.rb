36283fa7da9b65e11337fe2c18d64f24ff95b8b8
James Glover <james.glover@sanger.ac.uk>
2021-08-26 16:10:25 +0100

    missing user in tag groups task

--- app/models/tag_groups_task.rb
+++ app/models/tag_groups_task.rb
@@ -3,7 +3,7 @@ class TagGroupsTask < Task # rubocop:todo Style/Documentation
     'tag_groups_batches'
   end
 
-  def render_task(workflow, params)
+  def render_task(workflow, params, _user)
     super
     workflow.render_tag_groups_task(self, params)
   end
