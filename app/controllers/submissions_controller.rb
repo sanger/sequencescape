1ce8ee79a87d6f838093944badc1b556a125edc6
Stuart McHattie <stuart.mchattie@gmail.com>
2021-09-01 17:55:31 +0100

    Update app/controllers/submissions_controller.rb

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -65,7 +65,7 @@ class SubmissionsController < ApplicationController
   #
   # Displays a list of submissions for the current user.
   # building => Submissions which haven't yet been queued for building and may still be edited. Submissions begin in
-  # this state, and leave when the used clicks 'Build Submission'
+  # this state, and leave when the user clicks 'Build Submission'
   # pending  => Submissions which the user has finished setting up, and has queued for processing by the delayed job
   # ready    => Submissions which the delayed job has finished processing. The final state of a submission.
   def index # rubocop:todo Metrics/AbcSize
