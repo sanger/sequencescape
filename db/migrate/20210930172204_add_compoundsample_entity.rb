36b35f9cecdc4450c1e7c1dd4ad81c6260e160a7
Stuart McHattie <sm49@sanger.ac.uk>
2021-09-30 18:25:47 +0100

    Start creating a migration for the Samples table

---
+++ db/migrate/20210930172204_add_compoundsample_entity.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Add the STI Type column needed to separate Samples from CompoundSamples
+class AddCompoundSampleEntity < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
