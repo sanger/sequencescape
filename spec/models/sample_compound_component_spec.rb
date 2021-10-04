1001a8587161ecaca4490c8193ca17065f090042
Stuart McHattie <sm49@sanger.ac.uk>
2021-10-04 15:21:50 +0100

    Allow changes requested by Prettier

--- spec/models/sample_compound_component_spec.rb
+++ spec/models/sample_compound_component_spec.rb
@@ -15,29 +15,33 @@ RSpec.describe SampleCompoundComponent, type: :model, cardinal: true do
 
     context 'another sample becomes a compound sample of our compound sample' do
       it 'fails to validate when the new sample adopts our compound sample as a component' do
-        expect do
-          another_sample.component_samples << compound_sample
-        end.to raise_error(ActiveRecord::RecordInvalid, /Component sample cannot have further component samples./)
+        expect { another_sample.component_samples << compound_sample }.to raise_error(
+          ActiveRecord::RecordInvalid,
+          /Component sample cannot have further component samples./
+        )
       end
 
       it 'fails to validate when our compound sample adopts the new sample as a compound' do
-        expect do
-          compound_sample.compound_samples << another_sample
-        end.to raise_error(ActiveRecord::RecordInvalid, /Component sample cannot have further component samples./)
+        expect { compound_sample.compound_samples << another_sample }.to raise_error(
+          ActiveRecord::RecordInvalid,
+          /Component sample cannot have further component samples./
+        )
       end
     end
 
     context 'another sample becomes a component sample of our component sample' do
       it 'fails to validate when the new sample adoptsour component sample as a compound' do
-        expect do
-          another_sample.compound_samples << component_sample
-        end.to raise_error(ActiveRecord::RecordInvalid, /Compound sample cannot have further compound samples./)
+        expect { another_sample.compound_samples << component_sample }.to raise_error(
+          ActiveRecord::RecordInvalid,
+          /Compound sample cannot have further compound samples./
+        )
       end
 
       it 'fails to validate when our component sample adopts the new sample as a component' do
-        expect do
-          component_sample.component_samples << another_sample
-        end.to raise_error(ActiveRecord::RecordInvalid, /Compound sample cannot have further compound samples./)
+        expect { component_sample.component_samples << another_sample }.to raise_error(
+          ActiveRecord::RecordInvalid,
+          /Compound sample cannot have further compound samples./
+        )
       end
     end
   end
