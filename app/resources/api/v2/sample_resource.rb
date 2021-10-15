ab42d6533389acd801efcbc7d49ee089d8e14425
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-10-15 09:11:54 +0100

    Split in separate migration

--- app/resources/api/v2/sample_resource.rb
+++ app/resources/api/v2/sample_resource.rb
@@ -21,11 +21,26 @@ module Api
       filter :name
 
       def sample_compound_component_data=(linking_data)
-        linking_data.each do |data|
-          SampleCompoundComponent.where(
-            compound_sample_id: self.id, component_sample_id: data[:sample_id]
-          ).each do |sample_compound_component|
-            sample_compound_component.update(asset_id: data[:asset_id], target_asset_id: data[:target_asset_id])
+        ActiveRecord::Base.transaction do 
+          linking_data.each do |data|
+            SampleCompoundComponent.where(
+              compound_sample_id: self.id, component_sample_id: data[:sample_id]
+            ).each do |sample_compound_component|
+              sample_compound_component.update(asset_id: data[:asset_id], target_asset_id: data[:target_asset_id])
+              
+              _pass_sample_compound_request_for_well(data)
+            end
+          end
+        end
+      end
+
+      def _pass_sample_compound_request_for_well(data)
+        well = Well.find(data[:asset_id])
+        if well
+          req = well.outer_requests.first 
+          if req.request_type.key == 'limber_cardinal_sample_compound'
+            req.update(target_asset_id: data[:target_asset_id])
+            req.pass!
           end
         end
       end
