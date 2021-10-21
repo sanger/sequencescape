c2f20a68ac032e28dbec04cae658ba4c09c3fb04
Harriet Craven <hc6@sanger.ac.uk>
2021-10-21 14:57:40 +0100

    add sample manifests controller

---
+++ app/controllers/api/v2/sample_manifests_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Api
+  module V2
+    # JSONAPI controller for {SampleManifestsController}
+    class SampleManifestsController < JSONAPI::ResourceController
+    end
+  end
+end
