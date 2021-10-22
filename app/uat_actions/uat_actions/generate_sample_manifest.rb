2e93a7d847725323413322d821ce069d01c6c1f7
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-10-22 13:01:53 +0100

    Added sample manifest generation for tubes for uat actions

--- app/uat_actions/uat_actions/generate_sample_manifest.rb
+++ app/uat_actions/uat_actions/generate_sample_manifest.rb
@@ -2,24 +2,26 @@
 
 # Will construct a sample manifest
 class UatActions::GenerateSampleManifest < UatActions
-  self.title = 'Generate Sample Manifest'
+  self.title = 'Generate sample manifest'
   self.description = 'Generate sample manifest with the provided information.'
-k
+
   form_field :study,
              :select,
              label: 'Study',
              help: 'The study under which samples begin. List includes all active studies.',
-             select_options: -> { Study.active.alphabetical.first }
+             select_options: -> { Study.active.alphabetical.pluck(:name) }
 
   form_field :supplier,
              :select,
              label: 'Supplier',
              help: 'The supplier under which samples originated.',
-             select_options: -> { Supplier.active.alphabetical.first }
+             select_options: -> { Supplier.alphabetical.pluck(:name) }
 
   form_field :asset_type,
-             :text_field,
-             label: 'Asset Type'
+             :select,
+             label: 'Asset Type',
+             help: 'Type of asset to generate for the manifest',
+             select_options: -> { SampleManifest::CoreBehaviour::BEHAVIOURS }
 
   form_field :count,
              :number_field,
@@ -30,13 +32,49 @@ k
                maximum: 96
              }
 
+  form_field :with_samples, :check_box, help: 'Create new samples for recipients?', label: 'With Samples?'
+
   def self.default
-    new(study: UatActions::StaticRecords.study, supplier: UatActions::StaticRecords.supplier, asset_type: 'tube', count: 2)
+    new(
+      study: UatActions::StaticRecords.study,
+      supplier: UatActions::StaticRecords.supplier,
+      asset_type: '1dtube',
+      count: 2
+    )
+  end
+
+  def generate_sample_for_receptacle(asset, _sample_params)
+    asset.aliquots.create!
+  end
+
+  def create_sample(sample_name, study)
+    Sample.create!(name: sample_name, studies: [study], sample_metadata_attributes: { supplier_name: sample_name })
+  end
+
+  def create_samples_for_asset(asset, asset_type, study)
+    raise 'Manifest for plates is not supported yet' unless asset_type == '1dtube'
+    sample = create_sample("Sample_#{asset.human_barcode}_1", study)
+    asset.aliquots.create!(sample: sample)
+  end
+
+  def print_report
+    report['manifest'] = sample_manifest.id
+    sample_manifest.assets.each_with_index do |asset, pos|
+      create_samples_for_asset(asset, asset_type, UatActions::StaticRecords.study) if with_samples == '1'
+      report["asset_#{pos}"] = asset.human_barcode
+    end
   end
 
   def perform
-    sample_manifest = SampleManifest.create!(study: study, supplier: supplier, asset_type: asset_type, count: count)
+    sample_manifest =
+      SampleManifest.create!(
+        study: UatActions::StaticRecords.study,
+        supplier: UatActions::StaticRecords.supplier,
+        asset_type: asset_type,
+        count: count
+      )
     sample_manifest.generate
+    print_report
 
     true
   end
--- spec/uat_actions/generate_sample_manifest_spec.rb
+++ spec/uat_actions/generate_sample_manifest_spec.rb
@@ -4,18 +4,42 @@ require 'rails_helper'
 
 describe UatActions::GenerateSampleManifest do
   context 'with valid options' do
-
     let(:study) { create(:study, name: 'Test Study') }
     let(:supplier) { create(:supplier, name: 'Test Supplier') }
     let(:uat_action) { described_class.new(parameters) }
 
     context 'when generating a sample manfiest for a list of barcodes' do
       let(:parameters) do
-        { study: study, supplier: supplier, asset_type: 'tube', count: 2 }
+        { study: study, supplier: supplier, asset_type: asset_type, count: num_assets, with_samples: with_samples }
       end
 
-      it 'can be performed' do
-        expect(uat_action.perform).to eq true
+      context 'when creating tubes' do
+        let(:num_assets) { 2 }
+        let(:asset_type) { '1dtube' }
+
+        context 'when specifying with samples' do
+          let(:with_samples) { '1' }
+
+          it 'generates tubes' do
+            expect { uat_action.perform }.to(change { Tube.all.count }.by(2))
+          end
+
+          it 'generates samples' do
+            expect { uat_action.perform }.to(change { Sample.all.count }.by(2))
+          end
+        end
+
+        context 'when specifying without samples' do
+          let(:with_samples) { '0' }
+
+          it 'generates tubes' do
+            expect { uat_action.perform }.to(change { Tube.all.count }.by(2))
+          end
+
+          it 'does not generate samples' do
+            expect { uat_action.perform }.not_to(change { Sample.all.count })
+          end
+        end
       end
     end
   end
