f3483b8aaed41e03ca610d616472c9ba730fb9f2
Katy Taylor <kt17@sanger.ac.uk>
2021-11-29 10:53:54 +0000

    prettier

--- spec/models/sequencing_request_spec.rb
+++ spec/models/sequencing_request_spec.rb
@@ -145,10 +145,6 @@ RSpec.describe SequencingRequest, type: :model do
     let(:sequencing_request) { create(:sequencing_request, asset: source, target_asset: destination) }
     let(:tags) { create_list :tag, 4 }
 
-    before do
-      expect(tags.map(&:oligo).uniq.size == tags.size)
-    end
-
     context 'when compound samples are not necessary because each aliquot has a unique tag combination' do
       let(:aliquot1) { create :aliquot, sample: samples[0], tag_id: tags[0].id, tag2_id: tags[1].id, study: study }
       let(:aliquot2) { create :aliquot, sample: samples[1], tag_id: tags[0].id, tag2_id: tags[2].id, study: study }
@@ -164,9 +160,11 @@ RSpec.describe SequencingRequest, type: :model do
     context 'when compound samples are necessary because each aliquot does not have a unique tag combination' do
       context 'when there is one tag combination' do
         let(:aliquot1) do
- create :aliquot, sample: samples[0], tag_id: tags[0].id, tag2_id: tags[1].id, tag_depth: 1, study: study end
+          create :aliquot, sample: samples[0], tag_id: tags[0].id, tag2_id: tags[1].id, tag_depth: 1, study: study
+        end
         let(:aliquot2) do
- create :aliquot, sample: samples[1], tag_id: tags[0].id, tag2_id: tags[1].id, tag_depth: 2, study: study end
+          create :aliquot, sample: samples[1], tag_id: tags[0].id, tag2_id: tags[1].id, tag_depth: 2, study: study
+        end
 
         it 'creates a compound sample and transfers an aliquot of it' do
           expect(sequencing_request.target_asset.aliquots.count).to eq(0)
@@ -179,13 +177,17 @@ RSpec.describe SequencingRequest, type: :model do
       context 'when there are two tag combinations' do
         let(:samples) { create_list :sample, 4 }
         let(:aliquot1) do
- create :aliquot, sample: samples[0], tag_id: tags[0].id, tag2_id: tags[1].id, tag_depth: 1, study: study end
+          create :aliquot, sample: samples[0], tag_id: tags[0].id, tag2_id: tags[1].id, tag_depth: 1, study: study
+        end
         let(:aliquot2) do
- create :aliquot, sample: samples[1], tag_id: tags[0].id, tag2_id: tags[1].id, tag_depth: 2, study: study end
+          create :aliquot, sample: samples[1], tag_id: tags[0].id, tag2_id: tags[1].id, tag_depth: 2, study: study
+        end
         let(:aliquot3) do
- create :aliquot, sample: samples[2], tag_id: tags[2].id, tag2_id: tags[3].id, tag_depth: 1, study: study end
+          create :aliquot, sample: samples[2], tag_id: tags[2].id, tag2_id: tags[3].id, tag_depth: 1, study: study
+        end
         let(:aliquot4) do
- create :aliquot, sample: samples[3], tag_id: tags[2].id, tag2_id: tags[3].id, tag_depth: 2, study: study end
+          create :aliquot, sample: samples[3], tag_id: tags[2].id, tag2_id: tags[3].id, tag_depth: 2, study: study
+        end
         let(:aliquots) { [aliquot1, aliquot2, aliquot3, aliquot4] }
 
         it 'creates two compound samples and transfers an aliquot of each' do
