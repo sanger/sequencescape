400527421dd93f1d7b3d2012e38b895523ea6aa5
Stuart McHattie <sm49@sanger.ac.uk>
2021-12-07 13:53:00 +0000

    Use correct casing for method names

--- db/migrate/20211202174714_add_library_type_emseq.rb
+++ db/migrate/20211202174714_add_library_type_emseq.rb
@@ -2,20 +2,20 @@
 # Add new library type emSEQ for bespoke pcr submission templates
 #
 class AddLibraryTypeEmseq < ActiveRecord::Migration[6.0]
-  def self.REQUEST_TYPES
+  def self.request_types
     %w[limber_chromium_bespoke limber_pcr_bespoke].map { |rt_key| RequestType.find_by(key: rt_key) }
   end
 
   def self.up
     ActiveRecord::Base.transaction do |_t|
       lt = LibraryType.find_or_create_by!(name: 'emSEQ')
-      self.REQUEST_TYPES.each { |rt| rt.library_types << lt }
+      self.request_types.each { |rt| rt.library_types << lt }
     end
   end
 
   def self.down
     ActiveRecord::Base.transaction do |_t|
-      self.REQUEST_TYPES.each do |rt|
+      self.request_types.each do |rt|
         lts = rt.library_types
         lts.find_by(name: 'emSEQ').destroy!
       end
