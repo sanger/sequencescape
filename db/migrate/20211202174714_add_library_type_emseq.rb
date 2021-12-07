8cef2b07157653174c7117fe0bee1f180cd09f12
Stuart McHattie <sm49@sanger.ac.uk>
2021-12-07 14:01:17 +0000

    Remove redundant self. references

--- db/migrate/20211202174714_add_library_type_emseq.rb
+++ db/migrate/20211202174714_add_library_type_emseq.rb
@@ -11,13 +11,13 @@ class AddLibraryTypeEmseq < ActiveRecord::Migration[6.0]
   def self.up
     ActiveRecord::Base.transaction do |_t|
       lt = LibraryType.find_or_create_by!(name: 'emSEQ')
-      self.request_types.each { |rt| rt.library_types << lt }
+      request_types.each { |rt| rt.library_types << lt }
     end
   end
 
   def self.down
     ActiveRecord::Base.transaction do |_t|
-      self.request_types.each do |rt|
+      request_types.each do |rt|
         lts = rt.library_types
         lts.find_by(name: 'emSEQ').destroy!
       end
