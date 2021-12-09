a950d247baee1ed331156fc5148eccdc86b815fb
Stuart McHattie <sm49@sanger.ac.uk>
2021-12-09 12:25:57 +0000

    Add support for requests coming from the Docker local host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Support requests coming from other Docker containers on localhost.
+  config.hosts << 'host.docker.internal'
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
