bbd03e596c626d5952baa403fb0d27ade6a32a08
James Glover <james.glover@sanger.ac.uk>
2022-01-27 13:10:44 +0000

    Add legacy browser support

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { resolve, join } from "path";
 
 import RubyPlugin, { projectRoot } from "vite-plugin-ruby";
 import { createVuePlugin } from "vite-plugin-vue2";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  plugins: [RubyPlugin(), createVuePlugin()],
+  plugins: [RubyPlugin(), createVuePlugin(), legacy({ targets: ["defaults"] })],
   resolve: {
     alias: {
       "@": resolve(join(projectRoot, "app/javascript")),
