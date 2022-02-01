c6aa4c01312793aed26f0e670c497cf7f78bf113
James Glover <james.glover@sanger.ac.uk>
2022-02-01 15:20:12 +0000

    Fix csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -13,34 +13,30 @@ Rails.application.config.content_security_policy do |policy|
   #   policy.object_src  :none
   #   policy.script_src  :self, :https
 
-  # Allow @vite/client to hot reload javascript changes in development
-  if Rails.env.development?
-    policy.script_src(*policy.script_src, :unsafe_eval, "http://#{ViteRuby.config.host_with_port}")
-  end
-
-  # You may need to enable this in production as well depending on your setup.
-  #    policy.script_src *policy.script_src, :blob if Rails.env.test?
-
-  #   policy.style_src   :self, :https
-  # Allow @vite/client to hot reload style changes in development
-  policy.style_src(*policy.style_src, :unsafe_inline) if Rails.env.development?
-
-  # Allow @vite/client to hot reload changes in development
-  policy.connect_src(*policy.connect_src, "ws://#{ViteRuby.config.host_with_port}") if Rails.env.development?
-
-  #   # Specify URI for violation reports
-  #   # policy.report_uri "/csp-violation-report-endpoint"
-
   # Snippet provided after running
   # `bundle exec rails webpacker:install:vue`
   # > You need to enable unsafe-eval rule.
   # > This can be done in Rails 5.2+ for development environment in the CSP initializer
   # > config/initializers/content_security_policy.rb with a snippet like this:
   if Rails.env.development?
-    policy.script_src :self, :https, :unsafe_eval
+    # Also allow @vite/client to hot reload javascript changes in development
+    policy.script_src :self, :https, :unsafe_eval, "http://#{ViteRuby.config.host_with_port}"
   else
     policy.script_src :self, :https
   end
+
+  # You may need to enable this in production as well depending on your setup.
+  policy.script_src(*policy.script_src, :blob) if Rails.env.test?
+
+  #   policy.style_src   :self, :https
+  # Allow @vite/client to hot reload style changes in development
+  policy.style_src(*policy.style_src, :unsafe_inline) if Rails.env.development?
+
+  # Allow @vite/client to hot reload changes in development
+  policy.connect_src(:self, "ws://#{ViteRuby.config.host_with_port}") if Rails.env.development?
+
+  #   # Specify URI for violation reports
+  #   # policy.report_uri "/csp-violation-report-endpoint"
 end
 
 # If you are using UJS then enable automatic nonce generation
