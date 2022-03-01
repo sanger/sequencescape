c00de9f3a8b8dc2cb29cb9a036e6243c9f2de618
Jonnie Bevan <jonnie.bevan@tessella.com>
2022-03-01 16:28:56 +0000

    Logging

--- data/data_fixes/fix_sample_ids.rb
+++ data/data_fixes/fix_sample_ids.rb
@@ -1,10 +1,28 @@
 require 'csv'
+require 'logger'
+
+ActiveRecord::Base.logger = nil
+logger = Logger.new('sample_ids_log.log')
 data = CSV.parse(File.read("data/data_fixes/root_sample_ids.csv"), headers: true)
+failed_ids = Array.new()
+success_ids = Array.new()
+
 data.each_with_index do |row, index|
+    if index % 1000 == 0
+        puts "."
+    end
     sample = Sample::Metadata.find_by(sample_description: row['original_root_sample_id'])
     if sample
+        puts "updating: #{sample.sample_description}"
         sample.sample_description = row['root_sample_id']
         sample.save
+        success_ids << row["original_root_sample_id"]
+    else
+        failed_ids << row["original_root_sample_id"]
     end
 end
 
+logger.info("Successes: #{success_ids.length}")
+logger.debug("Updated IDs: #{success_ids}")
+logger.info("Failures: #{failed_ids.length}")
+logger.debug("Couldn't find/update IDs: #{failed_ids}")
