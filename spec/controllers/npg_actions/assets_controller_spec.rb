d669ede0ba4eefdf8c80f1bbd4f48170495ecedb
Andrew Sparkes <as28@sanger.ac.uk>
2022-04-14 14:07:50 +0100

    added rspec tests for npg assets controller

--- spec/controllers/npg_actions/assets_controller_spec.rb
+++ spec/controllers/npg_actions/assets_controller_spec.rb
@@ -10,68 +10,148 @@ RSpec.describe NpgActions::AssetsController, type: :controller do
   let(:lane) { create :lane_with_stock_plate }
   let(:study) { create :study }
   let(:batch) { create :sequencing_batch, state: 'started' }
-  let!(:request) do
+  let(:validSeqRequest) do
     create :sequencing_request_with_assets,
     batch: batch,
     request_type: batch.pipeline.request_types.first,
     study: study,
-    target_asset: lane
+    target_asset: lane,
+    state: 'passed'
+  end
+  let(:cancelledSeqRequest) do
+    create :sequencing_request_with_assets,
+    batch: batch,
+    request_type: batch.pipeline.request_types.first,
+    study: study,
+    target_asset: lane,
+    state: 'cancelled'
+  end
+  let(:failedSeqRequest) do
+    create :sequencing_request_with_assets,
+    batch: batch,
+    request_type: batch.pipeline.request_types.first,
+    study: study,
+    target_asset: lane,
+    state: 'failed'
+  end
+
+  shared_examples 'a passed state change' do
+    it 'renders and creates events', aggregate_failures: true do
+      expect(response).to render_template :'assets/show.xml.builder'
+
+      # Lane QC event
+      expect(lane.events.last).to be_a Event::AssetSetQcStateEvent
+      expect(lane.events.last.message).to eq('qc passed ok')
+
+      # State event
+      expect(Event.last).to be_a Event
+      expect(Event.last.created_by).to eq('npg')
+      expect(Event.last.message).to eq('Passed manual qc')
+
+      # Batch state
+      expect(batch.reload.state).to eq('released')
+
+      # Broadcast sequencing completed event
+      expect(BroadcastEvent::SequencingComplete.find_by(seed: lane)).to be_a BroadcastEvent::SequencingComplete
+      expect(BroadcastEvent::SequencingComplete.find_by(seed: lane).properties[:result]).to eq('passed')
+    end
+  end
+
+  shared_examples 'a failed state change' do
+    it 'renders and creates events', aggregate_failures: true do
+      expect(response).to render_template :'assets/show.xml.builder'
+
+      # Lane QC event
+      expect(lane.events.last).to be_a Event::AssetSetQcStateEvent
+      expect(lane.events.last.message).to eq('failed qc')
+
+      # State event
+      expect(Event.last).to be_a Event
+      expect(Event.last.created_by).to eq('npg')
+      expect(Event.last.message).to eq('Failed manual qc')
+
+      # Batch state
+      expect(batch.reload.state).to eq('released')
+
+      # Broadcast sequencing completed event
+      expect(BroadcastEvent::SequencingComplete.find_by(seed: lane)).to be_a BroadcastEvent::SequencingComplete
+      expect(BroadcastEvent::SequencingComplete.find_by(seed: lane).properties[:result]).to eq('failed')
+    end
   end
 
   describe '#pass' do
     context 'with valid parameters' do
       before do
+        validSeqRequest
         post :pass, params: { asset_id: lane.id, qc_information: { message: 'qc passed ok'} }, session: { user: user.id }
       end
 
-      it 'renders the form and creates the correct events', aggregate_failures: true do
-        expect(response).to render_template :'assets/show.xml.builder'
+      it_behaves_like 'a passed state change'
+    end
 
-        # Lane QC event
-        expect(lane.events.last).to be_a Event::AssetSetQcStateEvent
-        expect(lane.events.last.message).to eq('qc passed ok')
+    context 'with no valid requests' do
+      before do
+        post :pass, params: { asset_id: lane.id, qc_information: { message: 'qc passed ok'} }, session: { user: user.id }
+      end
 
-        # State event
-        expect(Event.last).to be_a Event
-        expect(Event.last.created_by).to eq('npg')
-        expect(Event.last.message).to eq('Passed manual qc')
-
-        # Batch state
-        expect(batch.reload.state).to eq('released')
-
-        # Broadcast sequencing completed event
-        expect(BroadcastEvent::SequencingComplete.find_by(seed: lane)).to be_a BroadcastEvent::SequencingComplete
-        expect(BroadcastEvent::SequencingComplete.find_by(seed: lane).properties[:result]).to eq('passed')
+      it 'renders the exception page' do
+        regexp =
+          Regexp.new(
+            "<error><message>Unable to identify a suitable single active request for Asset: #{lane.id}</message></error>",
+            Regexp::MULTILINE
+          )
+        expect(response).to have_http_status(404)
+        expect(response.body).to match(regexp)
       end
     end
 
-    context 'with an additional cancelled request' do
-      let!(:request2) do
-        create :sequencing_request_with_assets,
-        batch: batch,
-        request_type: batch.pipeline.request_types.first,
-        study: study,
-        target_asset: lane,
-        state: 'cancelled'
-      end
-
+    context 'with a single cancelled request' do
       before do
-        request2
-        post :pass, params: { asset_id: lane.id, qc_information: { message: 'test'} }, session: { user: user.id }
+        cancelledSeqRequest
+        post :pass, params: { asset_id: lane.id, qc_information: { message: 'qc passed ok'} }, session: { user: user.id }
       end
 
-      it 'renders the form' do
-        expect(response).to render_template :'assets/show.xml.builder'
+      it 'renders the exception page' do
+        regexp =
+          Regexp.new(
+            "<error><message>Unable to identify a suitable single active request for Asset: #{lane.id}</message></error>",
+            Regexp::MULTILINE
+          )
+        expect(response).to have_http_status(404)
+        expect(response.body).to match(regexp)
       end
+    end
+
+    context 'with both a valid and an additional cancelled request' do
+      before do
+        validSeqRequest
+        cancelledSeqRequest
+        post :pass, params: { asset_id: lane.id, qc_information: { message: 'qc passed ok'} }, session: { user: user.id }
+      end
+
+      it_behaves_like 'a passed state change'
     end
   end
 
-  # describe "#fail" do
-  #   it 'can be failed' do
-  #     # event should be generated
+  describe "#fail" do
+    context 'with valid parameters' do
+      before do
+        failedSeqRequest
+        post :fail, params: { asset_id: lane.id, qc_information: { message: 'failed qc'} }, session: { user: user.id }
+      end
 
-  #     # batch state should be set
-  #   end
-  # end
+      it_behaves_like 'a failed state change'
+    end
+
+    context 'with both a valid and an additional cancelled request' do
+      before do
+        failedSeqRequest
+        cancelledSeqRequest
+        post :fail, params: { asset_id: lane.id, qc_information: { message: 'failed qc'} }, session: { user: user.id }
+      end
+
+      it_behaves_like 'a failed state change'
+    end
+  end
 end
 
