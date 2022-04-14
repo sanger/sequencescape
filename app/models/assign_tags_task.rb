41305b60e6706b8ad1ff119109c1dd6da23170d7
James Glover <james.glover@sanger.ac.uk>
2022-04-14 11:54:56 +0100

    Clarify task removal

--- app/models/assign_tags_task.rb
+++ app/models/assign_tags_task.rb
@@ -3,8 +3,8 @@
 # Applies the selected tags to the {LibraryTube library tubes}.
 # Also appears to create and pool into a {MultiplexedLibraryTube}
 #
-# @note At time of writing (3/4/2019) this is used in:
-#   "PacBio Tagged Library Prep" (As a subclass)
+# @note At time of writing (14/4/2022) this is unused and can be removed once the
+# corresponding migrations have been run
 #
 # @see Tasks::AssignTagsHandler for behaviour included in the {WorkflowsController}
 class AssignTagsTask < Task
