0f4064d477e6f573a3f6d83a0a7df2021bb38228
James Glover <james.glover@sanger.ac.uk>
2022-04-28 11:07:29 +0100

    Ignore built assets

--- app/javascript/legacy/select_2_handling.js
+++ app/javascript/legacy/select_2_handling.js
@@ -4,6 +4,9 @@
 import $ from "jquery";
 
 import select2 from "select2";
+import "select2/dist/css/select2.css";
+import "@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.min.css";
+
 select2();
 
 $(() => {
