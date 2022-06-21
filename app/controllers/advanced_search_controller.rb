930a3b1ab0a8281a688f2a771459fe8af24af127
Katy Taylor <kt17@sanger.ac.uk>
2022-06-21 11:19:39 +0100

    simplify controller

--- app/controllers/advanced_search_controller.rb
+++ app/controllers/advanced_search_controller.rb
@@ -4,25 +4,18 @@
 # This was initially made to handle searches for Custom Metadata,
 # but the ambition is to consolidate all Sequencescape search functionality into this one page.
 class AdvancedSearchController < ApplicationController
-  attr_reader :searched, :results, :key_search_term, :value_search_term
-
   def index
     @searched = false
-    @results = []
-    @key_search_term = nil
-    @value_search_term = nil
   end
 
   def search
+    @searched = true
     @key_search_term = params['metadata_key']
     @value_search_term = params['metadata_value']
 
-    if key_search_term.present? && value_search_term.present?
-      @results = CustomMetadatum.where(key: key_search_term, value: value_search_term)
+    if @key_search_term.present? && @value_search_term.present?
+      @results = CustomMetadatum.where(key: @key_search_term, value: @value_search_term)
     end
-
-    # else  @results = [] # it gets set to nil anyway - not sure how
-    @searched = true
 
     render :index
   end
