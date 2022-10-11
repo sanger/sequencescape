b5f461003d1c79d17260926f3d56d0a20c2ebfe0
Harriet Craven <hc6@sanger.ac.uk>
2022-10-11 15:54:08 +0100

    Added comment to column, in migration

--- db/migrate/20221005133322_add_collected_by_to_sample_metadata.rb
+++ db/migrate/20221005133322_add_collected_by_to_sample_metadata.rb
@@ -4,6 +4,9 @@
 # To allow for manifests uploading samples from multiple collection sites
 class AddCollectedByToSampleMetadata < ActiveRecord::Migration[6.0]
   def change
-    add_column :sample_metadata, :collected_by, :string
+    add_column :sample_metadata,
+               :collected_by,
+               :string,
+               comment: 'Name of persons or institute who collected the specimen'
   end
 end
