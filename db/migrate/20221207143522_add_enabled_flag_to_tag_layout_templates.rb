45bcd1156351d16d18a1058d2f0acdba347d81b7
Stuart McHattie <sm49@sanger.ac.uk>
2022-12-08 11:37:21 +0000

    Add comments to new migration

--- db/migrate/20221207143522_add_enabled_flag_to_tag_layout_templates.rb
+++ db/migrate/20221207143522_add_enabled_flag_to_tag_layout_templates.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Add an enabled column to tag_layout_templates with a default value of true
 class AddEnabledFlagToTagLayoutTemplates < ActiveRecord::Migration[6.0]
   def change
     add_column :tag_layout_templates, :enabled, :boolean, default: true, null: false
