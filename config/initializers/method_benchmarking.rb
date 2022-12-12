f563b5d3fe532e90450f26fb0c358cf0aa4036e3
Eduardo Martin Rojo <emr@sanger.ac.uk>
2022-12-12 11:18:40 +0000

    Extend module (not include anymore)

--- config/initializers/method_benchmarking.rb
+++ config/initializers/method_benchmarking.rb
@@ -2,13 +2,15 @@
 if Rails.env.development?
   benchmarking_classes = [
     # Add here any classes you want to benchmark in local
+    # Note: Remember to start the process with the env var RAILS_LOG_TO_FILE set to true
+    Plate
   ]
 
   if benchmarking_classes.length.positive?
     Rails.logger.debug 'Benchmarking methods from classes:'
     benchmarking_classes.each do |klass|
       Rails.logger.debug { "* Benchmarking <#{klass.name}>" }
-      klass.include MethodBenchmarking
+      klass.extend MethodBenchmarking
       (klass.methods - [Object.methods].flatten).sort.compact.uniq.each do |method_name|
         next unless klass.method_defined?(method_name)
         klass.instance_eval { benchmark_method method_name, tag: 'configured_at_init' }
