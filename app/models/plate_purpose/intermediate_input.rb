31d36f5937bdcc01a76c17bad12d2a806b7c4967
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-04-04 15:13:23 +0100

    Refactored logic to detect intermediate input

--- app/models/plate_purpose/intermediate_input.rb
+++ app/models/plate_purpose/intermediate_input.rb
@@ -1,7 +1,24 @@
 class PlatePurpose::IntermediateInput < PlatePurpose
   READY_STATE = 'passed'
   def state_of(plate)
-    return READY_STATE if plate.ancestors.count == 0
+    return READY_STATE if valid_intermediate_input?(plate)
     super(plate)
+  end
+
+  def valid_intermediate_input?(plate)
+    [
+      (plate.ancestors.count == 0),
+      has_library_creation?(plate)
+    ].all?
+  end
+
+  def has_library_creation?(plate)
+    return false if plate.wells.with_contents.empty?
+    plate.wells.with_contents.all? do |w| 
+      return false if w.requests.empty?
+      w.requests.all? do |r| 
+        r.kind_of?(Request::LibraryCreation)
+      end
+    end
   end
 end
