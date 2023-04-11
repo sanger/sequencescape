da8310ab9f481afdbb5b1c56e601424a849f3123
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-04-11 15:43:08 +0100

    Try using let! to create all entities before

--- spec/models/plate_purpose/intermediate_input_spec.rb
+++ spec/models/plate_purpose/intermediate_input_spec.rb
@@ -5,12 +5,12 @@ require 'rails_helper'
 # This behaviour is in use for defining inputs that can be added in
 # the middle of a workflow
 describe PlatePurpose::IntermediateInput do
-  let(:plate_purpose_input) { create(:purpose_intermediate_input) }
+  let!(:plate_purpose_input) { create(:purpose_intermediate_input) }
 
   describe '#state_of' do
     subject(:state_of) { plate_purpose_input.state_of(plate) }
 
-    let(:plate) { create :plate, :with_wells, well_count: 2, purpose: plate_purpose_input }
+    let!(:plate) { create :plate, :with_wells, well_count: 2, purpose: plate_purpose_input }
 
     context 'with no requests' do
       it 'is pending' do
@@ -19,8 +19,8 @@ describe PlatePurpose::IntermediateInput do
     end
 
     context 'with ancestors' do
-      let(:parent_plate) { create :plate }
-      let(:plate) { create(:target_plate, parent: parent_plate, purpose: plate_purpose_input) }
+      let!(:parent_plate) { create :plate }
+      let!(:plate) { create(:target_plate, parent: parent_plate, purpose: plate_purpose_input) }
 
       context 'with no library requests' do
         it 'is pending' do
@@ -41,7 +41,7 @@ describe PlatePurpose::IntermediateInput do
     end
 
     context 'with no ancestors' do
-      let(:plate) { create :plate_with_untagged_wells, well_count: 2, purpose: plate_purpose_input }
+      let!(:plate) { create :plate_with_untagged_wells, well_count: 2, purpose: plate_purpose_input }
 
       context 'with no library requests' do
         it 'is pending' do
