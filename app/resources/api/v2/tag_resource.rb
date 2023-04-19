f34c2f2182060e3d3fc8dbc0c0275896a41fc6ac
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-04-19 17:10:08 +0100

    Add migration for extending transfer template New tag resource Added test for tag

---
+++ app/resources/api/v2/tag_resource.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Api
+  module V2
+    # Provides a JSON API representation of aliquot
+    # See: http://jsonapi-resources.com/ for JSONAPI::Resource documentation
+    class TagResource < BaseResource
+      # Constants...
+
+      immutable # comment to make the resource mutable
+
+      # model_name / model_hint if required
+
+      # Associations:
+      has_one :tag_group
+
+      # Attributes
+      attribute :oligo, readonly: true
+      attribute :map_id, readonly: true
+
+      # Filters
+
+      # Custom methods
+      # These shouldn't be used for business logic, and a more about
+      # I/O and isolating implementation details.
+
+      # Class method overrides
+    end
+  end
+end
---
+++ spec/resources/api/v2/tag_resource_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require './app/resources/api/v2/tag_resource'
+
+RSpec.describe Api::V2::TagResource, type: :resource do
+  subject(:tag_resource) { described_class.new(resource_model, {}) }
+
+  let(:resource_model) { build_stubbed :tag }
+
+  # Test attributes
+  it 'works', :aggregate_failures do
+    expect(tag_resource).to have_attribute :oligo
+    expect(tag_resource).to have_attribute :map_id
+
+    expect(tag_resource).to have_one(:tag_group).with_class_name('TagGroup')
+  end
+
+  # Custom method tests
+  # Add tests for any custom methods you've added.
+end
