bab1c2a00914fed50ae4a035daa6f6d5c0bdcd6b
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-04-28 16:50:36 +0100

    Fixed problem with change in seed file

--- db/seeds/0015_transfer_templates.rb
+++ db/seeds/0015_transfer_templates.rb
@@ -6,6 +6,9 @@ def locations_for(row_range, column_range)
   row_range.map { |row| column_range.map { |column| "#{row}#{column}" } }.flatten
 end
 
+wells_96_locations = locations_for(('A'..'H'), (1..12))
+wells_384_locations = locations_for(('A'..'P'), (1..24))
+
 def pooling_row_to_first_column_transfer_layout_96
   layout = {}
   ('A'..'H').each { |row| (1..12).each { |column| layout["#{row}#{column}"] = "#{row}1" } }
@@ -56,21 +59,19 @@ ActiveRecord::Base.transaction do
   TransferTemplate.create!(
     name: 'Whole plate to tube',
     transfer_class_name: 'Transfer::FromPlateToTube',
-    transfers: locations_for(('A'..'H'), (1..12))
+    transfers: wells_96_locations
   )
 
   TransferTemplate.create!(
     name: '384 plate to tube',
     transfer_class_name: 'Transfer::FromPlateToTube',
-    transfers: locations_for(('A'..'P'), (1..24))
+    transfers: wells_384_locations
   )
-
-  wells = locations_for(('A'..'H'), (1..12))
 
   TransferTemplate.create!(
     name: 'Flip Plate',
     transfer_class_name: 'Transfer::BetweenPlates',
-    transfers: wells.zip(wells.reverse).to_h
+    transfers: wells_96_locations.zip(wells_96_locations.reverse).to_h
   )
 
   TransferTemplate.create!(
