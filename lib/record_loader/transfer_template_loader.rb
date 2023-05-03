e09f39992b0d55b426edb3ce6407e6bb07646191
yoldas <ay6@sanger.ac.uk>
2023-05-03 13:17:58 +0100

    Rubocop

--- lib/record_loader/transfer_template_loader.rb
+++ lib/record_loader/transfer_template_loader.rb
@@ -4,11 +4,11 @@
 # across different environments
 # @see https://rubydoc.info/github/sanger/record_loader/
 module RecordLoader
-    class TransferTemplateLoader < ApplicationRecordLoader
-        config_folder 'transfer_templates'
-    end
+  class TransferTemplateLoader < ApplicationRecordLoader
+    config_folder 'transfer_templates'
+  end
 
-    def create_or_update(name, options)
-        TransferTemplate.create_with(options).find_or_create_by!(name: name)
-    end
+  def create_or_update(name, options)
+    TransferTemplate.create_with(options).find_or_create_by!(name: name)
+  end
 end
--- lib/tasks/record_loader/transfer_template.rake
+++ lib/tasks/record_loader/transfer_template.rake
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 namespace :record_loader do
-    desc 'Automatically generate TransferTemplate through TransferTemplateLoader'
-    task transfer_template: :environment do
-        RecordLoader::TransferTemplateLoader.new.create!
-    end
+  desc 'Automatically generate TransferTemplate through TransferTemplateLoader'
+  task transfer_template: :environment do
+    RecordLoader::TransferTemplateLoader.new.create!
+  end
 end
