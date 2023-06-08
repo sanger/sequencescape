fc11710702c3cce0cd997addb26545a92dc165f8
yoldas <ay6@sanger.ac.uk>
2023-06-08 15:51:51 +0100

    bioscan well is the same as well specialised field

--- app/sequencescape_excel/sequencescape_excel/specialised_field/bioscan_well.rb
+++ app/sequencescape_excel/sequencescape_excel/specialised_field/bioscan_well.rb
@@ -4,9 +4,22 @@ module SequencescapeExcel
   module SpecialisedField
     # BioscanWell
     # Specialised field to link validations from bioscan control type
-    class BioscanWell < Well
+    class BioscanWell
       include Base
       include ValueRequired
+
+      validate :check_container
+
+      private
+
+      def check_container
+        return if value == asset.try(:map_description)
+
+        errors.add(
+          :sample,
+          'cannot be moved between wells. The well and Sanger sample id columns should not be changed.'
+        )
+      end
     end
   end
 end
