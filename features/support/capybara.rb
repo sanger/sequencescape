fc775f733fcb23863137da9b94848442baed045e
Katy Taylor <kt17@sanger.ac.uk>
2023-07-05 16:49:52 +0100

    Resolving capybara and vite errors

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -11,21 +11,12 @@ Webdrivers::Chromedriver.update
 # end
 
 Capybara.register_driver :headless_chrome do |app|
-  driver = Capybara.drivers[:selenium_chrome_headless].call(app)
+  options = Selenium::WebDriver::Chrome::Options.new
 
-  configure_window_size(driver)
-  enable_chrome_headless_downloads(driver)
-end
-
-def configure_window_size(driver)
-  # links in header disappear if window is too small, then capybara can't click on them
-  driver.options[:options].add_argument('--window-size=1600,3200')
-end
-
-def enable_chrome_headless_downloads(driver)
-  driver.options[:options].add_preference(:download, default_directory: Capybara.save_path)
-  driver.browser.download_path = Capybara.save_path
-  driver
+  options.add_argument('--headless')
+  options.add_argument('--window-size=1600,3200')
+  options.add_preference('download.default_directory', DownloadHelpers::PATH)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara.register_driver :chrome do |app|
