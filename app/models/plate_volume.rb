843134c1f4debcf58d22d080ed3649662c4d9aac
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-10-03 11:55:57 +0100

    In PlateVolume, calculate_barcode_from_filename is moved to before_validation because CarrierWave was modifying the supplied barcode value before and that was altering from SQPP-1234.csv into SQPP-1234\(2).csv.
    Some changes for keywords arguments.

--- app/models/plate_volume.rb
+++ app/models/plate_volume.rb
@@ -8,7 +8,7 @@ class PlateVolume < ApplicationRecord # rubocop:todo Style/Documentation
 
   has_uploaded :uploaded, serialization_column: 'uploaded_file_name'
 
-  before_save :calculate_barcode_from_filename
+  before_validation :calculate_barcode_from_filename
   after_save :update_well_volumes
 
   # Is an update required given the timestamp specified
@@ -17,6 +17,7 @@ class PlateVolume < ApplicationRecord # rubocop:todo Style/Documentation
   end
 
   def call(filename, file)
+    puts filename
     return unless update_required?(file.stat.mtime)
 
     db_files.map(&:destroy)
@@ -97,6 +98,7 @@ class PlateVolume < ApplicationRecord # rubocop:todo Style/Documentation
     def find_for_filename(filename)
       find_by(uploaded_file_name: filename) or
         lambda do |filename, file|
+          puts filename
           PlateVolume.create!(uploaded_file_name: filename, updated_at: file.stat.mtime, uploaded: file)
         end
     end
