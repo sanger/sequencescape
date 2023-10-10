64c107a9c52016b1e45478cfee1f60314ed1abc5
Stephen Hulme <sh51@sanger.ac.uk>
2023-10-10 14:39:26 +0100

    style: lint

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,11 +56,10 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  if ENV.fetch('USE_POLLING_FILE_WATCHER', 'false') == 'true'
-    config.file_watcher = ActiveSupport::FileUpdateChecker
-  else
-    config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  end
+  use_polling_file_watcher = ENV.fetch('USE_POLLING_FILE_WATCHER', 'false') == 'true'
+  polling_file_watcher = ActiveSupport::FileUpdateChecker
+  evented_file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = use_polling_file_watcher ? polling_file_watcher : evented_file_watcher
 
   config.after_initialize do
     Bullet.enable = ENV['WITH_BULLET'] == 'true'
