ef924a504f650e4f1abf7f513da09c96d40a8650
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-10-25 14:29:42 +0100

    When the UAT 96 Tag group is created for tests first time, it is using  an adapter type = nil by default. The next requests for creation will not recreate, but retrieve again the same tag; the problem is in the tests that require this tag group to be of adapter type Chromium, the link does not exist because the tag group was already created. We will raise an exception if we try to re-use a tag group with a different setting.

--- app/uat_actions/uat_actions/generate_tag_group.rb
+++ app/uat_actions/uat_actions/generate_tag_group.rb
@@ -61,7 +61,14 @@ class UatActions::GenerateTagGroup < UatActions
     # All the form fields are accessible as simple attributes.
     # Return true if everything works
     report[:name] = name
-    return true if existing_tag_group
+    if existing_tag_group
+      existing_adapter_type_name = existing_tag_group.adapter_type.name
+      return true  if existing_adapter_type_name == adapter_type_name
+      raise %{
+        The tag group you want to create already exists and has a different 
+        adapter type (#{existing_adapter_type_name}) than they one you specified (#{adapter_type_name})
+      }
+    end
 
     adapter_type = TagGroup::AdapterType.find_by(name: adapter_type_name)
 
