b52f58c292f5609fe314c11974100feb35bd6269
Stephen Hulme <sh51@sanger.ac.uk>
2023-10-26 11:35:42 +0100

    style: lint inquiry

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -9,27 +9,27 @@ describe ApplicationHelper do
     subject(:favicon) { helper.favicon }
 
     it 'returns the favicon path for the production environment' do
-      allow(Rails).to receive(:env).and_return('production'.inquiry)
+      allow(Rails).to receive(:env).and_return('production')
       expect(favicon).to eq('favicon.ico')
     end
 
     it 'returns the favicon path for the training environment' do
-      allow(Rails).to receive(:env).and_return('training'.inquiry)
+      allow(Rails).to receive(:env).and_return('training')
       expect(favicon).to eq('favicon-training.ico')
     end
 
     it 'returns the favicon path for the staging environment' do
-      allow(Rails).to receive(:env).and_return('staging'.inquiry)
+      allow(Rails).to receive(:env).and_return('staging')
       expect(favicon).to eq('favicon-staging.ico')
     end
 
     it 'returns the favicon path for the development environment' do
-      allow(Rails).to receive(:env).and_return('development'.inquiry)
+      allow(Rails).to receive(:env).and_return('development')
       expect(favicon).to eq('favicon-development.ico')
     end
 
     it 'returns the favicon path for an unknown environment' do
-      allow(Rails).to receive(:env).and_return('unknown'.inquiry)
+      allow(Rails).to receive(:env).and_return('unknown')
       expect(favicon).to eq('favicon-development.ico')
     end
   end
@@ -38,27 +38,27 @@ describe ApplicationHelper do
     subject(:apple_icon) { helper.apple_icon }
 
     it 'returns the apple icon path for the production environment' do
-      allow(Rails).to receive(:env).and_return('production'.inquiry)
+      allow(Rails).to receive(:env).and_return('production')
       expect(apple_icon).to eq('apple-icon.png')
     end
 
     it 'returns the apple icon path for the training environment' do
-      allow(Rails).to receive(:env).and_return('training'.inquiry)
+      allow(Rails).to receive(:env).and_return('training')
       expect(apple_icon).to eq('apple-icon-training.png')
     end
 
     it 'returns the apple icon path for the staging environment' do
-      allow(Rails).to receive(:env).and_return('staging'.inquiry)
+      allow(Rails).to receive(:env).and_return('staging')
       expect(apple_icon).to eq('apple-icon-staging.png')
     end
 
     it 'returns the apple icon path for the development environment' do
-      allow(Rails).to receive(:env).and_return('development'.inquiry)
+      allow(Rails).to receive(:env).and_return('development')
       expect(apple_icon).to eq('apple-icon-development.png')
     end
 
     it 'returns the apple icon path for an unknown environment' do
-      allow(Rails).to receive(:env).and_return('unknown'.inquiry)
+      allow(Rails).to receive(:env).and_return('unknown')
       expect(apple_icon).to eq('apple-icon-development.png')
     end
   end
