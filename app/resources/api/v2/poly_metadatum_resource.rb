96ce1a2784ce16b890e6847b05c01aedbcbc3789
Andrew Sparkes <as28@sanger.ac.uk>
2023-12-07 12:34:33 +0000

    added api v2 resource and controller for poly metadata

---
+++ app/resources/api/v2/poly_metadatum_resource.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Api
+  module V2
+    # Provides a JSON API representation of PolyMetadatum
+    # See: http://jsonapi-resources.com/ for JSONAPI::Resource documentation
+    class PolyMetadatumResource < BaseResource
+      # Constants...
+
+      # immutable # uncomment to make the resource immutable
+
+      # model_name / model_hint if required
+
+      # Associations:
+      has_one :metadatable, polymorphic: true
+
+      # Attributes
+      attribute :key
+      attribute :value
+      attribute :created_at, readonly: true
+      attribute :updated_at, readonly: true
+
+      # Filters
+
+      # Custom methods
+      # These shouldn't be used for business logic, and a more about
+      # I/O and isolating implementation details.
+
+      # Class method overrides
+    end
+  end
+end
--- spec/factories/poly_metadata.rb
+++ spec/factories/poly_metadata.rb
@@ -2,8 +2,8 @@
 
 FactoryBot.define do
   factory :poly_metadatum do
-    metadatable factory: %i[request]
     sequence(:key) { |n| "some_key_#{n}" }
     sequence(:value) { |n| "some_value_#{n}" }
+    metadatable factory: %i[request]
   end
 end
