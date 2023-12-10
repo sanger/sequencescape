4300fd2e500cd595f97a4725cec51dabe9d3e703
yoldas <ay6@sanger.ac.uk>
2023-12-10 03:10:09 +0000

    Added test for the rake task to add Stock RNA Plate to Working Dilution parents

---
+++ spec/tasks/support/add_stock_rna_plate_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+Rails.application.load_tasks
+
+describe 'support:add_stock_rna_plate', type: :task do
+  let(:source_purpose_name) { 'Stock RNA Plate' }
+  let(:target_purpose_name) { 'Working Dilution' }
+  let(:plate_creator_name) { 'Working dilution' }
+
+  let(:source_purpose) { create(:plate_purpose, name: source_purpose_name, stock_plate: true) }
+  let(:target_purpose) { create(:plate_purpose, name: target_purpose_name, stock_plate: false) }
+  let(:plate_creator) { create(:plate_creator, name: plate_creator_name) }
+
+  let(:task_reenable) { Rake::Task[self.class.top_level_description].reenable }
+  let(:task_invoke) { Rake::Task[self.class.top_level_description].invoke }
+
+  before do
+    task_reenable # Allows the task to be invoked again
+    plate_creator # The task assumes the plate creator already exists
+    target_purpose # The task assumes the target purpose already exists
+  end
+
+  context 'when the source purpose is not in parent purposes' do
+    before { source_purpose }
+
+    it 'adds the source purpose' do
+      expect(plate_creator.parent_plate_purposes).not_to include(source_purpose)
+      task_invoke
+      plate_creator.reload # Reload to get the updated parent purposes
+      expect(plate_creator.parent_plate_purposes).to include(source_purpose)
+    end
+  end
+
+  context 'when the source purpose is already in parent purposes' do
+    before do
+      source_purpose
+      plate_creator.parent_plate_purposes << source_purpose
+    end
+
+    it 'does not add the source purpose' do
+      expect(plate_creator.parent_plate_purposes).to include(source_purpose)
+      task_invoke
+      plate_creator.reload # Reload to get the updated parent purposes
+      expect(plate_creator.parent_plate_purposes).to include(source_purpose)
+    end
+  end
+
+  context 'when the source purpose does not exist' do
+    it 'creates the source purpose' do
+      expect(PlatePurpose.find_by(name: source_purpose_name)).to be_nil
+      task_invoke
+      expect(PlatePurpose.last.name).to eq(source_purpose_name)
+      expect(PlatePurpose.where(name: source_purpose_name).count).to eq(1)
+    end
+  end
+
+  context 'when the source purpose already exists' do
+    before { source_purpose }
+
+    it 'does not create the source purpose' do
+      expect(PlatePurpose.find_by(name: source_purpose_name)).not_to be_nil
+      task_invoke
+      expect(PlatePurpose.last.name).to eq(source_purpose_name)
+      expect(PlatePurpose.where(name: source_purpose_name).count).to eq(1)
+    end
+  end
+end
