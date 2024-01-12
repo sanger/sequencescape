b7521db6879f74f3f0a2ec7e4423689c59b3e59e
Andrew Sparkes <as28@sanger.ac.uk>
2024-01-12 12:22:37 +0000

    changed key and value to be not nullable

--- db/migrate/20231204163029_create_poly_metadata.rb
+++ db/migrate/20231204163029_create_poly_metadata.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 #
 # Add polymorphic metadata table to flexibly hold key value pairs
-# See concern metadatable
 class CreatePolyMetadata < ActiveRecord::Migration[6.0]
   def change
     create_table :poly_metadata do |t|
-      t.string :key
-      t.string :value
+      t.string :key, null: false
+      t.string :value, null: false
       t.references :metadatable, polymorphic: true, null: false
 
       t.timestamps
