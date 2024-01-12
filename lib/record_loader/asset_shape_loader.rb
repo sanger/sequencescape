ca59d2c7579e1b0854086820bde84286fa16f7c8
yoldas <ay6@sanger.ac.uk>
2024-01-12 02:50:18 +0000

    Fixed the comment as the method does not update records

--- lib/record_loader/asset_shape_loader.rb
+++ lib/record_loader/asset_shape_loader.rb
@@ -13,7 +13,7 @@ module RecordLoader
     # up to that size, unless they already exist.
     #
     # @param name [String] the name of the AssetShape record
-    # @param options [Hash] the options to be used for creating or updating the record
+    # @param options [Hash] the options to be used for creating the record
     # @param options [String] :name If provided, this will override the 'name' parameter
     # @option options [Integer] :horizontal_ratio The horizontal ratio of the plate
     # @option options [Integer] :vertical_ratio The vertical ratio of the plate
