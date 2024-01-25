537684ce9ff970a0dcbd2d325a23c5cb5e19b9fb
yoldas <ay6@sanger.ac.uk>
2024-01-25 21:01:38 +0000

    Adding a concern module for default includes

---
+++ app/controllers/api/v2/concerns/default_includes_parser.rb
@@ -0,0 +1,11 @@
+module Api
+    module V2
+      module Concerns
+        module DefaultIncludesParser
+          def parse_default_includes(resource_klass)
+            # method body
+          end
+        end
+      end
+    end
+end
