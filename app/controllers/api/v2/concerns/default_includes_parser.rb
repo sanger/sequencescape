b18b668d2f45344167ce896a5d8a06af5b19bb8a
yoldas <ay6@sanger.ac.uk>
2024-01-28 05:11:27 +0000

    Prepend before action for handling default includes

--- app/controllers/api/v2/concerns/default_includes_parser.rb
+++ app/controllers/api/v2/concerns/default_includes_parser.rb
@@ -2,8 +2,12 @@ module Api
     module V2
       module Concerns
         module DefaultIncludesParser
-          def parse_default_includes(resource_klass)
+          extend ActiveSupport::Concern
+          included { prepend_before_action :handle_default_includes }
+
+          def handle_default_includes
             # method body
+            # p params
           end
         end
       end
