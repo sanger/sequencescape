c33ad305ee5e2b172f97f56a863cac25fa836741
Dasun Pubudumal <pubudumald@gmail.com>
2024-03-06 12:32:07 +0000

    Including test helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -40,6 +40,8 @@ class ActiveSupport::TestCase
   extend Sanger::Testing::Controller::Macros
   include FactoryBot::Syntax::Methods
 
+  include ApplicationHelper
+
   # Transactional fixtures accelerate your tests by wrapping each test method
   # in a transaction that's rolled back on completion.  This ensures that the
   # test database remains unchanged so your fixtures don't have to be reloaded
