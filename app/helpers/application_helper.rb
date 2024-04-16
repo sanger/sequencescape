57eb34ee20b08b6914b0c7518950a75ac1141368
Katy Taylor <kt17@sanger.ac.uk>
2024-04-16 13:53:20 +0100

    prettier

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -339,7 +339,8 @@ module ApplicationHelper
   #
   def legacy_javascript_tag
     javascript_tag nonce: true do
-      concat 'if (document.readyState === "loading") {window.addEventListener("DOMContentLoaded", function() {'.html_safe
+      concat 'if (document.readyState === "loading") {window.addEventListener("DOMContentLoaded", function() {'
+               .html_safe
       yield
       concat '});} else {'.html_safe
       yield
