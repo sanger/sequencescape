5477dfe1a0baaf359a8f9e957b78111126cfdd8d
Dasun Pubudumal <pubudumald@gmail.com>
2024-04-29 16:33:34 +0100

    Removing redundant helper imports

--- app/sequencescape_excel/sequencescape_excel/helpers.rb
+++ app/sequencescape_excel/sequencescape_excel/helpers.rb
@@ -4,7 +4,6 @@ module SequencescapeExcel
   ##
   # Helpers
   module Helpers
-    include RetentionInstructionHelper
     def load_file(folder, filename)
       YAML.load_file(Rails.root.join(folder, "#{filename}.yml")).with_indifferent_access
     end
