5bc40cb09ad8ed405c283802804c469df4acb93f
Dasun Pubudumal <pubudumald@gmail.com>
2024-05-01 14:28:05 +0100

    Refactoring rspec feature tests for retention instruction updates

--- spec/features/labware/retention_instruction_spec.rb
+++ spec/features/labware/retention_instruction_spec.rb
@@ -13,15 +13,19 @@ describe 'Update retention instructions' do
     end
   end
 
+  before do
+    login_user(user)
+    visit labware_path(asset)
+    click_link 'Edit Retention Instruction'
+    expect(page).to have_content 'Edit Retention Instruction'
+  end
+
   context 'when retention instruction exists' do
     let(:user) { create :admin }
     let(:asset) { create :plate_with_3_wells, retention_instruction: :destroy_after_2_years }
 
-    before do
-      login_user(user)
-      visit labware_path(asset)
-      click_link 'Edit Retention Instruction'
-      expect(page).to have_content 'Edit Retention Instruction'
+    it 'does not display the warning message' do
+      expect(page).to have_no_content 'This asset does not currently have a retention instruction.'
     end
 
     it_behaves_like 'updating retention instruction'
@@ -30,13 +34,6 @@ describe 'Update retention instructions' do
   context 'when retention instruction does not exist' do
     let(:user) { create :admin }
     let(:asset) { create :plate_with_3_wells, retention_instruction: nil }
-
-    before do
-      login_user(user)
-      visit labware_path(asset)
-      click_link 'Edit Retention Instruction'
-      expect(page).to have_content 'Edit Retention Instruction'
-    end
 
     it "does not have a retention instruction yet" do
       expect(page).to have_content 'This asset does not currently have a retention instruction.'
