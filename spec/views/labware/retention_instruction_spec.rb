6de6a525e55f7e3bc734462fe3a2d95440400a21
Dasun Pubudumal <pubudumald@gmail.com>
2024-05-01 18:38:14 +0100

    Added rspec for tubes

--- spec/views/labware/retention_instruction_spec.rb
+++ spec/views/labware/retention_instruction_spec.rb
@@ -7,16 +7,7 @@ RSpec.describe 'labware/retention_instruction.html.erb' do
   include RetentionInstructionHelper
   let(:user) { create :user }
 
-  before do
-    assign(:asset, asset)
-    assign(:retention_instruction_options, retention_instruction_option_for_select)
-    render
-  end
-
-  context 'when rendering an existing retention instruction' do
-    let(:current_user) { user }
-    let(:asset) { create :plate_with_3_wells, retention_instruction: :destroy_after_2_years }
-
+  shared_examples 'displaying retention instruction' do
     it 'displays the retention instruction' do
       expect(rendered).to match(/Retention Instruction/)
     end
@@ -34,6 +25,26 @@ RSpec.describe 'labware/retention_instruction.html.erb' do
     it 'displays a submit button' do
       expect(rendered).to have_button('Update')
     end
+  end
+
+  before do
+    assign(:asset, asset)
+    assign(:retention_instruction_options, retention_instruction_option_for_select)
+    render
+  end
+
+  context 'when rendering an existing retention instruction - plate' do
+    let(:current_user) { user }
+    let(:asset) { create :plate_with_3_wells, retention_instruction: :destroy_after_2_years }
+
+    it_behaves_like 'displaying retention instruction'
+  end
+
+  context 'when rendering an existing retention instruction - tube' do
+    let(:current_user) { user }
+    let(:asset) { create :tube, retention_instruction: :destroy_after_2_years }
+
+    it_behaves_like 'displaying retention instruction'
   end
 
 end
