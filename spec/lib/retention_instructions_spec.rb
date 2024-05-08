913e28ad19bbfc6beb363da32222e430c02d760b
Dasun Pubudumal <pubudumald@gmail.com>
2024-05-08 11:04:29 +0100

    Adding rake tests

--- spec/lib/retention_instructions_spec.rb
+++ spec/lib/retention_instructions_spec.rb
@@ -5,12 +5,13 @@ require 'rails_helper'
 require 'rake'
 
 RSpec.describe 'retention_instructions:backfill' do
+
   before do
     Rake.application.rake_require "tasks/retention_instructions"
     Rake::Task.define_task(:environment)
   end
 
-  describe 'backfill' do
+  context 'when retention instructions are there' do
     let(:run_rake_task) do
       Rake::Task['retention_instructions:backfill'].reenable
       Rake.application.invoke_task 'retention_instructions:backfill'
@@ -24,16 +25,15 @@ RSpec.describe 'retention_instructions:backfill' do
                                               metadata: { 'retention_instruction' => 'Destroy after 2 years' }
                                        )
       )
-      labware_without_metadata = create(:labware, retention_instruction: nil)
 
       # Execute
       run_rake_task
 
       # Verify
       expect(labware_with_metadata.reload.retention_instruction).not_to be_nil
-      expect(labware_without_metadata.reload.retention_instruction).to be_nil
     end
   end
+
 end
 
 # rubocop:enable RSpec/DescribeClass
