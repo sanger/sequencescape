272f18efcaac7a47142bde7d20aab97b317da2ad
Stephen Hulme <sh51@sanger.ac.uk>
2024-05-14 13:13:24 +0100

    fix: rename filename to combination of template name and time
    Also fix some rubocop style issues

--- app/controllers/bulk_submission_excel/downloads_controller.rb
+++ app/controllers/bulk_submission_excel/downloads_controller.rb
@@ -9,20 +9,22 @@ class BulkSubmissionExcel::DownloadsController < ApplicationController
     @input_field_infos.reject! { |k| k.key == :customer_accepts_responsibility }
     render 'new', layout: !request.xhr?
   end
-  def create # rubocop:todo Metrics/AbcSize, Metrics/MethodLength
+
+  def create
     finder = Asset::Finder.new(submission_parameters.fetch(:asset_barcodes, '').split(/\s+/))
-    download =
-      BulkSubmissionExcel::Download.new(
-        column_list: BulkSubmissionExcel.configuration.columns.all,
-        range_list: BulkSubmissionExcel.configuration.ranges,
+    bulk_submission_excel_config = BulkSubmissionExcel.configuration
+    download = BulkSubmissionExcel::Download.new(
+        column_list: bulk_submission_excel_config.columns.all,
+        range_list: bulk_submission_excel_config.ranges,
         defaults: params[:defaults],
         assets: finder.resolve
       )
+
     file = Tempfile.new
     download.save(file)
-    send_file file.path,
+    send_file(file.path,
               content_type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-              filename: "#{finder.barcodes.join('_')}_#{Time.current.strftime('%Y%m%d')}.xlsx"
+              filename: build_filename)
   rescue Asset::Finder::InvalidInputException => e
     flash[:error] = e.message
     redirect_back fallback_location: bulk_submissions_path
@@ -33,4 +35,17 @@ class BulkSubmissionExcel::DownloadsController < ApplicationController
   def submission_parameters
     params.require(:bulk_submission_excel_download).permit(:asset_barcodes)
   end
+
+  private
+
+  # Build a filename for the file to be downloaded
+  # Consists of the template name in kebab-case, followed by the current date and time in ISO8601 format
+  # e.g. "submission-template_20190101T120000.xlsx"
+  def build_filename
+    template_name = params[:defaults].fetch(:template_name, 'Submission Template')
+    # convert to kebab-case, catching existing hyphens
+    template_name = template_name.downcase.gsub(/[-\s]+/, '-')
+    "#{template_name.dasherize}_#{Time.current.strftime('%Y%m%dT%H%M%S')}.xlsx"
+  end
+
 end
