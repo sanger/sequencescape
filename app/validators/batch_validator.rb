417f4e19514f22a1e111d54aad0d749ac9299fc7
Dasun Pubudumal <pubudumald@gmail.com>
2024-06-25 16:35:12 +0100

    Refactor for rubocop warnings

--- app/validators/batch_validator.rb
+++ app/validators/batch_validator.rb
@@ -21,12 +21,10 @@ class BatchValidator < ActiveModel::Validator
     record.errors.add :base, 'All requests must be ready to be added to a batch' unless record.requests.all?(&:ready?)
   end
 
-  # rubocop:disable Style/GuardClause
   def batch_meets_minimum_size(record)
-    if record.min_size && (record.requests.size < record.min_size)
-      record.errors.add :base, "You must create batches of at least #{record.min_size}
-        requests in the pipeline #{record.pipeline.name}"
-    end
+    return unless record.min_size && (record.requests.size < record.min_size)
+    record.errors.add :base, "You must create batches of at least #{record.min_size}
+      requests in the pipeline #{record.pipeline.name}"
   end
 
   def requests_have_same_target_purpose(record)
@@ -38,15 +36,13 @@ class BatchValidator < ActiveModel::Validator
   end
 
   def requests_have_same_read_length(record)
-    unless record.pipeline.is_read_length_consistent_for_batch?(record)
-      record.errors.add :base, "The selected requests must have the same values in their 'Read length' field."
-    end
+    record.errors.add :base, "The selected requests must have the same values in their 'Read length' field." unless
+      record.pipeline.is_read_length_consistent_for_batch?(record)
   end
 
   def requests_have_same_flowcell_type(record)
-    unless record.pipeline.is_flowcell_type_consistent_for_batch?(record)
-      record.errors.add :base, "The selected requests must have the same values in their 'Flowcell Requested' field."
-    end
+    record.errors.add :base,
+                      "The selected requests must have the same values in their 'Flowcell Requested' field." unless
+      record.pipeline.is_flowcell_type_consistent_for_batch?(record)
   end
-  # rubocop:enable Style/GuardClause
 end
