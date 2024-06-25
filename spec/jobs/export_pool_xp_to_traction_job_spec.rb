bb3ff50332a32cdbd47446b7674ea02bbe1706f2
Stuart McHattie <sm49@sanger.ac.uk>
2024-06-25 11:35:51 +0100

    Add remaining tests for Export job

--- spec/jobs/export_pool_xp_to_traction_job_spec.rb
+++ spec/jobs/export_pool_xp_to_traction_job_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe ExportPoolXpToTractionJob, type: :job do
   let(:export_job) { described_class.new(tube.human_barcode) }
   let(:tube) { create :multiplexed_library_tube, sample_count: 3 }
 
-  let(:schema_subject) { 'bioscan-pool-xp-tube-to-traction' }
-  let(:schema_version) { 1 }
+  let(:schema_subject) { configatron.amqp.schemas.subjects[:export_pool_xp_to_traction][:subject] }
+  let(:schema_version) { configatron.amqp.schemas.subjects[:export_pool_xp_to_traction][:version] }
 
   let(:message_data) { { key: 'value' } }
   let(:message_schema) { '{}' }
@@ -45,7 +45,7 @@ avro_encode_message: encoded_message)
 
   describe '#get_message_data' do
     let(:project) { create :project }
-    let(:message_data) { export_job.get_message_data(tube.human_barcode) }
+    let(:actual_message) { export_job.get_message_data(tube.human_barcode) }
     let(:study) { create :study }
     let(:compound_sample) { create :sample }
 
@@ -55,42 +55,47 @@ avro_encode_message: encoded_message)
     end
 
 
-    it 'returns the correct message structure' do
-      expect(message_data).to include(
+    it 'returns a message with the correct values and/or types' do
+      expect(actual_message).to include(
         messageUuid: kind_of(String),
         messageCreateDateUtc: kind_of(Time),
         tubeBarcode: tube.human_barcode,
-        library: kind_of(Hash),
-        request: kind_of(Hash),
-        sample: kind_of(Hash)
+        library: {
+          volume: 100.0,
+          concentration: 0.0,
+          boxBarcode: 'Unspecified'
+        },
+        request: {
+          costCode: project.project_cost_code,
+          libraryType: 'Pacbio_Amplicon',
+          studyUuid: kind_of(String)
+        },
+        sample: {
+          sampleName: compound_sample.name,
+          sampleUuid: compound_sample.uuid,
+          speciesName: 'Unidentified'
+        }
       )
     end
 
-    it 'returns valid library data' do
-      expect(message_data[:library]).to include(
-        volume: 100.0,
-        concentration: 0.0,
-        boxBarcode: 'Unspecified'
-      )
-    end
-
-    it 'returns valid request data' do
-      expect(message_data[:request]).to include(
-        costCode: project.project_cost_code,
-        libraryType: 'Pacbio_Amplicon',
-        studyUuid: kind_of(String)
-      )
-    end
-
-    it 'returns valid sample data' do
-      expect(message_data[:sample]).to include(
-        sampleName: compound_sample.name,
-        sampleUuid: compound_sample.uuid,
-        speciesName: 'Unidentified'
-      )
+    it 'returns valid uuids for relevant fields' do
+      expect(UUIDTools::UUID.parse(actual_message[:messageUuid])).to be_valid
+      expect(UUIDTools::UUID.parse(actual_message[:request][:studyUuid])).to be_valid
+      expect(UUIDTools::UUID.parse(actual_message[:sample][:sampleUuid])).to be_valid
     end
   end
 
+  describe '#get_message_schema' do
+    before do
+      # Mock HTTP requests to the schema registry
+      stub_request(:get, "http://test-redpanda/subjects/#{schema_subject}/versions/#{schema_version}")
+        .to_return(status: 200, body: '{"schema": "{the_schema}"}', headers: {})
+    end
+
+    it 'returns the schema from the schema registry' do
+      expect(export_job.get_message_schema(schema_subject, schema_version)).to eq('{the_schema}')
+    end
+  end
 
   describe '#avro_encode_message' do
     let(:mock_schema) { instance_double(Avro::Schema) }
@@ -117,4 +122,50 @@ avro_encode_message: encoded_message)
     end
   end
 
+  describe '#send_message' do
+    let(:encoded_message) { 'encoded_message' }
+    let(:mock_bunny) { instance_double(Bunny::Session, start: nil, create_channel: mock_channel, close: nil) }
+    let(:mock_channel) { instance_double(Bunny::Channel, headers: mock_exchange) }
+    let(:mock_exchange) { instance_double(Bunny::Exchange, publish: nil) }
+
+    before do
+      allow(Bunny).to receive(:new).and_return(mock_bunny)
+    end
+
+    it 'creates a valid connection to the AMQP broker' do
+      export_job.send_message(encoded_message, schema_subject, schema_version)
+
+      expect(Bunny).to have_received(:new).with(
+        host: configatron.amqp.isg.host,
+        username: configatron.amqp.isg.username,
+        password: configatron.amqp.isg.password,
+        vhost: configatron.amqp.isg.vhost,
+        tls: true,
+        tls_ca_certificates: [configatron.amqp.isg.ca_certificate]
+      )
+    end
+
+    it 'sends the encoded message to the AMQP broker' do
+      export_job.send_message(encoded_message, schema_subject, schema_version)
+
+      expect(mock_exchange).to have_received(:publish).with(
+        encoded_message,
+        headers: { subject: schema_subject, version: schema_version, encoder_type: 'binary' },
+        persistent: true
+      )
+    end
+
+    it 'closes the connection after sending the message' do
+      export_job.send_message(encoded_message, schema_subject, schema_version)
+
+      expect(mock_bunny).to have_received(:close)
+    end
+
+    it 'closes the connection even if an error is raised' do
+      allow(mock_exchange).to receive(:publish).and_raise('An error')
+
+      expect { export_job.send_message(encoded_message, schema_subject, schema_version) }.to raise_error('An error')
+      expect(mock_bunny).to have_received(:close)
+    end
+  end
 end
