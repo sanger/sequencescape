d94f9e5b15efbfaea7d4f666b73aecb4b45ee595
Dasun Pubudumal <pubudumald@gmail.com>
2024-07-08 10:06:54 +0100

    Testing for intermittent test failures

--- spec/lib/dynamic_validations_spec.rb
+++ spec/lib/dynamic_validations_spec.rb
@@ -2,20 +2,20 @@
 
 require 'rails_helper'
 
-class NovaSeq6000ValidatorStub < CustomValidatorBase
-  def validate(record)
-    record.errors.add :base, 'NovaSeq6000Validator failed'
-  end
-end
-
 # rubocop:disable Lint/ConstantDefinitionInBlock
 RSpec.describe 'DynamicValidations' do
     context 'when added, includes the dynamic validations' do
 
       before do
-        # Dynamically assign the stub class to the constant for the test environment
-        stub_const('NovaSeq6000Validator', NovaSeq6000ValidatorStub)
+        # Define an anonymous class with the desired behavior
+        nova_seq6000_validator = Class.new(CustomValidatorBase) do
+          def validate(record)
+            record.errors.add :base, 'NovaSeq6000Validator failed'
+          end
+        end
 
+        # Assign the anonymous class to a constant for this test
+        stub_const('NovaSeq6000Validator', nova_seq6000_validator)
       end
 
       let(:pipeline) { create :pipeline, validator_class_name: 'NovaSeq6000Validator'}
