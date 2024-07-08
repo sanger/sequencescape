23f7358e9999c8c94f0d7cb9ecdb48b8204839f7
Dasun Pubudumal <pubudumald@gmail.com>
2024-07-08 10:19:57 +0100

    Refactoring for intermittent test failures

--- spec/lib/dynamic_validations_spec.rb
+++ spec/lib/dynamic_validations_spec.rb
@@ -21,7 +21,6 @@ RSpec.describe 'DynamicValidations' do
       let(:pipeline) { create :pipeline, validator_class_name: 'NovaSeq6000Validator'}
       let(:batch) { Batch.new(pipeline: pipeline) }
 
-
       it 'adds dynamic validations' do
         expect(batch.valid?).to be false
         expect(batch.errors[:base]).to include('NovaSeq6000Validator failed')
