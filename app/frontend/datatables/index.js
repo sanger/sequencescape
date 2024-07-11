8c1ad70c8b223d2a644ccb4b8675795927dd8871
yoldas <ay6@sanger.ac.uk>
2024-07-11 16:51:45 +0100

    Change imports

--- app/frontend/datatables/index.js
+++ app/frontend/datatables/index.js
@@ -1,32 +1,18 @@
 // Load the datatables plugins
-import $ from "jquery";
+
 import "jszip";
 
-// I went in a few circles here.
-// Imports alone break styling when including rowReorder and responsive
-// Switching to the requires and immediately executed factory functions
-// resulted in the uncaught type-error mentioned in the link.
-// https://datatables.net/forums/discussion/43042/uncaught-typeerror-cannot-set-property-of-undefined/p2
-
-import dt from "datatables.net-bs4";
-import dtButtons from "datatables.net-buttons-bs4";
-import dtButtonsColVis from "datatables.net-buttons/js/buttons.colVis";
-import dtButtonsHtml5 from "datatables.net-buttons/js/buttons.html5";
-import dtFixedColumn from "datatables.net-fixedcolumns-bs4";
-import dtFixedHeader from "datatables.net-fixedheader-bs4";
-import dtResponsive from "datatables.net-responsive-bs4";
-import dtRowGroup from "datatables.net-rowgroup-bs4";
-import dtRowOrder from "datatables.net-rowreorder-bs4";
-
-dt(window, $);
-dtButtons(window, $);
-dtButtonsColVis(window, $);
-dtButtonsHtml5(window, $);
-dtFixedColumn(window, $);
-dtFixedHeader(window, $);
-dtResponsive(window, $);
-dtRowGroup(window, $);
-dtRowOrder(window, $);
+import "datatables.net";
+import "datatables.net-bs4";
+import "datatables.net-buttons";
+import "datatables.net-buttons-bs4";
+import "datatables.net-buttons/js/buttons.colVis";
+import "datatables.net-buttons/js/buttons.html5";
+import "datatables.net-fixedcolumns-bs4";
+import "datatables.net-fixedheader-bs4";
+import "datatables.net-responsive-bs4";
+import "datatables.net-rowgroup-bs4";
+import "datatables.net-rowreorder-bs4";
 
 // We won't import the CSS automatically, so do it here.
 // I couldn't import the non minified version here. While vite
