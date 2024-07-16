985dd25f36d731149205e58d5ddb9b2831ccbc1c
Ben Topping <ben.topping@outlook.com>
2024-07-16 15:41:21 +0100

    refactor(data-worksheet): removes code duplication in create_row

--- app/sample_manifest_excel/sample_manifest_excel/worksheet/data_worksheet.rb
+++ app/sample_manifest_excel/sample_manifest_excel/worksheet/data_worksheet.rb
@@ -84,15 +84,11 @@ module SampleManifestExcel
         axlsx_worksheet.add_row do |row|
           columns.each do |column|
             # If the row is invalid in the manifest, the cell should be locked
-            if sample_manifest.invalid_wells&.include?(detail.fetch(:position, nil))
-              row.add_cell column.attribute_value(detail),
-                           type: column.type,
-                           style: find_or_create_style([:disabled])&.reference
-            else
-              style = find_or_create_style(column.style)&.reference
+            style_name =
+              (sample_manifest.invalid_wells&.include?(detail.fetch(:position, nil)) ? [:disabled] : column.style)
 
-              row.add_cell column.attribute_value(detail), type: column.type, style: style
-            end
+            style = find_or_create_style(style_name)&.reference
+            row.add_cell column.attribute_value(detail), type: column.type, style: style
           end
         end
       end
