627b1b16f54e67e825fe6eceb385332e3cd68a03
Stuart McHattie <sm49@sanger.ac.uk>
2024-07-23 18:47:54 +0100

    Add tests for barcode printers on V2

---
+++ spec/requests/api/v2/barcode_printers_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require './spec/requests/api/v2/shared_examples/api_key_authenticatable'
+
+describe 'Barcode Printers API', with: :api_v2 do
+  let(:base_endpoint) { '/api/v2/barcode_printers' }
+
+  it_behaves_like 'ApiKeyAuthenticatable'
+
+  describe '#get all Barcode Printers' do
+    before { create_list(:barcode_printer, 5) }
+
+    it 'returns the list of Barcode Printers' do
+      api_get base_endpoint
+
+      expect(response).to have_http_status(:success)
+      expect(json['data'].length).to eq(5)
+    end
+  end
+
+  describe '#get a specific Barcode Printer' do
+    let(:resource_model) { create(:barcode_printer) }
+
+    it 'returns the template' do
+      api_get "#{base_endpoint}/#{resource_model.id}"
+      expect(response).to have_http_status(:success)
+      expect(json.dig('data', 'type')).to eq('barcode_printers')
+      expect(json.dig('data', 'attributes', 'uuid')).to eq(resource_model.uuid)
+      expect(json.dig('data', 'attributes', 'name')).to eq(resource_model.name)
+      expect(json.dig('data', 'attributes', 'print_service')).to eq(resource_model.print_service)
+      expect(json.dig('data', 'attributes', 'barcode_type')).to eq(resource_model.barcode_printer_type.name)
+    end
+  end
+
+  describe '#patch a specific Barcode Printer' do
+    let(:resource_model) { create(:barcode_printer) }
+    let(:payload) do
+      {
+        'data' => {
+          'id' => resource_model.id,
+          'type' => 'barcode_printers',
+          'attributes' => {
+            'name' => 'Updated Name'
+          }
+        }
+      }
+    end
+
+    it 'finds no route for the method' do
+      expect { api_patch "#{base_endpoint}/#{resource_model.id}", payload }.to raise_error(
+        ActionController::RoutingError
+      )
+    end
+  end
+
+  describe '#post a new Barcode Printer' do
+    let(:payload) { { 'data' => { 'type' => 'barcode_printers', 'attributes' => { 'name' => 'New Name' } } } }
+
+    it 'finds no routes for the method' do
+      expect { api_post base_endpoint, payload }.to raise_error(ActionController::RoutingError)
+    end
+  end
+end
---
+++ spec/resources/api/v2/barcode_printer_resource_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require './app/resources/api/v2/barcode_printer_resource'
+
+RSpec.describe Api::V2::BarcodePrinterResource, type: :resource do
+  subject(:resource) { described_class.new(resource_model, {}) }
+
+  let(:resource_model) { build_stubbed :barcode_printer }
+
+  # Test attributes
+  it 'has the expected attributes', :aggregate_failures do
+    expect(resource).not_to have_attribute :id
+    expect(resource).to have_attribute :uuid
+    expect(resource).to have_attribute :name
+    expect(resource).to have_attribute :print_service
+    expect(resource).to have_attribute :barcode_type
+  end
+
+  # Updatable fields
+  it 'allows updating of read-write fields', :aggregate_failures do
+    expect(resource).to have_updatable_field :name
+  end
+
+  it 'disallows updating of read only fields', :aggregate_failures do
+    expect(resource).not_to have_updatable_field :uuid
+    expect(resource).not_to have_updatable_field :print_service
+    expect(resource).not_to have_updatable_field :barcode_type
+  end
+
+  # Filters
+  # eg. it { is_expected.to filter(:order_type) }
+
+  # Associations
+  # eg. it { is_expected.to have_many(:samples).with_class_name('Sample') }
+
+  # Custom method tests
+  # Add tests for any custom methods you've added.
+end
