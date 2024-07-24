1b77c9f3bb7f005900cb4ea62ea2f4adf7035d69
Stuart McHattie <sm49@sanger.ac.uk>
2024-07-24 14:36:52 +0100

    Update tests for the resource class

--- spec/resources/api/v2/sample_metadata_resource_spec.rb
+++ spec/resources/api/v2/sample_metadata_resource_spec.rb
@@ -4,18 +4,32 @@ require 'rails_helper'
 require './app/resources/api/v2/sample_metadata_resource'
 
 RSpec.describe Api::V2::SampleMetadataResource, type: :resource do
-  describe 'it works' do
-    subject { described_class.new(sample_metadata, {}) }
+  subject(:resource) { described_class.new(sample_metadata, {}) }
 
-    let(:sample_metadata) { create(:sample_metadata) }
+  let(:sample_metadata) { create(:sample_metadata) }
 
-    it 'has the expected attributes' do
-      expect(subject).to have_attribute :sample_common_name
-      expect(subject).to have_attribute :supplier_name
-      expect(subject).to have_attribute :collected_by
-      expect(subject).to have_attribute :donor_id
-      expect(subject).to have_attribute :concentration
-      expect(subject).to have_attribute :volume
-    end
+  # Test attributes
+  it 'has the expected attributes', :aggregate_failures do
+    expect(resource).to have_attribute :sample_common_name
+    expect(resource).to have_attribute :supplier_name
+    expect(resource).to have_attribute :collected_by
+    expect(resource).to have_attribute :donor_id
+    expect(resource).to have_attribute :concentration
+    expect(resource).to have_attribute :volume
   end
+
+  # Updatable fields
+  it 'allows updating of read-write fields', :aggregate_failures do
+    expect(resource).to have_updatable_field :sample_common_name
+    expect(resource).to have_updatable_field :supplier_name
+    expect(resource).to have_updatable_field :collected_by
+    expect(resource).to have_updatable_field :donor_id
+    expect(resource).to have_updatable_field :concentration
+    expect(resource).to have_updatable_field :volume
+  end
+
+  # Non-updatable fields -- uncomment to use
+  # it 'disallows updating of read only fields', :aggregate_failures do
+  #   expect(resource).not_to have_updatable_field :uuid
+  # end
 end
