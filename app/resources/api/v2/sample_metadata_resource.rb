a183f721e88987f158bfe538368611402ab655b5
Stuart McHattie <sm49@sanger.ac.uk>
2024-07-24 14:36:27 +0100

    Populate Sample Metadata with YARD docs

--- app/resources/api/v2/sample_metadata_resource.rb
+++ app/resources/api/v2/sample_metadata_resource.rb
@@ -4,20 +4,50 @@ module Api
   module V2
     # SampleMetadataResource
     class SampleMetadataResource < BaseResource
-      attribute :sample_common_name
-      attribute :supplier_name
-      attribute :collected_by
-      attribute :cohort
-      attribute :sample_description
-      attribute :donor_id
-      attribute :concentration
-      attribute :volume
-
-      # set add_model_hint true to allow updates from Limber, otherwise get a
+      # Set add_model_hint true to allow updates from Limber, otherwise get a
       # 500 error as it looks for resource Api::V2::MetadatumResource
       model_name 'Sample::Metadata', add_model_hint: true
 
+      ###
+      # Attributes
+      ###
+
+      # @!attribute [rw]
+      # @return [String] The sample cohort.
+      attribute :cohort
+
+      # @!attribute [rw]
+      # @return [String] The name of the body collecting the sample.
+      attribute :collected_by
+
+      # @!attribute [rw]
+      # @return [String] The sample concentration.
+      attribute :concentration
+
+      # @!attribute [rw]
+      # @return [String] The ID of the sample donor.
+      attribute :donor_id
+
+      # @!attribute [rw]
+      # @return [String] The common name for the sample.
+      attribute :sample_common_name
+
+      # @!attribute [rw]
+      # @return [String] A description of the sample.
+      attribute :sample_description
+
+      # @!attribute [rw]
+      # @return [String] The supplier name for the sample.
+      attribute :supplier_name
+
+      # @!attribute [rw]
+      # @return [String] The volume of the sample.
+      attribute :volume
+
+      ###
       # Filters
+      ###
+
       filter :sample_id
     end
   end
