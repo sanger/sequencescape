9408927d25fe62afed1ec365188b323043db1b67
Ben Topping <ben.topping@outlook.com>
2024-07-31 11:32:11 +0100

    misc: removes enable_hiseq_submission_templates task due to complexity with existing templates

--- lib/tasks/support/disable_hiseq_submission_templates.rake
+++ lib/tasks/support/disable_hiseq_submission_templates.rake
@@ -22,24 +22,4 @@ namespace :support do
     end
     Rails.logger.info("Disabled #{templates_changed} HiSeq submission templates.")
   end
-
-  desc 'Enable the HiSeq submission templates'
-  task enable_hiseq_submission_templates: :environment do
-    # This task enables all HiSeq submission templates from the database.
-    templates_changed = 0
-    ActiveRecord::Base.transaction do
-      SubmissionTemplate
-        .where('name LIKE ?', '%hiseq%')
-        .find_each do |submission_template|
-          # Skip if the template is superced by a known template
-          next if submission_template.superceded_by_id != SubmissionTemplate::SUPERCEDED_BY_UNKNOWN_TEMPLATE
-          # Set the superceded_by_id to LATEST_VERSION (-1) to show the template
-          submission_template.superceded_by_id = SubmissionTemplate::LATEST_VERSION
-          submission_template.superceded_at = nil
-          submission_template.save!
-          templates_changed += 1
-        end
-    end
-    Rails.logger.info("Enabled #{templates_changed} HiSeq submission templates.")
-  end
 end
--- spec/tasks/support/disable_hiseq_submission_templates_spec.rb
+++ spec/tasks/support/disable_hiseq_submission_templates_spec.rb
@@ -34,31 +34,4 @@ describe 'support:disable_hiseq_submission_templates', type: :task do
       expect(SubmissionTemplate.where('name NOT LIKE ?', '%hiseq%').all? { |st| st.superceded_by_id == -1 }).to be true
     end
   end
-
-  context 'when the enable_hiseq_submission_templates task is invoked' do
-    it 'updates all hiseq submissions to have a superceded_by_id of -1' do
-      # Create unused submission templates
-      create_list(:submission_template, 5)
-      (1..5).each do |i|
-        # Create HiSeq submission templates
-        create(:submission_template, name: "HiSeq #{i}", superceded_by_id: -2)
-        # Create alternate name HiSeq submission templates
-        create(:submission_template, name: "Some other hiseq #{i}", superceded_by_id: -2)
-        # Create superceded HiSeq submission templates that should not be restored (already superceded)
-        create(:submission_template, name: "Superceded HiSeq #{i}", superceded_by_id: 5)
-      end
-
-      allow(Rails.logger).to receive(:info)
-
-      Rake::Task['support:enable_hiseq_submission_templates'].invoke
-
-      expect(Rails.logger).to have_received(:info).with('Enabled 10 HiSeq submission templates.').at_least(:once)
-      # Expect all hiseq submissions to have a superceded_by_id of -1
-      expect(SubmissionTemplate.where('name LIKE ?', '%hiseq%').count { |st| st.superceded_by_id == -1 }).to eq(10)
-      # Expect all pre-existing superceded hiseq submissions to have not been updated
-      expect(
-        SubmissionTemplate.where('name LIKE ?', '%superceded hiseq%').count { |st| st.superceded_by_id == 5 }
-      ).to eq(5)
-    end
-  end
 end
