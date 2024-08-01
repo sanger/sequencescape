c92f90c6377a79ceb262e805c3dbb159a01b0cf8
yoldas <ay6@sanger.ac.uk>
2024-08-01 23:32:03 +0100

    Remove the indexing method

--- app/models/tag_set.rb
+++ app/models/tag_set.rb
@@ -8,11 +8,6 @@ class TagSet < ApplicationRecord
   belongs_to :tag_group, class_name: 'TagGroup', optional: true
   belongs_to :tag2_group, class_name: 'TagGroup', optional: true
 
-  # Constants for indexing strings
-  INDEX_SINGLE = 'single'
-  INDEX_DUAL = 'dual'
-  INDEX_NONE = 'none'
-
   # Validations
   validates :name, presence: true, uniqueness: true
   validate :validate_adapter_type
@@ -25,16 +20,6 @@ class TagSet < ApplicationRecord
   # Returns visibility for a tag set by checking the visibility of its tag groups
   def visible?
     tag_group&.visible? && (tag2_group.nil? || tag2_group.visible?)
-  end
-
-  def indexing
-    if tag_group && tag2_group
-      INDEX_DUAL
-    elsif tag_group
-      INDEX_SINGLE
-    else
-      INDEX_NONE
-    end
   end
 
   # Validate that adapter type of tag groups of a tag set are equal
