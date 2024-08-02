8f66d7994265fd028ac6fbb839918eca174b4792
Stuart McHattie <sm49@sanger.ac.uk>
2024-08-02 10:12:21 +0100

    Update TagLayoutTemplate resource with YARD docs

--- app/resources/api/v2/tag_layout_template_resource.rb
+++ app/resources/api/v2/tag_layout_template_resource.rb
@@ -5,31 +5,40 @@ module Api
     # Provides a JSON API representation of TagLayoutTemplate
     # See: http://jsonapi-resources.com/ for JSONAPI::Resource documentation
     class TagLayoutTemplateResource < BaseResource
-      # Constants...
+      immutable
 
-      # immutable # uncomment to make the resource immutable
+      ###
+      # Relationships
+      ###
 
-      # model_name / model_hint if required
-
-      default_includes :uuid_object
-
-      # Associations:
       has_one :tag_group
       has_one :tag2_group
 
+      ###
       # Attributes
+      ###
+
+      # @!attribute [r]
+      # @return [String] The UUID of the tag layout template.
       attribute :uuid, readonly: true
+
+      # @!attribute [r]
+      # @return [String] The name of the tag layout template.
+      attribute :name, readonly: true
+
+      # @!attribute [r]
+      # @return [String] The direction algorithm name for the tag layout template.
       attribute :direction, readonly: true
+
+      # @!attribute [r]
+      # @return [String] The walking_by algorithm name for the tag layout template.
       attribute :walking_by, readonly: true
 
+      ###
       # Filters
+      ###
+
       filter :enabled, default: true
-
-      # Custom methods
-      # These shouldn't be used for business logic, and a more about
-      # I/O and isolating implementation details.
-
-      # Class method overrides
     end
   end
 end
