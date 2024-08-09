4422d5f31ab6b48ffbb4a22d644d8b0475a562d5
Stuart McHattie <sm49@sanger.ac.uk>
2024-08-09 14:13:11 +0100

    Add documentation to the UserResource plus a new uuid filter

--- app/resources/api/v2/user_resource.rb
+++ app/resources/api/v2/user_resource.rb
@@ -5,22 +5,40 @@ module Api
     # Provides a JSON API representation of user
     # See: http://jsonapi-resources.com/ for JSONAPI::Resource documentation
     class UserResource < BaseResource
-      # Constants...
+      immutable
 
-      immutable # comment to make the resource mutable
-
-      # model_name / model_hint if required
-
-      default_includes :uuid_object
-
+      ###
       # Attributes
+      ###
+
+      # @!attribute [r] uuid
+      #   @return [String] the UUID of the user.
       attribute :uuid, readonly: true
+
+      # @!attribute [r] login
+      #   @return [String] the user's login identifier.
       attribute :login, readonly: true
+
+      # @!attribute [r] first_name
+      #   @return [String] the user's first/given name.
       attribute :first_name, readonly: true
+
+      # @!attribute [r] last_name
+      #   @return [String] the user's last/surname.
       attribute :last_name, readonly: true
 
+      ###
       # Filters
+      ###
+
+      # @!method user_code
+      #   A filter to return only users with the given user code.
+      #   The given user code will be compared with the swipecodes and barcodes for users until matches are found.
       filter :user_code, apply: lambda { |records, value, _options| records.with_user_code(*value) }
+
+      # @!method uuid
+      #   A filter to return only users with the given UUID.
+      filter :uuid, apply: lambda { |records, value, _options| records.with_uuid(*value) }
     end
   end
 end
