269224c2820a6f04a9f326916aeafcab8b685269
Ben Topping <ben.topping@outlook.com>
2024-08-14 14:53:23 +0100

    misc: adds further documentation, updates validator name

--- app/models/tag_set.rb
+++ app/models/tag_set.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
+# Links together two related tag groups - i7 and i5 - to represent a dual index tag set
+# It can also be used to represent single index tag sets
+# Background explained in Y24-170 (https://github.com/sanger/sequencescape/issues/4160)
 class TagSet < ApplicationRecord
+  # For dual index tags, tag_group is i7 oligos and tag2_group is i5 oligos
   belongs_to :tag_group, class_name: 'TagGroup', optional: false
   belongs_to :tag2_group, class_name: 'TagGroup', optional: true
 
@@ -9,7 +13,7 @@ class TagSet < ApplicationRecord
   delegate :adapter_type, to: :tag_group
 
   validates :name, presence: true, uniqueness: true
-  validate :tag_group_adapter_types
+  validate :tag_group_adapter_types_must_match
 
   # Dynamic method to determine the visibility of a tag_set based on the visibility of its tag_groups
   def visible
@@ -17,7 +21,7 @@ class TagSet < ApplicationRecord
   end
 
   # Method to determine that both tag groups have the same adapter type
-  def tag_group_adapter_types
+  def tag_group_adapter_types_must_match
     return unless tag2_group && tag_group.adapter_type != tag2_group.adapter_type
     errors.add(:tag_group, 'Adapter types of tag groups must match')
   end
