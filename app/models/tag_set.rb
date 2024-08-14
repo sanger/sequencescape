e7edbba604a47617c2fbe15e7f4afa223e8a5207
Ben Topping <54811755+BenTopping@users.noreply.github.com>
2024-08-14 14:47:13 +0100

    docs: fix typo
    Co-authored-by: KatyTaylor <kt17@sanger.ac.uk>

--- app/models/tag_set.rb
+++ app/models/tag_set.rb
@@ -5,7 +5,7 @@ class TagSet < ApplicationRecord
   belongs_to :tag2_group, class_name: 'TagGroup', optional: true
 
   # We can assume adapter_type is the same for both tag groups
-  # But tag2_group may be present so we delegate to tag_group
+  # But tag2_group may not be present so we delegate to tag_group
   delegate :adapter_type, to: :tag_group
 
   validates :name, presence: true, uniqueness: true
