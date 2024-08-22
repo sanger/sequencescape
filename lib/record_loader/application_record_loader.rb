ffca885ed3ad55922f59bea1551313016da79a08
Wendy Yang <wy1@sanger.ac.uk>
2024-08-22 13:35:08 +0100

    fix formating

--- lib/record_loader/application_record_loader.rb
+++ lib/record_loader/application_record_loader.rb
@@ -12,19 +12,17 @@ module RecordLoader
     adapter RecordLoader::Adapter::Rails.new
 
     def wip_list
-      if Rails.application.config.deploy_wip_pipelines
-        wip_files = []
-        wip_files_path = Rails.root.join('config/default_records')
-        Find.find(wip_files_path) do |path|
-          if path.match?(/\wip\.yml$/)
-            file_name = File.basename(path, '.wip.yml')
-            wip_files << file_name
-          end
+      return [] unless Rails.application.config.deploy_wip_pipelines
+
+      wip_files = []
+      wip_files_path = Rails.root.join('config/default_records')
+      Find.find(wip_files_path) do |path|
+        if path.match?(/\wip\.yml$/)
+          file_name = File.basename(path, '.wip.yml')
+          wip_files << file_name
         end
-        wip_files
-      else
-        []
       end
+      wip_files
     end
   end
 end
