1c7476f255480cbd9acf08053581251bc642f1be
Ben Topping <ben.topping@outlook.com>
2024-08-22 14:15:57 +0100

    feat(tag-sets): removes tag set resource, adds further controller tests

--- spec/controllers/admin/tag_sets_controller_spec.rb
+++ spec/controllers/admin/tag_sets_controller_spec.rb
@@ -31,11 +31,26 @@ RSpec.describe Admin::TagSetsController do
       end
     end
 
-    describe '#create' do
-      before { post :create, params: { tag_set: { name: 'test-123', tag_group_id: create(:tag_group).id } } }
+    describe '#new' do
+      before { get :new }
 
-      it 'renders the show template' do
+      it 'is successful' do
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template('new')
+      end
+    end
+
+    describe '#create' do
+      it 'redirects to the tag set show path' do
+        post :create, params: { tag_set: { name: 'test-123', tag_group_id: create(:tag_group).id } }
         expect(response).to redirect_to admin_tag_set_path(TagSet.last)
+      end
+
+      it 'renders the new template when there are errors' do
+        post :create, params: { tag_set: { name: 'test-123', tag_group_id: 'bad ID' } }
+        # Successful status because we handle and display the errors and remain on the new page
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template('new')
       end
     end
   end
@@ -53,6 +68,14 @@ RSpec.describe Admin::TagSetsController do
     describe '#new' do
       it 'redirects' do
         get :new
+        expect(response).to redirect_to('/')
+      end
+    end
+
+    describe '#show' do
+      it 'redirects' do
+        tag_set = create(:tag_set)
+        get :show, params: { id: tag_set.id }
         expect(response).to redirect_to('/')
       end
     end
