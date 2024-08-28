8ccc9f7aaa58cb34253ffb6efb431c05fc9a9d95
Stuart McHattie <sm49@sanger.ac.uk>
2024-08-28 17:20:10 +0100

    Update the State Change endpoint in API v2

--- app/resources/api/v2/state_change_resource.rb
+++ app/resources/api/v2/state_change_resource.rb
@@ -6,7 +6,7 @@ module Api
     # @todo This documentation does not yet include detailed descriptions for relationships, attributes and filters.
     # @todo This documentation does not yet include any example usage of the API via cURL or similar.
     #
-    # @note This resource is immutable: its endpoint will not accept `POST`, `PATCH`, or `DELETE` requests.
+    # @note This resource cannot be modified after creation: its endpoint will not accept `PATCH` requests.
     # @note Access this resource via the `/api/v2/state_changes/` endpoint.
     #
     # Provides a JSON:API representation of {StateChange}.
@@ -15,14 +15,83 @@ module Api
     # or look at the [JSONAPI::Resources](http://jsonapi-resources.com/) package for Sequencescape's implementation
     # of the JSON:API standard.
     class StateChangeResource < BaseResource
-      immutable
+      ###
+      # Attributes
+      ###
 
+      # @!attribute [rw] contents
+      #   @return [Array] Array of "contents" to fail, deciphered by the target.
+      attribute :contents
+
+      # @!attribute [rw] customer_accepts_responsibility
+      #   @return [Boolean] Whether the customer accepts responsibility for the state change.
+      attribute :customer_accepts_responsibility
+
+      # @!attribute [r] previous_state
+      #   @return [String] The previous state of the target before this state change.
       attribute :previous_state
-      attribute :target_state
-      attribute :created_at
-      attribute :updated_at
 
-      has_one :labware
+      # @!attribute [rw] reason
+      #   @return [String] The previous state of the target before this state change.
+      attribute :reason
+
+      # @!attribute [rw] target_state
+      #   @return [String] The state of the target after this state change.
+      #   @note This attribute is required.
+      attribute :target_state
+
+      # @!attribute [w] target_uuid
+      #   This is declared for convenience where the target is not available to set as a relationship.
+      #   Setting this attribute alongside the `target` relationship will prefer the relationship value.
+      #   @return [String] The UUID of the target labware this state change applies to.
+      #   @see #target
+      attribute :target_uuid
+
+      def target_uuid=(uuid)
+        @model.target = Labware.with_uuid(uuid).first
+      end
+
+      # @!attribute [w] user_uuid
+      #   This is declared for convenience where the user is not available to set as a relationship.
+      #   Setting this attribute alongside the `user` relationship will prefer the relationship value.
+      #   @return [String] The UUID of the user who initiated this state change.
+      #   @see #user
+      attribute :user_uuid
+
+      def user_uuid=(uuid)
+        @model.user = User.with_uuid(uuid).first
+      end
+
+      # @!attribute [r] uuid
+      #   @return [String] The UUID of the state change.
+      attribute :uuid
+
+      ###
+      # Relationships
+      ###
+
+      # @!attribute [rw] user
+      #   Setting this relationship alongside the `user_uuid` attribute will override the attribute value.
+      #   @return [UserResource] The user who initiated this state change.
+      #   @note This relationship is required.
+      has_one :user
+
+      # @!attribute [rw] target
+      #   Setting this relationship alongside the `target_uuid` attribute will override the attribute value.
+      #   @return [LabwareResource] The target labware this state change applies to.
+      #   @note This relationship is required.
+      has_one :target, class_name: 'Labware'
+
+      def self.creatable_fields(context)
+        # Previous state and UUID are set by the system.
+        super - %i[previous_state uuid]
+      end
+
+      def fetchable_fields
+        # UUIDs for relationships are not fetchable.
+        # They should be accessed via the relationship itself.
+        super - %i[target_uuid user_uuid]
+      end
     end
   end
 end
