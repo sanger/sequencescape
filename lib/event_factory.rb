bd0b9ed7ddc3ecaafcd4a08f7024420ca36531ac
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-08-29 09:55:42 +0100

    Update tests in event_factory

--- lib/event_factory.rb
+++ lib/event_factory.rb
@@ -91,10 +91,7 @@ class EventFactory
   # rubocop:enable Metrics/MethodLength
 
   def self.record_retention_instruction_updates(labware, user, old_retention_instruction)
-    return if labware.retention_instruction.blank?
-
     old_retention_instruction = 'nil' if [nil, ''].include?(old_retention_instruction)
-
     Event.create!(
       eventful: labware,
       message: "Set retention instruction from #{old_retention_instruction} to #{labware.retention_instruction}",
