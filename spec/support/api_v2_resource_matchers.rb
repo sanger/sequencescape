43a822d39cf8a0e7f8d4b104e8727b1c44582678
Stuart McHattie <sm49@sanger.ac.uk>
2024-09-05 15:38:24 +0100

    Use new resource spec matchers across new resources

--- spec/support/api_v2_resource_matchers.rb
+++ spec/support/api_v2_resource_matchers.rb
@@ -2,7 +2,7 @@
 
 module ApiV2Matchers
   RSpec::Matchers.define :have_readonly_attribute do |attribute|
-    description { "have readonly attribute `#{attribute.to_s}`" }
+    description { "have read-only attribute `#{attribute.to_s}`" }
 
     failure_message { "expected #{resource.class.name.demodulize} to #{description}" }
     failure_message_when_negated do
@@ -12,6 +12,34 @@ module ApiV2Matchers
     match do |resource|
       expect(resource).to have_attribute attribute
       expect(resource).not_to have_updatable_field attribute
+    end
+  end
+
+  RSpec::Matchers.define :have_readwrite_attribute do |attribute|
+    description { "have read-write attribute `#{attribute.to_s}`" }
+
+    failure_message { "expected #{resource.class.name.demodulize} to #{description}" }
+    failure_message_when_negated do
+      "expected #{resource.class.name.demodulize} not to #{description}"
+    end
+
+    match do |resource|
+      expect(resource).to have_attribute attribute
+      expect(resource).to have_updatable_field attribute
+    end
+  end
+
+  RSpec::Matchers.define :have_writeonly_attribute do |attribute|
+    description { "have write-only attribute `#{attribute.to_s}`" }
+
+    failure_message { "expected #{resource.class.name.demodulize} to #{description}" }
+    failure_message_when_negated do
+      "expected #{resource.class.name.demodulize} not to #{description}"
+    end
+
+    match do |resource|
+      expect(resource).not_to have_attribute attribute
+      expect(resource).to have_updatable_field attribute
     end
   end
 end
