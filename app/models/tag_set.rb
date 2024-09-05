6805d70b699190e292c6bf4e9c738cffb5ae81f2
Harriet Craven <hc6@sanger.ac.uk>
2024-09-05 16:11:35 +0100

    Removing all y24-055 code from develop

--- app/models/tag_set.rb
+++ app/models/tag_set.rb
@@ -7,8 +7,6 @@ class TagSet < ApplicationRecord
   # For dual index tags, tag_group is i7 oligos and tag2_group is i5 oligos
   belongs_to :tag_group, class_name: 'TagGroup', optional: false
 
-  # In order to support a unified access to dual and single index sets,
-  # it allows tag2_group to be null.
   belongs_to :tag2_group, class_name: 'TagGroup', optional: true
 
   # We can assume adapter_type is the same for both tag groups
@@ -18,16 +16,7 @@ class TagSet < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validate :tag_group_adapter_types_must_match
 
-  scope :dual_index, -> { where.not(tag2_group: nil) }
-  scope :visible,
-        -> { joins(:tag_group, :tag2_group).where(tag_group: { visible: true }, tag2_group: { visible: true }) }
-
-  # The scoping retrieves the visible tag sets and makes sure they are dual index.
-  scope :visible_dual_index, -> { dual_index.visible }
-
   # Dynamic method to determine the visibility of a tag_set based on the visibility of its tag_groups
-  # TagSet has a method to check if itself is visible by checking
-  # the visibility of both tag_group and (if not null) tag2_group.
   def visible
     tag_group.visible && (tag2_group.nil? || tag2_group.visible)
   end
