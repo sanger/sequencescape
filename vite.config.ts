8925b11a0c24b23ff5c9f43abd05217e80af30c6
Shiv <44001656+SHIV5T3R@users.noreply.github.com>
2024-09-16 13:50:01 +0100

    Revert "Y24-309 - Set vite build target to chrome65"

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import { createVuePlugin } from "vite-plugin-vue2";
 import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  build: {
-    emptyOutDir: true,
-    target: ["chrome65", "es2019"],
-  },
+  build: { emptyOutDir: true },
   plugins: [RubyPlugin(), createVuePlugin(), legacy({ targets: ["defaults"] })],
   resolve: {
     alias: {
