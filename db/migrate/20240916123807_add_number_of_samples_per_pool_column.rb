72146787e9e6516f6b9421dc82c8b62470528a50
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-09-16 13:45:16 +0100

    Making rubocop happy

--- db/migrate/20240916123807_add_number_of_samples_per_pool_column.rb
+++ db/migrate/20240916123807_add_number_of_samples_per_pool_column.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class AddNumberOfSamplesPerPoolColumn < ActiveRecord::Migration[6.1]
   def change
     add_column :request_metadata, :number_of_samples_per_pool, :integer
