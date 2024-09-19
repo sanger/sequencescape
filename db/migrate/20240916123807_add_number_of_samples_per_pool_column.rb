a3f29ff983c4b2b7da9a9d4cddf8415a15498582
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-09-19 10:56:21 +0100

    Updating from branch

--- db/migrate/20240916123807_add_number_of_samples_per_pool_column.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class AddNumberOfSamplesPerPoolColumn < ActiveRecord::Migration[6.1]
-  def change
-    add_column :request_metadata, :number_of_samples_per_pool, :integer
-  end
-end
