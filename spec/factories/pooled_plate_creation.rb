219a09ee219648319cbfd97d9d5c0dc15ff7a28f
Stuart McHattie <sm49@sanger.ac.uk>
2024-09-19 17:05:26 +0100

    Remove unnecessary override of Tube prefixes in factory

--- spec/factories/pooled_plate_creation.rb
+++ spec/factories/pooled_plate_creation.rb
@@ -3,10 +3,7 @@
 FactoryBot.define do
   factory(:pooled_plate_creation) do
     child_purpose { |target| target.association(:plate_purpose) }
+    parents { |target| [target.association(:plate), target.association(:tube)] }
     user { |target| target.association(:user) }
-
-    # When giving a tube as a parent, we change the prefix away from NT to avoid clashes with tubes created as children
-    # by other instances of this model.
-    parents { |target| [target.association(:plate), target.association(:tube, prefix: 'PT')] }
   end
 end
