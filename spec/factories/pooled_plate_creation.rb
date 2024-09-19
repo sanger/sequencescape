fee438c5a91cdf166385e7d76dd973fce9716a29
Stuart McHattie <sm49@sanger.ac.uk>
2024-09-19 18:21:54 +0100

    Continue to fight with tests trying to go to Baracoda

--- spec/factories/pooled_plate_creation.rb
+++ spec/factories/pooled_plate_creation.rb
@@ -2,7 +2,9 @@
 
 FactoryBot.define do
   factory :pooled_plate_creation do
-    barcode { create(:plate_barcode) }
+    # Without this, create_children! tries to go to Baracoda for a barcode.
+    sanger_barcode { create(:plate_barcode) }
+
     child_purpose { |target| target.association(:plate_purpose) }
     parents { |target| [target.association(:plate), target.association(:tube)] }
     user { |target| target.association(:user) }
