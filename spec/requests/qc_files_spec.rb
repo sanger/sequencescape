e9a2293d708cde01f6525d07506162a242fafbf4
Ben Topping <ben.topping@outlook.com>
2024-09-23 15:34:45 +0100

    tests: corrects filename in qc files spec

--- spec/requests/qc_files_spec.rb
+++ spec/requests/qc_files_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'QcFiles' do
   let(:authorised_app) { create :api_application }
 
   describe 'create qc file' do
-    let(:filename) { Rails.root.join('spec/data/parsers/cardinal_pbmc_count.csv').expand_path }
+    let(:filename) { Rails.root.join('spec/data/parsers/pbmc_count.csv').expand_path }
     let(:file) { File.open(filename) }
     let(:plate) { create(:plate_with_empty_wells, well_count: 96) }
 
@@ -13,7 +13,7 @@ RSpec.describe 'QcFiles' do
       headers = {
         'HTTP_ACCEPT' => 'application/json',
         'CONTENT_TYPE' => 'sequencescape/qc_file',
-        'HTTP_CONTENT_DISPOSITION' => 'form-data; filename="cardinal_pbmc_count.csv"',
+        'HTTP_CONTENT_DISPOSITION' => 'form-data; filename="pbmc_count.csv"',
         'HTTP_X_SEQUENCESCAPE_CLIENT_ID' => authorised_app.key,
         'HTTP_COOKIE' => ''
       }
