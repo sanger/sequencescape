4ebd702b5a0f35588f642bcafd400eb0d69e3547
yoldas <ay6@sanger.ac.uk>
2024-09-25 01:31:39 +0100

    Rewrite test for unique validation error

--- spec/models/asset_links_create_edge_spec.rb
+++ spec/models/asset_links_create_edge_spec.rb
@@ -3,14 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe AssetLink, type: :model do
+  # rubocop:disable RSpec/ExampleLength,RSpec/InstanceVariable
   describe '.create_edge' do
-
     after do
-      @ancestor.destroy if @ancestor
-      @descendant.destroy if @descendant
+      @ancestor&.destroy
+      @descendant&.destroy
+      @edge&.destroy
     end
 
-    # rubocop:disable RSpec/ExampleLength
     it 'handles race condition at find_link' do
       # Parent
       ActiveRecord::Base.connection.reconnect!
@@ -24,8 +24,6 @@ RSpec.describe AssetLink, type: :model do
       pid1 =
         fork do
           ActiveRecord::Base.connection.reconnect!
-          ancestor = Labware.find(ancestor.id)
-          descendant = Labware.find(descendant.id)
           find_link_call_count = 0
           allow(described_class).to receive(:find_link).and_wrap_original do |m, *args|
             find_link_call_count += 1
@@ -44,6 +42,7 @@ RSpec.describe AssetLink, type: :model do
             link
           end
           described_class.create_edge(ancestor, descendant)
+
           ActiveRecord::Base.connection.close
         end
 
@@ -51,8 +50,6 @@ RSpec.describe AssetLink, type: :model do
       pid2 =
         fork do
           ActiveRecord::Base.connection.reconnect!
-          ancestor = Labware.find(ancestor.id)
-          descendant = Labware.find(descendant.id)
           message = 'child2: Timeout waiting for paused message'
           raise StandardError, message unless second_socket.wait_readable(10)
           message = 'paused'
@@ -64,22 +61,106 @@ RSpec.describe AssetLink, type: :model do
         end
 
       # Parent
+      exits = []
       [pid1, pid2].each
         .with_index(1) do |pid, index|
-          Timeout.timeout(10) { Process.waitpid(pid) }
+          Timeout.timeout(10) do
+            _pid, status = Process.wait2(pid)
+            exits << status.exitstatus
+          end
         rescue Timeout::Error
           raise StandardError, "parent: Timeout waiting for child#{index} to finish"
         end
 
+      expect(exits).to eq([0, 0])
+
       expect(described_class.where(ancestor: ancestor, descendant: descendant).count).to eq(1)
-      link = described_class.last
-      expect(link.ancestor).to eq(ancestor)
-      expect(link.descendant).to eq(descendant)
-      expect(link.direct).to be_truthy
-      expect(link.count).to eq(1)
+      @edge = edge = described_class.last
+      expect(edge.ancestor).to eq(ancestor)
+      expect(edge.descendant).to eq(descendant)
+      expect(edge.direct).to be_truthy
+      expect(edge.count).to eq(1)
 
       ActiveRecord::Base.connection.close
     end
-    # rubocop:enable RSpec/ExampleLength
+
+    it 'handles unique validation error' do
+      # Parent
+      ActiveRecord::Base.connection.reconnect!
+      @ancestor = ancestor = create(:labware)
+      @descendant = descendant = create(:labware)
+      ActiveRecord::Base.connection.commit_db_transaction
+
+      first_socket, second_socket = UNIXSocket.pair
+
+      # First child
+      pid1 =
+        fork do
+          ActiveRecord::Base.connection.reconnect!
+          find_link_call_count = 0
+          allow(described_class).to receive(:find_link).and_wrap_original do |m, *args|
+            find_link_call_count += 1
+            link = m.call(*args)
+            if find_link_call_count == 1
+              expect(link).to be_nil
+              message = 'paused'
+              first_socket.send(message, 0)
+              message = 'child1: Timeout waiting for resume message'
+              raise StandardError, message unless first_socket.wait_readable(10)
+              message = 'resume'
+              first_socket.recv(message.size)
+            end
+            link
+          end
+
+          unique_validation_error_return_value = nil
+          allow(described_class).to receive(:unique_validation_error?).and_wrap_original do |m, *args|
+            unique_validation_error_return_value = m.call(*args)
+          end
+
+          result = described_class.create_edge(ancestor, descendant)
+          expect(result).to be_truthy
+          expect(described_class).to have_received(:unique_validation_error?)
+          expect(unique_validation_error_return_value).to be_truthy
+
+          ActiveRecord::Base.connection.close
+        end
+
+      # Second child
+      pid2 =
+        fork do
+          ActiveRecord::Base.connection.reconnect!
+          message = 'child2: Timeout waiting for paused message'
+          raise StandardError, message unless second_socket.wait_readable(10)
+          message = 'paused'
+          second_socket.recv(message.size)
+          described_class.create_edge(ancestor, descendant)
+          message = 'resume'
+          second_socket.send(message, 0)
+          ActiveRecord::Base.connection.close
+        end
+
+      # Parent
+      exits = []
+      [pid1, pid2].each
+        .with_index(1) do |pid, index|
+          Timeout.timeout(10) do
+            _pid, status = Process.wait2(pid)
+            exits << status.exitstatus
+          end
+        rescue Timeout::Error
+          raise StandardError, "parent: Timeout waiting for child#{index} to finish"
+        end
+
+      expect(exits).to eq([0, 0])
+
+      expect(described_class.where(ancestor: ancestor, descendant: descendant).count).to eq(1)
+      @edge = edge = described_class.last
+      expect(edge.ancestor).to eq(ancestor)
+      expect(edge.descendant).to eq(descendant)
+      expect(edge.direct).to be_truthy
+      expect(edge.count).to eq(1)
+    end
   end
+  # rubocop:enable RSpec/ExampleLength,RSpec/InstanceVariable
 end
