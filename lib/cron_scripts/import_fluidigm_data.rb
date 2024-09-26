2a4c470636e3be94bc51fbfeb6aea080e231a98a
Ben Topping <ben.topping@outlook.com>
2024-09-26 10:52:31 +0100

    refactor(zeitwerk): remove require exception notifier from cron jobs

--- lib/cron_scripts/import_fluidigm_data.rb
+++ lib/cron_scripts/import_fluidigm_data.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-require 'exception_notification'
 
 module CronScripts
   module ImportFluidigmData
--- lib/volume_check.rb
+++ lib/volume_check.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-require 'exception_notification'
 
 module VolumeCheck
   def self.process
