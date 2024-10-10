cfbe3fee61beb9da20024bd208aa1b4b2c0f5f47
Andrew Sparkes <as28@sanger.ac.uk>
2024-10-10 11:38:02 +0100

    allowed for creation of tube racks from tube rack purpose

--- app/models/tube_rack/purpose.rb
+++ app/models/tube_rack/purpose.rb
@@ -3,11 +3,19 @@
 # The purpose of a tube rack is to hold tubes.
 # Created to hold the size of the tube rack for use when generating manifests.
 class TubeRack::Purpose < Purpose
-  self.default_prefix = 'TR'
-
   has_many :sample_manifests, inverse_of: :tube_rack_purpose, dependent: :restrict_with_exception
+
+  # TODO: change to purpose_id
+  has_many :tube_racks, foreign_key: :plate_purpose_id
 
   def self.standard_tube_rack
     TubeRack::Purpose.find_by(name: 'TR Stock 96')
+  end
+
+  def create!(*args, &block)
+    options = args.extract_options!
+    options[:purpose] = self
+    options[:size] = self.size
+    target_class.create!(*args, options, &block).tap { |tr| tube_racks << tr }
   end
 end
