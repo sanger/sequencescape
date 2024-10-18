a2118474d55c62abddb3cd3c77c571f44015ca5c
Stuart McHattie <sm49@sanger.ac.uk>
2024-10-18 12:10:16 +0100

    Create tests for the bulk_transfer requests

--- spec/factories/bulk_transfers.rb
+++ spec/factories/bulk_transfers.rb
@@ -2,7 +2,39 @@
 
 FactoryBot.define do
   factory :bulk_transfer do
+    transient do
+      source_plates { create_list(:plate, 2) }
+      destination_plates { create_list(:plate, 2) }
+    end
+
     user
-    transfers { create_list(:transfer_between_plates, 3) }
+    well_transfers do
+      [
+        {
+          'source_uuid' => source_plates[0].uuid,
+          'source_location' => 'A1',
+          'destination_uuid' => destination_plates[0].uuid,
+          'destination_location' => 'A1'
+        },
+        {
+          'source_uuid' => source_plates[0].uuid,
+          'source_location' => 'B1',
+          'destination_uuid' => destination_plates[1].uuid,
+          'destination_location' => 'A1'
+        },
+        {
+          'source_uuid' => source_plates[1].uuid,
+          'source_location' => 'A1',
+          'destination_uuid' => destination_plates[0].uuid,
+          'destination_location' => 'B1'
+        },
+        {
+          'source_uuid' => source_plates[1].uuid,
+          'source_location' => 'B1',
+          'destination_uuid' => destination_plates[1].uuid,
+          'destination_location' => 'B1'
+        }
+      ]
+    end
   end
 end
