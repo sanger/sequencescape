5d2e8fd235ae77b73a07dab364cf11e514c89110
Stephen Hulme <sh51@sanger.ac.uk>
2024-11-06 15:51:03 +0000

    style: lint

--- db/migrate/20241106150235_change_flipper_gates_value_to_text.rb
+++ db/migrate/20241106150235_change_flipper_gates_value_to_text.rb
@@ -5,11 +5,9 @@ class ChangeFlipperGatesValueToText < ActiveRecord::Migration[6.1]
     # Ensure this incremental update migration is idempotent
     return unless connection.column_exists? :flipper_gates, :value, :string
 
-    if index_exists? :flipper_gates, [:feature_key, :key, :value]
-      remove_index :flipper_gates, [:feature_key, :key, :value]
-    end
+    remove_index :flipper_gates, %i[feature_key key value] if index_exists? :flipper_gates, %i[feature_key key value]
     change_column :flipper_gates, :value, :text
-    add_index :flipper_gates, [:feature_key, :key, :value], unique: true, length: { value: 255 }
+    add_index :flipper_gates, %i[feature_key key value], unique: true, length: { value: 255 }
   end
 
   def down
