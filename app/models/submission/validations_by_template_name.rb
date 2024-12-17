3b1e4f10c7862a32d0e740e5b09cf042cfa2dbd4
yoldas <ay6@sanger.ac.uk>
2024-12-17 00:42:09 +0000

    Rubocop

--- app/models/submission/validations_by_template_name.rb
+++ app/models/submission/validations_by_template_name.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 # rubocop:todo Metrics/ModuleLength
 module Submission::ValidationsByTemplateName
-
   include Submission::ScrnaCoreCdnaPrepFeasibilityValidation
 
   # Template names
