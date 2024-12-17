8348f314d06167d9182147c0711b134dea5984db
Stuart McHattie <sm49@sanger.ac.uk>
2024-12-17 14:44:36 +0000

    Update documentation on SubmissionResource

--- app/resources/api/v2/submission_resource.rb
+++ app/resources/api/v2/submission_resource.rb
@@ -3,10 +3,11 @@
 module Api
   module V2
     # @todo This documentation does not yet include detailed descriptions for relationships, attributes and filters.
-    # @todo Confirm the API examples work as expected.
     #
     # Provides a JSON:API representation of {Submission} which represents a collection of {Order}s submitted by a
-    # {User}. Once submitted, the {Submission} is processed by a state machine.
+    # {User}. The initial state of a {Submission} is `building`, during which time, {Order}s are added to it.
+    # If the `and_submit` attribute is set to `true`, the new submission will be transitioned to the `pending` state
+    # after validation is applied.
     #
     # @note This resource cannot be modified after creation: its endpoint will not accept `PATCH` requests.
     # @note Access this resource via the `/api/v2/submissions/` endpoint.
@@ -45,9 +46,6 @@ module Api
     # @example GET request for a Submission with ID 123
     #   GET /api/v2/submissions/123/
     #
-    # @example GET request for all Submission resources associated with a User with ID 123
-    #   GET /api/v2/users/123/submissions/
-    #
     # For more information about JSON:API see the [JSON:API Specifications](https://jsonapi.org/format/)
     # or look at the [JSONAPI::Resources](http://jsonapi-resources.com/) package for Sequencescape's implementation
     # of the JSON:API standard.
@@ -64,12 +62,15 @@ module Api
       # will not show by default.
 
       # @!attribute [r] created_at
-      #   @return [DateTime] The date and time the Submission was created.
+      #   @return [DateTime] the date and time the {Submission} was created as an ISO8601 string.
       attribute :created_at, readonly: true
 
+      # @!attribute [r] updated_at
+      #   @return [DateTime] the date and time the {Submission} was last updated as an ISO8601 string.
+      attribute :updated_at, readonly: true
+
       # @!attribute [rw] lanes_of_sequencing
-      #   @todo: check types of this attribute
-      #   @return [Integer] The number of lanes of sequencing requested in the Submission.
+      #   @return [Integer] the number of lanes of sequencing requested in the Submission.
       #      This can only be written once on creation.
       attribute :lanes_of_sequencing, write_once: true
 
@@ -77,38 +78,76 @@ module Api
         _model.sequencing_requests.size
       end
 
+      # @!attribute [rw] name
+      #   @return [String] the name of the {Submission}.
+      #      This can only be written once on creation.
       attribute :name, write_once: true
 
+      # @!attribute [w] order_uuids
+      #   This is declared for convenience where the {Order} resources are not available to set as a relationship.
+      #   Setting this attribute alongside the `orders` relationship will prefer the relationship value.
+      #   @deprecated Use the `orders` relationship instead.
+      #   @param value [Array<String>] the UUID of the {Order} resources associated with this {Submission}.
+      #   @return [Void]
+      #   @see #orders
       attribute :order_uuids, writeonly: true
 
       def order_uuids=(value)
         @model.orders = value.map { |uuid| Order.with_uuid(uuid).first }
       end
 
+      # @!attribute [r] state
+      #   @return [String] a string version of the state name for this {Submission}.
+      #     This is one of `building`, `pending`, `processing`, `ready`, `failed` or `cancelled`.
       attribute :state, readonly: true
-      attribute :updated_at, readonly: true
+
+      # @!attribute [rw] used_tags
+      #   @return [String] the tags that were used in this {Submission}.
+      #      This can only be written once on creation.
       attribute :used_tags, write_once: true
 
+      # @!attribute [w] user_uuid
+      #   This is declared for convenience where the {User} is not available to set as a relationship.
+      #   Setting this attribute alongside the `user` relationship will prefer the relationship value.
+      #   @deprecated Use the `user` relationship instead.
+      #   @param value [Array<String>] the UUID of the {User} who created the {Submission}.
+      #   @return [Void]
+      #   @see #user
       attribute :user_uuid, writeonly: true
 
       def user_uuid=(value)
         @model.user = User.with_uuid(value).first
       end
 
+      # @!attribute [r] uuid
+      #   @return [String] the UUID of the {Submission}.
       attribute :uuid, readonly: true
 
       ###
       # Relationships
       ###
 
+      # @!attribute [rw] user
+      #   Setting this relationship alongside the `user_uuid` attribute will override the attribute value.
+      #   The {User} who created the {Submission}.
+      #   @return [Api::V2::UserResource]
+      #   @note This relationship is required.
       has_one :user, class_name: 'User'
 
+      # @!attribute [rw] orders
+      #   Setting this relationship alongside the `orders_uuids` attribute will override the attribute value.
+      #   The {Order} resources which are associated with the {Submission}.
+      #   @return [Array<Api::V2::OrderResource>]
       has_many :orders, class_name: 'Order'
 
       ###
       # Filters
       ###
 
+      # @!method filter_uuid
+      #   Filter the {Submission} resources by UUID.
+      #   @example GET request with UUID filter
+      #     GET /api/v2/submissions?filter[uuid]=12345678-1234-1234-1234-123456789012
       filter :uuid, apply: ->(records, value, _options) { records.with_uuid(value) }
 
       ###
