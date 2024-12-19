27530dec591df95acf0b12cec3e712fbc23e1b3a
yoldas <ay6@sanger.ac.uk>
2024-12-19 01:26:08 +0000

    Add tests for total number of pools

--- spec/models/scrna_core_cdna_prep_feasibility_validator_spec.rb
+++ spec/models/scrna_core_cdna_prep_feasibility_validator_spec.rb
@@ -7,11 +7,11 @@ require 'rails_helper'
 # module. CSV files are created for bulk submissions and the validations
 # provided by the module are tested.
 RSpec.describe BulkSubmission, with: :uploader do
-  # The CSV file headers have been copied form the headings in the config file
-  # config/bulk_submission_excel/worksheet/data_worksheet.rb
   # The test subject is initialised with the uploaded file.
   subject(:bulk_submission) { described_class.new(spreadsheet: submission_file) }
 
+  # The CSV headers are used to create the CSV content; copied from headings in
+  # config/bulk_submission_excel/columns.yml
   let(:csv_headers) do
     [
       'User Login',
@@ -45,10 +45,13 @@ RSpec.describe BulkSubmission, with: :uploader do
   let(:template_name) { 'Limber-Htp - scRNA Core cDNA Prep GEM-X 5p' }
   let(:scrna_core_number_of_pools) { 2 } # study-project
   let(:scrna_core_cells_per_chip_well) { 90_000 }
+
   # The request types are used to create the submission template record.
   let!(:request_types) { [create(:pbmc_pooling_customer_request_type)] }
+
   # User is created for the submission beforehand.
   let!(:user) { create(:user, login: 'user1') }
+
   # The submission template hash is used for creating the submission template
   # record using the SubmissionSerializer.construct! call.
   let(:submission_template_hash) do
@@ -64,10 +67,7 @@ RSpec.describe BulkSubmission, with: :uploader do
     }
   end
 
-  # Each test context will specify different rows of data.
-  let(:csv_data) { [] }
-
-  # CSV headers and data are combined to create the CSV content as string.
+  # CSV headers and CSV data are combined to create the CSV content as string.
   let(:csv_content) do
     CSV.generate do |csv|
       csv << csv_headers
@@ -98,8 +98,8 @@ RSpec.describe BulkSubmission, with: :uploader do
   # Helper method to create a new CSV row with the defaults and specified values.
   # The optional row_data hash argument is used to set the values corresponding
   # to the headers. Unspecified fields will be set to nil but they can be set
-  # later directly on the row object.
-  # @example Create a new row with defaults, specified values, and more later
+  # later directly on the row object. The nil fields are rendered as empty cells.
+  # @example Create a new row with defaults, specified values, and more later.
   #   row = new_csv_row('User Login' => 'user1', 'Barcode' => 'FX12345678')
   #   row['Study Name'] = 'Study 1'
   #   row['Project Name'] = 'Project 1'
@@ -140,80 +140,91 @@ RSpec.describe BulkSubmission, with: :uploader do
     csv_tempfile.close! # Close and unlink the temporary file after the test.
   end
 
-  context 'with total number of samples in the submission' do
+  # Three study-project groups with different number of samples.
+  let(:group_1_number_of_samples) { 15 } # Study 1, Project 1
+  let(:group_2_number_of_samples) { 15 }
+  let(:group_3_number_of_samples) { 15 }
+
+  # The number of pools for each study-project group.
+  let(:group_1_number_of_pools) { 1 }
+  let(:group_2_number_of_pools) { 1 }
+  let(:group_3_number_of_pools) { 1 }
+
+  # Donor IDs for each group to set on the samples.
+  let(:group_1_donors) { Array.new(group_1_number_of_samples) { |index| "group_1_donor_#{index + 1}" } }
+  let(:group_2_donors) { Array.new(group_2_number_of_samples) { |index| "group_2_donor_#{index + 1}" } }
+  let(:group_3_donors) { Array.new(group_3_number_of_samples) { |index| "group_3_donor_#{index + 1}" } }
+
+  # Tubes for each group with sample_metadata that contains the donor ID.
+  let(:group_1_tubes) do
+    Array.new(group_1_number_of_samples) do |index|
+      create(:sample_tube).tap { |tube| tube.samples.first.sample_metadata.update!(donor_id: group_1_donors[index]) }
+    end
+  end
+
+  let(:group_2_tubes) do
+    Array.new(group_2_number_of_samples) do |index|
+      create(:sample_tube).tap { |tube| tube.samples.first.sample_metadata.update!(donor_id: group_2_donors[index]) }
+    end
+  end
+
+  let(:group_3_tubes) do
+    Array.new(group_3_number_of_samples) do |index|
+      create(:sample_tube).tap { |tube| tube.samples.first.sample_metadata.update!(donor_id: group_3_donors[index]) }
+    end
+  end
+
+  # CSV rows for each group with the specified values.
+  let(:group_1_rows) do
+    Array.new(group_1_number_of_samples) do |index|
+      new_csv_row(
+        'Study Name' => 'Study 1',
+        'Project Name' => 'Project 1',
+        'Barcode' => group_1_tubes[index].human_barcode,
+        'Asset Group Name' => 'Asset Group 1',
+        'scRNA Core Number of Pools' => group_1_number_of_pools
+      )
+    end
+  end
+
+  let(:group_2_rows) do
+    Array.new(group_2_number_of_samples) do |index|
+      new_csv_row(
+        'Study Name' => 'Study 2',
+        'Project Name' => 'Project 2',
+        'Barcode' => group_2_tubes[index].human_barcode,
+        'Asset Group Name' => 'Asset Group 2',
+        'scRNA Core Number of Pools' => group_2_number_of_pools
+      )
+    end
+  end
+
+  let(:group_3_rows) do
+    Array.new(group_3_number_of_samples) do |index|
+      new_csv_row(
+        'Study Name' => 'Study 3',
+        'Project Name' => 'Project 3',
+        'Barcode' => group_3_tubes[index].human_barcode,
+        'Asset Group Name' => 'Asset Group 3',
+        'scRNA Core Number of Pools' => group_3_number_of_pools
+      )
+    end
+  end
+
+  # Combine the rows from all groups to create the CSV data.
+  let(:csv_data) { group_1_rows + group_2_rows + group_3_rows }
+
+  # The I18n scope for the error messages in the locale file.
+  let(:i18n_scope) { described_class::I18N_SCOPE_SCRNA_CORE_CDNA_PREP_FEASIBILITY_VALIDATOR }
+
+  # The total number of samples in the submission.
+  let(:total_number_of_samples) { group_1_number_of_samples + group_2_number_of_samples + group_3_number_of_samples }
+
+  # The total number of pools in the submission.
+  let(:total_number_of_pools) { group_1_number_of_pools + group_2_number_of_pools + group_3_number_of_pools }
+
+  context '#validate_scrna_core_cdna_prep_total_number_of_samples' do
     # Total number of samples in the submission must be between 5 and 96 (inclusive).
-
-    let(:group_1_number_of_samples) { 15 } # Study 1, Project 1
-    let(:group_2_number_of_samples) { 15 }
-    let(:group_3_number_of_samples) { 15 }
-
-    let(:group_1_number_of_pools) { 1 }
-    let(:group_2_number_of_pools) { 1 }
-    let(:group_3_number_of_pools) { 1 }
-
-    let(:group_1_donors) { Array.new(group_1_number_of_samples) { |index| "group_1_donor_#{index + 1}" } }
-    let(:group_2_donors) { Array.new(group_2_number_of_samples) { |index| "group_2_donor_#{index + 1}" } }
-    let(:group_3_donors) { Array.new(group_3_number_of_samples) { |index| "group_3_donor_#{index + 1}" } }
-
-    let(:group_1_tubes) do
-      Array.new(group_1_number_of_samples) do |index|
-        create(:sample_tube).tap { |tube| tube.samples.first.sample_metadata.update!(donor_id: group_1_donors[index]) }
-      end
-    end
-
-    let(:group_2_tubes) do
-      Array.new(group_2_number_of_samples) do |index|
-        create(:sample_tube).tap { |tube| tube.samples.first.sample_metadata.update!(donor_id: group_2_donors[index]) }
-      end
-    end
-
-    let(:group_3_tubes) do
-      Array.new(group_3_number_of_samples) do |index|
-        create(:sample_tube).tap { |tube| tube.samples.first.sample_metadata.update!(donor_id: group_3_donors[index]) }
-      end
-    end
-
-    let(:group_1_rows) do
-      Array.new(group_1_number_of_samples) do |index|
-        new_csv_row(
-          'Study Name' => 'Study 1',
-          'Project Name' => 'Project 1',
-          'Barcode' => group_1_tubes[index].human_barcode,
-          'Asset Group Name' => 'Asset Group 1',
-          'scRNA Core Number of Pools' => group_1_number_of_pools
-        )
-      end
-    end
-
-    let(:group_2_rows) do
-      Array.new(group_2_number_of_samples) do |index|
-        new_csv_row(
-          'Study Name' => 'Study 2',
-          'Project Name' => 'Project 2',
-          'Barcode' => group_2_tubes[index].human_barcode,
-          'Asset Group Name' => 'Asset Group 2',
-          'scRNA Core Number of Pools' => group_2_number_of_pools
-        )
-      end
-    end
-
-    let(:group_3_rows) do
-      Array.new(group_3_number_of_samples) do |index|
-        new_csv_row(
-          'Study Name' => 'Study 3',
-          'Project Name' => 'Project 3',
-          'Barcode' => group_3_tubes[index].human_barcode,
-          'Asset Group Name' => 'Asset Group 3',
-          'scRNA Core Number of Pools' => group_3_number_of_pools
-        )
-      end
-    end
-
-    let(:csv_data) { group_1_rows + group_2_rows + group_3_rows }
-
-    let(:i18n_scope) { described_class::I18N_SCOPE_SCRNA_CORE_CDNA_PREP_FEASIBILITY_VALIDATOR }
-
-    let(:total_number_of_samples) { group_1_number_of_samples + group_2_number_of_samples + group_3_number_of_samples }
 
     context 'when the total number of samples is between minimum and maximum allowed' do
       it { is_expected.to be_valid }
@@ -263,7 +274,7 @@ RSpec.describe BulkSubmission, with: :uploader do
     context 'when the total number of samples is greater than the maximum allowed' do
       let(:group_1_number_of_samples) { 32 }
       let(:group_2_number_of_samples) { 32 }
-      let(:group_3_number_of_samples) { 33 } # 97 samples
+      let(:group_3_number_of_samples) { 33 }
 
       let(:group_1_number_of_pools) { 3 }
       let(:group_2_number_of_pools) { 3 }
@@ -283,6 +294,97 @@ RSpec.describe BulkSubmission, with: :uploader do
         expect(bulk_submission.errors[:spreadsheet]).to include(error_message)
       end
       # rubocop:enable RSpec/ExampleLength, RSpec/MultipleExpectations
+    end
+  end
+
+  context '#validate_scrna_core_cdna_prep_total_number_of_pools' do
+    # Total (requested) number of pools must be between 1 and 8 (inclusive)
+
+    context 'when the total number of pools is between minimum and maximum allowed' do
+      it { is_expected.to be_valid }
+    end
+
+    context 'when the total number of pools is the minimum allowed' do
+      let(:group_1_number_of_samples) { 5 }
+      let(:group_2_number_of_samples) { 0 }
+      let(:group_3_number_of_samples) { 0 }
+
+      let(:group_1_number_of_pools) { 1 }
+      let(:group_2_number_of_pools) { 0 }
+      let(:group_3_number_of_pools) { 0 }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when the total number of pools is the maximum allowed' do
+      let(:group_1_number_of_samples) { 31 }
+      let(:group_2_number_of_samples) { 32 }
+      let(:group_3_number_of_samples) { 33 }
+
+      let(:group_1_number_of_pools) { 3 }
+      let(:group_2_number_of_pools) { 3 }
+      let(:group_3_number_of_pools) { 2 }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when the total number of pools is less than the minimum allowed' do
+      let(:group_1_number_of_samples) { 5 }
+      let(:group_2_number_of_samples) { 0 }
+      let(:group_3_number_of_samples) { 0 }
+
+      let(:group_1_number_of_pools) { 1 }
+      let(:group_2_number_of_pools) { 0 }
+      let(:group_3_number_of_pools) { 0 }
+
+      before do
+        # Because the allowed range for the total number of pools is configured
+        # as 1 to 8 (inclusive), testing it below the minimum value requires
+        # setting a zero number of pools, which causes a ZeroDivisionError
+        # before the feasibility validation. In order to test the error message,
+        # we will use a different range; 2 to 8 instead of 1 to 8. We will stub
+        # the scrna_config call on the Rails.application.config to return the
+        # modified scrna_config.
+        scrna_config_dup = Rails.application.config.scrna_config.dup
+        scrna_config_dup[:cdna_prep_minimum_total_number_of_pools] = 2
+        allow(Rails.application.config).to receive(:scrna_config).and_return(scrna_config_dup)
+      end
+
+      it 'sets the error message' do
+        error_message =
+          I18n.t(
+            'errors.total_number_of_pools',
+            min: scrna_config[:cdna_prep_minimum_total_number_of_pools],
+            max: scrna_config[:cdna_prep_maximum_total_number_of_pools],
+            count: total_number_of_pools,
+            scope: i18n_scope
+          )
+        expect { bulk_submission.process }.to raise_error(ActiveRecord::RecordInvalid)
+        expect(bulk_submission.errors[:spreadsheet]).to include(error_message)
+      end
+    end
+
+    context 'when the total number of pools is greater than the maximum allowed' do
+      let(:group_1_number_of_samples) { 31 }
+      let(:group_2_number_of_samples) { 32 }
+      let(:group_3_number_of_samples) { 33 }
+
+      let(:group_1_number_of_pools) { 3 }
+      let(:group_2_number_of_pools) { 3 }
+      let(:group_3_number_of_pools) { 3 }
+
+      it 'sets the error message' do
+        error_message =
+          I18n.t(
+            'errors.total_number_of_pools',
+            min: scrna_config[:cdna_prep_minimum_total_number_of_pools],
+            max: scrna_config[:cdna_prep_maximum_total_number_of_pools],
+            count: total_number_of_pools,
+            scope: i18n_scope
+          )
+        expect { bulk_submission.process }.to raise_error(ActiveRecord::RecordInvalid)
+        expect(bulk_submission.errors[:spreadsheet]).to include(error_message)
+      end
     end
   end
 end
