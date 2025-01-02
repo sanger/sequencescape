d0b65e23a67ebd0d9f9d31d9d8468cd697c46760
Wendy Yang <wy1@sanger.ac.uk>
2025-01-02 14:58:07 +0000

    tidy up the code

--- config/initializers/ena_requirement_fields.rb
+++ config/initializers/ena_requirement_fields.rb
@@ -3,7 +3,7 @@
 file_path = Rails.root.join('config/ena_requirement_fields.yml')
 
 if File.exist?(file_path)
-    EBI_REQUIREMENT_FIELDS = YAML.load(File.open(file_path))
+  EBI_REQUIREMENT_FIELDS = YAML.load(File.open(file_path))
 else
-    Rails.logger.warn "File #{file_path} does not exist"
+  Rails.logger.warn "File #{file_path} does not exist"
 end
