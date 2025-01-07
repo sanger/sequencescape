0c938c264a9ed8e58fe37294a5636ce3a60b7e73
Wendy Yang <wy1@sanger.ac.uk>
2025-01-07 17:13:37 +0000

    combine migration files to one

--- db/migrate/20250107170807_add_ebi_fields_to_study_metadata.rb
+++ db/migrate/20250107170807_add_ebi_fields_to_study_metadata.rb
@@ -1,18 +1,8 @@
 # frozen_string_literal: true
-class AddEbiLibraryStrategyToStudyMetadata < ActiveRecord::Migration[6.1]
+class AddEbiFieldsToStudyMetadata < ActiveRecord::Migration[6.1]
   def change
     add_column :study_metadata, :ebi_library_strategy, :string, default: nil
-  end
-end
-
-class AddEbiLibrarySourceToStudyMetadata < ActiveRecord::Migration[6.1]
-  def change
     add_column :study_metadata, :ebi_library_source, :string, default: nil
-  end
-end
-
-class AddEbiLibrarySelectionToStudyMetadata < ActiveRecord::Migration[6.1]
-  def change
     add_column :study_metadata, :ebi_library_selection, :string, default: nil
   end
 end
