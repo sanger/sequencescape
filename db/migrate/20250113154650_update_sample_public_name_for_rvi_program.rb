62a8de8aebe6a717f7a831bbaff1b05aaa83eb7c
sabrine33 <sa33@sanger.ac.uk>
2025-01-13 16:10:00 +0000

    run format

--- db/migrate/20250113154650_update_sample_public_name_for_rvi_program.rb
+++ db/migrate/20250113154650_update_sample_public_name_for_rvi_program.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class UpdateSamplePublicNameForRviProgram < ActiveRecord::Migration[7.0]
   def change
     ActiveRecord::Base.connection.execute('SET autocommit = 0')
