f4a4147eae21ae66a88f3b59be61ae6364bc104f
Wendy Yang <wy1@sanger.ac.uk>
2025-01-14 16:01:41 +0000

    make config available for all environments

--- config/initializers/ena_requirement_fields.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# file_path = Rails.root.join('config/ena_requirement_fields.yml')
-
-# if File.exist?(file_path)
-#   # EBI_REQUIREMENT_FIELDS = YAML.load(File.open(file_path))
-# else
-#   Rails.logger.warn "File #{file_path} does not exist"
-# end
