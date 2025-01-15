2b09474f9ba26225bc90eb6edc043e4cf0d8b4bb
Wendy Yang <wy1@sanger.ac.uk>
2025-01-15 11:39:47 +0000

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_11_143636) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_08_172249) do
   create_table "aliquot_indices", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.integer "aliquot_id", null: false
     t.integer "lane_id", null: false
