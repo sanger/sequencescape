99312609c51e46ee2b10a70429230b1a32ffbc99
sabrine33 <sa33@sanger.ac.uk>
2025-01-23 10:46:40 +0000

    run format

--- db/migrate/20250113154650_update_sample_public_name_for_rvi_program.rb
+++ db/migrate/20250113154650_update_sample_public_name_for_rvi_program.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class UpdateSamplePublicNameForRviProgram < ActiveRecord::Migration[7.0]
- def up
+  def up
     execute(<<~SQLQUERY)
       UPDATE sample_metadata
       JOIN samples ON sample_metadata.sample_id = samples.id
