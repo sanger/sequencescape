f010b1a77c262db3a52627d5910a5a6bbaa8ab80
Stephen Hulme <sh51@sanger.ac.uk>
2025-01-29 08:50:15 +0000

    fix: rename action -> strategy

--- app/frontend/entrypoints/cherrypick_strategies.js
+++ app/frontend/entrypoints/cherrypick_strategies.js
@@ -1,26 +1,26 @@
-// apply a border highlight to the card, based on the cherrypick action selected
+// apply a border highlight to the card, based on the cherrypick strategy selected
 // this is admittedly a gratuitous addition to the user experience, but it's a nice touch
 
 // highlight only the card that corresponds to the provided index
 function highlightCard(cardIndex) {
   const borderStyle = "border-primary";
 
-  const cards = document.querySelectorAll(`.card[data-group='cherrypick[action]']`);
+  const cards = document.querySelectorAll(`.card[data-group='cherrypick[strategy]']`);
   cards.forEach((card) => card.classList.remove(borderStyle));
 
   cards[cardIndex].classList.add(borderStyle);
 }
 
-const radioButtons = document.querySelectorAll('input[name="cherrypick[action]"]');
+const radioButtons = document.querySelectorAll('input[name="cherrypick[strategy]"]');
 radioButtons.forEach((radio) => {
   radio.addEventListener("change", function () {
-    const action = this.value;
-    const actionToIndexMap = {
+    const strategy = this.value;
+    const strategyToIndexMap = {
       nano_grams_per_micro_litre: 0,
       nano_grams: 1,
       micro_litre: 2,
     };
-    const cardIndex = actionToIndexMap[action];
+    const cardIndex = strategyToIndexMap[strategy];
 
     highlightCard(cardIndex);
   });
