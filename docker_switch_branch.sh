4084d55e34ccb8bd9e294857c247e76fee76d017
Stephen Hulme <sh51@sanger.ac.uk>
2025-02-03 09:52:22 +0000

    build: add check for existance of tagged docker image

--- docker_switch_branch.sh
+++ docker_switch_branch.sh
@@ -16,6 +16,14 @@ if [ -z ${PRECOMPILE_ASSETS+x} ]; then PRECOMPILE_ASSETS="false"; fi
 
 # Get the current branch name, replacing invalid characters with underscores, and converting to lowercase
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD | tr -cd '[:alnum:]_-' | tr '[:upper:]' '[:lower:]')
+
+# Check if the image for the current branch exists
+if ! docker image inspect sequencescape_local_image:$BRANCH_NAME > /dev/null 2>&1; then
+  echo "Docker image tagged '$BRANCH_NAME' does not exist. Run docker_build_and_tag_branch.sh first."
+  exit 1
+fi
+
+echo "Using image for branch: $BRANCH_NAME"
 # Start the container using the image tagged with the current branch name
 BRANCH_IMAGE_TAG=$BRANCH_NAME \
 RESET_DATABASE=$RESET_DATABASE \
