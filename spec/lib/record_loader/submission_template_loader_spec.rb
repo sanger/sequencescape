4798f2a4a1bd723ed7bb207dec1c523229f83610
Andrew Sparkes <as28@sanger.ac.uk>
2025-02-07 12:26:02 +0000

    improved test coverage

--- spec/lib/record_loader/submission_template_loader_spec.rb
+++ spec/lib/record_loader/submission_template_loader_spec.rb
@@ -9,15 +9,17 @@ RSpec.describe RecordLoader::SubmissionTemplateLoader, :loader, type: :model do
 
   # Tests use a separate directory to avoid coupling your specs to the data
   let(:test_directory) { Rails.root.join('spec/data/record_loader/submission_templates') }
+  let(:selected_files) { 'two_submission_templates' }
+
+  let!(:study) { UatActions::StaticRecords.study }
+  let!(:project) { UatActions::StaticRecords.project }
+  let!(:order_role) { UatActions::StaticRecords.order_role }
 
   context 'with two_submission_templates selected' do
-    let(:selected_files) { 'two_submission_templates' }
-    let!(:project) { UatActions::StaticRecords.project }
     let!(:product_line) { ProductLine.create!(name: 'my_product_line') }
     let!(:product_cat) { ProductCatalogue.create!(name: 'my_product_cat') }
     let!(:request_type) { create(:request_type, key: 'my_request_type') }
     let!(:request_type2) { create(:request_type, key: 'my_request_type2') }
-    let!(:order_role) { OrderRole.create!(role: 'my_order_role') }
 
     it 'creates two records' do
       expect { record_loader.create! }.to change(SubmissionTemplate, :count).by(2)
@@ -56,6 +58,69 @@ RSpec.describe RecordLoader::SubmissionTemplateLoader, :loader, type: :model do
         product_line_id: product_line.id,
         product_catalogue_id: product_cat.id
       )
+    end
+  end
+
+  describe '#find_order_role' do
+    context 'when in production environment' do
+      before { allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('production')) }
+
+      it 'finds the order role by role' do
+        allow(OrderRole).to receive(:find_by!).with(role: 'Test Role').and_return(order_role)
+        expect(record_loader.find_order_role('Test Role')).to eq(order_role)
+      end
+    end
+
+    context 'when not in production environment' do
+      before { allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('development')) }
+
+      it 'finds the order role by role or returns the UAT order role' do
+        allow(OrderRole).to receive(:find_by).with(role: 'Test Role').and_return(nil)
+        allow(UatActions::StaticRecords).to receive(:order_role).and_return(order_role)
+        expect(record_loader.find_order_role('Test Role')).to eq(order_role)
+      end
+    end
+  end
+
+  describe '#find_project' do
+    context 'when in production environment' do
+      before { allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('production')) }
+
+      it 'finds the project by name' do
+        allow(Project).to receive(:find_by!).with(name: 'Test Project').and_return(project)
+        expect(record_loader.find_project('Test Project')).to eq(project)
+      end
+    end
+
+    context 'when not in production environment' do
+      before { allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('development')) }
+
+      it 'finds the project by name or returns the UAT project' do
+        allow(Project).to receive(:find_by).with(name: 'Test Project').and_return(nil)
+        allow(UatActions::StaticRecords).to receive(:project).and_return(project)
+        expect(record_loader.find_project('Test Project')).to eq(project)
+      end
+    end
+  end
+
+  describe '#find_study' do
+    context 'when in production environment' do
+      before { allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('production')) }
+
+      it 'finds the study by name' do
+        allow(Study).to receive(:find_by!).with(name: 'Test Study').and_return(study)
+        expect(record_loader.find_study('Test Study')).to eq(study)
+      end
+    end
+
+    context 'when not in production environment' do
+      before { allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('development')) }
+
+      it 'finds the study by name or returns the UAT study' do
+        allow(Study).to receive(:find_by).with(name: 'Test Study').and_return(nil)
+        allow(UatActions::StaticRecords).to receive(:study).and_return(study)
+        expect(record_loader.find_study('Test Study')).to eq(study)
+      end
     end
   end
 end
