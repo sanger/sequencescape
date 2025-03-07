54080f86cf1e06ab5471b31079631b589aa2834f
Dasun Pubudumal <pubudumald@gmail.com>
2025-03-07 15:10:55 +0000

    Add documentation comments for PlatesFromTubesController actions and constants

--- test/controllers/plates_from_tubes_controller_test.rb
+++ test/controllers/plates_from_tubes_controller_test.rb
@@ -2,6 +2,24 @@
 
 require 'test_helper'
 
+# The PlatesFromTubesController handles the creation of plates from tubes.
+# It provides actions to display the form for creating plates and to process
+# the form submission.
+#
+# The interface for this controller is through the endpoint /plates/from_tubes.
+#
+# Actions:
+# - new: Renders the form for creating plates from tubes.
+# - create: Processes the form submission and creates plates from the provided tubes.
+#
+# Before Actions:
+# - set_barcode_printers: Sets the available barcode printers.
+# - set_plate_creators: Sets the available plate creators.
+# - find_plate_creator: Finds the plate creator based on the user's selection.
+# - clear_flashes: Clears any flash messages.
+#
+# Constants:
+# - VIEW_PATH: The path to the view template for rendering the form.
 class PlatesFromTubesControllerTest < ActionController::TestCase
   context 'Plates from Tubes' do
     setup do
