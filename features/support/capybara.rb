03c013bb350411ecf375b2bad291c928a3cd8478
yoldas <ay6@sanger.ac.uk>
2025-03-13 16:02:37 +0000

    Experiment implicit_wait

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -15,14 +15,18 @@ Capybara.register_driver :headless_chrome do |app|
   options.add_argument('--headless')
   options.add_argument('--disable-gpu')
   options.add_argument('--disable-search-engine-choice-screen')
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  driver.manage.timeouts.implicit_wait = 10
+  driver
 end
 
 Capybara.register_driver :chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_preference('download.default_directory', DownloadHelpers::PATH.to_s)
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  driver.manage.timeouts.implicit_wait = 10
+  driver
 end
 
 Capybara.default_max_wait_time = 10
