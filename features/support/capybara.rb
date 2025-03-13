7c6fbdc8214cd33c5b62203f2938d64dc3ec9b46
yoldas <ay6@sanger.ac.uk>
2025-03-13 16:52:47 +0000

    Try --browser-version=133

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -15,18 +15,16 @@ Capybara.register_driver :headless_chrome do |app|
   options.add_argument('--headless')
   options.add_argument('--disable-gpu')
   options.add_argument('--disable-search-engine-choice-screen')
-  driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-  driver.browser.manage.timeouts.implicit_wait = 10
-  driver
+  options.add_argument('--browser-version=133')
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara.register_driver :chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_preference('download.default_directory', DownloadHelpers::PATH.to_s)
 
-  driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-  driver.browser.manage.timeouts.implicit_wait = 10
-  driver
+  options.add_argument('--browser-version=133')
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara.default_max_wait_time = 10
