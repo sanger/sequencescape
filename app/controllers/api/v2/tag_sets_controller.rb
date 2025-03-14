228877a349929d618a04617e5f39334a2704e770
Seena Nair <55585488+seenanair@users.noreply.github.com>
2025-03-14 16:04:17 +0000

    Add feature flag check for TagSets API in controller

--- app/controllers/api/v2/tag_sets_controller.rb
+++ app/controllers/api/v2/tag_sets_controller.rb
@@ -2,11 +2,18 @@
 
 module Api
   module V2
-    # Provides a JSON API controller for TagGroup
+    # Provides a JSON API controller for TagSet
     # See: http://jsonapi-resources.com/ for JSONAPI::Resource documentation
     class TagSetsController < JSONAPI::ResourceController
       # By default JSONAPI::ResourceController provides most the standard
       # behaviour, and in many cases this file may be left empty.
+      before_action :check_feature_flag
+
+      private
+
+      def check_feature_flag
+        render json: { error: 'TagSets API is disabled' } unless Flipper.enabled?(:y24_220_enable_tag_set_api)
+      end
     end
   end
 end
