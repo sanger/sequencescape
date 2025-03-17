295cf6005ccf1eaf764c6bafa57310b9f65a2eee
Stephen Hulme <sh51@sanger.ac.uk>
2025-03-17 22:24:16 +0000

    test: update select patch to handle id cases and return better errors

--- lib/capybara_select2_patch.rb
+++ lib/capybara_select2_patch.rb
@@ -2,6 +2,7 @@
 
 # Select2 breaks Capybara since select dropdowns are changed to span dropdowns.
 # This patch identifies when Select2 is being used and changes the dropdown using Javascript instead.
+require 'English'
 module CapybaraSelect2Patch
   # Intercept the standard Capybara select method
   def select(*args, wait: 3, **kwargs)
@@ -14,17 +15,24 @@ module CapybaraSelect2Patch
 
   def select_js(option_text, from: nil)
     command = <<-JS
-      let for_id = Array.from(document.getElementsByTagName('label')).filter(l => l.textContent == '#{from}')[0].htmlFor;
-      let select = document.getElementById(for_id);
-      select.selectedIndex = [...select.options].findIndex(option => option.text === '#{option_text}');
-    JS
+    let select = document.getElementById('#{from}'); // when from is an id
+    if (select === null) { // when from is label text
+      let for_id = Array.from(document.getElementsByTagName('label')).filter(l => l.textContent === '#{from}')[0].htmlFor;
+      select = document.getElementById(for_id);
+    }
+    select.selectedIndex = [...select.options].findIndex(option => option.text === '#{option_text}');
+  JS
     execute_script(command)
   rescue Selenium::WebDriver::Error::JavascriptError => e
-    raise $!, <<-ERROR_MESSAGE, $!.backtrace
+    test_trace = e.backtrace.select { |filepath| filepath.include?('_spec.rb') || filepath.include?('.feature') }
+    error_message = <<-ERROR_MESSAGE
     Cannot find option '#{option_text}' from select '#{from}'.
     See below for more info:
-    #{e.backtrace[5..15].join("\n")}
+    #{test_trace}
     ERROR_MESSAGE
+
+    # Include link to line in failing test
+    raise $ERROR_INFO, error_message, $ERROR_INFO.backtrace
   end
 end
 
