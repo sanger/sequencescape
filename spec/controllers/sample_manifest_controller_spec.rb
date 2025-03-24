7fc9bb5c5a96750ac718d3a8a139098002e8704c
Dasun Pubudumal <pubudumald@gmail.com>
2025-03-24 13:46:06 +0000

    Fix linting issues

--- spec/controllers/sample_manifest_controller_spec.rb
+++ spec/controllers/sample_manifest_controller_spec.rb
@@ -3,16 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe Sdb::SampleManifestsController do
-
   describe 'GET #new' do
+    let!(:user) { create(:user, swipecard_code: '123456') }
+
     before do
-      @user = FactoryBot.create(:user, swipecard_code: '123456')
-      @user.grant_administrator
-      session[:user] = @user.id
+      user.grant_administrator
+      session[:user] = user.id
     end
 
     context 'when printing tubes with 2D barcodes' do
-
       let!(:sample_manifests) { SampleManifest.count }
       let(:study) { create(:study) }
       let(:supplier) { create(:supplier) }
@@ -25,25 +24,38 @@ RSpec.describe Sdb::SampleManifestsController do
           config.load!
         end
         post :create,
-          params: {
-            sample_manifest: {
-              template: "tube_default",
-              purpose_id: purpose.id,
-              study_id: study.id,
-              supplier_id: supplier.id,
-              count: 1,
-              barcode_printer: 'xyz',
-              barcode_type: '2D Barcode',
-              only_first_label: '0',
-            },
-            asset_type: '1dtube'
-          }
+             params: {
+               sample_manifest: {
+                 template: 'tube_default',
+                 purpose_id: purpose.id,
+                 study_id: study.id,
+                 supplier_id: supplier.id,
+                 count: 1,
+                 barcode_printer: 'xyz',
+                 barcode_type: '2D Barcode',
+                 only_first_label: '0'
+               },
+               asset_type: '1dtube'
+             }
       end
 
       it 'generates a new sample manifest' do
         expect(SampleManifest.count).to eq(sample_manifests + 1)
       end
+
+      it 'generates a new sample manifest with the correct attributes' do
+        sample_manifest = SampleManifest.last
+        expect_correct_attributes(sample_manifest, study, supplier, purpose)
+      end
     end
   end
 
+  def expect_correct_attributes(sample_manifest, study, supplier, purpose) # rubocop:todo Metrics/AbcSize
+    expect(sample_manifest.study_id).to eq(study.id)
+    expect(sample_manifest.supplier_id).to eq(supplier.id)
+    expect(sample_manifest.purpose_id).to eq(purpose.id)
+    expect(sample_manifest.asset_type).to eq('1dtube')
+    expect(sample_manifest.rows_per_well).to eq(1)
+    expect(sample_manifest.invalid_wells).to eq([])
+  end
 end
