05fe4c48671def67656a5e36f961d538c4c8743b
Wendy Yang <wy1@sanger.ac.uk>
2025-03-26 15:15:41 +0000

    change the method name

--- app/models/plate_creation.rb
+++ app/models/plate_creation.rb
@@ -45,11 +45,11 @@ class PlateCreation < AssetCreation
 
   def record_creation_of_children
     parent.events.create_plate!(child_purpose, child, user)
-    register_stock! if register_stock
+    register_stock_for_plate if register_stock
   end
 
-  def register_stock!
+  def register_stock_for_plate
     return if child.blank?
-    child.wells.each(&:register_stock!)
+      child.wells.with_contents.each(&:register_stock!)
   end
 end
