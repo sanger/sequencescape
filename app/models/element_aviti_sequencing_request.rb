1b542c3245624b9618e14234a8ba47dce4fdc127
yoldas <ay6@sanger.ac.uk>
2025-03-26 16:58:17 +0000

    Add custom attributes to element aviti sequencing request

--- app/models/element_aviti_sequencing_request.rb
+++ app/models/element_aviti_sequencing_request.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ElementAvitiSequencingRequest < SequencingRequest
+  YES_OR_NO = %w[Yes No].freeze
+
+  has_metadata as: Request do
+    custom_attribute(:percent_element_phix_needed, integer: true, required: true, minimum: 0, maximum: 100)
+    custom_attribute(:low_diversity, required: true, in: YES_OR_NO)
+  end
 end
