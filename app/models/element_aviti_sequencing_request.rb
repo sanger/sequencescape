db4b55d5f527b0d8a571a2f0dd8b4c8f0b4313d1
sabrine33 <sa33@sanger.ac.uk>
2025-03-26 15:13:28 +0000

    add element aviti request metadat

--- app/models/element_aviti_sequencing_request.rb
+++ app/models/element_aviti_sequencing_request.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ElementAvitiSequencingRequest < SequencingRequest
+
+  has_metadata as: Request do
+    custom_attribute(:fragment_size_required_from, integer: true, minimum: 1)
+    custom_attribute(:fragment_size_required_to, integer: true, minimum: 1)
+
+    custom_attribute(:read_length, integer: true, validator: true, required: true, selection: true)
+    custom_attribute(:requested_flowcell_type, required: false, validator: true, selection: true, on: :create)
+    custom_attribute(:percent_element_phix_needed, integer: true, required: true, selection: true)
+    custom_attribute(:low_diversity, required: true, selection: true)
+  end
 end
