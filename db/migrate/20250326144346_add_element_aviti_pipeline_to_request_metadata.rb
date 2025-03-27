7af285712e49b1f4c43d281fa09170fd61a994d2
yoldas <ay6@sanger.ac.uk>
2025-03-27 23:41:13 +0000

    Rename percent element phix needed to requested

--- db/migrate/20250326144346_add_element_aviti_pipeline_to_request_metadata.rb
+++ db/migrate/20250326144346_add_element_aviti_pipeline_to_request_metadata.rb
@@ -2,6 +2,6 @@
 class AddElementAvitiPipelineToRequestMetadata < ActiveRecord::Migration[7.0]
   def change
     add_column :request_metadata, :low_diversity, :boolean
-    add_column :request_metadata, :percent_element_phix_needed, :integer
+    add_column :request_metadata, :percent_element_phix_requested, :integer
   end
 end
