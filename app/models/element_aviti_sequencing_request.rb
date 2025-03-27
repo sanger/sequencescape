631c29eb156b12219e6ec1463f66f4856925f2bf
yoldas <ay6@sanger.ac.uk>
2025-03-27 23:48:42 +0000

    Rename percent element phix requested to percent phix requested

--- app/models/element_aviti_sequencing_request.rb
+++ app/models/element_aviti_sequencing_request.rb
@@ -4,7 +4,7 @@ class ElementAvitiSequencingRequest < SequencingRequest
   YES_OR_NO = %w[Yes No].freeze
 
   has_metadata as: Request do
-    custom_attribute(:percent_element_phix_requested, integer: true, required: true, minimum: 0, maximum: 100)
+    custom_attribute(:percent_phix_requested, integer: true, required: true, minimum: 0, maximum: 100)
     custom_attribute(:low_diversity, required: true, in: YES_OR_NO)
   end
 end
