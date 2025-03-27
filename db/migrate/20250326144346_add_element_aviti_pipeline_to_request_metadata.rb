4a5591c9248cc9c738f9a341e835cf138fd95a24
yoldas <ay6@sanger.ac.uk>
2025-03-27 09:27:55 +0000

    Apply request metadata migration

--- db/migrate/20250326144346_add_element_aviti_pipeline_to_request_metadata.rb
+++ db/migrate/20250326144346_add_element_aviti_pipeline_to_request_metadata.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class AddElementAvitiPipelineToRequestMetadata < ActiveRecord::Migration[7.0]
   def change
     add_column :request_metadata, :low_diversity, :boolean
