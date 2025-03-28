7907f8b3f0df0ebf87d859b6881681653d3bea88
yoldas <ay6@sanger.ac.uk>
2025-03-28 09:20:47 +0000

    Investigate has_metadata call in subclass

--- app/models/element_aviti_sequencing_request.rb
+++ app/models/element_aviti_sequencing_request.rb
@@ -3,7 +3,14 @@
 class ElementAvitiSequencingRequest < SequencingRequest
   YES_OR_NO = %w[Yes No].freeze
 
+  # XXX: Calling has_metadata again overrides the parent call. Parent attributes
+  # are added here for now.
   has_metadata as: Request do
+    custom_attribute(:fragment_size_required_from, integer: true, minimum: 1)
+    custom_attribute(:fragment_size_required_to, integer: true, minimum: 1)
+    custom_attribute(:read_length, integer: true, validator: true, required: true, selection: true)
+    custom_attribute(:requested_flowcell_type, required: false, validator: true, selection: true, on: :create)
+
     custom_attribute(:percent_phix_requested, integer: true, required: true, minimum: 0, maximum: 100)
     custom_attribute(:low_diversity, required: true, in: YES_OR_NO)
   end
