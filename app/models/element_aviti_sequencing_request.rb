c63e60bbe90ce45b605e36b974ae8e4e503d5060
yoldas <ay6@sanger.ac.uk>
2025-04-01 13:09:19 +0100

    Use boolean_select in low_diversity custom attribute declaration

--- app/models/element_aviti_sequencing_request.rb
+++ app/models/element_aviti_sequencing_request.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class ElementAvitiSequencingRequest < SequencingRequest
-  YES_OR_NO = %w[Yes No].freeze
-
-  # XXX: Calling has_metadata again overrides the parent call. Parent attributes
-  # are added here for now.
   has_metadata as: Request do
     custom_attribute(:fragment_size_required_from, integer: true, minimum: 1)
     custom_attribute(:fragment_size_required_to, integer: true, minimum: 1)
@@ -12,6 +8,29 @@ class ElementAvitiSequencingRequest < SequencingRequest
     custom_attribute(:requested_flowcell_type, required: false, validator: true, selection: true, on: :create)
 
     custom_attribute(:percent_phix_requested, integer: true, required: true, minimum: 0, maximum: 100)
-    custom_attribute(:low_diversity, required: true, in: YES_OR_NO)
+    custom_attribute(
+      :low_diversity,
+      boolean_select: true,
+      required: true,
+      default: 0,
+      select_options: {
+        Yes: 1,
+        No: 0
+      }
+    )
+  end
+
+  class ElementAvitiRequestOptionsValidator < SequencingRequest::RequestOptionsValidator
+    validates :percent_phix_requested,
+              numericality: {
+                integer_only: true,
+                greater_than_or_equal_to: 0,
+                less_than_or_equal_to: 100,
+                allow_nil: false
+              }
+  end
+
+  def self.deletage_validator
+    ElementAvitiSequencingRequest::ElementAvitiRequestOptionsValidator
   end
 end
