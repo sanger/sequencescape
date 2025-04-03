17d256b15d86345849c27f45718ab8d5b426d06d
Dasun Pubudumal <pubudumald@gmail.com>
2025-04-03 15:43:50 +0100

    Fixing typos

--- config/initializers/plates_from_tubes.rb
+++ config/initializers/plates_from_tubes.rb
@@ -3,5 +3,5 @@
 # Holds configs related to the plates/from_tubes endpoint.
 Rails.application.config.plates_from_tubes_config = {
   plate_creator_names_for_creating_from_tubes: ['Stock Plate', 'Stock RNA Plate'],
-  plate_purpose_options_for_creating_from_tubes: ['Stock Plate', 'RNA Stock Plate', 'All of the above']
+  plate_purpose_options_for_creating_from_tubes: ['Stock Plate', 'Stock RNA Plate', 'All of the above']
 }
