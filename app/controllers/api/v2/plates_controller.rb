935abe1fcdd17e5ee30288739f165968efe67b58
Wendy Yang <wy1@sanger.ac.uk>
2025-04-03 16:57:19 +0100

    save the current changes

--- app/controllers/api/v2/plates_controller.rb
+++ app/controllers/api/v2/plates_controller.rb
@@ -7,6 +7,21 @@ module Api
     class PlatesController < JSONAPI::ResourceController
       # By default JSONAPI::ResourceController provides most the standard
       # behaviour, and in many cases this file may be left empty.
+      def register_stock_for_plate
+        plate = Plate.find_by(uuid: params[:id])
+
+        if plate.blank?
+          return render json: { error: 'Plate not found' }, status: :not_found
+        end
+
+        begin
+          plate.wells.with_contents.each(&:register_stock!)
+          render json: { message: 'Stock successfully registered for plate wells' }, status: :ok
+        rescue StandardError => e
+          render json: { error: "Stock registration failed: #{e.message}" }, status: :unprocessable_entity
+        end
+
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,12 @@ Rails.application.routes.draw do
       jsonapi_resources :plate_creations, except: %i[update]
       jsonapi_resources :plate_purposes, except: %i[update]
       jsonapi_resources :plate_templates
-      jsonapi_resources :plates, except: %i[update]
+      # jsonapi_resources :plates, except: %i[update]
+      jsonapi_resources :plates, except: %i[update] do
+        member do
+          post :register_stock_for_plate
+        end
+      end
       jsonapi_resources :poly_metadata
       jsonapi_resources :pooled_plate_creations, except: %i[update]
       jsonapi_resources :pre_capture_pools
