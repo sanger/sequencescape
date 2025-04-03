59388a58b32b9f5f978c04734ddd5af4fc33fc02
Harriet Craven <harrietc52@hotmail.co.uk>
2025-04-03 14:47:33 +0100

    Add comment about the linkage data not being included

--- app/resources/api/v2/request_resource.rb
+++ app/resources/api/v2/request_resource.rb
@@ -119,19 +119,31 @@ module Api
       # Relationships
       ###
 
+      # @note `always_include_linkage_data` is currently broken. It is supposed to ensure that
+      #   linkage data is always included in the response. However, it is not working as expected.
+      #   To ensure linkage data is always included, explicitly include the relationship in the request.
+      #   For example, use `?include=submission` in the request URL to include the `submission` relationship.
+      #   See Y25-164 for more details and a possible fix.
       # @!attribute [r] submission
       #   The submission associated with this request.
-
       has_one :submission, always_include_linkage_data: true
 
+      # @note `always_include_linkage_data` is currently broken. It is supposed to ensure that
+      #   linkage data is always included in the response. However, it is not working as expected.
+      #   To ensure linkage data is always included, explicitly include the relationship in the request.
+      #   For example, use `?include=submission` in the request URL to include the `submission` relationship.
+      #   See Y25-164 for more details and a possible fix.
       # @!attribute [r] order
       #   The order associated with this request.
-
       has_one :order, always_include_linkage_data: true
 
+      # @note `always_include_linkage_data` is currently broken. It is supposed to ensure that
+      #   linkage data is always included in the response. However, it is not working as expected.
+      #   To ensure linkage data is always included, explicitly include the relationship in the request.
+      #   For example, use `?include=submission` in the request URL to include the `submission` relationship.
+      #   See Y25-164 for more details and a possible fix.
       # @!attribute [r] request_type
       #   The type of the request, such as "analysis" or "testing."
-
       has_one :request_type, always_include_linkage_data: true
 
       # @!attribute [r] primer_panel
