a53c33a9cb43d1c39e456c3b4a9aebfbc9980106
Wendy Yang <wy1@sanger.ac.uk>
2025-04-10 09:24:09 +0100

    fix the test

--- spec/controllers/api/plate_controller_spec.rb
+++ spec/controllers/api/plate_controller_spec.rb
@@ -4,19 +4,22 @@ require 'rails_helper'
 
 RSpec.describe Api::V2::PlatesController, type: :request do
   describe 'POST /api/v2/plates/:id/register_stock_for_plate' do
-    let(:plate) { instance_double(Plate, id: '123', wells: wells) }
-    let(:wells) { instance_double('Wells', with_contents: [well1, well2]) }
-    let(:well1) { instance_double(Well) }
-    let(:well2) { instance_double(Well) }
+    let(:plate) { create(:plate, well_count: 96) }
+    let(:wells) { plate.wells }
+    let(:well1) { wells[0] }
+    let(:well2) { wells[1] }
 
     before do
-      allow(well1).to receive(:register_stock!)
-      allow(well2).to receive(:register_stock!)
+      # Add content (aliquots) to only the first two wells
+      plate.wells[0].aliquots << create(:aliquot, sample: create(:sample), receptacle: plate.wells[0])
+      plate.wells[1].aliquots << create(:aliquot, sample: create(:sample), receptacle: plate.wells[1])
     end
 
     context 'when the plate exists' do
       before do
         allow(Plate).to receive(:find_by).with(id: '123').and_return(plate)
+        allow(well1).to receive(:register_stock!)
+        allow(well2).to receive(:register_stock!)
         post '/api/v2/plates/123/register_stock_for_plate'
       end
 
@@ -35,6 +38,10 @@ RSpec.describe Api::V2::PlatesController, type: :request do
         post '/api/v2/plates/123/register_stock_for_plate'
       end
 
+      it 'returns a 404 Not Found status' do
+        expect(response).to have_http_status(:not_found)
+      end
+
       it 'returns a not found error' do
         expect(response.parsed_body['error']).to eq('Plate not found')
       end
@@ -43,8 +50,14 @@ RSpec.describe Api::V2::PlatesController, type: :request do
     context 'when an error occurs during registration' do
       before do
         allow(Plate).to receive(:find_by).with(id: '123').and_return(plate)
-        allow(well1).to receive(:register_stock!).and_raise(StandardError.new('Some error'))
+        allow(plate).to receive(:wells).and_return(wells)
+        allow(wells).to receive(:with_contents).and_return([well1])
+        allow(well1).to receive(:register_stock!).and_raise(StandardError.new('Stock registration failed'))
         post '/api/v2/plates/123/register_stock_for_plate'
+      end
+
+      it 'returns a 422 Unprocessable Entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
       end
 
       it 'returns an error message' do
