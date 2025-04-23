f02619128c6e4326cd91dafe2a2422aa1beb3962
sabrine33 <sa33@sanger.ac.uk>
2025-04-23 10:39:46 +0100

    update requests table column order for all the existing pipelines

--- app/validators/element_aviti_validator.rb
+++ app/validators/element_aviti_validator.rb
@@ -5,20 +5,9 @@ class ElementAvitiValidator < ActiveModel::Validator
     '300PE (MO, HO) require only one request.'
   end
 
-  # Validates that a batch does not contain multiple requests with a read length of 300.
-  #
-  # @param record [Object] The batch record being validated.
-  # @return [Boolean, nil] Returns false if validation fails, otherwise nil.
-  #
   # Adds an error to the record if more than one request exists with a read length of 300.
   def validate(record)
-    # requests = record.requests
-    # return true unless requests.size > 1 && requests.any? { |r| r.request_metadata.read_length == 300 }
-    # record.errors.add(:base, 'Batches can contain only one request when the read length is 300')
-    # false
-    return true if record.requests.one? || record.requests.none? { |r| r.request_metadata.read_length == 300 }
-
+    return if record.requests.one? || record.requests.none? { |r| r.request_metadata.read_length == 300 }
     record.errors.add(:base, 'Batches can contain only one request when the read length is 300')
-    false
   end
 end
