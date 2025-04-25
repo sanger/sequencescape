285557c3641577a9620a93194b7692812101aac2
sabrine33 <sa33@sanger.ac.uk>
2025-04-25 14:48:20 +0100

    loading action descriptors for Element Aviti pipeline

---
+++ lib/record_loader/descriptor_loader.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+# This file was automatically generated via `rails g record_loader`
+
+# RecordLoader handles automatic population and updating of database records
+# across different environments
+# @see https://rubydoc.info/github/sanger/record_loader/
+module RecordLoader
+  # Creates the specified Descriptor if they are not present
+  class DescriptorLoader < ApplicationRecordLoader
+    config_folder 'descriptors'
+
+    def create_or_update!(name, options)
+      workflow_name = options.delete('workflow')
+      workflow = find_workflow!(workflow_name, name)
+      return nil if workflow.nil?
+      task_name = options.delete('task')
+      task = find_task!(task_name, workflow.id, name)
+      return nil if task.nil?
+      options[:task_id] = task.id
+      Descriptor.find_or_initialize_by(name: options['name'], task_id: task.id).tap { |desc| desc.update!(options) }
+    end
+
+    private
+
+    def find_workflow!(workflow_name, descriptor_name)
+      return unless workflow_name
+      Workflow.find_by!(name: workflow_name)
+    rescue ActiveRecord::RecordNotFound
+      message =
+        "Descriptor '#{descriptor_name}' creation or update failed " \
+          "because there was no workflow named '#{workflow_name}'"
+
+      handle_missing_reference(message)
+    end
+
+    def find_task!(task_name, workflow_id, descriptor_name)
+      Task.find_by!(name: task_name, pipeline_workflow_id: workflow_id)
+    rescue ActiveRecord::RecordNotFound
+      message =
+        "Descriptor '#{descriptor_name}' creation or update failed " \
+          "because there was no task with name '#{task_name}' " \
+          "associated with the workflow id '#{workflow_id}'"
+
+      handle_missing_reference(message)
+    end
+
+    def handle_missing_reference(message)
+      unless Rails.env.development? || Rails.env.staging? || Rails.env.cucumber?
+        raise ActiveRecord::RecordNotFound, message
+      end
+      Rails.logger.warn(message)
+      nil
+    end
+  end
+end
