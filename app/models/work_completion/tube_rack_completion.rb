bc3e973962a356693cca5a1a9bb47434d7b67ef9
Dasun Pubudumal <pubudumald@gmail.com>
2025-05-02 12:39:54 +0100

    [skip ci] adding documentation

--- app/models/work_completion/tube_rack_completion.rb
+++ app/models/work_completion/tube_rack_completion.rb
@@ -8,6 +8,15 @@
 # @author Genome Research Ltd.
 #
 class WorkCompletion::TubeRackCompletion < WorkCompletion::LabwareCompletion
+  # Connects upstream requests to the target labware's racked tubes.
+  #
+  # Iterates through each upstream request and links it to the receptacle
+  # of each tube in the target labware's racked tubes.
+  #
+  # @return [void]
+  #
+  # @example
+  #   connect_requests
   def connect_requests
     detect_upstream_requests.each do |upstream|
       target_labware.racked_tubes.each { |tube| pass_and_link_up_requests(tube.tube.receptacle, upstream) }
@@ -15,6 +24,16 @@ class WorkCompletion::TubeRackCompletion < WorkCompletion::LabwareCompletion
     @order_ids.uniq!
   end
 
+  # Detects upstream customer requests associated with the target labware.
+  #
+  # Queries the database for customer requests linked to the aliquots
+  # of the target labware.
+  #
+  # @return [ActiveRecord::Relation] A collection of customer requests.
+  #
+  # @example
+  #   detect_upstream_requests
+  #   # => [#<CustomerRequest id: 1>, #<CustomerRequest id: 2>]
   def detect_upstream_requests
     CustomerRequest.includes(WorkCompletion::REQUEST_INCLUDES).where(id: target_labware.aliquots.pluck(:request_id))
   end
