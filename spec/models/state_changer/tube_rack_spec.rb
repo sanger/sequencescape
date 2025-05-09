cfd34b82b762e736345028a21029ddf905f99049
Dasun Pubudumal <pubudumald@gmail.com>
2025-05-09 11:31:11 +0100

    Completing first unit test phase

--- spec/models/state_changer/tube_rack_spec.rb
+++ spec/models/state_changer/tube_rack_spec.rb
@@ -32,21 +32,18 @@ RSpec.describe StateChanger::TubeRack do
 
     context 'when transitioning to "passed" state for a rack with multiple tubes' do
       let(:labware) { create(:tube_rack_with_tubes, locations: %w[A1 A2 A3]) }
-      let(:transfer_request_state) { 'started' }
-      let(:request_state) { 'started' }
-      let(:target_state) { 'passed' }
-      let(:requests) do
+      let!(:requests) do
         [
-          create(:request, target_asset: labware.tube_receptacles[0], state: request_state),
+          create(:request, target_asset: labware.tube_receptacles.first, state: request_state),
           create(:request, target_asset: labware.tube_receptacles[1], state: request_state),
-          create(:request, target_asset: labware.tube_receptacles[2], state: request_state)
+          create(:request, target_asset: labware.tube_receptacles.last, state: request_state)
         ]
       end
-      let(:transfer_requests) do
+      let!(:transfer_requests) do
         [
-          create(:transfer_request, target_asset: labware.tube_receptacles[0], state: transfer_request_state),
+          create(:transfer_request, target_asset: labware.tube_receptacles.first, state: transfer_request_state),
           create(:transfer_request, target_asset: labware.tube_receptacles[1], state: transfer_request_state),
-          create(:transfer_request, target_asset: labware.tube_receptacles[2], state: transfer_request_state)
+          create(:transfer_request, target_asset: labware.tube_receptacles.last, state: transfer_request_state)
         ]
       end
 
@@ -57,10 +54,8 @@ RSpec.describe StateChanger::TubeRack do
         state_changer.update_labware_state
       end
 
-      xit 'updates the tube to "passed" for receptacles with "started" requests', :aggregate_failures do
-        labware.tube_receptacles.each do |tube_receptacle|
-          expect(tube_receptacle.transfer_requests_as_target.first.reload.state).to eq('passed')
-        end
+      it 'updates the tube to "passed" for receptacles with "started" requests', :aggregate_failures do
+        transfer_requests.each { |transfer_request| expect(transfer_request.reload.state).to eq(target_state) }
       end
     end
   end
