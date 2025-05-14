a9c2b0c492a382a94a51ca9bfa05cfc1c3a10448
Wendy Yang <wy1@sanger.ac.uk>
2025-05-14 14:36:47 +0100

    add migration file

---
+++ db/migrate/20250514132842_add_ebi_requirement_fields_to_study_metadata.rb
@@ -0,0 +1,7 @@
+class AddEbiRequirementFieldsToStudyMetadata < ActiveRecord::Migration[7.1]
+  def change
+    add_column :study_metadata, :ebi_library_strategy, :string, default: nil
+    add_column :study_metadata, :ebi_library_source, :string, default: nil
+    add_column :study_metadata, :ebi_library_selection, :string, default: nil
+  end
+end
