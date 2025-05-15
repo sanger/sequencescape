5f5adb07e77e1f2b2691277262e27dd16cf4a229
Stephen Hulme <sh51@sanger.ac.uk>
2025-05-15 10:48:14 +0100

    style: lint prettier

--- test/lib/linefeed_fix_test.rb
+++ test/lib/linefeed_fix_test.rb
@@ -5,7 +5,7 @@ require './lib/linefeed_fix'
 
 class LinefeedFixTest < ActiveSupport::TestCase
   context 'LinefeedFix' do
-    setup { Rails.root.join("test/data/bad.csv").open { |f| @string = f.read } }
+    setup { Rails.root.join('test/data/bad.csv').open { |f| @string = f.read } }
 
     should 'raise if we don\'t do anything' do
       # Makes sure sublime hasn't 'fixed' the file for us
