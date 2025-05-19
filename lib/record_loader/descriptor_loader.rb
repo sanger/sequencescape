664a872dfa919c6e0115dac2a349a063b4da71ad
sabrine33 <sa33@sanger.ac.uk>
2025-05-19 12:02:00 +0100

    remove unnecessary error handling

--- lib/record_loader/descriptor_loader.rb
+++ lib/record_loader/descriptor_loader.rb
@@ -15,45 +15,12 @@ module RecordLoader
 
     def create_or_update!(name, options)
       workflow_name = options.delete('workflow')
-      workflow = find_workflow!(workflow_name, name)
-      return unless workflow
+      workflow =  Workflow.find_by!(name: workflow_name)
       task_name = options.delete('task')
-      task = find_task!(task_name, workflow.id, name)
-      return unless task
+      task = Task.find_by!(name: task_name, pipeline_workflow_id: workflow.id)
       options[:task_id] = task.id
-      Descriptor.create_with(options).find_or_create_by!(name: options['name'], task_id: task.id)
+      Descriptor.create_with(options).find_or_create_by!(name:name, task_id: task.id)
     end
 
-    private
-
-    def find_workflow!(workflow_name, descriptor_name)
-      return unless workflow_name
-      Workflow.find_by!(name: workflow_name)
-    rescue ActiveRecord::RecordNotFound
-      message =
-        "Descriptor '#{descriptor_name}' creation or update failed " \
-          "because there was no workflow named '#{workflow_name}'"
-
-      handle_missing_reference(message)
-    end
-
-    def find_task!(task_name, workflow_id, descriptor_name)
-      Task.find_by!(name: task_name, pipeline_workflow_id: workflow_id)
-    rescue ActiveRecord::RecordNotFound
-      message =
-        "Descriptor '#{descriptor_name}' creation or update failed " \
-          "because there was no task with name '#{task_name}' " \
-          "associated with the workflow id '#{workflow_id}'"
-
-      handle_missing_reference(message)
-    end
-
-    def handle_missing_reference(message)
-      unless Rails.env.development? || Rails.env.staging? || Rails.env.cucumber?
-        raise ActiveRecord::RecordNotFound, message
-      end
-      Rails.logger.warn(message)
-      nil
-    end
   end
 end
