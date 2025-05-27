b1c34feac7c1c0460150d58ca0cfbe3c44e4b735
Stephen Hulme <sh51@sanger.ac.uk>
2025-05-27 17:10:30 +0100

    fix: revert raise_on_missing_callback_actions to pre Rails 7.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,6 +78,15 @@ Rails.application.configure do
 
   # load WIP features flag
   config.deploy_wip_pipelines = true
+
+  # Annotate rendered view with file names.
+  # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Uncomment if you wish to allow Action Cable access from any origin.
+  # config.action_cable.disable_request_forgery_protection = true
+
+  # Raise error when a before_action's only/except options reference missing actions
+  config.action_controller.raise_on_missing_callback_actions = false
 end
 
 Rack::MiniProfiler.config.position = 'right'
