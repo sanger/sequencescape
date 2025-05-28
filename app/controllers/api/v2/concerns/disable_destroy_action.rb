0dd6599f0b067fb9b0d72f095de45a72f486b464
Harriet Craven <harrietc52@hotmail.co.uk>
2025-05-28 16:12:54 +0100

    move concern to api folder

--- app/controllers/api/v2/concerns/disable_destroy_action.rb
+++ app/controllers/api/v2/concerns/disable_destroy_action.rb
@@ -5,16 +5,15 @@
 # Existing endpoints already exclude the destroy action via `except: :destroy` in routes.rb,
 # so this concern acts as a safeguard against future misconfigurations.
 #
-# NOTE: This file is located in `app/controllers/concerns/api/v2/disable_destroy_action.rb`
-# instead of `app/controllers/api/v2/concerns/disable_destroy_action.rb` due to test suite expectations.
-# Moving it to `app/controllers/api/v2/concerns` causes test failures as the file cannot be located.
 module Api
   module V2
-    module DisableDestroyAction
-      extend ActiveSupport::Concern
+    module Concerns
+      module DisableDestroyAction
+        extend ActiveSupport::Concern
 
-      def destroy
-        head :method_not_allowed
+        def destroy
+          head :method_not_allowed
+        end
       end
     end
   end
--- spec/controllers/concerns/api/v2/disable_destroy_action_spec.rb
+++ spec/controllers/concerns/api/v2/disable_destroy_action_spec.rb
@@ -7,14 +7,14 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe Api::V2::DisableDestroyAction, type: :controller do
+RSpec.describe Api::V2::Concerns::DisableDestroyAction, type: :controller do
   # We explicitly use the constant `Api::V2::DisableDestroyAction` here instead of `described_class`
   # because `described_class` is not accessible at the top level of the spec file where the
   # anonymous controller is being defined. Attempting to use `described_class` in this context
   # results in a NameError, as RSpec has not yet defined it. Therefore, we disable the RuboCop
   # RSpec/DescribedClass rule for this line only.
   # rubocop:disable RSpec/DescribedClass
-  controller(ActionController::Base) { include Api::V2::DisableDestroyAction }
+  controller(ActionController::Base) { include Api::V2::Concerns::DisableDestroyAction }
   # rubocop:enable RSpec/DescribedClass
 
   before { routes.draw { delete 'destroy' => 'anonymous#destroy' } }
