9c090c6d4f03c80a1c75d12e4d5b4f67d0da8045
Wendy Yang <wy1@sanger.ac.uk>
2025-05-29 18:53:01 +0100

    fix the linting

--- app/models/element_avit_sequencing_pipeline.rb
+++ app/models/element_avit_sequencing_pipeline.rb
@@ -2,10 +2,9 @@
 
 # Specialized sequencing pipeline for Element Aviti
 class ElementAvitiSequencingPipeline < SequencingPipeline
-    def post_release_batch(batch, _user)
-      # Same logic as the superclass, but with a different Messenger root
-      batch.assets.compact.uniq.each(&:index_aliquots)
-      Messenger.create!(target: batch, template: 'FlowcellIo', root: 'eseq_flowcell')
-    end
+  def post_release_batch(batch, _user)
+    # Same logic as the superclass, but with a different Messenger root
+    batch.assets.compact.uniq.each(&:index_aliquots)
+    Messenger.create!(target: batch, template: 'FlowcellIo', root: 'eseq_flowcell')
   end
-  
+end
