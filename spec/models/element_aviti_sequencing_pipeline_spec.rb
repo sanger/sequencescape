84b28823a8f9586fe2006576f3a2ae910b436944
Wendy Yang <wy1@sanger.ac.uk>
2025-06-02 10:40:24 +0100

    add unit test

---
+++ spec/models/element_aviti_sequencing_pipeline_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe ElementAvitiSequencingPipeline, type: :model do
+  describe '#post_release_batch' do
+    let(:pipeline) { create(:element_aviti_sequencing_pipeline) }
+    let(:batch) { create(:batch) }
+    # let(:asset) { create(:lane_labware) }
+
+    # before do
+    #   batch.assets << asset
+    # end
+
+    it 'calls Messenger with eseq_flowcell root' do
+      expect(Messenger).to receive(:create!).with(hash_including(target: batch, template: 'FlowcellIo', root: 'eseq_flowcell'))
+
+      pipeline.post_release_batch(batch, create(:user))
+    end
+  end
+end
