2f1b2fb89444af932af23bfe0ffce1690f94d46a
Katy Taylor <kt17@sanger.ac.uk>
2025-06-06 14:49:19 +0100

    fix test after changing message

--- spec/controllers/npg_actions/assets_controller_spec.rb
+++ spec/controllers/npg_actions/assets_controller_spec.rb
@@ -446,7 +446,8 @@ RSpec.describe NpgActions::AssetsController, type: :request do
       it 'returns a warning' do
         regexp =
           Regexp.new(
-            ['<error><message>', 'NPG user run this action. Please, contact USG', '</message></error>'].join,
+            "<error><message>The requests on this lane have already been completed with state: 'pass'. "\
+            "Unable to set them to new state: 'fail'.</message></error>",
             Regexp::MULTILINE
           )
         expect(response).to have_http_status(:bad_request)
