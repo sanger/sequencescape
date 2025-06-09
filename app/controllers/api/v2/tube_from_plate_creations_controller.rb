5490f07b3799fe169fb4be88d15c036e9cb34bc0
Dasun Pubudumal <dp27@sanger.ac.uk>
2025-06-09 15:10:16 +0100

    Update app/controllers/api/v2/tube_from_plate_creations_controller.rb
    Co-authored-by: KatyTaylor <kt17@sanger.ac.uk>

--- app/controllers/api/v2/tube_from_plate_creations_controller.rb
+++ app/controllers/api/v2/tube_from_plate_creations_controller.rb
@@ -2,7 +2,7 @@
 
 module Api
   module V2
-    # Provides a JSON API controller for Tube From Tube Creations
+    # Provides a JSON API controller for Tube From Plate Creations
     # See: http://jsonapi-resources.com/ for JSONAPI::Resource documentation
     class TubeFromPlateCreationsController < JSONAPI::ResourceController
       # By default JSONAPI::ResourceController provides most the standard
