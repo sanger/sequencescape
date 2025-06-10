36c0a6d1956773ec78916b2ddcb1e85f81bdbc44
Wendy Yang <wy1@sanger.ac.uk>
2025-06-10 11:19:41 +0100

    fix the linting

--- test/unit/element_aviti_sequencing_pipeline_test.rb
+++ test/unit/element_aviti_sequencing_pipeline_test.rb
@@ -1,34 +1,32 @@
+# frozen_string_literal: true
 require 'test_helper'
 
 class ElementAvitiSequencingPipelineTest < ActiveSupport::TestCase
   setup do
-    @pipeline = ElementAvitiSequencingPipeline.new(
-      workflow: Workflow.new,
-      request_types: [create(:sequencing_request_type)]
-    )
+    @pipeline =
+      ElementAvitiSequencingPipeline.new(workflow: Workflow.new, request_types: [create(:sequencing_request_type)])
 
     @batch = create(:batch, pipeline: @pipeline)
 
     # Attach request and metadata
-    @request = create(
-      :sequencing_request,
-      batch: @batch,
-      request_metadata_attributes: {
-        read_length: 75,
-        fragment_size_required_from: 100,
-        fragment_size_required_to: 200,
-        requested_flowcell_type: 'ElementAvitiFlowcell'
-      }
-    )
+    @request =
+      create(
+        :sequencing_request,
+        batch: @batch,
+        request_metadata_attributes: {
+          read_length: 75,
+          fragment_size_required_from: 100,
+          fragment_size_required_to: 200,
+          requested_flowcell_type: 'ElementAvitiFlowcell'
+        }
+      )
 
     # Force target_asset if needed
     @flowcell = create(:flowcell, batch: @batch)
   end
 
   test 'generates valid flowcell XML message on post_release_batch' do
-    assert_difference('Messenger.count', 1) do
-      @pipeline.post_release_batch(@batch, create(:user))
-    end
+    assert_difference('Messenger.count', 1) { @pipeline.post_release_batch(@batch, create(:user)) }
 
     messenger = Messenger.last
     assert_equal @batch, messenger.target
@@ -46,23 +44,23 @@ class ElementAvitiSequencingPipelineTest < ActiveSupport::TestCase
   test 'post_release_batch generates correct flowcell message' do
     user = create(:user)
     batch = create(:batch, pipeline: ElementAvitiSequencingPipeline.create!(workflow: create(:workflow)))
-    request = create(:sequencing_request, batch: batch)
-  
+    request = create(:sequencing_request, batch:)
+
     # This should trigger the Messenger creation
     ElementAvitiSequencingPipeline.new.post_release_batch(batch, user)
-  
+
     messenger = Messenger.last
     message = messenger.as_json
-  
+
     # Basic structure checks
     assert message.key?('flowcell'), 'Missing flowcell root key'
     assert_equal configatron.amqp.lims_id, message['lims']
-  
+
     flowcell = message['flowcell']
     assert_equal batch.id, flowcell['flowcell_id']
     assert flowcell['lanes'].any?, 'Expected at least one lane in flowcell'
-  
+
     sample = flowcell['lanes'].first['samples'].first
     assert_equal request.sample.sample_uuid, sample['sample_uuid']
-  end  
+  end
 end
