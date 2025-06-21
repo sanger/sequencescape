a33d9ff9a4b9fae447511e926f8f5b7fa2b998da
yoldas <ay6@sanger.ac.uk>
2025-06-22 00:24:03 +0100

    Migrate features/11803383_bug_NPG_batch_state_released.feature

---
+++ spec/features/migrated/bug_npg_batch_state_released_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'NPG batch state released via XML API', :allow_rescue, :api, :xml do
+  let(:user) { create(:admin) }
+  let(:lane) { create(:lane, name: 'NPG_Action_Lane_Test', qc_state: 'passed', external_release: 1) }
+  let(:library_tube) { create(:library_tube) }
+  let(:pipeline) { create(:sequencing_pipeline) }
+  let(:request) do
+    create(:request_with_sequencing_request_type,
+           asset: library_tube, target_asset: lane,
+           request_type: pipeline.request_types.last, state: 'started')
+  end
+  let(:batch) { create(:batch, state: 'started', qc_state: 'qc_manual', pipeline: pipeline) }
+
+  before do
+    login_user(user)
+    create(:batch_request, request: request, batch: batch, position: 1)
+  end
+
+  scenario 'POST XML to change qc_state on an asset' do
+    visit batch_path(batch)
+    expect(page).to have_content('started')
+
+    xml = { qc_information: { message: 'NPG change status in failed' } }.to_xml # string or symbol keys
+    path = "/npg_actions/assets/#{lane.id}/pass_qc_state"
+    headers = { 'HTTP_ACCEPT' => 'application/xml', 'CONTENT_TYPE' => 'application/xml' }
+    page.driver.post path, xml, headers
+
+    expect(page.status_code).to eq(200)
+
+    hash = Hash.from_xml(page.body) # string keys
+    expect(hash['asset']['qc_state']).to eq('passed')
+
+    visit batch_path(batch)
+    expect(page).to have_content("Pipeline #{pipeline.name}")
+    expect(page).to have_content('released')
+  end
+end
