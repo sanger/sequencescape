9b88adc4fbf70e6d2f4642c32b3d583d3a0da22d
sabrine33 <sa33@sanger.ac.uk>
2025-06-26 12:03:52 +0100

    add library types for NPG pipeline

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_15_133642) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_26_105725) do
   create_table "aliquot_indices", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.integer "aliquot_id", null: false
     t.integer "lane_id", null: false
