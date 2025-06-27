cd19f061a40179b4a8411ea0a1648268f5dd3121
yoldas <ay6@sanger.ac.uk>
2025-06-27 18:17:13 +0100

    Revert changes to the tag set model

--- app/models/tag_set.rb
+++ app/models/tag_set.rb
@@ -59,15 +59,11 @@ class TagSet < ApplicationRecord
     errors.add(:tag_group, 'Adapter types of tag groups must match')
   end
 
-  # This method allows setting the tag_group by name if present.
-  # @return [void]
   def tag_group_name=(name)
-    self.tag_group = TagGroup.find_by!(name:) if name.present?
+    self.tag_group = TagGroup.find_by!(name:)
   end
 
-  # This method allows setting the tag2_group by name if present.
-  # @return [void]
   def tag2_group_name=(name)
-    self.tag2_group = TagGroup.find_by!(name:) if name.present?
+    self.tag2_group = TagGroup.find_by!(name:)
   end
 end
