cfe07b13f1c0c96033178b4450fdd72dcbb65dd0
yoldas <ay6@sanger.ac.uk>
2025-07-09 01:46:37 +0100

    Add test for batch print labels view when target asset is a lane

---
+++ spec/views/batches/print_labels_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'batches/print_labels.html.erb', type: :view do
+  include AuthenticatedSystem
+  let(:current_user) { create(:user) }
+
+  before do
+    assign(:batch, batch)
+    render
+  end
+
+  context 'when target_asset is a lane' do
+    let(:lane) { create(:lane).tap { |lane| lane.labware.parents << tube } }
+    let(:tube) { create(:multiplexed_library_tube) }
+    let(:request) { create(:sequencing_request, target_asset: lane, asset: tube.receptacle) }
+    let(:batch) { create(:batch).tap { |batch| batch.requests << request } }
+    let(:printable) { { request.id => 'on' } }
+    let(:options) { { count: '1', printable: printable, batch: batch, stock: false } }
+
+    it 'shows the parent tube barcode' do
+      expect(rendered).to include(tube.human_barcode)
+    end
+  end
+end
