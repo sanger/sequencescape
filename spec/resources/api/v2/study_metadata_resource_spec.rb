5d47aa90c5ba5a577fbaf52e0f32f34aabe545cf
Ben Topping <ben.topping@outlook.com>
2025-07-10 13:18:15 +0100

    feat(api-v2): adds sample_metadata and faculty_sponsor resources

---
+++ spec/resources/api/v2/study_metadata_resource_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require './app/resources/api/v2/study_metadata_resource'
+
+RSpec.describe Api::V2::StudyMetadataResource, type: :resource do
+  subject(:resource) { described_class.new(study_metadata, {}) }
+
+  let(:study_metadata) { create(:study_metadata) }
+
+  it { is_expected.to filter(:study_id) }
+
+  it { is_expected.to have_a_readonly_has_one(:faculty_sponsor).with_class_name('FacultySponsor') }
+end
