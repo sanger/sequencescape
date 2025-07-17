5aa85ddd101a01df899ea2a4a9ecfe27b7cc7ebf
Harriet Craven <harrietc52@hotmail.co.uk>
2025-07-17 15:12:49 +0100

    Updated test for multiple samples in one well

--- spec/models/api/messages/well_stock_resource_io_spec.rb
+++ spec/models/api/messages/well_stock_resource_io_spec.rb
@@ -11,25 +11,30 @@ RSpec.describe Api::Messages::WellStockResourceIo do
   after { Timecop.return }
 
   let(:sample) { create(:sample) }
+  let(:sample2) { create(:sample) }
   let(:plate_barcode) { build(:plate_barcode) }
+  let(:plate) { create(:plate, barcode: plate_barcode.barcode) }
   let(:well) do
     create(
       :well,
       map: Map.find_by!(description: 'A1', asset_shape: AssetShape.default, asset_size: 96),
-      plate: create(:plate, barcode: plate_barcode.barcode),
+      plate: plate,
       well_attribute: create(:complete_well_attribute)
     )
   end
   let(:study) { create(:study) }
   let(:aliquot) { create(:aliquot, study: study, sample: sample, receptacle: well) }
+  let(:aliquot2) { create(:aliquot, study: study, sample: sample2, receptacle: well) }
 
   before { aliquot } # rubocop:todo RSpec/ScatteredSetup
+  before { aliquot2 } # rubocop:todo RSpec/ScatteredSetup
 
   let(:expected_json) do
     {
       'created_at' => '2012-03-11T10:22:42+00:00',
       'updated_at' => '2012-03-11T10:22:42+00:00',
-      'samples' => [{ 'id_sample_tmp' => sample.id, 'sample_uuid' => sample.uuid, 'study_uuid' => study.uuid }],
+      'samples' => [{ 'id_sample_tmp' => sample.id, 'sample_uuid' => sample.uuid, 'study_uuid' => study.uuid },
+                    { 'id_sample_tmp' => sample2.id, 'sample_uuid' => sample2.uuid, 'study_uuid' => study.uuid }],
       'stock_resource_id' => well.id,
       'stock_resource_uuid' => well.uuid,
       'machine_barcode' => plate_barcode.barcode,
