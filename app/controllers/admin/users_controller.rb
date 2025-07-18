9deb7290fbe2d4f82d1644369015e868f9974547
Stephen Hulme <sh51@sanger.ac.uk>
2025-07-18 17:29:14 +0100

    style: lint

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-class Admin::UsersController < ApplicationController
+class Admin::UsersController < ApplicationController # rubocop:todo Metrics/ClassLength
   # WARNING! This filter bypasses security mechanisms in rails 4 and mimics rails 2 behviour.
   # It should be removed wherever possible and the correct Strong  Parameter options applied in its place.
   before_action :evil_parameter_hack!
