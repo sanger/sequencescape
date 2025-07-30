376446d95bb341f1eeacc1ad32f3fa302c98668d
Katy Taylor <kt17@sanger.ac.uk>
2025-07-30 15:15:58 +0100

    add qcable_creators into v2 api (untested)

---
+++ app/resources/api/v2/qcable_creator_resource.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+module Api
+  module V2
+    # Provides a JSON:API representation of {QcableCreator} which represents ...
+    #
+    # @note Access this resource via the `/api/v2/qcable_creators/` endpoint.
+    #
+    #
+    # @example POST request to create a {QcableCreator}.
+    #    to do
+    #
+    # @example PATCH request to change the {Asset} of a {QcableCreator}.
+    #   to do
+    #
+    # @example GET request for all {QcableCreator} resources
+    #   to do
+    #
+    # @example GET request for a {QcableCreator} with ID 123
+    #   to do
+    #
+    # For more information about JSON:API see the [JSON:API Specifications](https://jsonapi.org/format/)
+    # or look at the [JSONAPI::Resources](http://jsonapi-resources.com/) package for Sequencescape's implementation
+    # of the JSON:API standard.
+    class QcableCreatorResource < BaseResource
+      default_includes :uuid_object
+
+      ###
+      # Attributes
+      ###
+
+      # @!attribute [r] uuid
+      #   @return [String] the UUID of this {QcableCreator}.
+      attribute :uuid, readonly: true
+
+      # @!attribute [rw] barcodes
+      #   Either this or 'count' is passed in when creating a {QcableCreator}.
+      #   @return [Array<String>] the barcodes to use when creating the associated {Qcable}s.
+      attribute :barcodes
+
+      # @!attribute [rw] count
+      #   Either this or 'barcodes' is passed in when creating a {QcableCreator}.
+      #   @return [Integer] the number of {Qcable}s to create under this {QcableCreator}.
+      attribute :count
+
+      ###
+      # Relationships
+      ###
+
+      # @!attribute [rw] lot
+      #   @return [LotResource] the {Lot} resource associated with this {QcableCreator}.
+      has_one :lot
+
+      # @!attribute [rw] user
+      #   @return [UserResource] the {User} resource associated with this {QcableCreator}.
+      has_one :user
+
+      # @!attribute [r] qcables
+      #   @return [Array<QcableResource>] the {Qcable} resources created by this {QcableCreator}.
+      has_many :qcables, class_name: 'QcableResource', read_only: true
+
+      ###
+      # Filters
+      ###
+
+      # @!method filter_uuid
+      #   Apply a filter across all {QcableCreator} resources, matching by UUID.
+      #   @example Get all {QcableCreator} resources with a specific UUID.
+      #     /api/v2/qcable_creators?filter[uuid]=12345678-1234-1234-1234-123456789012
+      filter :uuid, apply: ->(records, value, _options) { records.with_uuid(value) }
+    end
+  end
+end
