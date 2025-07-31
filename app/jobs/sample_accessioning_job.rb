176d139673f83f09144c6393952b9e0cd4450d85
Stephen Hulme <sh51@sanger.ac.uk>
2025-07-31 11:15:03 +0100

    refactor: reduce complexity

--- app/jobs/sample_accessioning_job.rb
+++ app/jobs/sample_accessioning_job.rb
@@ -13,24 +13,11 @@ SampleAccessioningJob =
 
       # update_accession_number returns true if an accession has been supplied, and the sample has been saved.
       # If this returns false, then we fail the job. This should catch any failure situations
-      sample_id = accessionable.sample.sanger_sample_id
-      user = submission.user
       cause = 'EBI failed to update accession number, data may be invalid'
-      message = "SampleAccessioningJob failed for sample '#{sample_id}' by user '#{user.username}' (#{user.id}): #{cause}"
 
-      submission.update_accession_number || raise(AccessionService::AccessionServiceError, message)
+      submission.update_accession_number || raise_accession_error(submission, cause)
     rescue AccessionService::AccessionServiceError => e
-      Rails.logger.error(job_error_message)
-      ExceptionNotifier.notify_exception(e, data: {
-                                           cause_message: cause,
-                                           sample_id: sample_id,
-                                           user_id: user.id,
-                                           user_username: user.username,
-                                           job_class: self.class.name,
-                                           job_id: jid,
-                                           accessionable_id: accessionable.id,
-                                           accessionable_type: accessionable.class.name
-                                         })
+      handle_accession_error(e, submission, cause)
     end
 
     def reschedule_at(current_time, _attempts)
@@ -43,5 +30,30 @@ SampleAccessioningJob =
 
     def queue_name
       'sample_accessioning'
+    end
+
+    private
+
+    def raise_accession_error(submission, cause)
+      sample_id = submission.sample.sanger_sample_id
+      user = submission.user
+      message = "SampleAccessioningJob failed for sample '#{sample_id}' by user '#{user.username}' (#{user.id}): #{cause}"
+
+      raise(AccessionService::AccessionServiceError, message)
+    end
+
+    def handle_accession_error(error, submission, cause)
+      sample_id = submission.sample.sanger_sample_id
+      user = submission.user
+      service = submission.service
+
+      Rails.logger.error(job_error_message)
+      ExceptionNotifier.notify_exception(error, data: {
+                                           cause_message: cause,
+                                           sample_id: sample_id,
+                                           user_id: user.id,
+                                           user_username: user.username,
+                                           service: service.name
+                                         })
     end
   end
