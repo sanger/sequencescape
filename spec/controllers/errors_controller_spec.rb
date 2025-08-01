d63f602a9b6173ba654032bb42e7795c074ba8c1
Stephen Hulme <sh51@sanger.ac.uk>
2025-08-01 09:07:09 +0100

    ai: add specs for error controller

---
+++ spec/controllers/errors_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe ErrorsController, type: :controller do
+  render_views
+
+  describe 'GET #not_found' do
+    before { get :not_found }
+
+    it 'renders the not_found template' do
+      expect(response).to render_template(:not_found)
+    end
+
+    it 'returns 404 status' do
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe 'GET #internal_server' do
+    before { get :internal_server }
+
+    it 'renders the internal_server template' do
+      expect(response).to render_template(:internal_server)
+    end
+
+    it 'returns 500 status' do
+      expect(response).to have_http_status(:internal_server_error)
+    end
+  end
+
+  describe 'GET #service_unavailable' do
+    before { get :service_unavailable }
+
+    it 'renders the service_unavailable template' do
+      expect(response).to render_template(:service_unavailable)
+    end
+
+    it 'returns 503 status' do
+      expect(response).to have_http_status(:service_unavailable)
+    end
+  end
+end
