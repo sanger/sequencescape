45f6b1c17fde4f231b0457c64ce6f1a8629e879d
Shiv <44001656+SHIV5T3R@users.noreply.github.com>
2025-08-18 11:54:33 +0100

    test: update samples table expectation to include conditional supplier name

--- spec/features/assets/show_plate_spec.rb
+++ spec/features/assets/show_plate_spec.rb
@@ -13,13 +13,21 @@ describe 'Show plate', :js do
   it 'the samples table shows empty wells' do
     login_user user
     visit labware_path(plate)
-    expect(fetch_table('#plate-samples-table')).to eq(
-      [
-        ['Well', 'Sample Name', 'Sanger Sample Id', 'Tag', 'Tag2', 'Control?'],
-        ['A1', '[Empty]', '', '', '', ''],
-        ['B1', '[Empty]', '', '', '', ''],
-        ['C1', '[Empty]', '', '', '', '']
-      ]
-    )
+    eq begin
+      expected = []
+      header = ['Well', 'Sample Name', 'Sanger Sample Id']
+      header << 'Sample Supplier Name' if plate.plate_purpose.cherrypickable_target
+      header += ['Tag', 'Tag2', 'Control?']
+      expected << header
+
+      %w[A1 B1 C1].each do |well|
+        row = [well, '[Empty]', '']
+        row << '' if plate.plate_purpose.cherrypickable_target
+        row += ['', '', '']
+        expected << row
+      end
+
+      expected
+    end
   end
 end
