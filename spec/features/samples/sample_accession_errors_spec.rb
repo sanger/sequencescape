f353dfa857e6ae25cb70835c2396311f188341db
Stephen Hulme <sh51@sanger.ac.uk>
2025-08-18 12:34:33 +0100

    test: add addional test for disabled link

--- spec/features/samples/sample_accession_errors_spec.rb
+++ spec/features/samples/sample_accession_errors_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Sample accession errors', :accessioning_enabled, type: :feature do
+RSpec.describe 'Sample accession errors', type: :feature do
   let(:user) { create(:admin) }
   let(:study) { create(:managed_study, accession_number: 'ENA123') }
   let(:sample) { create(:sample, studies: [study]) }
@@ -12,12 +12,22 @@ RSpec.describe 'Sample accession errors', :accessioning_enabled, type: :feature 
     visit sample_path(sample)
   end
 
-  it 'shows informative errors when required fields are missing for accessioning' do
-    expect(page).to have_content('Sample Specification')
-    expect(page).to have_content('Generate Accession Number')
-    click_link 'Generate Accession Number'
-    expect(page).to have_content('Please fill in the required fields:')
-    expect(page).to have_content('gender is required')
-    expect(page).to have_content('phenotype is required')
+  context 'when accessioning is enabled', :accessioning_enabled do
+    it 'shows informative errors when required fields are missing for accessioning' do
+      expect(page).to have_content('Sample Specification')
+      expect(page).to have_content('Generate Accession Number')
+      click_link 'Generate Accession Number'
+      expect(page).to have_content('Please fill in the required fields:')
+      expect(page).to have_content('gender is required')
+      expect(page).to have_content('phenotype is required')
+    end
+  end
+
+  context 'when accessioning is disabled' do
+    it 'shows a disabled link for accessioning' do
+      expect(page).to have_content('Sample Specification')
+      expect(page).to have_content('Generate Accession Number')
+      expect(page).to have_no_link('Generate Accession Number')
+    end
   end
 end
