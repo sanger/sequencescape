2df2d0edd326e4bd4cfd0bfa124b60385a4667ea
Stephen Hulme <sh51@sanger.ac.uk>
2025-08-18 12:57:56 +0100

    test: replacing breaking feature with rspec equivalent
    XML checks can be found in spec/lib/accession/submission_spec.rb

--- spec/features/samples/sample_accession_errors_spec.rb
+++ spec/features/samples/sample_accession_errors_spec.rb
@@ -5,29 +5,51 @@ require 'rails_helper'
 RSpec.describe 'Sample accession errors', type: :feature do
   let(:user) { create(:admin) }
   let(:study) { create(:managed_study, accession_number: 'ENA123') }
-  let(:sample) { create(:sample, studies: [study]) }
 
   before do
     login_user(user)
     visit sample_path(sample)
   end
 
-  context 'when accessioning is enabled', :accessioning_enabled do
-    it 'shows informative errors when required fields are missing for accessioning' do
-      expect(page).to have_content('Sample Specification')
-      expect(page).to have_content('Generate Accession Number')
-      click_link 'Generate Accession Number'
-      expect(page).to have_content('Please fill in the required fields:')
-      expect(page).to have_content('gender is required')
-      expect(page).to have_content('phenotype is required')
+  context 'when the sample has not been accessioned' do
+    let(:sample) { create(:sample, studies: [study]) }
+
+    context 'when accessioning is enabled', :accessioning_enabled do
+      it 'shows informative errors when required fields are missing for accessioning' do
+        expect(page).to have_content('Sample Specification')
+        expect(page).to have_link('Generate Accession Number')
+        click_link 'Generate Accession Number'
+        expect(page).to have_content('Please fill in the required fields:')
+        expect(page).to have_content('gender is required')
+        expect(page).to have_content('phenotype is required')
+      end
+    end
+
+    context 'when accessioning is disabled' do
+      it 'shows a disabled link for accessioning' do
+        expect(page).to have_content('Sample Specification')
+        expect(page).to have_content('Generate Accession Number')
+        expect(page).to have_no_link('Generate Accession Number')
+      end
     end
   end
 
-  context 'when accessioning is disabled' do
-    it 'shows a disabled link for accessioning' do
-      expect(page).to have_content('Sample Specification')
-      expect(page).to have_content('Generate Accession Number')
-      expect(page).to have_no_link('Generate Accession Number')
+  context 'when the sample has been accessioned' do
+    let(:sample) { create(:accessioned_sample, studies: [study]) }
+
+    context 'when accessioning is enabled', :accessioning_enabled do
+      it 'shows a link for accessioning' do
+        expect(page).to have_content('Sample Specification')
+        expect(page).to have_link('Update Sample Data for Accessioning')
+      end
+    end
+
+    context 'when accessioning is disabled' do
+      it 'shows a disabled link for accessioning' do
+        expect(page).to have_content('Sample Specification')
+        expect(page).to have_content('Update Sample Data for Accessioning')
+        expect(page).to have_no_link('Update Sample Data for Accessioning')
+      end
     end
   end
 end
