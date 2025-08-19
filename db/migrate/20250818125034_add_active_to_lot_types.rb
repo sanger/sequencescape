c022c3ce2e2dff4e35cb49da192b69a8b36e3a84
Katy Taylor <kt17@sanger.ac.uk>
2025-08-19 14:24:21 +0100

    rubocop

--- db/migrate/20250818125034_add_active_to_lot_types.rb
+++ db/migrate/20250818125034_add_active_to_lot_types.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
 class AddActiveToLotTypes < ActiveRecord::Migration[7.1]
   def change
-    add_column :lot_types, :active, :boolean, :default => true
+    add_column :lot_types, :active, :boolean, default: true
   end
 end
