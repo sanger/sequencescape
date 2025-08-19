1ea2c4d04346d7ec28c911044d93beb019e26884
Stephen Hulme <sh51@sanger.ac.uk>
2025-08-19 11:11:49 +0100

    refactor: rename factory successful_accession_response to include sample

--- spec/support/mock_accession.rb
+++ spec/support/mock_accession.rb
@@ -4,7 +4,7 @@ module MockAccession
   Response = Struct.new(:code, :body)
 
   # for samples
-  def successful_accession_response
+  def successful_sample_accession_response
     Response.new(200, '<RECEIPT success="true"><SAMPLE accession="EGA00001000240" /></RECEIPT>')
   end
 
@@ -16,5 +16,6 @@ module MockAccession
     Response.new(200, '<RECEIPT success="false"><ERROR>Error 1</ERROR><ERROR>Error 2</ERROR></RECEIPT>')
   end
 
-  module_function :successful_accession_response, :successful_study_accession_response, :failed_accession_response
+  module_function :successful_sample_accession_response, :successful_study_accession_response,
+                  :failed_accession_response
 end
