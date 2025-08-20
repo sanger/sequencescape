7b5f47079ebe023088f7353082b50c0605b96b85
Shiv <44001656+SHIV5T3R@users.noreply.github.com>
2025-08-20 13:30:25 +0100

    test: refactor samples table tests for clarity and consistency

--- spec/features/assets/show_plate_spec.rb
+++ spec/features/assets/show_plate_spec.rb
@@ -3,31 +3,48 @@
 require 'rails_helper'
 
 describe 'Show plate', :js do
-  let(:plate) { create(:plate, well_count: 3) }
-  let(:user) { create(:user) }
+  context 'when the plate has samples' do
+    let(:plate) { create(:plate, well_count: 3) }
+    let(:user) { create(:user) }
 
-  before do
-    plate # has been created
+    before do
+      plate # has been created
+    end
+
+    it 'shows the samples table shows empty wells' do
+      login_user user
+      visit labware_path(plate)
+      expect(fetch_table('#plate-samples-table')).to eq(
+        [
+          ['Well', 'Sample Name', 'Sanger Sample Id', 'Sample Supplier Name', 'Tag', 'Tag2', 'Control?'],
+          ['A1', '[Empty]', '', '', '', ''],
+          ['B1', '[Empty]', '', '', '', ''],
+          ['C1', '[Empty]', '', '', '', '']
+        ]
+      )
+    end
   end
 
-  it 'the samples table shows empty wells' do
-    login_user user
-    visit labware_path(plate)
-    eq begin
-      expected = []
-      header = ['Well', 'Sample Name', 'Sanger Sample Id']
-      header << 'Sample Supplier Name' if plate.plate_purpose.cherrypickable_target
-      header += ['Tag', 'Tag2', 'Control?']
-      expected << header
+  context 'when the plate does not have samples with supplier names and is not a cherrypickable_target' do
+    let(:plate) { create(:plate, well_count: 3) }
+    let(:user) { create(:user) }
 
-      %w[A1 B1 C1].each do |well|
-        row = [well, '[Empty]', '']
-        row << '' if plate.plate_purpose.cherrypickable_target
-        row += ['', '', '']
-        expected << row
-      end
+    before do
+      plate # has been created
+      plate.plate_purpose.update!(cherrypickable_target: false) # When false, the plate should not show supplier names
+    end
 
-      expected
+    it 'does not show the sample supplier name' do
+      login_user user
+      visit labware_path(plate)
+      expect(fetch_table('#plate-samples-table')).to eq(
+        [
+          ['Well', 'Sample Name', 'Sanger Sample Id', 'Tag', 'Tag2', 'Control?'],
+          ['A1', '[Empty]', '', '', '', ''],
+          ['B1', '[Empty]', '', '', '', ''],
+          ['C1', '[Empty]', '', '', '', '']
+        ]
+      )
     end
   end
 end
