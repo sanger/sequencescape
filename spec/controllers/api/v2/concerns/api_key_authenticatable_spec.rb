438fdddc83637654c8b8f60e7f9c9d1d13395a4c
yoldas <ay6@sanger.ac.uk>
2025-09-07 22:45:34 +0100

    Test enabling and disabling feature flag

--- spec/controllers/api/v2/concerns/api_key_authenticatable_spec.rb
+++ spec/controllers/api/v2/concerns/api_key_authenticatable_spec.rb
@@ -37,4 +37,35 @@ RSpec.describe Api::V2::Concerns::ApiKeyAuthenticatable do
       end
     end
   end
+
+  describe '#authenticate_with_api_key' do
+    before do
+      allow(controller.request).to receive_messages(env: {}, path_parameters: {}, get?: true)
+      allow(controller).to receive(:render_unauthorized)
+    end
+
+    context 'when feature flag is enabled' do
+      before do
+        Flipper.enable(:y25_442_make_api_key_mandatory)
+      end
+
+      it 'renders unauthorized when no API key is provided' do
+        controller.send(:authenticate_with_api_key)
+
+        expect(controller).to have_received(:render_unauthorized)
+      end
+    end
+
+    context 'when feature flag is disabled' do
+      before do
+        Flipper.disable(:y25_442_make_api_key_mandatory)
+      end
+
+      it 'does not render unauthorized when no API key is provided' do
+        controller.send(:authenticate_with_api_key)
+
+        expect(controller).not_to have_received(:render_unauthorized)
+      end
+    end
+  end
 end
