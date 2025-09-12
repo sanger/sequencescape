2ef9d6ea5c5f4ed59f650e3d1c5207c5e3f3db30
Wendy Yang <wy1@sanger.ac.uk>
2025-09-12 17:06:49 +0100

    add op_recipe_consistent validation

--- app/models/ultima_sequencing_pipeline.rb
+++ app/models/ultima_sequencing_pipeline.rb
@@ -2,4 +2,20 @@
 
 # Specialized sequencing pipeline for Ultima
 class UltimaSequencingPipeline < SequencingPipeline
+  def op_recipe_consistent_for_batch?(batch) # rubocop:todo Metrics/AbcSize
+    if batch.requests.empty? || batch.requests.first.request_metadata.nil?
+      # No requests selected or the pipeline doesn't contain metadata to check
+      return true
+    end
+
+    op_recipe_list = batch.requests.filter_map { |request| request.request_metadata.ot_recipe }
+
+    # The pipeline doen't contain the ot_recipe attribute
+    return true if op_recipe_list.empty?
+
+    # There are some requests that don't have the ot_recipe attribute
+    return false if op_recipe_list.size != batch.requests.size
+
+    (op_recipe_list.uniq.size == 1)
+  end
 end
--- app/validators/batch_creation_validator.rb
+++ app/validators/batch_creation_validator.rb
@@ -10,6 +10,7 @@ class BatchCreationValidator < ActiveModel::Validator
     batch_meets_minimum_size(record)
     all_requests_are_ready?(record)
     requests_have_same_target_purpose(record)
+    requests_have_same_op_recipe(record) if record.pipeline.is_a?(UltimaSequencingPipeline)
   end
 
   private
@@ -45,5 +46,11 @@ class BatchCreationValidator < ActiveModel::Validator
     return if record.pipeline.is_flowcell_type_consistent_for_batch?(record)
 
     record.errors.add :base, "The selected requests must have the same values in their 'Flowcell Requested' field."
+  end
+
+  def requests_have_same_op_recipe(record)
+    return if record.pipeline.op_recipe_consistent_for_batch?(record)
+
+    record.errors.add :base, "The selected requests must have the same values in their 'OT Recipe' field."
   end
 end
