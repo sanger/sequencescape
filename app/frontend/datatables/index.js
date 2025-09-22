d9721ab6b363be39ab349c88035d265a7e8c66ce
Dasun Pubudumal <pubudumald@gmail.com>
2025-09-22 11:19:35 +0100

    Refactor datatables imports for improved readability and organization

--- app/frontend/datatables/index.js
+++ app/frontend/datatables/index.js
@@ -1,13 +1,25 @@
+import $ from "jquery";
+
 // Load the datatables plugins
-import "datatables.net-bs4";
-import "datatables.net-buttons-bs4";
-import "datatables.net-buttons/js/buttons.colVis";
-import "datatables.net-buttons/js/buttons.html5";
-import "datatables.net-fixedcolumns-bs4";
-import "datatables.net-fixedheader-bs4";
-import "datatables.net-responsive-bs4";
-import "datatables.net-rowgroup-bs4";
-import "datatables.net-rowreorder-bs4";
+import dt from "datatables.net-bs4";
+import dtButtons from "datatables.net-buttons-bs4";
+import dtButtonsColVis from "datatables.net-buttons/js/buttons.colVis";
+import dtButtonsHtml5 from "datatables.net-buttons/js/buttons.html5";
+import dtFixedColumn from "datatables.net-fixedcolumns-bs4";
+import dtFixedHeader from "datatables.net-fixedheader-bs4";
+import dtResponsive from "datatables.net-responsive-bs4";
+import dtRowGroup from "datatables.net-rowgroup-bs4";
+import dtRowOrder from "datatables.net-rowreorder-bs4";
+
+dt(window, $);
+dtButtons(window, $);
+dtButtonsColVis(window, $);
+dtButtonsHtml5(window, $);
+dtFixedColumn(window, $);
+dtFixedHeader(window, $);
+dtResponsive(window, $);
+dtRowGroup(window, $);
+dtRowOrder(window, $);
 
 // Load individual datatables implementations
 import "./tableSortable";
