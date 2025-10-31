113760fb94e71392b04f5290081599adcf1db28a
yoldas <ay6@sanger.ac.uk>
2025-10-31 16:09:54 +0000

    Update factorybot definition for ultima_sequencing_pipeline with request type name chosen before

--- spec/factories/pipelines_factories.rb
+++ spec/factories/pipelines_factories.rb
@@ -182,7 +182,7 @@ FactoryBot.define do
     workflow { build(:lab_workflow_for_pipeline) }
 
     after(:build) do |pipeline|
-      pipeline.request_types << create(:ultima_sequencing_request_type)
+      pipeline.request_types << create(:ultima_sequencing)
       pipeline.validator_class_name { 'UltimaValidator' }
       pipeline.add_control_request_type
     end
