bb7fb588d07311bc084f7a0895693be9b32d1250
Stephen Hulme <sh51@sanger.ac.uk>
2025-11-06 15:37:53 +0000

    test: refactor plate-template factories

--- spec/factories/tag_qc_factories.rb
+++ spec/factories/tag_qc_factories.rb
@@ -74,15 +74,14 @@ FactoryBot.define do
     end
   end
 
-  factory :plate_template do
-    name { 'testtemplate' }
+ factory :plate_template, class: 'PlateTemplate' do
+    name { 'test_plate_template' }
     size { 96 }
-  end
 
-  factory :plate_template_with_well, class: 'PlateTemplate' do
-    sequence(:name) { |n| "testtemplate#{n}" }
-    size { 96 }
-    wells { [create(:well_with_sample_and_without_plate, map: create(:map))] }
+    factory :plate_template_with_well do
+      sequence(:name) { |n| "test_plate_template_#{n}" }
+      wells { [association(:well_with_sample_and_without_plate, map: association(:map))] }
+    end
   end
 
   factory :tag_layout_qcable_plate_purpose, class: 'QcablePlatePurpose' do
