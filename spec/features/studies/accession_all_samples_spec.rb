c93cdc5e742b71d02ca020a56e45b04dfca4f226
Stephen Hulme <sh51@sanger.ac.uk>
2025-11-15 11:24:24 +0000

    test: remove references to accession request

--- spec/features/studies/accession_all_samples_spec.rb
+++ spec/features/studies/accession_all_samples_spec.rb
@@ -3,12 +3,16 @@
 require 'rails_helper'
 
 describe 'Accession all samples', :accessioning_enabled do
+  include AccessionV1ClientHelper
+
   let!(:user) { create(:user, api_key: configatron.accession_local_key) }
   let!(:study) { create(:open_study, accession_number: 'ENA123', samples: create_list(:sample_for_accessioning, 5)) }
 
   before do
     Delayed::Worker.delay_jobs = false
-    allow(Accession::Request).to receive(:post).and_return(build(:successful_sample_accession_response))
+    allow_any_instance_of(Accession::Submission).to receive(:client).and_return(
+      stub_accession_client(:submit_and_fetch_accession_number, return_value: 'EGA00001000240')
+    )
   end
 
   after do
