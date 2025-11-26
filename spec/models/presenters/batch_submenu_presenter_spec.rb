d9c69ff77997f9d9ecba77a5e0acbc6ab15707f5
Katy Taylor <kt17@sanger.ac.uk>
2025-11-26 15:37:13 +0000

    add new unit tests for controller and submenu presenter

--- spec/models/presenters/batch_submenu_presenter_spec.rb
+++ spec/models/presenters/batch_submenu_presenter_spec.rb
@@ -10,6 +10,16 @@ RSpec.describe Presenters::BatchSubmenuPresenter, type: :model do
     let(:amp_plate_labels_option) do
       { label: 'Print AMP plate batch ID', url: "/batches/#{batch.id}/print_amp_plate_labels" }
     end
+    let(:amp_plate_verify_option) do
+      { label: 'Verify AMP plate layout', url: "/batches/#{batch.id}/verify?verification_flavour=amp_plate" }
+    end
+    let(:tube_verify_option) do
+      { label: 'Verify tube layout', url: "/batches/#{batch.id}/verify?verification_flavour=tube" }
+    end
+
+    before do
+      allow_any_instance_of(described_class).to receive(:can?).and_return(true)
+    end
 
     it 'includes a link to print AMP plate labels' do
       found_amp_plate_labels_option = false
@@ -19,6 +29,54 @@ RSpec.describe Presenters::BatchSubmenuPresenter, type: :model do
       end
 
       expect(found_amp_plate_labels_option).to be true
+    end
+
+    it 'includes a link to verify AMP plates' do
+      found_amp_plate_verify_option = false
+
+      batch_submenu_presenter.each_option do |option|
+        puts option
+        found_amp_plate_verify_option = true if option == amp_plate_verify_option
+      end
+
+      expect(found_amp_plate_verify_option).to be true
+    end
+
+    it 'includes a link to verify tubes' do
+      found_tube_verify_option = false
+
+      batch_submenu_presenter.each_option do |option|
+        found_tube_verify_option = true if option == tube_verify_option
+      end
+
+      expect(found_tube_verify_option).to be true
+    end
+
+    context 'when layout has already been verified' do
+      before do
+        batch.lab_events.create!(description: 'Tube layout verified')
+        batch.lab_events.create!(description: 'AMP plate layout verified')
+      end
+
+      it 'does not include a link to verify AMP plates' do
+        found_amp_plate_verify_option = false
+
+        batch_submenu_presenter.each_option do |option|
+          found_amp_plate_verify_option = true if option == amp_plate_verify_option
+        end
+
+        expect(found_amp_plate_verify_option).to be false
+      end
+
+      it 'does not include a link to verify tubes' do
+        found_tube_verify_option = false
+
+        batch_submenu_presenter.each_option do |option|
+          found_tube_verify_option = true if option == tube_verify_option
+        end
+
+        expect(found_tube_verify_option).to be false
+      end
     end
   end
 end
