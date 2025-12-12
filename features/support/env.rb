0cfa6889e752792f3754d70471e04d15e6d7e65d
Ben Topping <ben.topping@outlook.com>
2025-12-12 10:53:19 +0000

    tests: attempt to persist seed data and truncate test data

--- features/support/env.rb
+++ features/support/env.rb
@@ -14,6 +14,7 @@ end
 require_relative 'simplecov' # loading order is important
 require 'cucumber/rails'
 require_relative 'parameter_types' # does not seem to be automatically loaded correctly...
+require_relative 'seeded_deletion'
 
 # By default, any exception happening in your Rails application will bubble up
 # to Cucumber so that your scenario will fail. This is a different from how
@@ -35,25 +36,26 @@ ActionController::Base.allow_rescue = false
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
 begin
-  DatabaseCleaner.strategy = :transaction
+  DatabaseCleaner[:active_record].strategy = DatabaseCleaner::ActiveRecord::SeededDeletion.new
+
+  Rails.application.load_tasks
+  begin
+    Rails.application.load_seed
+  rescue StandardError => e
+    puts "Seed data already loaded"
+  end
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
 
 # You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
 # See the DatabaseCleaner documentation for details. Example:
-Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
-  # { except: [:widgets] } may not do what you expect here
-  # as Cucumber::Rails::Database.javascript_strategy overrides
-  # this setting.
-  DatabaseCleaner.strategy = :truncation
+Before() do
+  DatabaseCleaner[:active_record].strategy = DatabaseCleaner::ActiveRecord::SeededDeletion.new
+
+  DatabaseCleaner.start
 end
 
-Before('not @no-txn', 'not @selenium', 'not @culerity', 'not @celerity', 'not @javascript') do
-  DatabaseCleaner.strategy = :transaction
+After() do
+  DatabaseCleaner.clean
 end
-
-# Possible values are :truncation and :transaction
-# The :transaction strategy is faster, but might give you threading problems.
-# See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
-Cucumber::Rails::Database.javascript_strategy = :transaction # :transaction is currently required for sequencescape tests to pass
