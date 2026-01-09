3fa0ae805b723ff078abf8e4ba1052f2e8362e36
Stephen Hulme <sh51@sanger.ac.uk>
2026-01-09 16:55:39 +0000

    build: add database_cleaner-activerecord-seeded_deletion
    https://github.com/ManageIQ/database_cleaner-activerecord-seeded_deletion

--- features/support/env.rb
+++ features/support/env.rb
@@ -32,26 +32,10 @@ require_relative 'parameter_types' # does not seem to be automatically loaded co
 #
 ActionController::Base.allow_rescue = false
 
-# Remove/comment out the lines below if your app doesn't have a database.
-# For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
-begin
-  DatabaseCleaner.strategy = :transaction
-rescue NameError
-  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
-end
+require 'database_cleaner-activerecord-seeded_deletion'
 
-# You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
-# See the DatabaseCleaner documentation for details. Example:
-Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
-  # { except: [:widgets] } may not do what you expect here
-  # as Cucumber::Rails::Database.javascript_strategy overrides
-  # this setting.
-  DatabaseCleaner.strategy = :truncation
-end
-
-Before('not @no-txn', 'not @selenium', 'not @culerity', 'not @celerity', 'not @javascript') do
-  DatabaseCleaner.strategy = :transaction
-end
+# Configure DatabaseCleaner to use the SeededDeletion strategy
+DatabaseCleaner[:active_record].strategy = DatabaseCleaner::ActiveRecord::SeededDeletion.new
 
 # Possible values are :truncation and :transaction
 # The :transaction strategy is faster, but might give you threading problems.
