a2806da7f9ebedec8a13fef4d05b81c7e04877a5
Stephen Hulme <sh51@sanger.ac.uk>
2026-01-15 16:58:53 +0000

    refactor: remove references to Accessionable::Sample

--- app/models/accession_service.rb
+++ app/models/accession_service.rb
@@ -15,7 +15,6 @@
 #
 # Accessionables
 # --------------
-# {Accessionable::Sample}     Represents information about the sample, maps to a Sequencescape {Sample}.
 # {Accessionable::Study}      Represents information about the study. Indicates WHY the samples have been sequenced.
 #                             Maps to a Sequencescape {Study}.
 # {Accessionable::Submission} Wrapper object required by the submission service. We use one per accessionable.
@@ -23,7 +22,7 @@
 # {Accessionable::Dac}    Data access committee. Information about who to contact to gain access to the data. (EGA)
 # {Accessionable::Policy} Details about how the data may be used. (EGA)
 #
-# Accessioning of samples has been partially migrated to {Accession 'a separate accession library'}
+# Accessioning of samples has been fully migrated to {Accession 'a separate accession library'}
 module AccessionService
   # Define custom error classes for the AccessionService
   AccessionServiceError = Class.new(StandardError)
@@ -50,30 +49,5 @@ module AccessionService
     else
       AccessionService::NoService.new(study)
     end
-  end
-
-  # Return the highest priority accession service that this sample should use.
-  #
-  # @param sample [Sample] The sample for which to select the accession service.
-  # @return [AccessionService::BaseService] An instance of the selected accession service
-  def self.select_for_sample(sample)
-    services = sample.studies.group_by { |study| AccessionService.select_for_study(study).priority }
-    return AccessionService::UnsuitableService.new([]) if services.empty?
-
-    highest_priority = services.keys.max
-    suitable_study = services[highest_priority].detect { |study| send_samples_to_service?(study) }
-    return AccessionService.select_for_study(suitable_study) if suitable_study
-
-    AccessionService::UnsuitableService.new(services[highest_priority])
-  end
-
-  # Determines if samples from the given study should be sent to the accession service.
-  #
-  # @param study [Study] The study to check.
-  # @return [Boolean] True if the samples can be sent without the study requiring accessioning,
-  # or if the study has already been accessioned, false otherwise.
-  def self.send_samples_to_service?(study)
-    accession_service = select_for_study(study)
-    accession_service.no_study_accession_needed || (!study.study_metadata.never_release? && study.accession_number?)
   end
 end
