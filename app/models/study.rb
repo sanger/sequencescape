c9a8f5111fda61b51b38648da43cd3693dd6f5d8
Stephen Hulme <sh51@sanger.ac.uk>
2026-01-22 10:49:46 +0000

    refactor: add semantic meaning to data release timings

--- app/models/study.rb
+++ app/models/study.rb
@@ -79,7 +79,23 @@ class Study < ApplicationRecord # rubocop:todo Metrics/ClassLength
   DATA_RELEASE_TIMING_IMMEDIATE = 'immediate'
   DATA_RELEASE_TIMING_PUBLICATION = 'delay until publication'
 
-  DATA_RELEASE_TIMINGS = [
+  # The list of all possible data release timings
+  ALL_DATA_RELEASE_TIMINGS = [
+    DATA_RELEASE_TIMING_STANDARD,
+    DATA_RELEASE_TIMING_NEVER,
+    DATA_RELEASE_TIMING_DELAYED,
+    DATA_RELEASE_TIMING_IMMEDIATE,
+    DATA_RELEASE_TIMING_PUBLICATION
+  ].freeze
+  # Release timings for open studies
+  DATA_RELEASE_TIMINGS_FOR_OPEN_RELEASE = [
+    DATA_RELEASE_TIMING_STANDARD,
+    DATA_RELEASE_TIMING_IMMEDIATE,
+    DATA_RELEASE_TIMING_DELAYED,
+    DATA_RELEASE_TIMING_PUBLICATION
+  ].freeze
+  # Release timings for managed studies
+  DATA_RELEASE_TIMINGS_FOR_MANAGED_RELEASE = [
     DATA_RELEASE_TIMING_STANDARD,
     DATA_RELEASE_TIMING_IMMEDIATE,
     DATA_RELEASE_TIMING_DELAYED
@@ -249,7 +265,7 @@ class Study < ApplicationRecord # rubocop:todo Metrics/ClassLength
       :data_release_timing,
       required: true,
       default: DATA_RELEASE_TIMING_STANDARD,
-      in: DATA_RELEASE_TIMINGS + [DATA_RELEASE_TIMING_NEVER] + [DATA_RELEASE_TIMING_PUBLICATION]
+      in: ALL_DATA_RELEASE_TIMINGS
     )
     custom_attribute(
       :data_release_delay_reason,
@@ -378,16 +394,6 @@ class Study < ApplicationRecord # rubocop:todo Metrics/ClassLength
         ->(assets) do
           select('DISTINCT studies.*').joins(['LEFT JOIN aliquots ON aliquots.study_id = studies.id']).where(
             ['aliquots.receptacle_id IN (?)', assets.map(&:id)]
-          )
-        end
-
-  scope :for_sample_accessioning,
-        -> do
-          joins(:study_metadata).where("study_metadata.study_ebi_accession_number <> ''").where(
-            study_metadata: {
-              data_release_strategy: [Study::DATA_RELEASE_STRATEGY_OPEN, Study::DATA_RELEASE_STRATEGY_MANAGED],
-              data_release_timing: Study::DATA_RELEASE_TIMINGS + [Study::DATA_RELEASE_TIMING_PUBLICATION]
-            }
           )
         end
 
@@ -705,18 +711,12 @@ class Study < ApplicationRecord # rubocop:todo Metrics/ClassLength
 
     validate :sanity_check_y_separation, if: :separate_y_chromosome_data?
 
-    validates :data_release_timing, inclusion: { in: DATA_RELEASE_TIMINGS }, if: :data_release_strategy_must_be_managed?
-    validates :data_release_timing,
-              inclusion: {
-                in: [DATA_RELEASE_TIMING_NEVER]
-              },
-              if: :data_release_timing_must_be_never?
-
-    validates :data_release_timing,
-              inclusion: {
-                in: DATA_RELEASE_TIMINGS + [DATA_RELEASE_TIMING_PUBLICATION]
-              },
-              if: :data_release_timing_must_be_open?
+    validates :data_release_timing, inclusion: { in: DATA_RELEASE_TIMINGS_FOR_MANAGED_RELEASE },
+                                    if: :data_release_strategy_must_be_managed?
+    validates :data_release_timing, inclusion: { in: DATA_RELEASE_TIMINGS_FOR_OPEN_RELEASE },
+                                    if: :data_release_timing_must_be_open?
+    validates :data_release_timing, inclusion: { in: [DATA_RELEASE_TIMING_NEVER] },
+                                    if: :data_release_timing_must_be_never?
 
     def data_release_timing_must_be_never?
       Flipper.enabled?(:y24_052_enable_data_release_timing_validation) && data_release_strategy.present? &&
