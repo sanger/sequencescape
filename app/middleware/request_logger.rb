bf4836c529b9a034dc7e6e0b45b355a359744f58
Stephen Hulme <sh51@sanger.ac.uk>
2026-02-16 12:39:21 +0000

    style: lint

--- app/middleware/request_logger.rb
+++ app/middleware/request_logger.rb
@@ -3,11 +3,13 @@
 # Log request and response details for monitoring and high-level profiling.
 #
 # @param log_level [Symbol] the log level to use for logging requests (default: :info)
+# @param environment_context [Hash] additional context to include in log entries, such as host and version information
 #
 # Returns a JSON parseable log entry like:
 # [INFO] [RequestLogger] {"method":"GET","path":"/samples/1234","format":"html","status_code":200,
 #   "status_message":"OK","duration_ms":935,"client_ip":"172.21.43.210",
-#   "request_id":"9fd18098-dea3-46f0-83c8-c41852441db3","@timestamp":"2026-02-12T12:10:50.284+00:00"}
+#   "request_id":"9fd18098-dea3-46f0-83c8-c41852441db3","tags":["request","success"],
+#   "@timestamp":"2026-02-12T12:10:50.284+00:00"}
 class RequestLogger
   def initialize(app, log_level: :info, environment_context: nil)
     @app = app
@@ -50,14 +52,18 @@ class RequestLogger
     end
   end
 
+  def tags(status_code)
+    tags = ['request']
+    tags << tag_for_status(status_code)
+    tags.compact!
+    tags
+  end
+
   def log_request(request, response, elapsed_ms)
     status_code, _headers, _body = response
 
     status_message = Rack::Utils::HTTP_STATUS_CODES[status_code] || 'Unknown Status'
     timestamp = Time.zone.now.iso8601(3)
-    tags = ['request']
-    tags << tag_for_status(status_code)
-    tags.compact!
 
     record = {
       method: request.request_method,
@@ -68,7 +74,7 @@ class RequestLogger
       duration_ms: elapsed_ms,
       client_ip: request.remote_ip,
       request_id: request.request_id,
-      tags: tags,
+      tags: tags(status_code),
       '@timestamp': timestamp
     }
     record.merge!(@environment_context) if @environment_context.present?
