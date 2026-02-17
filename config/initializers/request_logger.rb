627eff12259defa35255f91de1ceac6161fe374a
Stephen Hulme <sh51@sanger.ac.uk>
2026-02-17 15:28:22 +0000

    fix: remove environment context that is already in SS logs

--- config/initializers/request_logger.rb
+++ config/initializers/request_logger.rb
@@ -5,11 +5,7 @@ require_relative '../../app/middleware/request_logger'
 
 Rails.application.configure do
   # Insert RequestLogger near the top, before Rails::Rack::Logger
-  environment_context = {
-    host: Deployed::HOSTNAME,
-    version: Deployed::BRANCH
-  }
-  config.middleware.insert_before(Rails::Rack::Logger, RequestLogger, environment_context:)
+  config.middleware.insert_before(Rails::Rack::Logger, RequestLogger)
 end
 
 # Add backtrace silencers for middleware so that we don't see it in backtraces.
