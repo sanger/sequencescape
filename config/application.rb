11303f7340ae1d5e054d865a06a3a77e5a50c610
Stephen Hulme <sh51@sanger.ac.uk>
2026-04-08 16:58:38 +0100

    fix: update Rails defaults to 8.0

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Sequencescape
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 8.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
