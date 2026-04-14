e44650f1cb1fbe6215a5c11e7c1ed497d6181d36
Wendy Yang <wy1@sanger.ac.uk>
2026-04-14 15:10:27 +0100

    fix test

--- spec/models/api/messages/useq_wafer_io_spec.rb
+++ spec/models/api/messages/useq_wafer_io_spec.rb
@@ -106,6 +106,7 @@ RSpec.describe Api::Messages::UseqWaferIo do
               'amp_assign_control_bead_tube' => 'BeadTube002',
               'amp_instrument_name' => 'UG Amp 2',
               'ot_recipe' => 'Free',
+              'wafer_size' => nil,
               'samples' => [
                 {
                   'tag_sequence' => tags[0].oligo,
@@ -171,6 +172,7 @@ RSpec.describe Api::Messages::UseqWaferIo do
               'amp_assign_control_bead_tube' => 'BeadTube001',
               'amp_instrument_name' => 'UG Amp 1',
               'ot_recipe' => 'Free',
+              'wafer_size' => nil,
               'samples' => [
                 {
                   'tag_sequence' => tags[0].oligo,
@@ -226,6 +228,7 @@ RSpec.describe Api::Messages::UseqWaferIo do
               'amp_assign_control_bead_tube' => nil,
               'amp_instrument_name' => 'UG Amp 1',
               'ot_recipe' => 'Free',
+              'wafer_size' => nil,
               'samples' => [
                 {
                   'tag_sequence' => tags[0].oligo,
