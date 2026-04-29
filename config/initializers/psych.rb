6f880c8d8495efa87800ef172592764383918303
Stephen Hulme <sh51@sanger.ac.uk>
2026-04-29 15:00:48 +0100

    fix: allow the use of unsafe loading in Psych 4, same behaviour as in 3

--- config/initializers/psych.rb
+++ config/initializers/psych.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  # Fix for Psych::DisallowedClass: Tried to load unspecified class
-  config.active_record.yaml_column_permitted_classes =
-    Array(config.active_record.yaml_column_permitted_classes) +
-    %w[
-      Symbol
-      ActiveSupport::HashWithIndifferentAccess
-      ActiveSupport::TimeWithZone
-      ActiveSupport::TimeZone
-      HashWithIndifferentAccess
-      RequestType::Validator::ArrayWithDefault
-      RequestType::Validator::LibraryTypeValidator
-      RequestType::Validator::FlowcellTypeValidator
-      ActionController::Parameters
-      Set
-      Range
-      FieldInfo
-      Time
-    ]
+  # Allow legacy YAML deserialization for ActiveRecord serialized columns
+  # Ideally replace with `permitted_classes`, such as those used in the list below:
+  #   Symbol
+  #   ActiveSupport::HashWithIndifferentAccess
+  #   ActiveSupport::TimeWithZone
+  #   ActiveSupport::TimeZone
+  #   HashWithIndifferentAccess
+  #   RequestType::Validator::ArrayWithDefault
+  #   RequestType::Validator::LibraryTypeValidator
+  #   RequestType::Validator::FlowcellTypeValidator
+  #   ActionController::Parameters
+  #   Set
+  #   Range
+  #   FieldInfo
+  #   Time
+  config.active_record.use_yaml_unsafe_load = true
 end
