d4620706999d1c83dd622890d7c21c5378e6c68d
Stephen Hulme <sh51@sanger.ac.uk>
2026-05-05 15:31:23 +0100

    style: lint

--- app/models/submission/request_options_behaviour.rb
+++ app/models/submission/request_options_behaviour.rb
@@ -7,7 +7,9 @@ module Submission::RequestOptionsBehaviour
     def self.load(hash_yaml)
       return hash_yaml if hash_yaml.nil?
 
-      YAML.safe_load(hash_yaml, aliases: true, permitted_classes: [
+      YAML.safe_load(hash_yaml,
+                     aliases: true,
+                     permitted_classes: [
                        ActiveSupport::HashWithIndifferentAccess,
                        ActiveSupport::TimeWithZone,
                        ActiveSupport::TimeZone,
@@ -33,22 +35,20 @@ module Submission::RequestOptionsBehaviour
     super
   end
 
+  private
+
   def check_request_options
     check_multipliers_are_valid
   end
-  private :check_request_options
 
-  # rubocop:todo Metrics/PerceivedComplexity, Metrics/AbcSize
-  def check_multipliers_are_valid # rubocop:todo Metrics/CyclomaticComplexity
+  def check_multipliers_are_valid # rubocop:disable Metrics/CyclomaticComplexity
     multipliers = request_options.try(:[], :multiplier)
     return if multipliers.blank? # We're ok with nothing being specified!
 
     # TODO[xxx]: should probably error if they've specified a request type that isn't being used
-    errors.add(:request_options, 'negative multiplier supplied') if multipliers.values.map(&:to_i).any?(&:negative?)
-    errors.add(:request_options, 'zero multiplier supplied') if multipliers.values.map(&:to_i).any?(&:zero?)
+    multiplier_values = multipliers.values.map(&:to_i)
+    errors.add(:request_options, 'negative multiplier supplied') if multiplier_values.any?(&:negative?)
+    errors.add(:request_options, 'zero multiplier supplied') if multiplier_values.any?(&:zero?)
     false unless errors.empty?
   end
-
-  # rubocop:enable Metrics/AbcSize, Metrics/PerceivedComplexity
-  private :check_multipliers_are_valid
 end
