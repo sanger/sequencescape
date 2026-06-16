04752a2e66e4ea97f132c24c1b624f2885571342
yoldas <ay6@sanger.ac.uk>
2026-06-16 13:17:40 +0100

    Revert "Merge pull request #5848 from sanger/revert-pr-5082-id-sample-tmp"
    This reverts commit 52a4be941adf10b34ea21320d2b31a4b665cbd18, reversing
    changes made to d7de4fb05887d96be94761dc53210ada08da66e8.

--- spec/models/api/messages/well_stock_resource_io_spec.rb
+++ spec/models/api/messages/well_stock_resource_io_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Api::Messages::WellStockResourceIo do
   after { Timecop.return }
 
   let(:sample) { create(:sample) }
+  let(:sample2) { create(:sample) }
   let(:plate_barcode) { build(:plate_barcode) }
   let(:well) do
     create(
@@ -22,14 +23,19 @@ RSpec.describe Api::Messages::WellStockResourceIo do
   end
   let(:study) { create(:study) }
   let(:aliquot) { create(:aliquot, study: study, sample: sample, receptacle: well) }
+  let(:aliquot2) { create(:aliquot, study: study, sample: sample2, receptacle: well) }
 
-  before { aliquot } # rubocop:todo RSpec/ScatteredSetup
+  before do # rubocop:todo RSpec/ScatteredSetup
+    aliquot
+    aliquot2
+  end
 
   let(:expected_json) do
     {
       'created_at' => '2012-03-11T10:22:42+00:00',
       'updated_at' => '2012-03-11T10:22:42+00:00',
-      'samples' => [{ 'sample_uuid' => sample.uuid, 'study_uuid' => study.uuid }],
+      'samples' => [{ 'id_sample_tmp' => sample.id, 'sample_uuid' => sample.uuid, 'study_uuid' => study.uuid },
+                    { 'id_sample_tmp' => sample2.id, 'sample_uuid' => sample2.uuid, 'study_uuid' => study.uuid }],
       'stock_resource_id' => well.id,
       'stock_resource_uuid' => well.uuid,
       'machine_barcode' => plate_barcode.barcode,
