b414489187d02209bab54c7790bada28997590af
yoldas <ay6@sanger.ac.uk>
2026-07-02 18:30:37 +0100

    feat(study-search): add program resource attributes

--- app/resources/api/v2/sapio/program_resource.rb
+++ app/resources/api/v2/sapio/program_resource.rb
@@ -5,6 +5,22 @@ module Api
     module Sapio
       class ProgramResource < Api::V2::BaseResource
         immutable
+
+        ##
+        # Attributes
+        #
+
+        # @!attribute [r] name
+        #   @return [String, nil] Study type name.
+        attribute :name
+
+        # @!attribute [r] created_at
+        #   @return [String, nil] Timestamp when the study type was created.
+        attribute :created_at
+
+        # @!attribute [r] updated_at
+        #   @return [String, nil] Timestamp when the study type was last updated.
+        attribute :updated_at
       end
     end
   end
