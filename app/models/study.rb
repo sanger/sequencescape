5d39baeb8868d6776bc1f9b4bbd360516f682c38
Wendy Yang <wy1@sanger.ac.uk>
2026-07-07 10:56:24 +0100

    add restriction to sample accessioning

--- app/models/study.rb
+++ app/models/study.rb
@@ -576,6 +576,7 @@ class Study < ApplicationRecord # rubocop:todo Metrics/ClassLength
     # If updating this method, please also update app/views/studies/information/_study_accession_status.html.erb
     [
       active?,
+      !ui_locked?,
       !study_metadata.strategy_not_applicable?,
       !study_metadata.never_release?,
       accession_required?,
@@ -653,6 +654,8 @@ class Study < ApplicationRecord # rubocop:todo Metrics/ClassLength
   # i.e. only Integration Hub can set/change the value of mastered_in_sapio
   def prevent_mastered_in_sapio_changes_unless_integration_hub
     return unless sapio_restrictions_enabled?
+    
+    # will_save_change_to_#{field_name}? is an ActiveRecord dirty-tracking method.
     return unless will_save_change_to_mastered_in_sapio?
     return if integration_hub_request?
 
