e608cd4d4457758e0bcf08b0e4ee44a319e28126
Stephen Hulme <sh51@sanger.ac.uk>
2026-08-03 11:59:12 +0100

    fix: udpate simplecov config and call in test runners

--- features/support/simplecov.rb
+++ features/support/simplecov.rb
@@ -1,3 +1,24 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+SimpleCov.start :rails do
+  skip '/test/'
+  skip '/config/'
+  skip '/coverage/'
+  skip '/data/'
+  skip '/db/'
+  skip '/doc/'
+  skip '/log/'
+  skip '/public/'
+  skip '/script/'
+  skip '/features/'
+  skip '/vendor/'
+  skip '/tmp/'
+
+  enable_coverage :branch
+
+  # You can skip here to add anything else you don't want to cover
+
+  # Mainly here for reference, and wont be running it again
+  skip '/lib/ability_analysis/spec_generator.rb'
+end
