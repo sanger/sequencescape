e14d753f11c1212279007de6fdaf20445a0b473b
Wendy Yang <wy1@sanger.ac.uk>
2026-08-13 16:25:26 +0100

    add unique key for external_id in uuid table

---
+++ db/migrate/20260813145512_add_unique_index_to_uuids_external_id.rb
@@ -0,0 +1,6 @@
+class AddUniqueIndexToUuidsExternalId < ActiveRecord::Migration[8.0]
+  def change
+    remove_index :uuids, :external_id, if_exists: true
+    add_index :uuids, :external_id, unique: true
+  end
+end
