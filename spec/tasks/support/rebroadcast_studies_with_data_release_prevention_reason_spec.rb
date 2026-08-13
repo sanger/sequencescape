bc165f7228bc5514b6d3d328a6de1384852d43cb
Wendy Yang <wy1@sanger.ac.uk>
2026-08-13 09:15:23 +0100

    fix linting

--- spec/tasks/support/rebroadcast_studies_with_data_release_prevention_reason_spec.rb
+++ spec/tasks/support/rebroadcast_studies_with_data_release_prevention_reason_spec.rb
@@ -31,20 +31,24 @@ describe 'support:rebroadcast_with_data_release_prevention_reason', type: :task 
   after { ENV.delete('DRY_RUN') }
 
   context 'when there are no studies with a data_release_prevention_reason' do
-    let!(:study_without_reason) { create(:study) }
+    let(:study_without_reason) { create(:study) }
+
+    before { study_without_reason }
 
     it 'reports finding 0 studies' do
       expect { task.invoke }.to output(/Found 0 studies/).to_stdout
     end
 
-    it 'does not rebroadcast any study' do
-      expect_any_instance_of(Study).not_to receive(:rebroadcast)
-      task.invoke
+    it 'does not rebroadcast any study', :warren do
+      Warren.handler.clear_messages
+      expect { task.invoke }.not_to change(Warren.handler.messages, :count)
     end
   end
 
   context 'when studies have a data_release_prevention_reason' do
-    let!(:study) { create_study_with_prevention_reason }
+    let(:study) { create_study_with_prevention_reason }
+
+    before { study }
 
     it 'reports the count of matching studies' do
       expect { task.invoke }.to output(/Found 1 studies/).to_stdout
@@ -62,9 +66,9 @@ describe 'support:rebroadcast_with_data_release_prevention_reason', type: :task 
       ).to_stdout
     end
 
-    it 'calls rebroadcast on the study' do
-      expect_any_instance_of(Study).to receive(:rebroadcast).once
-      task.invoke
+    it 'calls rebroadcast on the study', :warren do
+      Warren.handler.clear_messages
+      expect { task.invoke }.to change(Warren.handler.messages, :count).by(1)
     end
 
     it 'outputs Done at the end' do
@@ -73,29 +77,35 @@ describe 'support:rebroadcast_with_data_release_prevention_reason', type: :task 
   end
 
   context 'when only some studies have a data_release_prevention_reason' do
-    let!(:studies_with_reason) { Array.new(2) { create_study_with_prevention_reason } }
-    let!(:study_without_reason) { create(:study) }
+    let(:studies_with_reason) { Array.new(2) { create_study_with_prevention_reason } }
+    let(:study_without_reason) { create(:study) }
+
+    before do
+      studies_with_reason
+      study_without_reason
+    end
 
     it 'reports only the count of matching studies' do
       expect { task.invoke }.to output(/Found 2 studies/).to_stdout
     end
 
-    it 'calls rebroadcast exactly once per matching study' do
-      rebroadcast_count = 0
-      allow_any_instance_of(Study).to receive(:rebroadcast) { rebroadcast_count += 1 }
-      task.invoke
-      expect(rebroadcast_count).to eq(2)
+    it 'calls rebroadcast exactly once per matching study', :warren do
+      Warren.handler.clear_messages
+      expect { task.invoke }.to change(Warren.handler.messages, :count).by(2)
     end
   end
 
   context 'when DRY_RUN=true' do
-    let!(:study) { create_study_with_prevention_reason }
+    let(:study) { create_study_with_prevention_reason }
 
-    before { ENV['DRY_RUN'] = 'true' }
+    before do
+      study
+      ENV['DRY_RUN'] = 'true'
+    end
 
-    it 'does not call rebroadcast' do
-      expect_any_instance_of(Study).not_to receive(:rebroadcast)
-      task.invoke
+    it 'does not call rebroadcast', :warren do
+      Warren.handler.clear_messages
+      expect { task.invoke }.not_to change(Warren.handler.messages, :count)
     end
 
     it 'outputs (dry run) for each matching study' do
