e7270fb47b582b0e6d67cc961f3c19cfc5af963d
Stephen Hulme <sh51@sanger.ac.uk>
2026-08-18 15:17:16 +0100

    fix: synchronise duplicate externally managed migration

--- db/migrate/20260807140209_add_externally_managed_to_studies.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class AddExternallyManagedToStudies < ActiveRecord::Migration[8.0]
-  def change
-    add_column :studies, :externally_managed, :boolean, default: false, null: false
-  end
-end
