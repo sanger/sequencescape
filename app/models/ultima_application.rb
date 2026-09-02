e659b47ffce668f49d07d60904e849617639e895
yoldas <ay6@sanger.ac.uk>
2026-09-02 16:09:38 +0100

    feat: rename associations and remove delegates

--- app/models/ultima_application.rb
+++ app/models/ultima_application.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 
 class UltimaApplication < ApplicationRecord
-  belongs_to :ug100_configuration, class_name: 'UltimaConfiguration'
-  belongs_to :ug200_configuration, class_name: 'UltimaConfiguration'
+  belongs_to :ug100_preset, class_name: 'UltimaPreset'
+  belongs_to :ug200_preset, class_name: 'UltimaPreset'
 
   belongs_to :uga_primer, class_name: 'UltimaPrimer'
   belongs_to :ugb_primer, class_name: 'UltimaPrimer'
-
-  delegate :application_preset, to: :ug100_configuration, prefix: true
-  delegate :application_preset, to: :ug200_configuration, prefix: true
-  delegate :application_type, to: :ug100_configuration, prefix: true
-  delegate :application_type, to: :ug200_configuration, prefix: true
-
-  delegate :name, to: :uga_primer, prefix: true
-  delegate :name, to: :ugb_primer, prefix: true
 end
