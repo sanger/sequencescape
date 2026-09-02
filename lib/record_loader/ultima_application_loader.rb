e4034ff2faa6933e67b43557419dfb65d275c52d
yoldas <ay6@sanger.ac.uk>
2026-09-03 00:55:00 +0100

    fix: resolve ultima presets and primers by name to create applications

--- lib/record_loader/ultima_application_loader.rb
+++ lib/record_loader/ultima_application_loader.rb
@@ -8,7 +8,19 @@ module RecordLoader
 
     def create_or_update!(name, options)
       name = options['name'] || name # use name from options if provided
-      UltimaApplication.create_with(options).find_or_create_by!(name:)
+      attributes = options.except(
+        'ug100_preset_name',
+        'ug200_preset_name',
+        'uga_primer_name',
+        'ugb_primer_name'
+      ).merge(
+        ug100_preset: UltimaPreset.find_by!(name: options['ug100_preset_name']),
+        ug200_preset: UltimaPreset.find_by!(name: options['ug200_preset_name']),
+        uga_primer: UltimaPrimer.find_by!(name: options['uga_primer_name']),
+        ugb_primer: UltimaPrimer.find_by!(name: options['ugb_primer_name'])
+      )
+
+      UltimaApplication.create_with(attributes).find_or_create_by!(name:)
     end
   end
 end
