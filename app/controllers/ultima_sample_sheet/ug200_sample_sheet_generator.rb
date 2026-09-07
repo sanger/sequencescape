fccdcf4b5ba6afea6a8c4bd2b8a2f5b689765481
kshriniwas92 <sk34@sanger.ac.uk>
2026-09-07 15:11:10 +0000

    Set application to WGS Native Amplified based on the library type

--- app/controllers/ultima_sample_sheet/ug200_sample_sheet_generator.rb
+++ app/controllers/ultima_sample_sheet/ug200_sample_sheet_generator.rb
@@ -24,11 +24,8 @@ module UltimaSampleSheet::UG200SampleSheetGenerator
     # The request parameter is currently unused.
     # @param csv [CSV] the CSV object to append rows to
     # @param _request [UltimaSequencingRequest] the request whose global data is to be added
-    def add_global_section(csv, _request)
-      csv << pad(global_title_config)
-      csv << pad(global_headers_config)
-      data = ['WGS Native'] # Application
-      csv << pad(data)
+    def add_global_section(csv, request)
+      add_support_global_section(csv, request)
     end
   end
 end
