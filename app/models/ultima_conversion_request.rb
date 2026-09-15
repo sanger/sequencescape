af1657882b88e753482e956cf4cd6659bd46f3f0
Ben Topping <ben.topping@outlook.com>
2026-09-15 14:47:30 +0100

    feat(ultima-conversion): adds ultima_application to bulk submission templates

--- app/models/ultima_conversion_request.rb
+++ app/models/ultima_conversion_request.rb
@@ -3,7 +3,9 @@
 # Used for Illumina to Ultima library conversion.
 class UltimaConversionRequest < CustomerRequest
   has_metadata as: Request do
-    belongs_to :ultima_application, class_name: 'UltimaApplication', optional: false
+    belongs_to :ultima_application
+
+    association(:ultima_application, :name, required: true)
   end
 
   delegate :ultima_application, to: :request_metadata
