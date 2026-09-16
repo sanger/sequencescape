a349b0e7d890069ade3c6c8595c93116d1bfb977
Miguel Rada <mr33@sanger.ac.uk>
2026-09-16 23:37:06 +0100

    Fix db:seed creating duplicate read_length request_type_validators rows

--- db/seeds/0001_workflows.rb
+++ db/seeds/0001_workflows.rb
@@ -1425,7 +1425,7 @@ RequestType.find_each do |request_type|
   read_lengths =
     {
       # By request class
-      # 'HiSeqSequencingRequest' => [50, 75, 100, 150],
+      'HiSeqSequencingRequest' => [50, 75, 100, 150],
       'MiSeqSequencingRequest' => [25, 50, 130, 150, 250, 300],
       'SequencingRequest' => [37, 54, 76, 108]
     }[
@@ -1433,10 +1433,9 @@ RequestType.find_each do |request_type|
     ]
 
   if read_lengths.present?
-    RequestType::Validator.create!(
+    RequestType::Validator.find_or_create_by!(
       request_type: request_type,
-      request_option: 'read_length',
-      valid_options: read_lengths
-    )
+      request_option: 'read_length'
+    ) { |validator| validator.valid_options = read_lengths }
   end
 end
